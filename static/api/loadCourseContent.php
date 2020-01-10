fb1cddfb46e9d33fa43ede5ae48aa5d993074026
Kevin Charles <char0042@umn.edu>
2020-01-10 11:40:13 -0600

    Start of fixing page

--- static/api/loadCourseContent.php
+++ static/api/loadCourseContent.php
@@ -8,27 +8,54 @@ header('Content-Type: application/json');
 include "db_connection.php";
 
 
+$courseId =  mysqli_real_escape_string($conn,$_REQUEST["courseId"]); 
+
 $sql="
 SELECT 
- c.branchId as branchId,
- c.title as title,
- c.contentId as contentId,
- c.timestamp as publishDate,
- c.draft as draft
-FROM content AS c
-WHERE removedFlag = 0
-ORDER BY c.branchId, c.timestamp DESC
+f.folderId as folderId,
+f.title as title,
+f.parentId as parentId,
+f.creationDate as creationDate
+FROM folder AS f
+LEFT JOIN course_content cc ON f.folderId=cc.itemId
+WHERE cc.courseId='$courseId'
 ";
 
 $result = $conn->query($sql); 
 $response_arr = array();
-$branchId_info_arr = array();
-$sort_order_arr = array();
-$ci_array = array();
-
+$folder_info_arr = array();
+$branch_info_arr = array();
          
 if ($result->num_rows > 0){
+  while($row = $result->fetch_assoc()){ 
+    $folder_info_arr[$row["folderId"]] = array(
+      "title" => $row["title"],
+      "publishDate" => $row["creationDate"],
+      "parentId" => $row["parentId"]
+    );
+  }
+}
 
+// get course content
+$sql="
+  SELECT 
+  c.branchId as branchId,
+  c.title as title,
+  c.contentId as contentId,
+  c.timestamp as publishDate,
+  c.removedFlag as removedFlag,
+  c.draft as draft
+  FROM content AS c
+  LEFT JOIN course_content cc ON cc.itemId = c.branchId
+  WHERE cc.courseId='$courseId'
+  ORDER BY c.branchId, c.timestamp DESC
+";
+
+$result = $conn->query($sql); 
+$ci_array = array();
+$sort_order_arr = array();
+         
+if ($result->num_rows > 0){
     while($row = $result->fetch_assoc()){ 
             
         $end_bi = end($sort_order_arr);
@@ -44,7 +71,6 @@ if ($result->num_rows > 0){
         //     echo $row["contentId"]."\n";
         //     var_dump($ci);
         //     echo "\n----\n\n";
-
         if ($bi != $end_bi){
                 if ($end_bi !== false){ //Only add when we have a branchId
                         $branchId_info_arr[$end_bi]["contentIds"] = $ci_array;
@@ -73,37 +99,17 @@ if ($result->num_rows > 0){
     }
 }
 
-// get parent
-$sql="
-SELECT 
- f.folderId as folderId,
- f.childId as childId
-FROM folder_content AS f
-WHERE removedFlag=0 
-AND childType='content'
-ORDER BY f.folderId
-";
-$result = $conn->query($sql); 
-
-if ($result->num_rows > 0){
-  while($row = $result->fetch_assoc()){ 
-    $branchId_info_arr[$row["childId"]]["parentId"] = $row["folderId"];
-  }
-}
-
 $response_arr = array(
-        "branchId_info"=>$branchId_info_arr,
-        "sort_order"=>$sort_order_arr,
+  "folderInfo"=>$folder_info_arr,
+  "branchInfo"=>$branchId_info_arr
 );
     
- // set response code - 200 OK
- http_response_code(200);
-     
+// set response code - 200 OK
+http_response_code(200);
 
- // make it json format
- echo json_encode($response_arr);
+// make it json format
+echo json_encode($response_arr);
 $conn->close();
 
 
 ?>
-           
--- static/api/loadUserContent.php
+++ static/api/loadUserContent.php
@@ -7,28 +7,46 @@ header('Content-Type: application/json');
 
 include "db_connection.php";
 
+$folderId =  mysqli_real_escape_string($conn,$_REQUEST["folderId"]);  // root or folderId
 
-$sql="
-SELECT 
- c.branchId as branchId,
- c.title as title,
- c.contentId as contentId,
- c.timestamp as publishDate,
- c.draft as draft
-FROM content AS c
-WHERE removedFlag = 0
-ORDER BY c.branchId, c.timestamp DESC
-";
+if ($folderId === "root") {
+  $sql="
+    SELECT 
+    c.branchId as branchId,
+    c.title as title,
+    c.contentId as contentId,
+    c.timestamp as publishDate,
+    c.removedFlag as removedFlag,
+    c.draft as draft
+    FROM content AS c
+    LEFT JOIN user_content uc ON uc.branchId = c.branchId
+    WHERE 
+      uc.username='$remoteuser' AND c.removedFlag=0
+    ORDER BY c.branchId, c.timestamp DESC
+  ";
+} else {
+  $sql="
+    SELECT 
+    c.branchId as branchId,
+    c.title as title,
+    c.contentId as contentId,
+    c.timestamp as publishDate,
+    c.removedFlag as removedFlag,
+    c.draft as draft
+    FROM content AS c
+    LEFT JOIN folder_content fc ON fc.childId = c.branchId
+    WHERE fc.folderId='$folderId' AND fc.childType='content' AND c.removedFlag=0
+    ORDER BY c.branchId, c.timestamp DESC
+  ";
+}
 
 $result = $conn->query($sql); 
 $response_arr = array();
 $branchId_info_arr = array();
 $sort_order_arr = array();
 $ci_array = array();
-
          
 if ($result->num_rows > 0){
-
     while($row = $result->fetch_assoc()){ 
             
         $end_bi = end($sort_order_arr);
@@ -44,7 +62,6 @@ if ($result->num_rows > 0){
         //     echo $row["contentId"]."\n";
         //     var_dump($ci);
         //     echo "\n----\n\n";
-
         if ($bi != $end_bi){
                 if ($end_bi !== false){ //Only add when we have a branchId
                         $branchId_info_arr[$end_bi]["contentIds"] = $ci_array;
@@ -73,24 +90,6 @@ if ($result->num_rows > 0){
     }
 }
 
-// get parent
-$sql="
-SELECT 
- f.folderId as folderId,
- f.childId as childId
-FROM folder_content AS f
-WHERE removedFlag=0 
-AND childType='content'
-ORDER BY f.folderId
-";
-$result = $conn->query($sql); 
-
-if ($result->num_rows > 0){
-  while($row = $result->fetch_assoc()){ 
-    $branchId_info_arr[$row["childId"]]["parentId"] = $row["folderId"];
-  }
-}
-
 $response_arr = array(
         "branchId_info"=>$branchId_info_arr,
         "sort_order"=>$sort_order_arr,
@@ -99,11 +98,8 @@ $response_arr = array(
  // set response code - 200 OK
  http_response_code(200);
      
-
  // make it json format
  echo json_encode($response_arr);
 $conn->close();
-
-
 ?>
            
