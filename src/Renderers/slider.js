c65689d437e214548e182a75fe2004d4c8c8fd34
Kevin Charles <char0042@umn.edu>
2020-06-09 16:59:18 -0500

    First functioning useDoenetRenderer

--- src/Renderers/slider.js
+++ src/Renderers/slider.js
@@ -1,81 +1,24 @@
-import React from 'react';
-import DoenetRenderer from './DoenetRenderer';
+import React, { useState } from 'react';
+// import DoenetRenderer from './DoenetRenderer';
 // import styled from "styled-components";
-// import useDoenetRender from './useDoenetRenderer';
+import useDoenetRender from './useDoenetRenderer';
 
-// export default function Slider(props) {
-//   let [name, SVs, actions, children] = useDoenetRender(props);
+export default function Slider(props) {
+  let [name, SVs, actions, children] = useDoenetRender(props);
+  // let [handlePos,setHandlePos] = useState(100);
 
-//   if (SVs.hide) {
-//     return null;
-//   }
-
-//   return (
-//     <>
-//       <div> {name}'s Slider Value {SVs.items[SVs.index]} </div>
-//       <button onClick={() => actions.changeValue({ value: SVs.items[SVs.index - 1] })}>Prev</button>
-//       <button onClick={() => actions.changeValue({ value: SVs.items[SVs.index + 1] })}>Next</button>
-//     </>
-//   )
-// }
-
-
-
-
-export default class Slider extends DoenetRenderer {
-  constructor(props) {
-    super(props);
-
-    this.handleInput = this.handleInput.bind(this);
-
-    this.state = {
-    }
-
-
-    console.log("this.doenetSvData");
-    // console.log(this.doenetSvData);
-    // console.log(this.doenetSvData.items)
-    // console.log(this.doenetSvData.index)
-    // console.log(this.doenetSvData.sliderType);
-    // console.log(this.actions);
-    console.log(props.rendererUpdateMethods[this.componentName])
-
-
-
+  if (SVs.hide) {
+    return null;
   }
 
 
-  handleInput(e, inputState) {
-
-
-  }
-
-
-
-  render() {
-
-    console.log('RENDER')
-
-    if (this.doenetSvData.hide) {
-      return null;
-    }
-
-    console.log("Current Value")
-    console.log(this.doenetSvData.items[this.doenetSvData.index]);
-
-
-
-    return (
-      <>
-        <div> {this.componentName}'s Slider Value {this.doenetSvData.items[this.doenetSvData.index]} </div>
-        <button onClick={() => this.actions.changeValue({ value: this.doenetSvData.items[this.doenetSvData.index - 1] })}>Prev</button>
-        <button onClick={() => this.actions.changeValue({ value: this.doenetSvData.items[this.doenetSvData.index + 1] })}>Next</button>
-      </>
-    );
-
-
-
-  }
+  return (
+    <>
+      <div> {name}'s Slider Value {SVs.items[SVs.index]} </div>
+      <button onClick={() => actions.changeValue({ value: SVs.items[SVs.index - 1] })}>Prev</button>
+      <button onClick={() => actions.changeValue({ value: SVs.items[SVs.index + 1] })}>Next</button>
+    </>
+  )
 }
 
 {/* 
@@ -100,3 +43,63 @@ export default class Slider extends DoenetRenderer {
 </sequence>
 </slider> 
 */}
+
+
+
+
+// export default class Slider extends DoenetRenderer {
+//   constructor(props) {
+//     super(props);
+
+//     this.handleInput = this.handleInput.bind(this);
+
+//     this.state = {
+//     }
+
+
+//     console.log("this.doenetSvData");
+//     console.log(this.doenetSvData);
+//     // console.log(this.doenetSvData.items)
+//     // console.log(this.doenetSvData.index)
+//     // console.log(this.doenetSvData.sliderType);
+//     // console.log(this.actions);
+//     // console.log(props.rendererUpdateMethods[this.componentName])
+
+
+
+//   }
+
+
+//   handleInput(e, inputState) {
+
+
+//   }
+
+
+
+//   render() {
+
+//     console.log('RENDER')
+
+//     if (this.doenetSvData.hide) {
+//       return null;
+//     }
+
+//     console.log("Current Value")
+//     console.log(this.doenetSvData.items[this.doenetSvData.index]);
+
+
+
+//     return (
+//       <>
+//         <div> {this.componentName}'s Slider Value {this.doenetSvData.items[this.doenetSvData.index]} </div>
+//         <button onClick={() => this.actions.changeValue({ value: this.doenetSvData.items[this.doenetSvData.index - 1] })}>Prev</button>
+//         <button onClick={() => this.actions.changeValue({ value: this.doenetSvData.items[this.doenetSvData.index + 1] })}>Next</button>
+//       </>
+//     );
+
+
+
+//   }
+// }
+
