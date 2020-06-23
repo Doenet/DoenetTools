ea33aa9ae1ceba3231f539cd43532eb46a8b5942
Kevin Charles <char0042@umn.edu>
2020-06-23 18:06:07 -0500

    Fix db connection without Shib on server

--- dist/api/db_connection.php
+++ dist/api/db_connection.php
@@ -1,14 +1,14 @@
 <?php
 
 $env_path = "/var/www/html/dev.doenet.org/etc/env.ini";
-$remoteuser = $_SERVER[ 'REMOTE_USER' ];
+$remoteuser = "devuser";
+  // $remoteuser = $_SERVER[ 'REMOTE_USER' ];
 $db_temp = "cse_doenet";
 
 
 if ($_SERVER['HTTP_HOST'] == 'localhost' || $_SERVER['HTTP_HOST'] == 'localhost:3000'){
   $env_path = "../open_api/env.ini";
   $remoteuser = "devuser";
-  $db_temp = "doenet_local";
 }
 
 $ini_array = parse_ini_file($env_path);
@@ -18,13 +18,14 @@ $username = $ini_array["username"];
 $password = $ini_array["password"];
 $database = $ini_array["database"];
 
-$database = $db_temp; //TODO: remove all $db_temp's
-
-if ($_SERVER['HTTP_HOST'] == 'localhost:3000'){
+if ($_SERVER['HTTP_HOST'] == 'localhost' || $_SERVER['HTTP_HOST'] == 'localhost:3000'){
+  $db_temp = "doenet_local";
   $dbhost = "127.0.0.1";
 }
 
+$database = $db_temp; //TODO: remove all $db_temp's
 
+echo "$dbhost, $username, $password, $database";
 $conn = mysqli_connect($dbhost, $username, $password, $database);
 // Check connection
 if (!$conn) {
