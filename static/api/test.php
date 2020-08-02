623b686bb8f35e8e478f923252ae1f39d86575c8
Kevin Charles <char0042@umn.edu>
2020-08-01 22:14:46 -0500

    Unique device names

--- static/api/test.php
+++ static/api/test.php
@@ -7,15 +7,30 @@ header('Content-Type: application/json');
 
 include "db_connection.php";
 
+$user_id = "g468lpuBGYwxldAMt5vT4";
+$deviceNames = include "deviceNames.php";
+//Remove device names which are already in use
 $sql = "
-SELECT keyword
-FROM keyword
+SELECT deviceName
+FROM user_device
+WHERE userId='$user_id'
 ";
-$result = $conn->query($sql);
-while($row = $result->fetch_assoc()){
-  echo $row['keyword']."\n";
 
+$result = $conn->query($sql);
+$used_deviceNames = array();
+while($row = $result->fetch_assoc()){
+  array_push($used_deviceNames,$row['deviceName']);
 }
+$deviceNames = array_values(array_diff($deviceNames,$used_deviceNames));
+if (count($deviceNames) < 1){
+  //Ran out of device names
+  $deviceName = include 'randomId.php';
+}else{
+  //Pick from what is left
+  $randomNumber = rand(0,(count($deviceNames) - 1));
+  $deviceName = $deviceNames[$randomNumber];
+}
+
 $conn->close();
 
 ?>
