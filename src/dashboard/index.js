2dfbfb61f94cd31f69e14c755878bfc2d8e6d164
Kevin Charles <char0042@umn.edu>
2020-08-01 23:14:46 -0500

    React IndexedDB

--- src/dashboard/index.js
+++ src/dashboard/index.js
@@ -1,7 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { initDB } from 'react-indexed-db';
 
 import DoenetDashboard from '../Tools/DoenetDashboard';
+const DBConfig = {
+  name: 'CoursesDB',
+  version: 1,
+  objectStoresMeta: [
+    {
+      store: 'SelectedCourse',
+      storeConfig: { keyPath: 'id', autoIncrement: true },
+      storeSchema: [
+        { name: 'name', keypath: 'name', options: { unique: false } },
+        { name: 'courseId', keypath: 'courseId', options: { unique: true } }
+      ]
+    },
+    {
+      store: 'CourseList',
+      storeConfig: { keyPath: 'id', autoIncrement: true },
+      storeSchema: [
+        { name: 'name', keypath: 'name', options: { unique: false } },
+        { name: 'courseId', keypath: 'courseId', options: { unique: true } }
+      ]
+    },
+
+  ]
+};
+initDB(DBConfig);
 
     ReactDOM.render(
       <DoenetDashboard/>
