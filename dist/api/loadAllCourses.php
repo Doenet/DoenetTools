285837f2fd95fefa75dab2472f76f43980db19b8
Kevin Charles <char0042@umn.edu>
2020-08-23 18:03:31 -0500

    Finish merge with upstream

--- dist/api/loadAllCourses.php
+++ dist/api/loadAllCourses.php
@@ -6,17 +6,6 @@ header("Access-Control-Allow-Credentials: true");
 header('Content-Type: application/json');
 
 include "db_connection.php";
-<<<<<<< HEAD
-
-$jwtArray = include "jwtArray.php";
-$userId = $jwtArray['userId'];
-
-$overview =  mysqli_real_escape_string($conn,$_GET["overview"]);
-$grade =  mysqli_real_escape_string($conn,$_GET["grade"]);
-$syllabus =  mysqli_real_escape_string($conn,$_GET["syllabus"]);
-$assignment =  mysqli_real_escape_string($conn,$_GET["assignment"]);
-=======
->>>>>>> upstream/master
 
 $jwtArray = include "jwtArray.php";
 $userId = $jwtArray['userId'];
@@ -46,56 +35,6 @@ $result = $conn->query($sql);
 $response_arr = array();
 $courseId_info_arr = array();
 $ci_array = array();
-<<<<<<< HEAD
-         
-if ($result->num_rows > 0){
-  while($row = $result->fetch_assoc()){ 
-    array_push($ci_array, $row["courseId"]);
-    $courseId_info_arr[$row["courseId"]] = array(
-          "courseName" => $row["courseName"],
-          "courseCode" => $row["courseCode"],
-          "term" => $row["term"],
-          "description" => $row["description"],
-          "overviewId" => $row["overviewId"],
-          "syllabusId" => $row["syllabusId"],
-          "overviewEnabled" => $row["overviewEnabled"],
-          "syllabusEnabled" => $row["syllabusEnabled"],
-          "gradeEnabled" => $row["gradeEnabled"],
-          "assignmentEnabled" => $row["assignmentEnabled"],
-          "department" => $row["department"],
-          "section" => $row["section"],
-          "content" => array(),
-          "folders" => array(),
-          "urls" => array(),
-    );
-  }
-}
-
-// // get course content
-// $sql="
-// SELECT 
-//  cc.courseId as courseId,
-//  cc.itemId as itemId,
-//  cc.itemType as itemType
-// FROM course_content AS cc
-// WHERE removedFlag=0
-// ORDER BY cc.courseId
-// ";
-
-// $result = $conn->query($sql); 
-
-// if ($result->num_rows > 0){
-//   while($row = $result->fetch_assoc()){ 
-//     if ($row["itemType"] == "content") {
-//       array_push($courseId_info_arr[$row["courseId"]]["content"], $row["itemId"]);
-//     } else if ($row["itemType"] == "folder"){
-//       array_push($courseId_info_arr[$row["courseId"]]["folders"], $row["itemId"]);
-//     } else if ($row["itemType"] == "url"){
-//       array_push($courseId_info_arr[$row["courseId"]]["urls"], $row["itemId"]);
-//     }
-//   }
-// }
-=======
     
 if ($result->num_rows > 0){
   while($row = $result->fetch_assoc()){ 
@@ -121,7 +60,6 @@ if ($result->num_rows > 0){
     }
   }
 }
->>>>>>> upstream/master
 
 $response_arr = array(
   "courseInfo"=>$courseId_info_arr,
--- dist/api/saveUserCourseModifications.php
+++ dist/api/saveUserCourseModifications.php
@@ -19,58 +19,6 @@ if ($userId == ""){
 
 }else{
 
-<<<<<<< HEAD
-    // $_POST = json_decode(file_get_contents("php://input"),true);
-    // $number_items = count($_POST);
-    //var_dump(variable);
-    $_POST = array(
-        array(
-            "courseId" => 'uTMfKhSmcNtLDaK8oJ3U',
-            "order" => 2,
-            "color" => "aaaaaa",
-            "image" => null
-        ),
-        array(
-            "courseId" => 'NfzKqYtTgYRyPnmaxc7XB',
-            "order" => 1,
-            "color" => "bbbbbb",
-            "image" => null
-        )
-    );
-    
-    for ($i = 0; $i < count($_POST); $i++) {
-
-        $courseId =  mysqli_real_escape_string($conn,$_POST[$i]["courseId"]);
-        $position =  mysqli_real_escape_string($conn,$_POST[$i]["order"]);
-        $color =  mysqli_real_escape_string($conn,$_POST[$i]["color"]);
-        $image =  mysqli_real_escape_string($conn,$_POST[$i]["image"]);
-
-        $sql = "SELECT *
-        FROM user_dashboard_modification as udm
-        WHERE courseId = '$courseId'
-        AND userId = '$userId'";
-
-        $result = $conn->query($sql);
-
-        if ($result->num_rows < 1){
-            echo '1';
-            $sql = "
-            INSERT INTO user_dashboard_modification
-            (userId, courseId, position, color, image)
-            VALUES
-            ('$userId','$courseId','$position','$color','$image')
-            ";
-
-        }else{
-            echo '2';
-            $sql = "UPDATE user_dashboard_modification
-            SET position = $position, color = '$color', image = '$image'
-            WHERE courseId = '$courseId'
-            AND userId = '$userId'";
-        }
-
-        $result = $conn->query($sql); 
-=======
     $_POST = json_decode(file_get_contents("php://input"),true);
     $number_items = count($_POST);
     //var_dump(variable);
@@ -163,7 +111,6 @@ if ($userId == ""){
 
             $result = $conn->query($sql); 
         }
->>>>>>> upstream/master
 
     }
 
