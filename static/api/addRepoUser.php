95b8475f76d7090f85193a300435c5b395567a58
iamchuckss <chuck017@umn.edu>
2020-08-25 20:09:51 +0800

    Prevent regranting access to repo

--- static/api/addRepoUser.php
+++ static/api/addRepoUser.php
@@ -14,44 +14,63 @@ $owner = mysqli_real_escape_string($conn,$_REQUEST["owner"]);
 $jwtArray = include "jwtArray.php";
 $userId = $jwtArray['userId'];
 
+$success = "0";
+$response_message = "";
+
+// Test if current user has grant permission
 $sql = "
 SELECT id
 FROM repo_access
 WHERE userId = '$userId' AND repoId = '$repoId'
 ";
-
 $result = $conn->query($sql); 
-$permisionToInsert = $result->num_rows;
+$hasPermissionToInsert = $result->num_rows > 0;
+if (!$hasPermissionToInsert) {
+  $response_message = "No permission to grant access to this repo";
+}
 
+// Test if new repo
 $sql = "
 SELECT repoId
 FROM folder
 WHERE folderId = '$repoId'
 ";
-
 $result = $conn->query($sql); 
 $newRepo = $result->num_rows == 0;
 
-//Test if user exists
+// Test if user exists
 $sql = "
 SELECT userId
 FROM user
 WHERE email = '$email' 
 ";
-
 $result = $conn->query($sql); 
-$targetUserExists = $result->num_rows;
+$targetUserExists = $result->num_rows > 0;
+if (!$targetUserExists) {
+  $response_message = "User with email $email not found";
+}
 $row = $result->fetch_assoc();
 $targetUserId = $row["userId"];
 
-//TODO: TEST IF USERNAME ALREADY HAS ACCESS
+
+// check if target user already has access to repo
+$sql = "
+SELECT id
+FROM repo_access
+WHERE email='$email' AND repoId='$repoId'
+";
+$result = $conn->query($sql); 
+$targetUserNotAlreadyHasAccess = $result->num_rows == 0;
+if (!$targetUserNotAlreadyHasAccess) {
+  $response_message = "User with email $email already has access";
+}
 
 $response_arr = array(
   "success"=>"0",
-  "message"=>"User with email $email does not exist"
+  "message"=>$response_message
 );
 
-if (($permisionToInsert > 0 || $newRepo) && $targetUserExists > 0){
+if (($hasPermisionToInsert || $newRepo) && $targetUserExists && $targetUserNotAlreadyHasAccess){
 //  User has permission to insert the new user, so insert it
   $sql = "
   INSERT INTO repo_access
@@ -94,8 +113,8 @@ if (($permisionToInsert > 0 || $newRepo) && $targetUserExists > 0){
 } 
 
 
-  http_response_code(200);
-  echo json_encode($response_arr);
+http_response_code(200);
+echo json_encode($response_arr);
 
 
 $conn->close();
