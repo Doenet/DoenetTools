f1a5767ef2c015f5c58e6bb8c01ed9aa470665ee
Clara Selchow <claraselchow@gmail.com>
2020-08-25 11:53:51 -0700

    fix merge errors

--- static/api/loadUserCourses.php
+++ static/api/loadUserCourses.php
@@ -10,54 +10,6 @@ include "db_connection.php";
 $jwtArray = include "jwtArray.php";
 $userId = $jwtArray['userId']; 
 
-// $sql = "SELECT course.courseId, course.longName, course.shortName, course.term, course.imageUrl, course.color
-//         FROM course_enrollment
-//         JOIN course 
-//         ON course_enrollment.courseId = course.courseId
-//         AND course_enrollment.userId = '$userId'
-//         ";
-
-// $result = $conn->query($sql);
-
-// $courseId_arr = array();
-// $courseIndex = 0;
-// $courseIdToIndexHash = array();
-// while ($row = $result->fetch_assoc()) {
-//     array_push($courseId_arr,
-//     array(
-//         "courseId" => $row['courseId'],
-//         "longName" => $row['longName'],
-//         "shortName" => $row['shortName'],
-//         "term" => $row['term'],
-//         "imageUrl" => $row['imageUrl'],
-//         "color" => $row['color'],
-//         "order" => null
-//     )
-//     );
-//     $courseIdToIndexHash[$row['courseId']] = $courseIndex;
-//     $courseIndex += 1;
-// }
-
-// //ToDo check in modified db
-// $sql = "SELECT udm.courseId, udm.color, udm.order, udm.imageUrl
-//         FROM user_dashboard_modification as udm
-//         WHERE udm.userId = '$userId'
-// ";
-
-// $result = $conn->query($sql);
-// while ($row = $result->fetch_assoc()) {
-//     if($row['color'] != null){
-//         $courseId_arr[$courseIdToIndexHash[$row['courseId']]]['color'] = $row['color'];
-//     }
-//     if($row['order'] != null){
-//         $courseId_arr[$courseIdToIndexHash[$row['courseId']]]['order'] = $row['order'];
-//     }
-//     if($row['imageUrl'] != null){
-//         $courseId_arr[$courseIdToIndexHash[$row['courseId']]]['imageUrl'] = $row['imageUrl'];
-//     }
-// }
-
-
 
 $courseInfo = array();
 $courseId_arr = array();
@@ -65,10 +17,19 @@ $courseIndex = 0;
 //Add courses where user is an instructor
 $sql="
 SELECT 
-c.courseId As courseId,
-c.shortName AS shortName,
+c.courseId AS courseId,
+c.shortName AS courseCode,
 c.longName AS longName,
+c.term as term,
 c.description AS description,
+c.overview_branchId as overviewId,
+c.syllabus_branchId as syllabusId,
+c.overviewEnabled as overviewEnabled,
+c.syllabusEnabled as syllabusEnabled,
+c.gradeEnabled as gradeEnabled,
+c.assignmentEnabled as assignmentEnabled,
+c.department as department,
+c.section as section,
 c.color AS color,
 c.term AS term,
 c.image AS image
@@ -86,12 +47,24 @@ if ($result->num_rows > 0){
   while($row = $result->fetch_assoc()){ 
     $course = array(
           "courseId" => $row["courseId"],
-          "shortname" => $row["shortName"],
+          "courseCode" => $row["courseCode"],
+          "shortName" => $row["courseCode"],
           "longname" => $row["longName"],
+          "term" => $row["term"],
           "description" => $row["description"],
+          "overviewId" => $row["overviewId"],
+          "syllabusId" => $row["syllabusId"],
+          "overviewEnabled" => $row["overviewEnabled"],
+          "syllabusEnabled" => $row["syllabusEnabled"],
+          "gradeEnabled" => $row["gradeEnabled"],
+          "assignmentEnabled" => $row["assignmentEnabled"],
+          "department" => $row["department"],
+          "section" => $row["section"],
+          "content" => array(),
+          "folders" => array(),
+          "urls" => array(),
           "color" => $row["color"],
           "image" => $row["image"],
-          "term" => $row["term"],
           "role" => "Instructor"
     );
     array_push($courseInfo, $course);
@@ -105,11 +78,19 @@ if ($result->num_rows > 0){
 $sql="
 SELECT 
 c.courseId As courseId,
-c.shortName AS shortName,
+c.shortName AS courseCode,
 c.longName AS longName,
+c.term as term,
 c.description AS description,
+c.overview_branchId as overviewId,
+c.syllabus_branchId as syllabusId,
+c.overviewEnabled as overviewEnabled,
+c.syllabusEnabled as syllabusEnabled,
+c.gradeEnabled as gradeEnabled,
+c.assignmentEnabled as assignmentEnabled,
+c.department as department,
+c.section as section,
 c.color AS color,
-c.term AS term,
 c.image AS image
 FROM course AS c
 LEFT JOIN course_enrollment AS ce
@@ -127,12 +108,24 @@ if ($result->num_rows > 0){
   while($row = $result->fetch_assoc()){ 
     $course = array(
           "courseId" => $row["courseId"],
-          "shortname" => $row["shortName"],
+          "courseCode" => $row["courseCode"],
+          "shortName" => $row["courseCode"],
           "longname" => $row["longName"],
+          "term" => $row["term"],
           "description" => $row["description"],
+          "overviewId" => $row["overviewId"],
+          "syllabusId" => $row["syllabusId"],
+          "overviewEnabled" => $row["overviewEnabled"],
+          "syllabusEnabled" => $row["syllabusEnabled"],
+          "gradeEnabled" => $row["gradeEnabled"],
+          "assignmentEnabled" => $row["assignmentEnabled"],
+          "department" => $row["department"],
+          "section" => $row["section"],
+          "content" => array(),
+          "folders" => array(),
+          "urls" => array(),
           "color" => $row["color"],
           "image" => $row["image"],
-          "term" => $row["term"],
           "role" => "Student"
     );
     array_push($courseInfo, $course);
