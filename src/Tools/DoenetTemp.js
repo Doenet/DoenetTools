bf569c5e1ed5fc401783d33da98414f11b428e70
Kevin Charles <char0042@umn.edu>
2020-11-12 15:35:55 -0600

    imports

--- src/Tools/DoenetTemp.js
+++ src/Tools/DoenetTemp.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import SearchBar from "../imports/PanelHeaderComponents/SearchBar.js";
-import Textfield from "../imports/PanelHeaderComponents/Textfield.js";
-import VerticalDivider from "../imports/PanelHeaderComponents/VerticalDivider.js";
-import Button from "../imports/PanelHeaderComponents/Button.js";
-import ActionButton from "../imports/PanelHeaderComponents/ActionButton.js";
-import ToggleButton from "../imports/PanelHeaderComponents/ToggleButton.js";
-import ProgressBar from "../imports/PanelHeaderComponents/ProgressBar.js";
-import styled from 'styled-components';
+import {
+  useQuery,
+  useQueryCache,
+  QueryCache,
+  ReactQueryCacheProvider,
+} from 'react-query'
+import { ReactQueryDevtools } from 'react-query-devtools'
 
-export default function attempt() {
-return (
-<div>
-<ProgressBar percent='15'/>
-</div>
-);
+export default function app() {
+return <>
+<Demo />
+</>
 };
 
+function Demo(){
+  return "Demo"
+}
