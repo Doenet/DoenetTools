affe6a2555a29ff4690c0e120742d9a314bfa528
Clara Selchow <claraselchow@gmail.com>
2020-11-15 22:13:50 -0600

    Complete UnitMenu

--- src/Tools/DoenetTemp.js
+++ src/Tools/DoenetTemp.js
@@ -4,15 +4,22 @@ import Textfield from "../imports/PanelHeaderComponents/Textfield.js";
 import VerticalDivider from "../imports/PanelHeaderComponents/VerticalDivider.js";
 import Button from "../imports/PanelHeaderComponents/Button.js";
 import ActionButton from "../imports/PanelHeaderComponents/ActionButton.js";
+// import ActionButtonGroup from "../imports/PanelHeaderComponents/ActionButtonGroup.js";
 import ToggleButton from "../imports/PanelHeaderComponents/ToggleButton.js";
 import ProgressBar from "../imports/PanelHeaderComponents/ProgressBar.js";
-import styled from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
+import ActionButtonGroup from "../imports/PanelHeaderComponents/ActionButtonGroup.js";
+import UnitMenu from "../imports/PanelHeaderComponents/UnitMenu.js";
 
 export default function attempt() {
 return (
-<div>
-<ProgressBar percent='15'/>
-</div>
+<>
+<ActionButtonGroup>
+  <ActionButton text='X'></ActionButton>
+  <ActionButton text='Y'></ActionButton>
+  <ActionButton text='Z'></ActionButton>
+</ActionButtonGroup>
+</>
 );
 };
 
