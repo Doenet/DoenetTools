541e13f53fa146b01a4e9e04e75d278b8fe63529
clary149 <68914928+clary149@users.noreply.github.com>
2020-11-18 20:20:54 -0600

    ActionButtonGroup

--- src/imports/PanelHeaderComponents/ActionButton.js
+++ src/imports/PanelHeaderComponents/ActionButton.js
@@ -1,20 +1,34 @@
 import React from 'react';
-import { doenetComponentForegroundInactive } from "./theme.js"
+import styled, { ThemeProvider, css } from "styled-components";
+
+const Button = styled.button`
+  margin: ${props => props.theme.margin};
+  height: 24px;
+  border-style: solid;
+  border-color: black;
+  border-width: 2px;
+  color: black;
+  background-color: #FFF;
+  border-radius: ${props => props.theme.borderRadius};
+  padding: ${props => props.theme.padding};
+  cursor: pointer;
+  font-size: 12px
+ ;
+`
+
+Button.defaultProps = {
+  theme: {
+    margin: "0",
+    borderRadius: "5px",
+    padding: '0px 10px 0px 10px'
+  }
+}
+
 
 export default function ActionButton(props) {
   //Assume small
   var actionButton = {
-        margin: '0px',
-        height: '24px',
-        border: `2px solid ${doenetComponentForegroundInactive}`,
-        color: '#000',
-        backgroundColor: '#FFF',
-        fontFamily: 'Arial',
-        borderRadius: '5px',
         text: 'Action Button',
-        padding: '0px 10px 0px 10px',
-        cursor: 'pointer',
-        fontSize: '12px'
       };
   if (props.size === "medium") {
     actionButton.height = '36px',
@@ -23,9 +37,19 @@ export default function ActionButton(props) {
   if (props.text) {
       actionButton.text = props.text;
   };
+  if (props.num === 'first') {
+    actionButton.borderRadius = '5px 0px 0px 5px'
+  }
+  if (props.num === 'last') {
+    actionButton.borderRadius = '0px 5px 5px 0px'
+  }
+  function handleClick() {
+    if (props.callback) props.callback()
+  }
+  //TODO handleClick() is not defined
     return (
         <>
-            <button id="actionButton" style={actionButton} onClick={() => { handleClick() }}>{actionButton.text}</button>
+            <Button id="actionButton" style={actionButton} onClick={() => { handleClick() }}>{actionButton.text}</Button>
         </>
     )
 }
