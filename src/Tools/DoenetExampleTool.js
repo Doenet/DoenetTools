e7d18943d5245e8159d0edbd2f483467f4990748
Kevin Charles <char0042@umn.edu>
2020-12-23 10:48:06 -0600

    Start of lowercase children of <Tool>

--- src/Tools/DoenetExampleTool.js
+++ src/Tools/DoenetExampleTool.js
@@ -1,218 +1,206 @@
 import React, { useState } from "react";
-import styled from "styled-components";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faWaveSquare,
-  faDatabase,
-  faServer,
-} from "@fortawesome/free-solid-svg-icons";
 import Tool from "../imports/Tool/Tool";
-import NavPanel from "../imports/Tool/NavPanel";
-import MainPanel from "../imports/Tool/MainPanel";
-import SupportPanel from "../imports/Tool/SupportPanel";
-import MenuPanel from "../imports/Tool/MenuPanel";
-import HeaderMenuPanelButton from "../imports/Tool/HeaderMenuPanelButton";
-import ResponsiveControls from "../imports/Tool/ResponsiveControls";
-import Overlay from "../imports/Tool/Overlay";
-import CollapseSection from "../imports/CollapseSection";
-import MenuPanelSection from "../imports/Tool/MenuPanelSection";
-import ActionButton from "../imports/PanelHeaderComponents/ActionButton";
-import MenuItem from "../imports/PanelHeaderComponents/MenuItem";
-import Menu from "../imports/PanelHeaderComponents/Menu";
-import SectionDivider from "../imports/PanelHeaderComponents/SectionDivider";
-import { SelectedElementStore } from "./SelectedElementContext";
 
-const alphabet =
-  "a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z ";
+// import styled from "styled-components";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import {
+//   faWaveSquare,
+//   faDatabase,
+//   faServer,
+// } from "@fortawesome/free-solid-svg-icons";
+// import NavPanel from "../imports/Tool/NavPanel";
+// import MainPanel from "../imports/Tool/MainPanel";
+// import SupportPanel from "../imports/Tool/SupportPanel";
+// import MenuPanel from "../imports/Tool/MenuPanel";
+// import HeaderMenuPanelButton from "../imports/Tool/HeaderMenuPanelButton";
+// import ResponsiveControls from "../imports/Tool/ResponsiveControls";
+// import Overlay from "../imports/Tool/Overlay";
+// import CollapseSection from "../imports/CollapseSection";
+// import MenuPanelSection from "../imports/Tool/MenuPanelSection";
+// import ActionButton from "../imports/PanelHeaderComponents/ActionButton";
+// import MenuItem from "../imports/PanelHeaderComponents/MenuItem";
+// import Menu from "../imports/PanelHeaderComponents/Menu";
+// import SectionDivider from "../imports/PanelHeaderComponents/SectionDivider";
+// import { SelectedElementStore } from "./SelectedElementContext";
 
-const finalIcon1 = (
-  <FontAwesomeIcon
-    icon={faServer}
-    style={{
-      width: "10px",
-      padding: "2px",
-      backgroundColor: "#e2e2e2",
-      alignSelf: "center",
-      fontSize: "16px",
-      color: "grey",
-    }}
-  />
-);
-
-const finalIcon2 = (
-  <FontAwesomeIcon
-    icon={faDatabase}
-    style={{
-      width: "10px",
-      padding: "2px",
-      backgroundColor: "#e2e2e2",
-      alignSelf: "center",
-      fontSize: "16px",
-      color: "grey",
-    }}
-  />
-);
-const finalIcon3 = (
-  <FontAwesomeIcon
-    icon={faWaveSquare}
-    style={{
-      width: "10px",
-      padding: "2px",
-      backgroundColor: "#e2e2e2",
-      alignSelf: "center",
-      fontSize: "16px",
-      color: "grey",
-    }}
-  />
-);
 
 export default function DoenetExampleTool(props) {
-  const [showHideNewOverLay, setShowHideNewOverLay] = useState(false);
 
-  const showHideOverNewOverlayOnClick = () => {
-    setShowHideNewOverLay(!showHideNewOverLay);
-  };
+  return <Tool>
+     <navPanel>
+        {/* <drive types={['content','courses']} /> */}
+        <p>navigate to important stuff</p>
+      </navPanel>
 
-  return (
-    // <SelectedElementStore>
-    <>
-      {!showHideNewOverLay ? (
-        <Tool
-          initSupportPanelOpen
-          onUndo={() => {
-            console.log(">>>undo clicked");
-          }}
-          onRedo={() => {
-            console.log(">>>redo clicked");
-          }}
-          title={"My Doc"}
-          // responsiveControls={[]}
-          headerMenuPanels={[
-            <HeaderMenuPanelButton buttonText="Add">
-              {"content 1"}
-            </HeaderMenuPanelButton>,
-            <HeaderMenuPanelButton buttonText="Save">
-              {"content 2"}
-            </HeaderMenuPanelButton>,
-          ]}
-        >
-          <NavPanel>
-            Nav Panel
-          </NavPanel>
+      <mainPanel>
+        <p>do important stuff</p>
+        {/* <drive id="abc123" /> */}
+      </mainPanel>
 
-          <MainPanel
-            setShowHideNewOverLay={setShowHideNewOverLay}
-            // responsiveControls={[]}
-          >
-            <div
-              onClick={() => {
-                showHideOverNewOverlayOnClick();
-              }}
-            >
-              Click for Overlay
-            </div>
+      {/* <menuPanel>
+        <p>control important stuff</p>
+      </menuPanel>
 
-            <h3> This is Main Panel</h3>
-            <p>click Switch button in header to see support panel</p>
-            <p>
-              Define responsiveControls to see for standard components section
-              which are responsive and collapses according the width available
-            </p>
-
-            <h2>Header Menu Panels </h2>
-            <p>Click add and save to see header menu panels section </p>
-          </MainPanel>
-
-          <SupportPanel
-          // responsiveControls={[]}
-          >
-            <h3>Support Panel Content</h3>
-
-            <p>
-              Define responsiveControls to see for standard components section
-              which are responsive and collapses according the width available
-            </p>
-          </SupportPanel>
-          <MenuPanel>
-            <MenuPanelSection title="Font">
-              <CollapseSection>
-                <SectionDivider type="single">
-                  <ActionButton
-                    handleClick={() => {
-                      alert();
-                    }}
-                  />
-                  <Menu label="actions">
-                    <MenuItem
-                      value="Times"
-                      onSelect={() => {
-                        alert("Times Selected");
-                      }}
-                    />
-                    <MenuItem
-                      value="Ariel"
-                      onSelect={() => {
-                        alert("Ariel Selected");
-                      }}
-                    />
-                  </Menu>
-                </SectionDivider>
-              </CollapseSection>
-              <CollapseSection></CollapseSection>
-            </MenuPanelSection>
-            <MenuPanelSection title="style">
-              Menu Panel Style Content
-            </MenuPanelSection>
-          </MenuPanel>
-        </Tool>
-      ) : (
-        <Overlay
-          isOpen={showHideNewOverLay}
-          onUndo={() => {}}
-          onRedo={() => {}}
-          title={"my doc"}
-          onClose={() => {
-            setShowHideNewOverLay(false);
-          }}
-          // responsiveControls={[<ResponsiveControls/>]}
-          headerMenuPanels={[]}
-        >
-          <MainPanel responsiveControls={[]}>Overlay Main panel</MainPanel>
-          <SupportPanel responsiveControls={[]}>Overlay Support</SupportPanel>
-          <MenuPanel>
-            <MenuPanelSection title="Font">
-              <CollapseSection>
-                <SectionDivider type="single">
-                  <ActionButton
-                    handleClick={() => {
-                      alert();
-                    }}
-                  />
-                  <Menu label="actions">
-                    <MenuItem
-                      value="Times"
-                      onSelect={() => {
-                        alert("Times Selected");
-                      }}
-                    />
-                    <MenuItem
-                      value="Ariel"
-                      onSelect={() => {
-                        alert("Ariel Selected");
-                      }}
-                    />
-                  </Menu>
-                </SectionDivider>
-              </CollapseSection>
-              <CollapseSection></CollapseSection>
-            </MenuPanelSection>
-            <MenuPanelSection title="style">
-              Menu Panel Style Content
-            </MenuPanelSection>
-          </MenuPanel>
-        </Overlay>
-      )}
-      </>
-    // </SelectedElementStore>
-  );
+      <menuPanel>
+        <p>control more important stuff</p>
+      </menuPanel> */}
+  </Tool>
 }
+
+
+
+
+
+
+
+
+
+
+// const [showHideNewOverLay, setShowHideNewOverLay] = useState(false);
+
+// const showHideOverNewOverlayOnClick = () => {
+//   setShowHideNewOverLay(!showHideNewOverLay);
+// };
+
+// return (
+//   // <SelectedElementStore>
+//   <>
+//     {!showHideNewOverLay ? (
+//       <Tool
+//         initSupportPanelOpen
+//         onUndo={() => {
+//           console.log(">>>undo clicked");
+//         }}
+//         onRedo={() => {
+//           console.log(">>>redo clicked");
+//         }}
+//         title={"My Doc"}
+//         // responsiveControls={[]}
+//         headerMenuPanels={[
+//           <HeaderMenuPanelButton buttonText="Add">
+//             {"content 1"}
+//           </HeaderMenuPanelButton>,
+//           <HeaderMenuPanelButton buttonText="Save">
+//             {"content 2"}
+//           </HeaderMenuPanelButton>,
+//         ]}
+//       >
+//         <NavPanel>
+//           Nav Panel
+//         </NavPanel>
+
+//         <MainPanel
+//           setShowHideNewOverLay={setShowHideNewOverLay}
+//           // responsiveControls={[]}
+//         >
+//           <div
+//             onClick={() => {
+//               showHideOverNewOverlayOnClick();
+//             }}
+//           >
+//             Click for Overlay
+//           </div>
+
+//           <h3> This is Main Panel</h3>
+//           <p>click Switch button in header to see support panel</p>
+//           <p>
+//             Define responsiveControls to see for standard components section
+//             which are responsive and collapses according the width available
+//           </p>
+
+//           <h2>Header Menu Panels </h2>
+//           <p>Click add and save to see header menu panels section </p>
+//         </MainPanel>
+
+//         <SupportPanel
+//         // responsiveControls={[]}
+//         >
+//           <h3>Support Panel Content</h3>
+
+//           <p>
+//             Define responsiveControls to see for standard components section
+//             which are responsive and collapses according the width available
+//           </p>
+//         </SupportPanel>
+//         <MenuPanel>
+//           <MenuPanelSection title="Font">
+//             <CollapseSection>
+//               <SectionDivider type="single">
+//                 <ActionButton
+//                   handleClick={() => {
+//                     alert();
+//                   }}
+//                 />
+//                 <Menu label="actions">
+//                   <MenuItem
+//                     value="Times"
+//                     onSelect={() => {
+//                       alert("Times Selected");
+//                     }}
+//                   />
+//                   <MenuItem
+//                     value="Ariel"
+//                     onSelect={() => {
+//                       alert("Ariel Selected");
+//                     }}
+//                   />
+//                 </Menu>
+//               </SectionDivider>
+//             </CollapseSection>
+//             <CollapseSection></CollapseSection>
+//           </MenuPanelSection>
+//           <MenuPanelSection title="style">
+//             Menu Panel Style Content
+//           </MenuPanelSection>
+//         </MenuPanel>
+//       </Tool>
+//     ) : (
+//       <Overlay
+//         isOpen={showHideNewOverLay}
+//         onUndo={() => {}}
+//         onRedo={() => {}}
+//         title={"my doc"}
+//         onClose={() => {
+//           setShowHideNewOverLay(false);
+//         }}
+//         // responsiveControls={[<ResponsiveControls/>]}
+//         headerMenuPanels={[]}
+//       >
+//         <MainPanel responsiveControls={[]}>Overlay Main panel</MainPanel>
+//         <SupportPanel responsiveControls={[]}>Overlay Support</SupportPanel>
+//         <MenuPanel>
+//           <MenuPanelSection title="Font">
+//             <CollapseSection>
+//               <SectionDivider type="single">
+//                 <ActionButton
+//                   handleClick={() => {
+//                     alert();
+//                   }}
+//                 />
+//                 <Menu label="actions">
+//                   <MenuItem
+//                     value="Times"
+//                     onSelect={() => {
+//                       alert("Times Selected");
+//                     }}
+//                   />
+//                   <MenuItem
+//                     value="Ariel"
+//                     onSelect={() => {
+//                       alert("Ariel Selected");
+//                     }}
+//                   />
+//                 </Menu>
+//               </SectionDivider>
+//             </CollapseSection>
+//             <CollapseSection></CollapseSection>
+//           </MenuPanelSection>
+//           <MenuPanelSection title="style">
+//             Menu Panel Style Content
+//           </MenuPanelSection>
+//         </MenuPanel>
+//       </Overlay>
+//     )}
+//     </>
+//   // </SelectedElementStore>
+// );
--- src/imports/Tool/Tool.js
+++ src/imports/Tool/Tool.js
@@ -9,65 +9,110 @@ const ToolContainer = styled.div`
 `;
 
 export default function Tool(props) {
-  //console.log(props.responsiveControls, "props.responsiveControls in tool");
-  const [supportPanelObj, setSupportPanelObj] = React.useState({});
+  console.log("=== Tool (only once)");
 
-  React.useEffect(() => {
-    if (props.children && Array.isArray(props.children)) {
-      props.children.map((obj, index) => {
-        if (
-          obj &&
-          obj.type &&
-          typeof obj.type === "function" &&
-          obj.type.name === "SupportPanel"
-        ) {
-          console.log(obj.props.children, "obj.props support panel");
-          setSupportPanelObj(
-            React.cloneElement(obj, {
-              responsiveControls: obj.props.responsiveControls,
-              key: index,
-            })
-          );
+  var toolParts = {};
+  
+  const implementedToolParts = ["navPanel", "mainPanel", "menuPanel"];
+  if (props.children) {
+    if (Array.isArray(props.children)) {
+      for (let child of props.children) {
+        if (implementedToolParts.includes(child.type)) {
+          if (child.type === "menuPanel") {
+            if (!toolParts.menuPanel){
+              toolParts['menuPanel'] = [];
+            }
+            toolParts.menuPanel.push({
+              children: child.props.children
+            });
+          }else{
+            toolParts[child.type] = {
+              children: child.props.children
+            };
+          }
+          
         }
-      });
+      }
+    } else {
+      //Only one child
+      if (implementedToolParts.includes(props.children.type)) {
+        toolParts[props.children.type] = {
+          children: props.children.props.children
+        };
+      }
     }
-  }, []);
+  }
 
-  const showNewOverlay = () => {
-    props.setShowHideNewOverLay(true);
-  };
+  console.log(">>>toolParts:",toolParts);
+  let mainPanel = null;
+  let menuPanel = null;
+  let navPanel = null;
 
-  console.log(">>>Tool Children: ", props.children);
+  if (toolParts.navPanel) {
+    navPanel = (
+      <>
+        <h2>Nav Panel</h2>
+        <div>{toolParts.navPanel.children}</div>
+      </>
+    );
+  }
+
+  if (toolParts.mainPanel) {
+    mainPanel = (
+      <>
+        <h2>Main Panel</h2>
+        <div>{toolParts.mainPanel.children}</div>
+      </>
+    );
+  }
+
+  if (toolParts.menuPanel) {
+    menuPanel = [];
+    for (let [i,menuPanelToolPart] of Object.entries(toolParts.menuPanel)){
+      menuPanel.push(<React.Fragment key={`menuPanel${i}`}>
+        <h2>Menu Panel</h2>
+        <div>{menuPanelToolPart.children}</div>
+      </React.Fragment>)
+    }
+
+  }
 
   return (
     <ToolContainer>
-      {props.children &&
-        Array.isArray(props.children) &&
-        props.children.map((obj, index) => {
-          switch (obj?.type?.name) {
-            case "MainPanel":
-              return React.cloneElement(obj, {
-                onClick: () => {
-                  props.setShowHideNewOverLay(true);
-                },
-                responsiveControlsFromTools: props.responsiveControls,
-                responsiveControls: obj.props.responsiveControls,
-                onUndo: props.onUndo,
-                onRedo: props.onRedo,
-                title: props.title,
-                supportPanelObj: supportPanelObj,
-                headerMenuPanels: props.headerMenuPanels,
-                initSupportPanelOpen: props.initSupportPanelOpen,
-                key: index,
-              });
-            case "SupportPanel":
-              return (null);
-            case "NavPanel":
-            case "MenuPanel":
-            default:
-              return React.cloneElement(obj, { key: index });
-          }
-        })}
-    </ToolContainer>
+      <h1>Tool</h1>
+      {navPanel}
+      {mainPanel}
+      {menuPanel}
+      </ToolContainer>
   );
+
 }
+
+
+// {props.children &&
+//   Array.isArray(props.children) &&
+//   props.children.map((obj, index) => {
+//     switch (obj?.type?.name) {
+//       case "MainPanel":
+//         return React.cloneElement(obj, {
+//           onClick: () => {
+//             props.setShowHideNewOverLay(true);
+//           },
+//           responsiveControlsFromTools: props.responsiveControls,
+//           responsiveControls: obj.props.responsiveControls,
+//           onUndo: props.onUndo,
+//           onRedo: props.onRedo,
+//           title: props.title,
+//           supportPanelObj: supportPanelObj,
+//           headerMenuPanels: props.headerMenuPanels,
+//           initSupportPanelOpen: props.initSupportPanelOpen,
+//           key: index,
+//         });
+//       case "SupportPanel":
+//         return (null);
+//       case "NavPanel":
+//       case "MenuPanel":
+//       default:
+//         return React.cloneElement(obj, { key: index });
+//     }
+//   })}
