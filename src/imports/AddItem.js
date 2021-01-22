e9591cf181d38ee01147f321c467f6dc3d38f704
iamchuckss <chuck017@umn.edu>
2021-01-21 20:28:50 -0600

    Put add forms in tab structure

--- src/imports/AddItem.js
+++ src/imports/AddItem.js
@@ -1,6 +1,7 @@
 import React, {useContext, useState, useCallback, useRef, useEffect} from 'react';
 import axios from "axios";
 import nanoid from 'nanoid';
+import styled from "styled-components";
 import './util.css';
 
 import {
@@ -45,8 +46,20 @@ function AddItemRouted(props){
   const folderId = globalSelected[0]?.parentFolderId;
   let folderInfo = useRecoilStateLoadable(folderDictionarySelector({driveId,folderId})); 
   let itemInfo = folderInfo[0]?.contents?.contentsDictionary?.[selectedItemId];
+  const [activeForm, setActiveForm] = useState(0);
+  
+  const handleClickFormTab = (id) => {
+    if (id !== activeForm) {
+      setActiveForm(id);
+    }
+  };
+  const onAddItemCallback = () => {
+    setIsExpanded(false);
+    setActiveForm(0);
+  }
   
   const [isExpanded, setIsExpanded] = useState(false);
+  
   if (itemInfo?.itemType !== "Folder") return <></>;
 
   return <div>
@@ -56,17 +69,29 @@ function AddItemRouted(props){
       </div> :
       <div>
         <div className="headingContainer" style={{display: "flex", justifyContent: "space-between", padding: "3px"}}>
-          <span><strong>Add Item to {itemInfo.label}</strong></span>
+          <h3>Add Item</h3>
           <button onClick={()=>{setIsExpanded(false)}}>X</button>
         </div>
         <div>
-          <AddNewFolderForm driveId={driveId} folderId={selectedItemId} callback={()=>{setIsExpanded(false)}}/>
-          <AddNewURLForm driveId={driveId} folderId={selectedItemId} callback={()=>{setIsExpanded(false)}}/>
-          <AddNewDoenetMLForm driveId={driveId} folderId={selectedItemId} callback={()=>{setIsExpanded(false)}}/>
+          <Tabs>
+            <Tab onClick={()=>handleClickFormTab(0)} active={activeForm===0} id={`addFormFolder`}>Folder</Tab>
+            <Tab onClick={()=>handleClickFormTab(1)} active={activeForm===1} id={`addFormUrl`}>URL</Tab>
+            <Tab onClick={()=>handleClickFormTab(2)} active={activeForm===2} id={`addFormDoenetML`}>DoenetML</Tab>
+          </Tabs>
+          <>
+            <Content active={activeForm===0}>
+              <AddNewFolderForm driveId={driveId} folderId={selectedItemId} callback={onAddItemCallback}/>
+            </Content>
+            <Content active={activeForm===1}>
+              <AddNewURLForm driveId={driveId} folderId={selectedItemId} callback={onAddItemCallback}/>
+            </Content>
+            <Content active={activeForm===2}>
+              <AddNewDoenetMLForm driveId={driveId} folderId={selectedItemId} callback={onAddItemCallback}/>
+            </Content>
+          </>
         </div>
       </div>
     }
-
   </div>
 }
 
@@ -103,9 +128,6 @@ function AddNewFolderForm(props) {
               required
             />
           </div>
-          <div className="form-group">
-            <label>Other details: </label>
-          </div>
           <div className="form-group" style={{display:"flex", justifyContent:"flex-end"}}>
             <input className="btn btn-primary" type="submit" value="Add"/>
           </div>
@@ -122,7 +144,7 @@ function AddNewURLForm(props) {
   const handleSubmit = (ev) => {
     ev.preventDefault();
     const label = ev.target.label.value;
-    const url = ev.target.label.url;
+    const url = ev.target.url.value;
     dispatchAddInstruction({label, url});
     props.callback?.();
   }
@@ -153,7 +175,7 @@ function AddNewURLForm(props) {
             <label>URL: </label>
             <input
               type="text"
-              name="label"
+              name="url"
               required
             />
           </div>
@@ -205,3 +227,34 @@ function AddNewDoenetMLForm(props) {
     </div> 
   )
 }
+
+const Tabs = styled.div`
+  overflow: hidden;
+  background: #fff;
+  font-family: Open Sans;
+  height: 1.5em;
+`;
+
+const Tab = styled.button`
+  border: none;
+  outline: none;
+  cursor: pointer;
+  width: 30%;
+  position: relative;
+
+  margin-right: 0.1em;
+  font-size: 1em;
+  border: ${props => (props.active ? "1px solid #ccc" : "")};
+  border-bottom: ${props => (props.active ? "none" : "")};
+  background-color: ${props => (props.active ? "white" : "lightgray")};
+  height: ${props => (props.active ? "1.5em" : "1.2em; top:.4em")};
+  transition: background-color 0.5s ease-in-out;
+
+  :hover {
+    background-color: white;
+  }
+`;
+
+const Content = styled.div`
+  ${props => (props.active ? "" : "display:none")}
+`;
