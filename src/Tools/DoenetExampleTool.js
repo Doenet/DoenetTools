db7dae6f4a8b8eb3678e27d8f350099888718711
Emilio Alvarez <e.l.alvarez@icloud.com>
2021-02-03 23:00:28 -0600

    Messing with Profile

--- src/Tools/DoenetExampleTool.js
+++ src/Tools/DoenetExampleTool.js
@@ -122,6 +122,45 @@ export default function DoenetExampleTool() {
 
         <menuPanel title="edit">
           <p>control important stuff</p>
+          <div>
+            <button
+              onClick={() => {
+                setOverlayOpen({
+                  name: "cal2",
+                  instructions: {
+                    action: "open",
+                    supportVisble: false,
+                    courseId: "d1",
+                    branchId: "e1",
+                  },
+                });
+              }}
+            >
+              Go to cal
+            </button>
+          </div>
+        </menuPanel>
+
+        <menuPanel title="other">
+          <p>control more important stuff</p>
+        </menuPanel>
+      </overlay>
+
+      <overlay name="cal2">
+        <headerPanel title="my title"></headerPanel>
+
+        <mainPanel>
+          <h1>calender</h1>
+          <h2>Data</h2>
+          <OverlayDataViwer />
+        </mainPanel>
+
+        <supportPanel width="40%">
+          <p>Support Panel</p>
+        </supportPanel>
+
+        <menuPanel title="edit">
+          <p>control important stuff</p>
         </menuPanel>
 
         <menuPanel title="other">
