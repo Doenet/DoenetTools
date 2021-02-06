ebab7a9e3625c2352a13760f8b82a3b410ebccd2
Vinitha Konduru <vinithak@umn.edu>
2021-02-06 10:13:33 -0600

    style left arrow

--- src/imports/Breadcrumb/BreadcrumbContainer.js
+++ src/imports/Breadcrumb/BreadcrumbContainer.js
@@ -59,5 +59,5 @@ export const BreadcrumbContainer = ({ divider = '/', ...props }) => {
   
   }
 
-  return (<ol style={breadcrumbContainerStyle}>{<div onClick={leftmostBreadcrumb}>{<FontAwesomeIcon icon={faChevronLeft}/>}</div>}{children}</ol>);
+  return (<ol style={breadcrumbContainerStyle}>{<div onClick={leftmostBreadcrumb} style={{marginLeft:'10px',marginRight:'10px'}} >{<FontAwesomeIcon icon={faChevronLeft}/>}</div>}{children}</ol>);
 };
