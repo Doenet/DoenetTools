e94ce1295c7a9f63d851aa5a8a481c6b6e592ce4
Vinitha Konduru <vinithak@umn.edu>
2021-02-13 10:00:54 -0600

    drive colors

---
+++ src/imports/Util.js
@@ -0,0 +1,14 @@
+export const driveColors={
+  red:'#FF0000',
+  orange:'#FFA500',
+  yellow:'#FFFF00',
+  darkblue: '#0000FF',
+  lightblue: '#1F85DE',
+  purple:'#800080',
+  green:'#008000',
+  grey:'#808080',
+  brown:'#964B00',
+  tan:'#D2B48C',
+  cream:'#FFFDD0'
+
+}
