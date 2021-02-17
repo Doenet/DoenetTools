9b0a5d681d504892351192f0b6aeac26a13dc4ad
Emilio Alvarez <e.l.alvarez@icloud.com>
2021-02-17 02:11:33 -0600

    Import Repair

--- src/exampletool/index.js
+++ src/exampletool/index.js
@@ -1,33 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {
-  HashRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
-import {
-  DropTargetsProvider,
-} from '../imports/DropTarget';
-import { 
-  BreadcrumbProvider 
-} from '../imports/Breadcrumb';
-import {
-  RecoilRoot,
-} from 'recoil';
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import { DropTargetsProvider } from "../imports/DropTarget";
+import { BreadcrumbProvider } from "../imports/Breadcrumb";
+import { RecoilRoot } from "recoil";
 
-import DoenetExampleTool from '../Tools/DoenetExampleTool';
+import DoenetExampleTool from "../Tools/DoenetExampleTool";
+import LayerRoot from "../imports/Tool/LayerRoot";
 
-    ReactDOM.render(
-      <DropTargetsProvider>
-        <BreadcrumbProvider>
-          <RecoilRoot>
-            <Router >
-              <Switch>
-                <Route path="/" render={(routeprops)=>
-                  <DoenetExampleTool route={{...routeprops}} />} />
-              </Switch>
-            </Router>
-          </RecoilRoot>
-        </BreadcrumbProvider>
-      </DropTargetsProvider>
-  ,document.getElementById('root'));
+ReactDOM.render(
+  <DropTargetsProvider>
+    <BreadcrumbProvider>
+      <RecoilRoot>
+        <Router>
+          <Switch>
+            <Route
+              path="/"
+              render={(routeprops) => (
+                <LayerRoot
+                  route={{ ...routeprops }}
+                  baseLayer={<DoenetExampleTool key={"BaseLayer"} />}
+                />
+              )}
+            />
+          </Switch>
+        </Router>
+      </RecoilRoot>
+    </BreadcrumbProvider>
+  </DropTargetsProvider>,
+  document.getElementById("root")
+);
