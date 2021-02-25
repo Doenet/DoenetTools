6347d1fd31ce183e50f8761004d97726e7610ac4
Kevin Charles <kevincharles@users.noreply.github.com>
2021-02-24 22:47:47 -0600

    Modify Course Owners and Admins (#267)

--- src/Tools/DoenetLibrary.js
+++ src/Tools/DoenetLibrary.js
@@ -3,6 +3,7 @@ import Tool, { openOverlayByName } from "../imports/Tool/Tool";
 import { useMenuPanelController } from "../imports/Tool/MenuPanel";
 import {driveColors,driveImages} from '../imports/Util';
 import DoenetDriveCardMenu from "../imports/DoenetDriveCardMenu";
+import './util.css';
 
 import Drive, { 
   folderDictionarySelector, 
@@ -10,14 +11,16 @@ import Drive, {
   folderDictionary, 
   clearDriveAndItemSelections,
   fetchDrivesSelector,
-  encodeParams
+  encodeParams,
+  fetchDriveUsers
 } from "../imports/Drive";
 import nanoid from 'nanoid';
 
 import { 
   faChalkboard,
   faCode,
-  faFolder
+  faFolder,
+  faUserCircle
  } from '@fortawesome/free-solid-svg-icons';
  import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
@@ -243,7 +246,7 @@ const updateItemHistorySelector = selectorFamily({
     return get(itemHistoryAtom(branchId))
   },
   set:(branchId)=> ({get,set},instructions)=>{
-    console.log(">>>instructions",instructions.instructions)
+    // console.log(">>>instructions",instructions.instructions)
     
 
     const doenetML = get(editorDoenetMLAtom);
@@ -417,7 +420,7 @@ function VersionHistoryPanel(props){
         onClick={()=>{
           if (version.timestamp !== selectedTimestamp){
             setSelectedTimestamp(version.timestamp)
-            console.log(">>>version.contentId",version.contentId)
+            // console.log(">>>version.contentId",version.contentId)
             setEditingContentId(version.contentId)
           }
         }}
@@ -493,8 +496,8 @@ function SetEditorDoenetMLandTitle(props){
   if (props.isDraft){ contentId = props.branchId;}
   const editingContentId = useRecoilValue(EditingContentIdAtom);
   if (editingContentId !== ""){ contentId = editingContentId}
-console.log(">>>SetEditorDoenetMLandTitle editingContentId",editingContentId)
-console.log(">>>SetEditorDoenetMLandTitle contentId",contentId)
+// console.log(">>>SetEditorDoenetMLandTitle editingContentId",editingContentId)
+// console.log(">>>SetEditorDoenetMLandTitle contentId",contentId)
   const loadedDoenetML = useRecoilValueLoadable(fileByContentId(contentId))
   const setEditorDoenetML = useSetRecoilState(editorDoenetMLAtom);
   const setViewerDoenetML = useSetRecoilState(viewerDoenetMLAtom);
@@ -523,13 +526,279 @@ console.log(">>>SetEditorDoenetMLandTitle contentId",contentId)
 return null;
 }
 
+
+
+
+function User(props){
+  let onClick = props.onClick;
+  if (!onClick){onClick = ()=>{}}
+  let emailAddress = null;
+  let emailStyle = {}
+  let buttons = [];
+  let star = null;
+  let screenName = props.screenName;
+  if (screenName === "" || screenName === null){ screenName = "Unknown" }
+  if (props.isUser){
+    star = <FontAwesomeIcon icon={faUserCircle}/>;
+  }
+    emailAddress = <span style={emailStyle}>{props.email}</span>;
+  let containerStyle = {}
+    if (props.isSelected){
+      if (props.isOwner || props.userRole == "admin"){
+        if (!(props.userRole === 'owner' && props.numOwners < 2)){
+          //Only show remove if two or more owners
+          buttons.push(
+            <div key={`remove${props.userId}`}>
+              <Button 
+              data-doenet-removeButton={props.userId}
+            text="Remove" 
+            callback={(e)=>{
+              e.preventDefault();
+              e.stopPropagation();
+              onClick("")
+              props.setDriveUsers({
+                driveId:props.driveId,
+                type:"Remove User",
+                userId:props.userId,
+                userRole:props.userRole
+              })
+            
+            
+            }
+            }/>
+           
+            </div>
+            )
+        }
+        
+      }
+      if (props.isOwner && props.userRole == "admin"){
+        
+        buttons.push(
+          <div key={`promote${props.userId}`}>
+            <Button 
+          data-doenet-removebutton={props.userId}
+          text="Promote to Owner" callback={(e)=>{
+            e.preventDefault();
+            e.stopPropagation();
+            onClick("")
+          props.setDriveUsers({
+              driveId:props.driveId,
+              type:"To Owner",
+              userId:props.userId,
+              userRole:props.userRole
+            })
+          }
+          } /></div>
+          )
+      }
+      if (props.isOwner && props.userRole == "owner"){
+        if (!(props.userRole === 'owner' && props.numOwners < 2)){
+          //Only show demote if two or more owners
+        buttons.push(
+          <div key={`demote${props.userId}`}>
+            <Button 
+          data-doenet-removebutton={props.userId}
+          text="Demote to Admin" callback={(e)=>{
+            e.preventDefault();
+            e.stopPropagation();
+            onClick("")
+            props.setDriveUsers({
+              driveId:props.driveId,
+              type:"To Admin",
+              userId:props.userId,
+              userRole:props.userRole
+            })
+          }
+          }/></div>
+          )
+        }
+      }
+      
+      containerStyle = {backgroundColor:"#B8D2EA"}
+      emailStyle = {border:"solid 1px black"}
+  }
+  
+  return <>
+    <div 
+    tabIndex={0}
+    className="noselect nooutline" 
+    onClick={()=>onClick(props.userId)}
+    onBlur={(e)=>{
+      if (e.relatedTarget?.dataset?.doenetRemovebutton !== props.userId){
+      // setTimeout(()=>onClick(""),500);
+      onClick("")
+      }
+    }}
+    >
+      <div style={containerStyle} >
+      <div>{star}{screenName}</div>
+      <div>{emailAddress}</div>
+      </div>
+      {buttons}
+    </div>
+    </>
+}
+
+function validateEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+function NewUser(props){
+  const [email,setEmail] = useState("")
+
+
+  function addUser(){
+    if (validateEmail(email)){
+      props.setDriveUsers({
+          driveId:props.driveId,
+          type:props.type,
+          email,
+          callback
+        })
+      props.open(false);
+    }else{
+      //Toast invalid email
+      console.log(`Not Added: Invalid email ${email}`)
+    }
+
+    //TODO: when set async available replace this.
+    function callback(resp){
+      props.setDriveUsers({
+        driveId:props.driveId,
+        type:`${props.type} step 2`,
+        email,
+        screenName:resp.screenName,
+        userId:resp.userId
+      })
+    }
+    
+  }
+
+  return <><div>
+    <label>User&#39;s Email Address<br />
+    <input type="text" value={email} 
+    onChange={(e)=>{setEmail(e.target.value)}}
+    onKeyDown={(e)=>{if (e.keyCode === 13){ 
+      addUser();
+    }}}
+    onBlur={()=>{
+      addUser();
+    }}
+    /></label>
+  </div>
+    <Button text="Submit" callback={()=>addUser()}/>
+    <Button text="Cancel" callback={()=>props.open(false)}/>
+    </>
+
+}
+
 const DriveInfoPanel = function(props){
   const [driveLabel,setDriveLabel] = useState(props.label);
   const [panelDriveLabel,setPanelDriveLabel] = useState(props.label);
   const setDrivesInfo = useSetRecoilState(fetchDrivesSelector);
+  const driveId = props.driveId;
+  const [driveUsers,setDriveUsers] = useRecoilStateLoadable(fetchDriveUsers(driveId));
 
+  const [selectedUserId,setSelectedUserId] = useState("");
+  const [shouldAddOwners,setAddOwners] = useState(false);
+  const [shouldAddAdmins,setAddAdmins] = useState(false);
 
+  if (driveUsers.state === "loading"){ return null;}
+    if (driveUsers.state === "hasError"){ 
+      console.error(driveUsers.contents)
+      return null;}
+
+  let isOwner = false;
+  if (driveUsers.contents.usersRole === "Owner"){
+    isOwner = true;
+  }
   let dIcon = <FontAwesomeIcon icon={faChalkboard}/>
+
+  let admins = [];
+  let owners = [];
+
+  let addOwners = null;
+  let addOwnersButton = <Button text="+ Add Owner" callback={()=>{
+    setAddOwners(true);
+    }} />
+
+  if (shouldAddOwners){ 
+    addOwners = <NewUser open={setAddOwners} driveId={driveId} type="Add Owner" setDriveUsers={setDriveUsers}/>
+    addOwnersButton = null;
+  }
+  let addAdmins = null;
+  let addAdminsButton = <Button text="+ Add Administrator" callback={()=>{
+    setAddAdmins(true);
+  }} />
+  if (shouldAddAdmins){
+    addAdmins = <NewUser open={setAddAdmins} driveId={driveId} type="Add Admin" setDriveUsers={setDriveUsers}/>
+    addAdminsButton = null;
+  }
+
+
+
+  
+
+  for (let owner of driveUsers.contents.owners){
+    let isSelected = false;
+    if (owner.userId === selectedUserId){
+      isSelected = true;
+    }
+    owners.push(<User 
+      key={`User${owner.userId}`} 
+      isSelected={isSelected}
+      onClick={setSelectedUserId}
+      userId={owner.userId} 
+      driveId={driveId} 
+      email={owner.email} 
+      isUser={owner.isUser} 
+      screenName={owner.screenName}
+      setDriveUsers={setDriveUsers}
+      userRole="owner"
+      isOwner={isOwner}
+      numOwners={driveUsers.contents.owners.length}
+      />)
+  }
+  for (let admin of driveUsers.contents.admins){
+    let isSelected = false;
+    if (admin.userId === selectedUserId){
+      isSelected = true;
+    }
+    
+    admins.push(<User 
+      key={`User${admin.userId}`} 
+      isSelected={isSelected}
+      onClick={setSelectedUserId}
+      userId={admin.userId} 
+      driveId={driveId} 
+      email={admin.email} 
+      isUser={admin.isUser} 
+      screenName={admin.screenName}
+      setDriveUsers={setDriveUsers}
+      userRole="admin"
+      isOwner={isOwner}
+      />)
+
+  }
+  let deleteCourseButton = null;
+  if (isOwner){
+    deleteCourseButton = <>
+    <Button text="Delete Course" callback={()=>{
+    // alert("Delete Drive")
+    setDrivesInfo({
+      color:props.color,
+      label:driveLabel,
+      image:props.image,
+      newDriveId:props.driveId,
+      type:"delete drive"
+    })
+  }} />
+  <br />
+  <br />
+    </>
+  }
 
   return <>
   <h2>{dIcon} {panelDriveLabel}</h2>
@@ -558,6 +827,8 @@ const DriveInfoPanel = function(props){
       type:"update drive label",
     })
   }}/></label>
+  <br />
+  <br />
   <DoenetDriveCardMenu
   key={`colorMenu${props.driveId}`}
   colors={driveColors} 
@@ -572,16 +843,17 @@ const DriveInfoPanel = function(props){
         })
   }}
   />
-  <Button text="Delete Course" callback={()=>{
-    // alert("Delete Drive")
-    setDrivesInfo({
-      color:props.color,
-      label:driveLabel,
-      image:props.image,
-      newDriveId:props.driveId,
-      type:"delete drive"
-    })
-  }} />
+  <br />
+  <br />
+  {deleteCourseButton}
+  <h3>Owners</h3>
+  {owners}
+  {addOwners}
+  {addOwnersButton}
+  <h3>Admins</h3>
+  {admins}
+  {addAdmins}
+  {addAdminsButton}
 
   </>
 }
---
+++ static/api/saveUserToDrive.php
@@ -0,0 +1,116 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Credentials: true");
+header('Content-Type: application/json');
+
+include "db_connection.php";
+
+$jwtArray = include "jwtArray.php";
+$userId = $jwtArray['userId'];
+
+$email = mysqli_real_escape_string($conn,$_REQUEST["email"]);
+$driveId = mysqli_real_escape_string($conn,$_REQUEST["driveId"]);
+$type = mysqli_real_escape_string($conn,$_REQUEST["type"]);
+$selected_userId = mysqli_real_escape_string($conn,$_REQUEST["userId"]);
+
+$success = TRUE;
+$response_arr = array(
+  "success"=>FALSE
+);
+
+//TODO: Check for permisions first
+
+
+if ($type === "Remove User"){
+    $sql = "
+    DELETE FROM drive_user
+    WHERE userId = '$selected_userId'
+    AND driveId = '$driveId'
+    ";
+    $result = $conn->query($sql); 
+    $response_arr = array(
+      "success"=>TRUE
+    );
+  }else if ($type === "To Owner"){
+    $sql = "
+    UPDATE drive_user
+    SET canDeleteDrive='1', canChangeAllDriveSettings= '1'
+    WHERE userId = '$selected_userId'
+    AND driveId = '$driveId'
+    ";
+    $result = $conn->query($sql); 
+    $response_arr = array(
+      "success"=>TRUE
+    );
+  }else if ($type === "To Admin"){
+    $sql = "
+    UPDATE drive_user
+    SET canDeleteDrive='0', canChangeAllDriveSettings= '0'
+    WHERE userId = '$selected_userId'
+    AND driveId = '$driveId'
+    ";
+    $result = $conn->query($sql); 
+    $response_arr = array(
+      "success"=>TRUE
+    );
+}else{
+
+  $sql = "
+  SELECT 
+  userId,
+  screenName
+  FROM user
+  WHERE email = '$email'
+  ";
+  $result = $conn->query($sql); 
+
+
+  if ($result->num_rows > 0){
+    $row = $result->fetch_assoc();
+
+    
+      
+      $response_arr = array(
+      "success"=>TRUE,
+      "userId"=>$row['userId'],
+      "screenName"=>$row['screenName'],
+      "email"=>$row['email']
+      );
+
+      $canDeleteDrive = '0';
+      if ($type == "Add Owner"){
+        $canDeleteDrive = '1';
+      }
+
+      $newUserId = $row['userId'];
+
+      $sql = "
+      INSERT INTO drive_user
+      (userId,driveId,canViewDrive,canDeleteDrive,canShareDrive,canAddItemsAndFolders,
+      canMoveItemsAndFolders,canRenameItemsAndFolders,canPublishItemsAndFolders, canChangeAllDriveSettings)
+      VALUES
+      ('$newUserId','$driveId','1',$canDeleteDrive,'1','1','1','1','1',$canDeleteDrive)
+      ";
+    
+      $result = $conn->query($sql); 
+    }
+    
+  }
+
+
+
+
+
+
+
+
+// set response code - 200 OK
+http_response_code(200);
+
+// make it json format
+echo json_encode($response_arr);
+$conn->close();
+
+?>
