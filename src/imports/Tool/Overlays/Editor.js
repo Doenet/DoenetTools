1fd7b1a9a9b3dad2599457d6d4baea85f453ca37
Kevin Charles <char0042@umn.edu>
2021-02-27 21:41:40 -0600

    Start of conversion

--- src/imports/Tool/Overlays/Editor.js
+++ src/imports/Tool/Overlays/Editor.js
@@ -1,47 +1,125 @@
 import React, { useEffect, useState } from "react";
 import Tool from "../Tool";
 import { useToolControlHelper } from "../ToolRoot";
-import { useRecoilValue, atom, useSetRecoilState } from "recoil";
+import axios from "axios";
+import { 
+  useRecoilValue, 
+  atom, 
+  atomFamily,
+  selectorFamily,
+  useSetRecoilState,
+  useRecoilValueLoadable 
+} from "recoil";
+import DoenetViewer from '../../../Tools/DoenetViewer';
 
-const stateTest = atom({
-  key: "stateTest",
-  default: 0,
-});
 
-function Consumer() {
-  const state = useRecoilValue(stateTest);
-  return <div>{state}</div>;
+
+const fileByContentId = atomFamily({
+  key:"fileByContentId",
+  default: selectorFamily({
+    key:"fileByContentId/Default",
+    get:(contentId)=> async ()=>{
+      if (!contentId){
+        return "";
+      }
+      return await axios.get(`/media/${contentId}`) 
+    }
+  })
+  
+})
+
+const editorDoenetMLAtom = atom({
+  key:"editorDoenetMLAtom",
+  default:""
+})
+
+const viewerDoenetMLAtom = atom({
+  key:"viewerDoenetMLAtom",
+  default:{updateNumber:0,doenetML:""}
+})
+
+function DoenetViewerPanel(){
+  const viewerDoenetML = useRecoilValue(viewerDoenetMLAtom);
+  console.log(">>>viewerDoenetML",viewerDoenetML)
+  let attemptNumber = 1;
+  let requestedVariant = { index: attemptNumber }
+  let assignmentId = "myassignmentid";
+  let solutionDisplayMode = "button";
+
+  return <DoenetViewer
+      key={"doenetviewer" + viewerDoenetML?.updateNumber}
+      doenetML={viewerDoenetML?.doenetML}
+      flags={{
+        showCorrectness: true,
+        readOnly: false,
+        solutionDisplayMode: solutionDisplayMode,
+        showFeedback: true,
+        showHints: true,
+      }}
+      attemptNumber={attemptNumber}
+      assignmentId={assignmentId}
+      ignoreDatabase={false}
+      requestedVariant={requestedVariant}
+      /> 
 }
 
-export default function Editor({ contentId, branchId }) {
-  const setState = useSetRecoilState(stateTest);
-  const { open } = useToolControlHelper();
+// function DoenetViewerUpdateButton(){
+//   const editorDoenetML = useRecoilValue(editorDoenetMLAtom);
+//   const setViewerDoenetML = useSetRecoilState(viewerDoenetMLAtom);
+//   const selectedTimestamp = useRecoilValue(versionHistorySelectedAtom);
+//   if (selectedTimestamp !== "") {return null;}
 
+//   return <button onClick={()=>{setViewerDoenetML((old)=>{
+//     let newInfo = {...old};
+//     newInfo.doenetML = editorDoenetML;
+//     newInfo.updateNumber = old.updateNumber+1;
+//     return newInfo;
+//   })}}>Update</button>
+// }
+
+export default function Editor({ contentId, branchId }) {
+  console.log("===Editor!");
+  const loadedDoenetML = useRecoilValueLoadable(fileByContentId(contentId))
+  let doenetML = loadedDoenetML.contents?.data;
+  const setEditorDoenetML = useSetRecoilState(editorDoenetMLAtom);
+  const setViewerDoenetML = useSetRecoilState(viewerDoenetMLAtom);
+  // const setEditorOverlayTitle = useSetRecoilState(overlayTitleAtom);
   useEffect(() => {
     //init code here
-    console.log(">>>Editor Init");
-    return () => {
-      console.log(">>>Editor exit");
-      setState(0);
-    }; //cleanup code here
-  }, []);
+
+    if (doenetML !== undefined){
+    console.log(">>>Editor Init!!!!!!!",doenetML);
+    setEditorDoenetML(doenetML);
+    setViewerDoenetML({updateNumber:1,doenetML});
+    }
+    // return () => console.log(">>>Cal exit"); //cleanup code here
+}, [doenetML]);
+
+  if (loadedDoenetML.state === "loading"){ return null;}
+  if (loadedDoenetML.state === "hasError"){ 
+    console.error(loadedDoenetML.contents)
+    return null;}
+
+
+
 
   return (
     <Tool>
-      <headerPanel></headerPanel>
+      <headerPanel title="How?"></headerPanel>
 
       <mainPanel>
-        <Consumer />
         This is the editor on branch: {branchId} with content: {contentId}
+        {/* <div><DoenetViewerUpdateButton  /></div> */}
+        <DoenetViewerPanel />
+
       </mainPanel>
 
-      <supportPanel></supportPanel>
+      <supportPanel>
+        <p>Stuff</p>
+      </supportPanel>
 
-      <menuPanel title={"actions"}>
-        <button onClick={() => setState((old) => old + 1)}>add one</button>
-        <button onClick={() => open("calendar", "fasdf", "f1234")}>
-          open Cal
-        </button>
+      <menuPanel title={"test"}>
+       <p>test</p>
       </menuPanel>
     </Tool>
   );
--- src/temp/index.js
+++ src/temp/index.js
@@ -1,36 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import { DropTargetsProvider } from "../imports/DropTarget";
+import { BreadcrumbProvider } from "../imports/Breadcrumb";
+import { RecoilRoot } from "recoil";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {
-  HashRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
-import {
-  DropTargetsProvider,
-} from '../imports/DropTarget';
-import { 
-  BreadcrumbProvider 
-} from '../imports/Breadcrumb';
-import {
-  RecoilRoot,
-} from 'recoil';
+import ToolRoot from "../imports/Tool/ToolRoot";
+import DoenetTemp from '../Tools/DoenetTemp';
 
-import  DoenetDateTime from "../Tools/DoenetDateTime";
-import App from '../Tools/DoenetTemp';
-
-    ReactDOM.render(
-      <DropTargetsProvider>
-        <BreadcrumbProvider>
-          <RecoilRoot>
-            <Router >
-              <Route path="/" render={()=>
-              <App />
-                // <DoenetDateTime  onDateChange = {(date) => console.log(date)} /> 
-                }/>
-            </Router>
-          </RecoilRoot>
-        </BreadcrumbProvider>
-      </DropTargetsProvider>
-  ,document.getElementById('root'));
+ReactDOM.render(
+  <DropTargetsProvider>
+    <BreadcrumbProvider>
+      <RecoilRoot>
+        <Router>
+          <Switch>
+            <Route
+              path="/"
+              render={(routeprops) => (
+                <ToolRoot
+                  route={{ ...routeprops }}
+                  tool={<DoenetTemp key={"BaseTool"} />}
+                />
+              )}
+            />
+          </Switch>
+        </Router>
+      </RecoilRoot>
+    </BreadcrumbProvider>
+  </DropTargetsProvider>,
+  document.getElementById("root")
+);
 
