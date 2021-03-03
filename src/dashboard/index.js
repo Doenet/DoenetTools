beb1f594702d50839c4b186ad4db84a670d2e1dd
vinnikonduru <46607860+vinnikonduru@users.noreply.github.com>
2021-03-03 08:30:02 -0600

    drivecard dashboard (#281)

--- src/dashboard/index.js
+++ src/dashboard/index.js
@@ -2,9 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import DoenetDashboard from '../Tools/DoenetDashboard';
 import { initialize } from '../imports/courseInfo';
-
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import {
+  RecoilRoot
+} from 'recoil';
+import {
+  DropTargetsProvider,
+} from '../imports/DropTarget';
+import { 
+  BreadcrumbProvider 
+} from '../imports/Breadcrumb';
 initialize();
 
     ReactDOM.render(
-      <DoenetDashboard/>
-  ,document.getElementById('root'));
+      <RecoilRoot>
+      <DropTargetsProvider>
+      <BreadcrumbProvider>
+     
+        <Router>
+              <Switch>
+                <Route path="/" render={(routeprops) => 
+                <DoenetDashboard route={{ ...routeprops }}  />
+                }></Route>
+              </Switch> 
+         </Router>
+      </BreadcrumbProvider>
+      </DropTargetsProvider>
+      </RecoilRoot>  ,document.getElementById('root'));
