18837f73385bed7c014cfb165bf38169192cb83f
Vinitha Konduru <vinithak@umn.edu>
2021-03-18 14:23:58 -0500

    fixed temp

--- src/temp/index.jsx
+++ src/temp/index.jsx
@@ -1,28 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {
-  HashRouter as Router,
-  // Switch,
-  Route,
-} from "react-router-dom";
-// import {
-//   DropTargetsProvider,
-// } from '../imports/DropTarget';
-// import { 
-//   BreadcrumbProvider 
-// } from '../imports/Breadcrumb';
-import {
-  RecoilRoot,
-} from 'recoil';
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import { DropTargetsProvider } from "../imports/DropTarget";
+import { BreadcrumbProvider } from "../imports/Breadcrumb";
+import { RecoilRoot } from "recoil";
+import ToolRoot from "../imports/Tool/ToolRoot";
 
-// eslint-disable-next-line import/no-unresolved
 import  DoenetTemp from "../Tools/DoenetTemp";
 
     ReactDOM.render(
-      <RecoilRoot>
-        <Router >
-          <Route path="/" render={()=>
-            <DoenetTemp  onDateChange = {(date) => console.log(date)} /> }/>
-        </Router>
-      </RecoilRoot>
+      <DropTargetsProvider>
+      <BreadcrumbProvider>
+        <RecoilRoot>
+          <Router >
+            <Switch>
+              <Route
+                path="/"
+                render={(routeprops) => (
+                  <ToolRoot
+                    tool={<DoenetTemp key={"BaseTool"} route={{ ...routeprops }}/>}
+                  />
+                )}
+              />
+            </Switch>
+          </Router>
+        </RecoilRoot>
+      </BreadcrumbProvider>
+    </DropTargetsProvider>
+    
   ,document.getElementById('root'));
