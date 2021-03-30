c1c29387b2e8db95d078eb63eca9d1dcb1e469bc
Emilio Alvarez <e.l.alvarez@icloud.com>
2021-03-29 20:25:05 -0500

    Temp Tests

--- src/Tools/temp/index.jsx
+++ src/Tools/temp/index.jsx
@@ -1,9 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import styled from 'styled-components';
+import { atom, useRecoilValue, RecoilRoot, useSetRecoilState } from 'recoil';
+import VisibilitySensor from 'react-visibility-sensor';
+import { useSpring } from 'react-spring';
+import {
+  useTable,
+  useSortBy,
+  useFilters,
+  useGlobalFilter,
+  useAsyncDebounce,
+} from 'react-table';
+
+const Test = styled.div`
+  background-color: black;
+  width: 60px;
+  height: 60px;
+`;
+
+const atomTest = atom({
+  key: 'test',
+  default: 0,
+});
+
+const AtomConsumer = () => {
+  const info = useRecoilValue(atomTest);
+
+  console.log('>>>Atom Refreh', info);
+  return <div>{info}</div>;
+};
+
+const AtomSetter = () => {
+  const set = useSetRecoilState(atomTest);
+
+  return (
+    <button
+      onClick={() => {
+        console.log('>>>Atom Set');
+        set((old) => old + 1);
+      }}
+    >
+      increment
+    </button>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>This is temp index.jsx </h1>
+    <RecoilRoot>
+      <h1>This is temp index.jsx </h1>
+      <Test />
+      <AtomConsumer />
+      <AtomSetter />
+    </RecoilRoot>
   </React.StrictMode>,
   document.getElementById('root'),
 );
