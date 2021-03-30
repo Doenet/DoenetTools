187a5a44f4c2ee45158bfa7b0c091d90710a5a5c
Kevin Charles <char0042@umn.edu>
2021-03-30 16:21:46 -0500

    npm run deploy

---
+++ snowpack.deploy.config.js
@@ -0,0 +1,57 @@
+
+
+module.exports = {
+  mount: {
+    'src/Tools/exampleTool': '/exampleTool',
+    'src/Tools/library': '/library',
+    // 'src/Tools/temp': '/temp',
+    'src/Core': '/core',
+    'src/API': '/api',
+    'src/Home': '/',
+    // 'src/Renderers': '/renderers',
+    // 'src/Tools': { url: '/', static: true,  resolve: false},
+    // public: { url: '/', static: true },
+    // src: { url: '/dist' },
+    // "src/Tools": "/",
+  },
+  plugins: [
+    '@snowpack/plugin-react-refresh',
+    // '@snowpack/plugin-dotenv',
+    [
+      'snowpack-plugin-raw-file-loader',
+      {
+        exts: ['.doenet', '.txt'], // Add file extensions saying what files should be loaded as strings in your snowpack application. Default: '.txt'
+      },
+    ],
+  ],
+  routes: [
+    /* Enable an SPA Fallback in development: */
+    // {"match": "routes", "src": ".*", "dest": "/index.html"},
+    
+    //Using this to map port 80 to 8080 for api requests
+    // {
+    //   src: '/api/.*',
+    //   dest: (req, res) => {
+    //     proxy.web(req, res);
+    //   },
+    // },
+  ],
+  optimize: {
+    bundle: true,
+    minify: true,
+    target: 'es2020',
+    treeshake: true,
+  },
+  packageOptions: {
+    polyfillNode : true
+    // cache:'aaa',
+  },
+  devOptions: {
+    openUrl: '/core',
+  },
+  buildOptions: {
+    out: 'dist',
+    clean: true,
+    minify: true
+  },
+};
