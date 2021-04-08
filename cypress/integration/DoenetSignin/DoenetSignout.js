77f7c7b52f902f8e6afb8fe50f043c54e8a8bf2b
Vinitha Konduru <vinithak@umn.edu>
2021-04-08 11:14:16 -0500

    signin tests

--- cypress/integration/DoenetSignin/DoenetSignout.js
+++ cypress/integration/DoenetSignin/DoenetSignout.js
@@ -1,9 +1,9 @@
 export const signOut = () => {
   
-  // cy.visit('/signout')
+  cy.visit('/signout') // TODO
 
-  cy.get('[data-cy=profileMenuButton]').click()  //TODO
-  cy.wait(500)
-  cy.get('[data-cy=SignOut]').click()
+  // cy.get('[data-cy=profileMenuButton]').click()  //TODO
+  // cy.wait(500)
+  // cy.get('[data-cy=SignOut]').click()
 
 }
