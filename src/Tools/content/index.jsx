f429e44e747518a8d1fa23ff919dd7689a3f3ade
Vinitha Konduru <vinithak@umn.edu>
2021-04-12 09:16:49 -0500

    content imports

--- src/Tools/content/index.jsx
+++ src/Tools/content/index.jsx
@@ -1,35 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { HashRouter as Router, Switch, Route } from 'react-router-dom';
-// import { DropTargetsProvider } from "../imports/DropTarget";
-// import { BreadcrumbProvider } from "../imports/Breadcrumb";
-import { RecoilRoot } from 'recoil';
+/**
+ * External dependencies
+ */
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import { RecoilRoot } from "recoil";
 
+/**
+ * Internal dependencies
+ */
 import Content from './Content';
-import ToolRoot from '../_framework/ToolRoot';
+import { DropTargetsProvider } from "../../_reactComponents/DropTarget";
+import { BreadcrumbProvider } from "../../_reactComponents/Breadcrumb";
+import ToolRoot from "../_framework/ToolRoot";
 
 ReactDOM.render(
-  // <DropTargetsProvider>
-  // <BreadcrumbProvider>
-  <RecoilRoot>
-    <Router>
-      <Switch>
-        <Route
-          path="/"
-          render={(routeprops) => (
-            <ToolRoot
-              route={{ ...routeprops }}
-              tool={<Content route={ routeprops } key={'BaseTool'} />}
+  <DropTargetsProvider>
+    <BreadcrumbProvider>
+      <RecoilRoot>
+        <Router >
+          <Switch>
+            <Route
+              path="/"
+              render={(routeprops) => (
+                <ToolRoot
+                  tool={<Content key={"BaseTool"} route={{ ...routeprops }}/>}
+                />
+              )}
             />
-          )}
-        />
-      </Switch>
-    </Router>
-  </RecoilRoot>,
-  // </BreadcrumbProvider>
-  // </DropTargetsProvider>,
-  document.getElementById('root'),
-);
+          </Switch>
+        </Router>
+      </RecoilRoot>
+    </BreadcrumbProvider>
+  </DropTargetsProvider>
+,document.getElementById('root'));
 
 // Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
 // Learn more: https://www.snowpack.dev/concepts/hot-module-replacement
