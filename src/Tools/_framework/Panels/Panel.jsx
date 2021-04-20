5f0bf0c2645ae36733587fbdd706c7f86892ddf2
Emilio Alvarez <e.l.alvarez@icloud.com>
2021-04-19 20:35:09 -0500

    Generic DragPanel

--- src/Tools/_framework/Panels/Panel.jsx
+++ src/Tools/_framework/Panels/Panel.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { animated, useSpring } from 'react-spring';
 import { useGesture } from 'react-use-gesture';
@@ -7,20 +7,33 @@ import {
   faGripLinesVertical,
   faGripLines,
 } from '@fortawesome/free-solid-svg-icons';
+import { useRecoilState } from 'recoil';
 
 export const handleDirection = {
   LEFT: {
     flexDir: 'row-reverse',
     vertical: true,
     rounding: '10px 0px 0px 10px',
+    drag: ['x', -1],
   },
-  RIGHT: { flexDir: 'row', vertical: true, rounding: '0px 10px 10px 0px' },
+  RIGHT: {
+    flexDir: 'row',
+    vertical: true,
+    rounding: '0px 10px 10px 0px',
+    drag: ['x', 1],
+  },
   UP: {
     flexDir: 'column-reverse',
     vertical: false,
     rounding: '10px 10px 0px 0px',
+    drag: ['y', -1],
   },
-  DOWN: { flexDir: 'column', vertical: false, rounding: '0px 0px 10px 10px' },
+  DOWN: {
+    flexDir: 'column',
+    vertical: false,
+    rounding: '0px 0px 10px 10px',
+    drag: ['y', 1],
+  },
 };
 
 const Wrapper = styled(animated.div)`
@@ -33,25 +46,22 @@ const Wrapper = styled(animated.div)`
 
 const Background = styled.div`
   flex: 1 1 auto;
-  display: flex;
-  flex-direction: ${({ $vertical }) => ($vertical ? 'column' : 'row')};
-  align-items: center;
-  overflow: ${({ $vertical }) => ($vertical ? 'hidden auto' : 'auto hidden')};
+  overflow: hidden;
   height: ${({ $vertical }) => ($vertical ? '100%' : '')};
   width: ${({ $vertical }) => ($vertical ? '' : '100%')};
-  background-color: hsl(0, 0%, 89%);
   border-radius: 4px;
+  background-color: hsl(0, 0%, 99%);
 `;
 
 const DragHandle = styled.div`
-  flex: 0 0 15px;
+  flex: 0 0 ${({ $handleSize }) => $handleSize}px;
   display: flex;
   align-items: center;
   justify-content: center;
   color: rgb(246, 248, 255);
   font-size: 12px;
   padding: 0;
-  cursor: ew-resize;
+  cursor: ${({ $vertical }) => ($vertical ? 'ew-resize' : 'ns-resize')};
   background-color: #1a5a99;
   border-radius: ${({ $rounding }) => $rounding};
   height: ${({ $vertical }) => ($vertical ? '25%' : '')};
@@ -60,44 +70,91 @@ const DragHandle = styled.div`
   touch-action: none;
 `;
 
-export default function Panel({ children, direction = handleDirection.DOWN }) {
-  const [{ x }, api] = useSpring(() => ({ x: 15 }));
+// const panelOpen = atomFamily({
+//   key: 'panelOpenAtom',
+//   default: false,
+// });
+
+export default function DragPanel({
+  children,
+  direction,
+  gridArea = 'auto',
+  handleSize = 10,
+  panelSize = 240,
+  isInitOpen = false,
+}) {
+  const [open, setOpen] = useState(isInitOpen);
+  const [{ dir }, api] = useSpring(() => ({
+    dir: isInitOpen ? handleSize + panelSize : handleSize,
+  }));
 
   // Set the drag hook and define component movement based on gesture data
-  const bind = useGesture(
+  const bindX = useGesture(
     {
-      onDrag: ({ tap, movement: [mx] }) => {
+      onDrag: ({ tap, movement: [mx, my] }) => {
+        let movDir = (direction.drag[0] === 'x' ? mx : my) * direction.drag[1];
+
         if (tap) {
-          api.start({ x: mx === 15 ? 240 : 15 });
+          // console.log(movDir === handleSize ? panelSize : handleSize, mx);
+          api.start({
+            dir: movDir === handleSize ? panelSize + handleSize : handleSize,
+          });
         } else {
-          api.start({ x: mx });
+          // console.log(movDir);
+          api.start({ dir: movDir });
         }
       },
-      onDragEnd: ({ direction: [dirx], movement: [mx] }) => {
-        api.start({ x: dirx === 1 || (mx >= 115 && dirx !== -1) ? 240 : 15 });
+      onDragEnd: ({ direction: [dirx, diry], movement: [mx, my] }) => {
+        let dragDir =
+          (direction.drag[0] === 'x' ? dirx : diry) * direction.drag[1];
+        let movDir = (direction.drag[0] === 'x' ? mx : my) * direction.drag[1];
+        api.start({
+          dir:
+            dragDir === 1 ||
+            (movDir >= (panelSize + handleSize) / 2 && dragDir !== -1)
+              ? panelSize + handleSize
+              : handleSize,
+        });
       },
     },
     {
       drag: {
         filterTaps: true,
-        bounds: { left: 10, right: 240, top: -188, bottom: 200 },
+        bounds: () => ({
+          left:
+            (direction === handleDirection.RIGHT ? handleSize : panelSize) *
+            direction.drag[1],
+          right:
+            (direction === handleDirection.RIGHT ? panelSize : handleSize) *
+            direction.drag[1],
+          top:
+            (direction === handleDirection.DOWN ? handleSize : panelSize) *
+            direction.drag[1],
+          bottom:
+            (direction === handleDirection.DOWN ? panelSize : handleSize) *
+            direction.drag[1],
+        }),
         rubberband: true,
-        axis: 'x',
-        initial: () => [x.get(), 0],
+        axis: direction.drag[0],
+        initial: () => [
+          dir.get() * direction.drag[1],
+          dir.get() * direction.drag[1],
+        ],
       },
     },
   );
   return (
     <Wrapper
-      $gridArea={'nav'}
+      $gridArea={gridArea}
       $flexDir={direction.flexDir}
-      style={{ width: x }}
+      style={direction.drag[0] === 'x' ? { width: dir } : { height: dir }}
     >
       <Background $vertical={direction.vertical}>{children}</Background>
       <DragHandle
         $vertical={direction.vertical}
         $rounding={direction.rounding}
-        {...bind()}
+        $handleSize={handleSize}
+        {...bindX()}
       >
         <FontAwesomeIcon
           icon={direction.vertical ? faGripLinesVertical : faGripLines}
