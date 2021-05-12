88b4ca939b07a774fc4f48b9b414ac88d201b647
Kevin Charles <char0042@umn.edu>
2021-05-11 23:59:28 -0500

    sideBySide

--- src/Viewer/renderers/sideBySide.jsx
+++ src/Viewer/renderers/sideBySide.jsx
@@ -1,20 +1,43 @@
 import React, { useRef, useState, useEffect } from 'react';
 
-import styled from "styled-components";
+// import styled from "styled-components";
 import useDoenetRender from './useDoenetRenderer';
 
 
-export default function Slider(props) {
-  let [name, SVs] = useDoenetRender(props);
-  // console.log("name: ", name, " value: ", SVs);
-
-  
+export default function sideBySide(props) {
+  let [name, SVs, _, children] = useDoenetRender(props);
+  console.log(">>>name: ", name, " value: ", SVs);
+  // console.log(">>>children",children)
 
   if(SVs.hidden){
     return null;
   }
 
+  let styledChildren = []
+  const marginRight =  (SVs.margins[0] + SVs.margins[1] + SVs.gapWidth); 
+  const marginLeft = SVs.margins[0];
+  
+  for (let [i,child] of children.entries()){
+    let width = SVs.widths[i];
+    // console.log(">>>marginLeft",marginLeft)
+    // console.log(">>>width",width)
+    // console.log(">>>marginRight",marginRight)
+    // console.log(">>>gap",SVs.gapWidth)
+    styledChildren.push(<span 
+      style={{
+        marginLeft:`${marginLeft}%`,
+        marginRight:`${marginRight}%`,
+        width:`${width}%`,
+        
+      }} key={child.key}>{child}</span>)
+    
+    //Gap is only between not at the end
+    if (children.length !== i+1){
+      styledChildren.push(<span style={{width:`${SVs.gapWidth}%`}} key={`gap${child.key}`}></span>)
+    }
+  }
 
-  return "sideBySide!";
+
+  return <div style={{display:"flex",width:"800px"}}>{styledChildren}</div>;
   
 }
