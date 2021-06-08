1960b73be40478de2dd3a862b79e7c7b528590b7
Kevin Charles <char0042@umn.edu>
2021-06-08 17:06:25 -0500

    free-solid-svg-icons.js

--- dist/_snowpack/pkg/@fortawesome/free-solid-svg-icons.js
+++ dist/_snowpack/pkg/@fortawesome/free-solid-svg-icons.js
@@ -77,6 +77,11 @@ var faFolder = {
   iconName: 'folder',
   icon: [512, 512, [], "f07b", "M464 128H272l-64-64H48C21.49 64 0 85.49 0 112v288c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48V176c0-26.51-21.49-48-48-48z"]
 };
+var faGripLines = {
+  prefix: 'fas',
+  iconName: 'grip-lines',
+  icon: [512, 512, [], "f7a4", "M496 288H16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h480c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16zm0-128H16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h480c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16z"]
+};
 var faGripLinesVertical = {
   prefix: 'fas',
   iconName: 'grip-lines-vertical',
@@ -163,4 +168,4 @@ var faUsersSlash = {
   icon: [640, 512, [], "e073", "M132.65,212.32,36.21,137.78A63.4,63.4,0,0,0,32,160a63.84,63.84,0,0,0,100.65,52.32Zm40.44,62.28A63.79,63.79,0,0,0,128,256H64A64.06,64.06,0,0,0,0,320v32a32,32,0,0,0,32,32H97.91A146.62,146.62,0,0,1,173.09,274.6ZM544,224a64,64,0,1,0-64-64A64.06,64.06,0,0,0,544,224ZM500.56,355.11a114.24,114.24,0,0,0-84.47-65.28L361,247.23c41.46-16.3,71-55.92,71-103.23A111.93,111.93,0,0,0,320,32c-57.14,0-103.69,42.83-110.6,98.08L45.46,3.38A16,16,0,0,0,23,6.19L3.37,31.46A16,16,0,0,0,6.18,53.91L594.53,508.63A16,16,0,0,0,617,505.82l19.64-25.27a16,16,0,0,0-2.81-22.45ZM128,403.21V432a48,48,0,0,0,48,48H464a47.45,47.45,0,0,0,12.57-1.87L232,289.13C173.74,294.83,128,343.42,128,403.21ZM576,256H512a63.79,63.79,0,0,0-45.09,18.6A146.29,146.29,0,0,1,542,384h66a32,32,0,0,0,32-32V320A64.06,64.06,0,0,0,576,256Z"]
 };
 
-export { faAngleLeft, faAngleRight, faArrowLeft, faBars, faBookOpen, faCaretDown, faCaretRight, faChalkboard, faCheck, faChevronDown, faChevronRight, faCloud, faCode, faExternalLinkAlt, faFolder, faGripLinesVertical, faLevelDownAlt, faLightbulb, faLink, faPercentage, faPuzzlePiece, faSearch, faSort, faSortDown, faSortUp, faTh, faTimes, faUser, faUserCircle, faUserEdit, faUsers, faUsersSlash };
+export { faAngleLeft, faAngleRight, faArrowLeft, faBars, faBookOpen, faCaretDown, faCaretRight, faChalkboard, faCheck, faChevronDown, faChevronRight, faCloud, faCode, faExternalLinkAlt, faFolder, faGripLines, faGripLinesVertical, faLevelDownAlt, faLightbulb, faLink, faPercentage, faPuzzlePiece, faSearch, faSort, faSortDown, faSortUp, faTh, faTimes, faUser, faUserCircle, faUserEdit, faUsers, faUsersSlash };
