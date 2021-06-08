e204e4be6df9bfc493c1894a983f6ae7cd6a5321
kelton <kelton@keltono.net>
2021-06-07 22:01:24 -0500

    finish(?) language extension

--- src/Tools/_framework/CodeMirror.jsx
+++ src/Tools/_framework/CodeMirror.jsx
@@ -1,11 +1,16 @@
-import React, { useEffect, useRef } from "react";
-import { basicSetup } from "@codemirror/basic-setup";
-import { EditorState, Transaction } from "@codemirror/state";
-import { EditorView, keymap  } from "@codemirror/view";
-import {doenet} from "../../Parser/editorParser"
-
+import React, {useEffect, useRef} from "react";
+import {basicSetup} from "@codemirror/basic-setup";
+import {EditorState, Transaction } from "@codemirror/state";
+import {EditorView, keymap} from "@codemirror/view";
+import {indentNodeProp, LezerLanguage, LanguageSupport} from "@codemirror/language"
+import {styleTags, tags as t} from "@codemirror/highlight"
+import {indentLess} from "@codemirror/commands"
+import {parser} from "../../Parser/doenet"
 
 export default function CodeMirror(props){
+    let openTags = [];
+    const noIndentTags = ["p"];
+
     let view = useRef(null);
     let parent = useRef(null);
 
@@ -49,6 +54,57 @@ export default function CodeMirror(props){
             doenet(view.current)
         ]
     });
+
+    let parserWithMetadata = parser.configure({
+        props : [
+            styleTags({
+                Comment: t.comment,
+                AttributeName: t.propertyName,
+                AttributeValue: t.string,
+                MismatchedCloseTag: t.invalid,
+                "( )": t.paren
+            }),
+            indentNodeProp.add({
+                OpenTag: context => {
+                    const tagNameNode = context.node.firstChild;
+                    const tagName = context.state.sliceDoc(tagNameNode.from,tagNameNode.to);
+                    if(noIndentTags.includes(tagName)){
+                        return context.baseIndent;
+                    } else  {
+                        openTags.push(tagName);
+                        return context.baseIndent + context.unit;
+                    }
+                },
+                CloseTag: context => {
+                    const tagNameNode = context.node.firstChild;
+                    const tagName = context.state.sliceDoc(tagNameNode.from,tagNameNode.to);
+                    const index = openTags.indexOf(tagName);
+                    if(index === -1){
+                        return context.baseIndent;
+                    } else {
+                        //this should have the correct behavior...
+                        indentLess({state : context.state, dispatch: view.current.dispatch });
+
+                        openTags.splice(index,1);
+                        //TODO this might need to be minus unit
+                        return context.baseIndent;
+                    }
+
+                }
+            })
+        ]
+    })
+
+    const doenetLanguage = LezerLanguage.define({
+        parser: parserWithMetadata,
+        //TODO look into languageData (looks like there's more than this (undocumented of course))
+        languageData: {
+            commentTokens: {block: {open: "<!--", close: "-->"}}
+        }
+    })
+    function doenet(){
+        return new LanguageSupport(doenetLanguage(), [])
+    }
 
     return (
         <div ref={parent} ></div>
