492f741eaffe856df3c4d771d2534c607e604f82
Kevin Charles <char0042@umn.edu>
2021-06-17 10:20:31 -0500

    Removed 2px right margin of content panel

--- src/Tools/_framework/NewToolRoot.jsx
+++ src/Tools/_framework/NewToolRoot.jsx
@@ -1,4 +1,4 @@
-import React, { useState, lazy, Suspense, useRef } from 'react';
+import React, { useState, lazy, Suspense, useRef, useEffect } from 'react';
 import {
   atom,
   selector,
@@ -34,7 +34,7 @@ const ToolContainer = styled(animated.div)`
   top: 0;
   left: 0;
   padding: 0px;
-  gap: 2px;
+  gap: 0px;
   box-sizing: border-box;
 `;
 
@@ -62,10 +62,23 @@ export const profileAtom = atom({
   })
 })
 
+export const panelRequestsAtom = atom({
+  key: "panelRequestsAtom",
+  default:{
+    menuPanels:[],
+    mainPanel:"",
+    supportPanel:[],
+  }
+})
+
 export default function ToolRoot(props){
   // console.log(">>>DoenetTool props",props)
   const profile = useRecoilValueLoadable(profileAtom)
+  const panelRequests = useRecoilValue(panelRequestsAtom);
 
+  useEffect(()=>{
+    console.log(">>>panelRequests",panelRequests)
+  },[panelRequests])
 
   if (profile.state === "loading"){ return null;}
     if (profile.state === "hasError"){ 
@@ -104,133 +117,133 @@ const layerStackAtom = atom({
   default: [],
 });
 
-export const useToolControlHelper = () => {
-  const setLayers = useSetRecoilState(layerStackAtom);
-  const activateMenuPanel = useMenuPanelController();
-  const activateSupportPanel = useSupportDividerController();
-  const [
-    Content,
-    Assignment,
-    Editor,
-    Image,
-    Calendar,
-    GradebookAssignmentView,
-    GradebookAttemptView,
-  ] = useRef([
-    lazy(() => import('./Overlays/Content')),
-    lazy(() => import('./Overlays/Assignment')),
-    lazy(() => import('./Overlays/Editor')),
-    lazy(() => import('./Overlays/Image')),
-    lazy(() => import('./Overlays/Calendar')),
-    lazy(() => import('./Overlays/GradebookAssignmentView')),
-    lazy(() => import('./Overlays/GradebookAttemptView')),
-  ]).current;
-  const openOverlay = ({
-    type,
-    title,
-    contentId,
-    courseId,
-    doenetId,
-    assignmentId,
-    attemptNumber,
-    userId,
-    driveId,
-    folderId,
-    itemId,
-  }) => {
-    switch (type.toLowerCase()) {
-      case 'gradebookassignmentview':
-        setLayers((old) => [
-          ...old,
-          <GradebookAssignmentView
-            assignmentId={assignmentId}
-            key={`GBAssign${old.length + 1}`}
-          />,
-        ]);
-        break;
-      case 'gradebookattemptview':
-        setLayers((old) => [
-          ...old,
-          <GradebookAttemptView
-            assignmentId={assignmentId}
-            userId={userId}
-            attemptNumber={attemptNumber}
-            key={`GBView${old.length + 1}`}
-          />,
-        ]);
-        break;
-      case 'editor':
-        setLayers((old) => [
-          ...old,
-          <Editor
-            doenetId={doenetId}
-            title={title}
-            driveId={driveId}
-            folderId={folderId}
-            itemId={itemId}
-            key={`EditorLayer${old.length + 1}`}
-          />,
-        ]);
-        break;
-      case 'content':
-        setLayers((old) => [
-          ...old,
-          <Content
-            contentId={contentId}
-            doenetId={doenetId}
-            title={title}
-            key={`ContentLayer${old.length + 1}`}
-          />,
-        ]);
-        break;
-      case 'assignment':
-        setLayers((old) => [
-          ...old,
-          <Assignment
-            doenetId={doenetId}
-            title={title}
-            assignmentId={assignmentId}
-            courseId={courseId}
-            contentId={contentId}
-            key={`AssignmentLayer${old.length + 1}`}
-          />,
-        ]);
-        break;
-      case 'calendar':
-        setLayers((old) => [
-          ...old,
-          <Calendar
-            doenetId={doenetId}
-            contentId={contentId}
-            key={`CalendarLayer${old.length + 1}`}
-          />,
-        ]);
-        break;
-      case 'image':
-        setLayers((old) => [
-          ...old,
-          <Image doenetId={doenetId} key={`ImageLayer${old.length + 1}`} />,
-        ]);
-        break;
-      default:
-    }
-  };
+// export const useToolControlHelper = () => {
+//   const setLayers = useSetRecoilState(layerStackAtom);
+//   const activateMenuPanel = useMenuPanelController();
+//   const activateSupportPanel = useSupportDividerController();
+//   const [
+//     Content,
+//     Assignment,
+//     Editor,
+//     Image,
+//     Calendar,
+//     GradebookAssignmentView,
+//     GradebookAttemptView,
+//   ] = useRef([
+//     lazy(() => import('./Overlays/Content')),
+//     lazy(() => import('./Overlays/Assignment')),
+//     lazy(() => import('./Overlays/Editor')),
+//     lazy(() => import('./Overlays/Image')),
+//     lazy(() => import('./Overlays/Calendar')),
+//     lazy(() => import('./Overlays/GradebookAssignmentView')),
+//     lazy(() => import('./Overlays/GradebookAttemptView')),
+//   ]).current;
+//   const openOverlay = ({
+//     type,
+//     title,
+//     contentId,
+//     courseId,
+//     doenetId,
+//     assignmentId,
+//     attemptNumber,
+//     userId,
+//     driveId,
+//     folderId,
+//     itemId,
+//   }) => {
+//     switch (type.toLowerCase()) {
+//       case 'gradebookassignmentview':
+//         setLayers((old) => [
+//           ...old,
+//           <GradebookAssignmentView
+//             assignmentId={assignmentId}
+//             key={`GBAssign${old.length + 1}`}
+//           />,
+//         ]);
+//         break;
+//       case 'gradebookattemptview':
+//         setLayers((old) => [
+//           ...old,
+//           <GradebookAttemptView
+//             assignmentId={assignmentId}
+//             userId={userId}
+//             attemptNumber={attemptNumber}
+//             key={`GBView${old.length + 1}`}
+//           />,
+//         ]);
+//         break;
+//       case 'editor':
+//         setLayers((old) => [
+//           ...old,
+//           <Editor
+//             doenetId={doenetId}
+//             title={title}
+//             driveId={driveId}
+//             folderId={folderId}
+//             itemId={itemId}
+//             key={`EditorLayer${old.length + 1}`}
+//           />,
+//         ]);
+//         break;
+//       case 'content':
+//         setLayers((old) => [
+//           ...old,
+//           <Content
+//             contentId={contentId}
+//             doenetId={doenetId}
+//             title={title}
+//             key={`ContentLayer${old.length + 1}`}
+//           />,
+//         ]);
+//         break;
+//       case 'assignment':
+//         setLayers((old) => [
+//           ...old,
+//           <Assignment
+//             doenetId={doenetId}
+//             title={title}
+//             assignmentId={assignmentId}
+//             courseId={courseId}
+//             contentId={contentId}
+//             key={`AssignmentLayer${old.length + 1}`}
+//           />,
+//         ]);
+//         break;
+//       case 'calendar':
+//         setLayers((old) => [
+//           ...old,
+//           <Calendar
+//             doenetId={doenetId}
+//             contentId={contentId}
+//             key={`CalendarLayer${old.length + 1}`}
+//           />,
+//         ]);
+//         break;
+//       case 'image':
+//         setLayers((old) => [
+//           ...old,
+//           <Image doenetId={doenetId} key={`ImageLayer${old.length + 1}`} />,
+//         ]);
+//         break;
+//       default:
+//     }
+//   };
 
-  const close = () => {
-    setLayers((old) => {
-      const newArray = [...old];
-      newArray.pop();
-      return newArray;
-    });
-  };
+//   const close = () => {
+//     setLayers((old) => {
+//       const newArray = [...old];
+//       newArray.pop();
+//       return newArray;
+//     });
+//   };
 
-  return {
-    openOverlay,
-    close,
-    activateMenuPanel,
-    activateSupportPanel,
-  };
-};
+//   return {
+//     openOverlay,
+//     close,
+//     activateMenuPanel,
+//     activateSupportPanel,
+//   };
+// };
 
 export const useStackId = () => {
   const getId = useRecoilCallback(({ snapshot }) => () => {
