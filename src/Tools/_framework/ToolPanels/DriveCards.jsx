873b149d6e60ea5a253151bc7f02e2be82baaf42
Kevin Charles <char0042@umn.edu>
2021-07-07 17:05:39 -0500

    Autogenerated url on recoil changes Recoil Updates on url changes

--- src/Tools/_framework/ToolPanels/DriveCards.jsx
+++ src/Tools/_framework/ToolPanels/DriveCards.jsx
@@ -5,7 +5,7 @@ import Button from '../../../_reactComponents/PanelHeaderComponents/Button';
 import { useRecoilCallback, useRecoilValue, useSetRecoilState } from 'recoil';
 import { selectedMenuPanelAtom } from '../Panels/NewMenuPanel';
 import { drivecardSelectedNodesAtom } from '../ToolHandlers/CourseToolHandler'
-import { toolViewAtom, searchParamAtomFamily } from '../NewToolRoot';
+import { toolViewAtom, searchParamAtomFamily, paramObjAtom } from '../NewToolRoot';
 import DriveCards from '../../../_reactComponents/Drive/DriveCards';
 
 export default function DriveCardsNew(props){
@@ -15,11 +15,12 @@ export default function DriveCardsNew(props){
     set(drivecardSelectedNodesAtom,driveIds)
     set(selectedMenuPanelAtom,"SelectedCourse");
   },[])
-  const goToNav = useRecoilCallback(({set})=>()=>{
-    //TODO: set the path to the current drive 
-    window.history.pushState('','','/new#/course?tool=navigation')
-    set(searchParamAtomFamily('tool'), "navigation")
-  },[])
+  const setParamObj = useSetRecoilState(paramObjAtom);
+  // const goToNav = useRecoilCallback(({set})=>()=>{
+    // window.history.pushState('','','/new#/course?tool=navigation')
+    // set(searchParamAtomFamily('tool'), "navigation")
+
+  // },[])
 
   const tempChangeMenus = useRecoilCallback(({set})=>(newMenus,menusTitles,initOpen)=>{
     set(toolViewAtom,(was)=>{
@@ -47,7 +48,7 @@ export default function DriveCardsNew(props){
   <button onClick={(e)=>{e.stopPropagation();setSelectedCourse(['A Id'])}}>Test A Selection</button>
   {/* <button onClick={(e)=>{e.stopPropagation();clearSelectedCourse()}}>Clear Selection</button> */}
   <hr />
-  <div><button onClick={(e)=>{e.stopPropagation();goToNav();}}>Go To navigation</button></div>
+  <div><button onClick={(e)=>{e.stopPropagation();setParamObj({tool:'navigation'});}}>Go To navigation</button></div>
 
 
   </div>
