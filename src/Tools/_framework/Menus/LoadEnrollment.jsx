d715d731ebe3d0ef78d91037bd38f7b639c8a4ae
Vinitha Konduru <vinithak@umn.edu>
2021-07-20 15:20:55 -0500

    load,manual enrollment

--- src/Tools/_framework/Menus/LoadEnrollment.jsx
+++ src/Tools/_framework/Menus/LoadEnrollment.jsx
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, {useState,useCallback } from 'react';
+import { useDropzone } from 'react-dropzone';
+import Button from '../../../_reactComponents/PanelHeaderComponents/Button';
+import parse from 'csv-parse';
 
 
 export default function LoadEnrollment(props){
+  const [process, setProcess] = useState('Loading'); //array containing column names
+  // console.log("process",process);
+  const [headers, setHeaders] = useState([]); //array containing column names
+  const [entries, setEntries] = useState([[]]); //2d array with each row representing a data point
 
-  
+  const onDrop = useCallback((file) => {
+    const reader = new FileReader();
+
+    reader.onabort = () => {};
+    reader.onerror = () => {};
+    reader.onload = () => {
+      parse(reader.result, { comment: '#' }, function (err, data) {
+        setHeaders(data[0]);
+        data.shift(); //Remove head row of data
+        setEntries(data);
+        setProcess('Choose Columns');
+      });
+    };
+    reader.readAsText(file[0]);
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
   return <div style={props.style}>
     <div>Load Enrollment</div>
+    <div key="drop" {...getRootProps()}>
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Drop the files here</p>
+        ) : (
+          <Button value="Enroll Learners"></Button>
+        )}
+      </div>
   </div>
 }
--- src/Tools/_framework/Menus/ManualEnrollment.jsx
+++ src/Tools/_framework/Menus/ManualEnrollment.jsx
@@ -1,10 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import { nanoid } from 'nanoid';
+import axios from 'axios';
+import Button from '../../../_reactComponents/PanelHeaderComponents/Button';
+import { useRecoilValue } from 'recoil';
+import { searchParamAtomFamily } from '../NewToolRoot';
 
 
 export default function ManualEnrollment(props){
+  const [enrollmentTableData, setEnrollmentTableData] = useState([]);
+  const [process, setProcess] = useState('Loading'); //array containing column names
+  // console.log("process",process);
+  const [headers, setHeaders] = useState([]); //array containing column names
+  const [entries, setEntries] = useState([[]]); //2d array with each row representing a data point
+  const [enrolllearner, setEnrolllearner] = useState();
 
+  const driveId = useRecoilValue(searchParamAtomFamily('driveId'))
+
+  const enrollManual = (e) => {
+    e.preventDefault();
+
+    let payload = {
+      email: enrolllearner,
+      userId: nanoid(),
+      driveId: driveId,
+    };
+    axios.post('/api/manualEnrollment.php', payload).then((resp) => {
+      const payload = { params: { driveId } };
+      axios
+        .get('/api/getEnrollment.php', payload)
+        .then((resp) => {
+          let enrollmentArray = resp.data.enrollmentArray;
+          setEnrollmentTableData(enrollmentArray);
+          setProcess('Display Enrollment');
+          setEnrolllearner('');
+        })
+        .catch((error) => {
+          console.warn(error);
+        });
+    });
+  };
+  const handleChange = (e) => {
+    setEnrolllearner(e.currentTarget.value);
+  };
+  let manualEnroll = (
+    <div>
+      <label>Email:</label>
+      <input
+        required
+        type="email"
+        name="email"
+        value={enrolllearner}
+        placeholder="example@example.com"
+        onChange={handleChange}
+      />
+      <Button value="Enroll" onClick={(e) => enrollManual(e)} />
+    </div>
+  );
   
   return <div style={props.style}>
     <div>Manual Enrollment</div>
+    {manualEnroll}
   </div>
 }
