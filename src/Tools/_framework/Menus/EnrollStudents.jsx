1ff31f92926c6d7b4b6bcd2afde2003d66f4ade8
Vinitha Konduru <46607860+vinnikonduru@users.noreply.github.com>
2021-07-20 15:19:05 -0500

    get driveId (#554)
    Co-authored-by: Vinitha Konduru <vinithak@umn.edu>

--- src/Tools/_framework/Menus/EnrollStudents.jsx
+++ src/Tools/_framework/Menus/EnrollStudents.jsx
@@ -11,7 +11,7 @@ const path = useRecoilValue(searchParamAtomFamily('path'))
 const driveId = path.split(':')[0];
   
   return <div style={props.style}>
-  <Button width="menu" onClick={()=>setPageToolView({page:'enrollment',tool:"",view:"",params:{driveId}})} value="Go to Enrollment">Go to Enrollment</Button>
+  <Button width="menu" onClick={()=>setPageToolView({page:'course',tool:"enrollment",view:"",params:{driveId}})} value="Go to Enrollment">Go to Enrollment</Button>
   </div>
 }
 
