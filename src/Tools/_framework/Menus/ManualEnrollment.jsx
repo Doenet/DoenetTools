659536fce7277ca5c6cd7e4162ff35f731303f31
Vinitha Konduru <vinithak@umn.edu>
2021-07-21 11:03:21 -0500

    manual enrollment

--- src/Tools/_framework/Menus/ManualEnrollment.jsx
+++ src/Tools/_framework/Menus/ManualEnrollment.jsx
@@ -1,43 +1,48 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React from 'react';
 import { nanoid } from 'nanoid';
 import axios from 'axios';
 import Button from '../../../_reactComponents/PanelHeaderComponents/Button';
-import { useRecoilValue } from 'recoil';
+import { useSetRecoilState, useRecoilValue } from 'recoil';
+import {
+  processAtom,
+  enrolllearnerAtom,
+  enrollmentTableDataAtom,
+} from '../ToolPanels/Enrollment';
 import { searchParamAtomFamily } from '../NewToolRoot';
 
+export default function ManualEnrollment(props) {
+  //array containing column names
+  const setProcess = useSetRecoilState(processAtom);
+  //array containing column data
+  const enrolllearner = useRecoilValue(enrolllearnerAtom);
+  const setEnrolllearner = useSetRecoilState(enrolllearnerAtom);
+  const setEnrollmentTableData = useSetRecoilState(enrollmentTableDataAtom);
 
-export default function ManualEnrollment(props){
-  const [enrollmentTableData, setEnrollmentTableData] = useState([]);
-  const [process, setProcess] = useState('Loading'); //array containing column names
-  // console.log("process",process);
-  const [headers, setHeaders] = useState([]); //array containing column names
-  const [entries, setEntries] = useState([[]]); //2d array with each row representing a data point
-  const [enrolllearner, setEnrolllearner] = useState();
-
-  const driveId = useRecoilValue(searchParamAtomFamily('driveId'))
+  const driveId = useRecoilValue(searchParamAtomFamily('driveId'));
 
   const enrollManual = (e) => {
     e.preventDefault();
-
-    let payload = {
-      email: enrolllearner,
-      userId: nanoid(),
-      driveId: driveId,
-    };
-    axios.post('/api/manualEnrollment.php', payload).then((resp) => {
-      const payload = { params: { driveId } };
-      axios
-        .get('/api/getEnrollment.php', payload)
-        .then((resp) => {
-          let enrollmentArray = resp.data.enrollmentArray;
-          setEnrollmentTableData(enrollmentArray);
-          setProcess('Display Enrollment');
-          setEnrolllearner('');
-        })
-        .catch((error) => {
-          console.warn(error);
-        });
-    });
+    if (enrolllearner) {
+      let payload = {
+        email: enrolllearner,
+        userId: nanoid(),
+        driveId: driveId,
+      };
+      axios.post('/api/manualEnrollment.php', payload).then((resp) => {
+        const payload = { params: { driveId } };
+        axios
+          .get('/api/getEnrollment.php', payload)
+          .then((resp) => {
+            let enrollmentArray = resp.data.enrollmentArray;
+            setEnrollmentTableData(enrollmentArray);
+            setProcess('Display Enrollment');
+            setEnrolllearner('');
+          })
+          .catch((error) => {
+            console.warn(error);
+          });
+      });
+    }
   };
   const handleChange = (e) => {
     setEnrolllearner(e.currentTarget.value);
@@ -56,9 +61,10 @@ export default function ManualEnrollment(props){
       <Button value="Enroll" onClick={(e) => enrollManual(e)} />
     </div>
   );
-  
-  return <div style={props.style}>
-    <div>Manual Enrollment</div>
-    {manualEnroll}
-  </div>
+
+  return (
+    <div style={props.style}>
+      {manualEnroll}
+    </div>
+  );
 }
