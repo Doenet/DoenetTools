103dd2c0d68a22ecd32aea041dc93cf892acb72e
Vinitha Konduru <vinithak@umn.edu>
2021-08-05 11:09:27 -0500

    Text to the center

--- src/_reactComponents/PanelHeaderComponents/TextArea.jsx
+++ src/_reactComponents/PanelHeaderComponents/TextArea.jsx
@@ -7,7 +7,7 @@ export default function Textfield(props) {
   //Assume small
   var textfield = {
         margin: '0px',
-        height: '24px',
+        height: '20px',
         border: `2px solid ${doenetComponentForegroundInactive}`,
         fontFamily: 'Arial',
         borderRadius: '5px',
@@ -15,6 +15,8 @@ export default function Textfield(props) {
         value: 'Enter text here',
         resize:'none',
         whiteSpace: 'nowrap',
+        padding:"0px 10px 0px 10px;",
+        lineHeight:"20px"
       }
       var label ={
         value: 'Label:',
