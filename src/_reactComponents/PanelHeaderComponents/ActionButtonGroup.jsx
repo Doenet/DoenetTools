f48cdbf31841197058e7c67c30f62865609c960e
Clara Selchow <claraselchow@gmail.com>
2021-08-07 14:15:17 -0500

    Vertical ActionButtonGroup

--- src/_reactComponents/PanelHeaderComponents/ActionButtonGroup.jsx
+++ src/_reactComponents/PanelHeaderComponents/ActionButtonGroup.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import styled, { ThemeProvider } from 'styled-components';
 
+const Container = styled.div`
+  display: ${(props) => props.vertical ? 'static' : 'flex'};
+  // margin: 2px 0px 2px 0px
+ ;
+`
+
 const actionGroup = {
   margin: "0px -2px 0px -2px",
   borderRadius: '0',
   padding: '0px 12px 0px 10px',
 };
+
+const verticalActionGroup = {
+  margin: "-2px 0px -2px 0px",
+  borderRadius: '0',
+  padding: '0px 10px 0px 10px'
+}; 
 
 const ActionButtonGroup = (props) => {
   // if (props.width) {
@@ -15,16 +27,18 @@ const ActionButtonGroup = (props) => {
   //     actionGroup.width = props.width
   //   }
   // }
+  let first_prop = props.vertical ? 'first_vert' : 'first';
+  let last_prop = props.vertical ? 'last_vert' : 'last';
   let elem = React.Children.toArray(props.children);
   if (elem.length > 1) {
-    elem = [React.cloneElement(elem[0], {num: 'first'})]
+    elem = [React.cloneElement(elem[0], {num: first_prop})]
       .concat(elem.slice(1,-1))
-      .concat(React.cloneElement(elem[elem.length - 1], {num: 'last'}));
+      .concat(React.cloneElement(elem[elem.length - 1], {num: last_prop}));
   }
   return (
-    <div style={{display: 'flex'}}>
-      <ThemeProvider theme={actionGroup}>{elem}</ThemeProvider>
-    </div>
+    <Container vertical={props.vertical}>
+      <ThemeProvider theme={props.vertical ? verticalActionGroup : actionGroup}>{elem}</ThemeProvider>
+    </Container>
   )
 }
 
