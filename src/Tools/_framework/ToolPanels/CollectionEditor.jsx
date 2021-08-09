224397956aeb6f929b2b31b5c8a1dbb7b5197297
Emilio Alvarez <e.l.alvarez@icloud.com>
2021-08-08 22:48:24 -0400

    collection file

--- src/Tools/_framework/ToolPanels/CollectionEditor.jsx
+++ src/Tools/_framework/ToolPanels/CollectionEditor.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import { useRecoilValue } from 'recoil';
+import { searchParamAtomFamily } from '../NewToolRoot';
 
 export default function CollectionEditor(props) {
+  const [driveId, folderId, itemId] = useRecoilValue(
+    searchParamAtomFamily('path'),
+  ).split(':');
+
   return <div style={{ ...(props?.style ?? {}) }}>CollectionEditor</div>;
 }
