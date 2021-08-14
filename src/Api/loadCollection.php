1f2d79b058e425b2e7661a9bd09996d27318ce4b
Emilio Alvarez <e.l.alvarez@icloud.com>
2021-08-13 20:57:44 -0400

    update loadCollection logic

--- src/Api/loadCollection.php
+++ src/Api/loadCollection.php
@@ -10,7 +10,7 @@ include 'db_connection.php';
 $jwtArray = include 'jwtArray.php';
 $userId = $jwtArray['userId'];
 
-$success = true;
+$allowed = false;
 
 if (array_key_exists('doenetId', $_REQUEST)) {
     $doenetId = mysqli_real_escape_string($conn, $_REQUEST['doenetId']);
@@ -26,71 +26,62 @@ if (array_key_exists('doenetId', $_REQUEST)) {
         $row = $result->fetch_assoc();
         $driveId = $row['driveId'];
     }
-
-    if (array_key_exists('driveId', get_defined_vars())) {
-        //check user has permission to edit drive
-        $sql = "
-            SELECT canChangeAllDriveSettings
-            FROM drive_user
-            WHERE userId = '$userId'
-            AND driveId = '$driveId'
-        ";
-        $result = $conn->query($sql);
-        if ($result->num_rows > 0) {
-            $row = $result->fetch_assoc();
-            $canAdd = $row['canChangeAllDriveSettings'];
-            if (!$canAdd) {
-                http_response_code(403); //User if forbidden from operation
-                $success = false;
-                $message = 'No permission to add';
-            }
-        } else {
-            //Fail because there is no DB row for the user on this drive so we shouldn't allow an add
-            http_response_code(401); //User has bad auth
-            $success = false;
-            $message = 'Database rejected update';
-        }
-    } else {
-        //bad doenetId
-        http_response_code(400);
-        $success = false;
-        $message = 'Bad doenetId'; //TODO: is this ok to say??
-        echo json_encode([$message, $success]);
-    }
-
-    if ($success) {
-        $sql = "
-            SELECT collectionDoenetId, entryDoenetId, entryId, variant, label
-            FROM collection
-            WHERE collectionDoenetId = '$doenetId'
-        ";
-        $result = $conn->query($sql);
-
-        $entry_arr = [];
-        if ($result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-                array_push($entry_arr, [
-                    'collectionDoenetId' => $row['collectionDoenetId'],
-                    'entryDoenetId' => $row['entryDoenetId'],
-                    'entryId' => $row['entryId'],
-                    'variant' => $row['variant'],
-                    'label' => $row['label'],
-                ]);
-            }
-
-            $response_arr = [
-                'success' => $success,
-                'message' => $message,
-                'entries' => $entry_arr,
-            ];
-            http_response_code(200);
-            echo json_encode($response_arr);
-        }
-    }
 } else {
     http_response_code(400);
-    $message = 'Missing doenetId';
-    echo json_encode([$message, $success]);
+    echo json_encode(['message' => 'Missing DoenetId']);
+}
+
+if (array_key_exists('driveId', get_defined_vars())) {
+    //check user has permission to edit drive
+    $sql = "
+        SELECT canChangeAllDriveSettings
+        FROM drive_user
+        WHERE userId = '$userId'
+        AND driveId = '$driveId'
+    ";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $allowed = $row['canChangeAllDriveSettings'];
+        if (!$allowed) {
+            http_response_code(403); //User if forbidden from operation
+        }
+    } else {
+        //Fail because there is no DB row for the user on this drive so we shouldn't allow an add
+        http_response_code(401); //User has bad auth
+    }
+} else {
+    //bad doenetId
+    http_response_code(400);
+}
+
+if ($allowed) {
+    $sql = "
+        SELECT collectionDoenetId, entryDoenetId, entryId, variant
+        FROM collection
+        WHERE collectionDoenetId = '$doenetId'
+    ";
+    $result = $conn->query($sql);
+
+    $entry_arr = [];
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            array_push($entry_arr, [
+                'collectionDoenetId' => $row['collectionDoenetId'],
+                'entryDoenetId' => $row['entryDoenetId'],
+                'entryId' => $row['entryId'],
+                'variant' => $row['variant'],
+            ]);
+        }
+
+        $response_arr = [
+            'success' => $success,
+            'message' => $message,
+            'entries' => $entry_arr,
+        ];
+        http_response_code(200);
+        echo json_encode($response_arr);
+    }
 }
 
 $conn->close();
