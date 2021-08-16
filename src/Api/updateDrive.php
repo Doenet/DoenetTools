182dc6e41514088f81b8f56386e62ae2e810d60b
Emilio Alvarez <e.l.alvarez@icloud.com>
2021-08-16 13:34:24 -0400

    added security

--- src/Api/updateDrive.php
+++ src/Api/updateDrive.php
@@ -1,75 +1,89 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: access");
-header("Access-Control-Allow-Methods: GET");
-header("Access-Control-Allow-Credentials: true");
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: access');
+header('Access-Control-Allow-Methods: GET');
+header('Access-Control-Allow-Credentials: true');
 header('Content-Type: application/json');
 
-include "db_connection.php";
+include 'db_connection.php';
 
-$jwtArray = include "jwtArray.php";
+$jwtArray = include 'jwtArray.php';
 $userId = $jwtArray['userId'];
 
-$driveId = $_REQUEST["driveId"];
-// $driveId = mysqli_real_escape_string($conn,$_REQUEST["driveId"]);
-$label = mysqli_real_escape_string($conn,$_REQUEST["label"]);
-$type = mysqli_real_escape_string($conn,$_REQUEST["type"]);
+$allowed = false;
 
-$success = TRUE;
-$results_arr = array();
-
-//TODO: Check for permisions first
-
-if ($type == "update drive label"){
-  $sql = "
-  UPDATE drive
-  SET label='$label'
-  WHERE driveId = '$driveId'
-  ";
-$result = $conn->query($sql); 
-}
-if ($type == "delete drive"){
-//   $sql = "
-//   UPDATE drive
-//   SET isDeleted='1'
-//   WHERE driveId = '$driveId'
-//   ";
-// $result = $conn->query($sql); 
-// }
-  for($k = 0; $k < count($driveId); $k++){
-
-    $driveData = $driveId[$k];
+if (
+    array_key_exists('driveId', $_REQUEST) &&
+    array_key_exists('label', $_REQUEST) &&
+    array_key_exists('type', $_REQUEST)
+) {
+    $driveId = $_REQUEST['driveId']; //why is this not like the others?
+    // $driveId = mysqli_real_escape_string($conn,$_REQUEST["driveId"]);
+    $label = mysqli_real_escape_string($conn, $_REQUEST['label']);
+    $type = mysqli_real_escape_string($conn, $_REQUEST['type']);
+    //check user has permission to edit drive
     $sql = "
-    UPDATE drive
-    SET isDeleted='1'
-    WHERE driveId = '$driveData'
+      SELECT canDeleteDrive
+      FROM drive_user
+      WHERE userId = '$userId'
+      AND driveId = '$driveId'
     ";
     $result = $conn->query($sql);
-  }
- 
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $allowed = $row['canDeleteDrive'];
+    } else {
+        //Fail because there is no DB row for the user on this drive so we shouldn't allow an add
+        http_response_code(401); //User has bad auth
+    }
+
+    if ($allowed) {
+        if ($type == 'update drive label') {
+            $sql = "
+              UPDATE drive
+              SET label='$label'
+              WHERE driveId = '$driveId'
+              ";
+            $result = $conn->query($sql);
+        }
+        if ($type == 'delete drive') {
+            //   $sql = "
+            //   UPDATE drive
+            //   SET isDeleted='1'
+            //   WHERE driveId = '$driveId'
+            //   ";
+            // $result = $conn->query($sql);
+            // }
+            for ($k = 0; $k < count($driveId); $k++) {
+                $driveData = $driveId[$k];
+                $sql = "
+                  UPDATE drive
+                  SET isDeleted='1'
+                  WHERE driveId = '$driveData'
+                  ";
+                $result = $conn->query($sql);
+            }
+        }
+        //   for($k = 0; $k < count($driveId); $k++){
+
+        //     $driveData = $driveId[$k];
+        //     $sql = "
+        //     UPDATE drive
+        //     SET isDeleted='1'
+        //     WHERE driveId = '$driveData'
+        //     ";
+        //     $result = $conn->query($sql);
+        //   }
+
+        // }
+        //should check for db success from result object
+        http_response_code(202);
+    } else {
+        http_response_code(403); //User if forbidden from operation
+    }
+} else {
+    http_response_code(400);
 }
-//   for($k = 0; $k < count($driveId); $k++){
-
-//     $driveData = $driveId[$k];
-//     $sql = "
-//     UPDATE drive
-//     SET isDeleted='1'
-//     WHERE driveId = '$driveData'
-//     ";
-//     $result = $conn->query($sql);
-//   }
- 
-// }
-
-$response_arr = array(
-  "success"=>$success
-  );
-
-// set response code - 200 OK
-http_response_code(200);
-
-// make it json format
-echo json_encode($response_arr);
 $conn->close();
 
 ?>
