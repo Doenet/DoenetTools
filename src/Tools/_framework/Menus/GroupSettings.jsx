aadb01f06a3eb95ae5213003c5657df42099ddf2
Emilio Alvarez <e.l.alvarez@icloud.com>
2021-09-01 00:33:03 -0500

    assign preAssigned group DB tables
    add into user_assignment and use_assignment_attempt

--- src/Tools/_framework/Menus/GroupSettings.jsx
+++ src/Tools/_framework/Menus/GroupSettings.jsx
@@ -1,9 +1,71 @@
 import axios from 'axios';
 import React, { useEffect, useReducer } from 'react';
-import { useRecoilValue } from 'recoil';
+import { useRecoilCallback, useRecoilValue } from 'recoil';
 import Button from '../../../_reactComponents/PanelHeaderComponents/Button';
 import CollapseSection from '../../../_reactComponents/PanelHeaderComponents/CollapseSection';
 import { searchParamAtomFamily } from '../NewToolRoot';
+import { itemHistoryAtom } from '../ToolHandlers/CourseToolHandler';
+
+function groupReducer(state, action) {
+  switch (action.type) {
+    case 'mount':
+      return { ...action.payload };
+    case 'min':
+      return {
+        preAssigned: state.preAssigned,
+        min: action.payload.min > 1 ? action.payload.min : 1,
+        max: state.max < action.payload.min ? action.payload.min : state.max,
+        pref: state.pref < action.payload.min ? action.payload.min : state.pref,
+      };
+    case 'max':
+      return {
+        preAssigned: state.preAssigned,
+
+        min: state.min,
+        max: state.min <= action.payload.max ? action.payload.max : state.max,
+        pref: state.pref < action.payload.max ? action.payload.max : state.pref,
+      };
+    case 'pref':
+      return {
+        ...state,
+        pref:
+          state.min <= action.payload.pref && action.payload.pref <= state.max
+            ? action.payload.pref
+            : state.pref,
+      };
+    case 'preAssigned':
+      return { ...state, preAssigned: action.payload.preAssigned };
+    case 'save':
+      try {
+        axios.post('/api/updateGroupSettings.php', { ...state });
+      } catch (error) {
+        console.error(error);
+      }
+      return state;
+    default:
+      throw new Error('Invaild groupSettings dispach');
+  }
+}
+
+function shuffle(array) {
+  // from https://bost.ocks.org/mike/shuffle/
+  var m = array.length,
+    t,
+    i;
+
+  // While there remain elements to shuffle…
+  while (m) {
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+
+    // And swap it with the current element.
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+
+  return array;
+}
 
 export default function GroupSettings() {
   const doenetId = useRecoilValue(searchParamAtomFamily('doenetId'));
@@ -13,48 +75,60 @@ export default function GroupSettings() {
     pref: 0,
     preAssigned: false,
   });
-  function groupReducer(state, action) {
-    switch (action.type) {
-      case 'mount':
-        return { ...action.payload };
-      case 'min':
-        return {
-          preAssigned: state.preAssigned,
-          min: action.payload.min > 1 ? action.payload.min : 1,
-          max: state.max < action.payload.min ? action.payload.min : state.max,
-          pref:
-            state.pref < action.payload.min ? action.payload.min : state.pref,
-        };
-      case 'max':
-        return {
-          preAssigned: state.preAssigned,
-
-          min: state.min,
-          max: state.min <= action.payload.max ? action.payload.max : state.max,
-          pref:
-            state.pref < action.payload.max ? action.payload.max : state.pref,
-        };
-      case 'pref':
-        return {
-          ...state,
-          pref:
-            state.min <= action.payload.pref && action.payload.pref <= state.max
-              ? action.payload.pref
-              : state.pref,
-        };
-      case 'preAssigned':
-        return { ...state, preAssigned: action.payload.preAssigned };
-      case 'save':
+  //TODO: load all entries from the collection table, shuffle the grouping array, assign
+  //proportional sections of each entry, commit data to assignment table
+  const assignCollection = useRecoilCallback(
+    ({ snapshot }) =>
+      async (doenetId, grouping) => {
         try {
-          axios.post('/api/updateGroupSettings.php', { ...state });
+          console.log('go', doenetId, grouping);
+          const {
+            data: { entries },
+          } = await axios.get('/api/loadCollection.php', {
+            params: { doenetId },
+          });
+          if (entries.length > 0) {
+            //GROUPS
+            // [ [ 'id1', 'id2', 'id3'] , ['id4', 'id5', 'id6'], ['id7', 'id8', 'id9']]
+            const shuffledEntries = shuffle(entries);
+            const shuffledGroups = shuffle(grouping);
+            const resp = axios.post('/api/assignCollection.php', {
+              doenetId,
+              groups: JSON.stringify(shuffledGroups),
+              entries: JSON.stringify(shuffledEntries),
+            });
+            console.log('test was', resp);
+          } else {
+            //Error toast
+          }
+
+          // const _doenetId = await snapshot.getPromise(
+          //   searchParamAtomFamily('doenetId'),
+          // );
+          // const versionHistory = await snapshot.getPromise(
+          //   itemHistoryAtom(doenetId),
+          // );
+
+          // //Find Assigned ContentId
+          // //Use isReleased as isAssigned for now
+          // //TODO: refactor isReleased to isAssigned
+
+          // //Set contentId and isAssigned
+          // let contentId = null;
+          // let isAssigned = false;
+          // for (let version of versionHistory.named) {
+          //   if (version.isReleased === '1') {
+          //     isAssigned = true;
+          //     contentId = version.contentId;
+          //     break;
+          //   }
+          // }
         } catch (error) {
           console.error(error);
         }
-        return state;
-      default:
-        throw new Error('Invaild groupSettings dispach');
-    }
-  }
+      },
+    [],
+  );
 
   useEffect(() => {
     let mounted = true;
@@ -97,7 +171,18 @@ export default function GroupSettings() {
       </label>
       {preAssigned ? (
         <div>
-          <Button alert value="Upload and Assign CSV" width="menu" />
+          <Button
+            alert
+            value="Upload and Assign CSV"
+            width="menu"
+            onClick={() => {
+              assignCollection(doenetId, [
+                ['temp1@dev.com', 'temp2@dev.com', 'temp3@dev.com'],
+                ['temp4@dev.com', 'temp5@dev.com', 'temp6@dev.com'],
+                ['temp7@dev.com', 'temp8@dev.com', 'temp9@dev.com'],
+              ]);
+            }}
+          />
           <br />
           <CollapseSection></CollapseSection>
         </div>
