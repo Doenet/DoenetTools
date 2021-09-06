ed10edabec899521b745836bdad1e4a65ba7bf5e
Kevin Charles <char0042@umn.edu>
2021-09-06 13:07:09 -0500

    Swap rows and columns of gradebookStudent tool

--- src/Tools/_framework/ToolPanels/GradebookStudent.jsx
+++ src/Tools/_framework/ToolPanels/GradebookStudent.jsx
@@ -24,87 +24,137 @@ export default function GradebookStudent() {
     // console.log(">>>>overView",overView.contents)
 
     let overviewTable = {}
+
     overviewTable.headers=[{
-        Header: "Name",
-        accessor: "name",
-    },];
-
-    if(assignments.state == 'hasValue'){
-        for(let doenetId in assignments.contents){
-            overviewTable.headers.push({
-                //`/assignment/?doenetId=${doenetId}`
-                Header: <a onClick = {(e) =>{
-                    e.stopPropagation()
-
-                    setPageToolView({
-                        page: 'course',
-                        tool: 'gradebookStudentAssignment',
-                        view: '',
-                        params: { driveId, userId, doenetId, source: 'student'},
-                    })
-                }
-                }>{assignments.contents[doenetId]}</a>,
-                accessor: doenetId,
-                disableFilters: true,
-                // <a onClick={() => {
-                //     open("calendar", "fdsa", "f001");
-                //   }}>
-                
-
-            })
-        }
-    }
-
-    overviewTable.headers.push(
-        {
-            Header: "Weighted Credt",
-            accessor: "weight",
-            disableFilters: true
-            
-        }
-    )
-    overviewTable.headers.push(
-        {
-            Header: "Grade",
-            accessor: "grade",
-            sortType: gradeSorting,
-            disableFilters: true
-        },
-    )
+        Header: "Assignment",
+        accessor: "assignment",
+    },
+    {
+        Header: "Score",
+        accessor: "score",
+        disableFilters: true,
+    }];
 
     overviewTable.rows = []
+
+ if(assignments.state == 'hasValue' && 
+    students.state === 'hasValue' && 
+    overView.state === 'hasValue' && 
+    userId !== null && userId !== ''){
+
+    for(let doenetId in assignments.contents){
+        let score = (overView.contents[userId].assignments[doenetId]) * 100 + "%";
+        let assignment = <a onClick = {(e) =>{
+                            // e.stopPropagation()
+                            setPageToolView({
+                                page: 'course',
+                                tool: 'gradebookStudentAssignment',
+                                view: '',
+                                params: { driveId, userId, doenetId, source: 'student'},
+                            })
+                        }
+                        }>{assignments.contents[doenetId]}</a>
+                        
+        overviewTable.rows.push({
+            assignment,
+            score
+        });
+        
+    }
+}
+overviewTable.rows.push({
+    assignment:"Weighted Credit",
+    score:"wc"
+});
+
+overviewTable.rows.push({
+    assignment:"Grade",
+    score:"g"
+});
+
+
+    // overviewTable.headers=[{
+    //     Header: "Name",
+    //     accessor: "name",
+    // },];
+
+    // if(assignments.state == 'hasValue'){
+    //     for(let doenetId in assignments.contents){
+    //         overviewTable.headers.push({
+    //             //`/assignment/?doenetId=${doenetId}`
+    //             Header: <a onClick = {(e) =>{
+    //                 e.stopPropagation()
+
+    //                 setPageToolView({
+    //                     page: 'course',
+    //                     tool: 'gradebookStudentAssignment',
+    //                     view: '',
+    //                     params: { driveId, userId, doenetId, source: 'student'},
+    //                 })
+    //             }
+    //             }>{assignments.contents[doenetId]}</a>,
+    //             accessor: doenetId,
+    //             disableFilters: true,
+    //             // <a onClick={() => {
+    //             //     open("calendar", "fdsa", "f001");
+    //             //   }}>
+                
+
+    //         })
+    //     }
+    // }
+
+    // overviewTable.headers.push(
+    //     {
+    //         Header: "Weighted Credt",
+    //         accessor: "weight",
+    //         disableFilters: true
+            
+    //     }
+    // )
+    // overviewTable.headers.push(
+    //     {
+    //         Header: "Grade",
+    //         accessor: "grade",
+    //         sortType: gradeSorting,
+    //         disableFilters: true
+    //     },
+    // )
+
+    // overviewTable.rows = []
     
 
 
-    if(students.state === 'hasValue' && userId !== null && userId !== ''){
-        //console.log(">>> userId", userId)
-        let firstName = students.contents[userId].firstName,
-            lastName = students.contents[userId].lastName,
-            credit = students.contents[userId].courseCredit,
-            generatedGrade = students.contents[userId].courseGrade,
-            overrideGrade = students.contents[userId].overrideCourseGrade;
+    // if(students.state === 'hasValue' && userId !== null && userId !== ''){
+    //     //console.log(">>> userId", userId)
+    //     let firstName = students.contents[userId].firstName,
+    //         lastName = students.contents[userId].lastName,
+    //         credit = students.contents[userId].courseCredit,
+    //         generatedGrade = students.contents[userId].courseGrade,
+    //         overrideGrade = students.contents[userId].overrideCourseGrade;
 
-        let grade = overrideGrade ? overrideGrade : generatedGrade
+    //     let grade = overrideGrade ? overrideGrade : generatedGrade
 
-        let row = {}
+    //     let row = {}
 
-        row["name"] = firstName + " " + lastName
+    //     row["name"] = firstName + " " + lastName
         
-        if(overView.state == 'hasValue' && assignments.state == 'hasValue'){
-            for (let doenetId in assignments.contents) {
-                row[doenetId] = (overView.contents[userId].assignments[doenetId]) * 100 + "%"
-            }
-        }
+    //     if(overView.state == 'hasValue' && assignments.state == 'hasValue'){
+    //         for (let doenetId in assignments.contents) {
+    //             row[doenetId] = (overView.contents[userId].assignments[doenetId]) * 100 + "%"
+    //         }
+    //     }
 
-        row["weight"] = credit
-        row["grade"] = grade
+    //     row["weight"] = credit
+    //     row["grade"] = grade
 
         
-        overviewTable.rows.push(row);
-    }
+    //     overviewTable.rows.push(row);
+    // }
 
     //console.log("debug overviewtable", overviewTable);
-    
+    // console.log(">>>>data",overviewTable.rows)
+    // console.log(">>>>columns",overviewTable.headers)
 
     return (
         <Styles>
