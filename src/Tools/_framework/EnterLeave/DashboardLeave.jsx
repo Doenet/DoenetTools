a78041091fbbc0c6df4f24bb56e64ef00467a497
Kevin Charles <char0042@umn.edu>
2021-09-09 12:14:21 -0500

    Clear next 7 days on leave

--- src/Tools/_framework/EnterLeave/DashboardLeave.jsx
+++ src/Tools/_framework/EnterLeave/DashboardLeave.jsx
@@ -1,13 +1,15 @@
 // import React from 'react';
 import { selectedMenuPanelAtom } from '../Panels/NewMenuPanel';
-import { globalSelectedNodesAtom } from '../../../_reactComponents/Drive/NewDrive';
+import { globalSelectedNodesAtom, clearDriveAndItemSelections } from '../../../_reactComponents/Drive/NewDrive';
 import { useRecoilCallback } from 'recoil';
 
 export default function DashboardLeave() {
   // console.log('>>>>DashbaordLeave');
+
   const setSelections = useRecoilCallback(({set})=>()=>{
-    set(selectedMenuPanelAtom,"");
+    set(clearDriveAndItemSelections, null);
     set(globalSelectedNodesAtom,[]);
+    set(selectedMenuPanelAtom,"");
   })
   setSelections()
   return null;
