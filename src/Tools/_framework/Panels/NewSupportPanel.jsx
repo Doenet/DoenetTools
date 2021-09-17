cc2ee8ae5cfd76955a68d24247a9cdc0db36fb75
Clara Selchow <claraselchow@gmail.com>
2021-09-17 12:43:21 -0500

    white color fix

--- src/Tools/_framework/Panels/NewSupportPanel.jsx
+++ src/Tools/_framework/Panels/NewSupportPanel.jsx
@@ -7,7 +7,7 @@ import {
 const SupportWrapper = styled.div`
   overflow: auto;
   grid-area: supportPanel;
-  background-color: hsl(0, 0%, 99%);
+  background-color: hsl(0, 0%, 100%);
   height: 100%;
   display: ${({$hide})=> $hide ? 'none' : 'block' }
   // border-radius: 0 0 4px 4px;
@@ -18,7 +18,7 @@ const ControlsWrapper = styled.div`
   grid-area: supportControls;
   display: flex;
   gap: 4px;
-  background-color: hsl(0, 0%, 99%);
+  background-color: hsl(0, 0%, 100%);
   display: ${({$hide})=> $hide ? 'none' : 'block' }
   // border-radius: 4px 4px 0 0;
   // border-bottom: 2px solid #e3e3e3;
--- src/Tools/_framework/Panels/Panel.jsx
+++ src/Tools/_framework/Panels/Panel.jsx
@@ -47,7 +47,7 @@ const Background = styled.div`
   height: ${({ $vertical }) => ($vertical ? '100%' : '')};
   width: ${({ $vertical }) => ($vertical ? '' : '100%')};
   border-radius: 4px;
-  background-color: hsl(0, 0%, 99%);
+  background-color: hsl(0, 0%, 100%);
 `;
 
 const DragHandle = styled.div`
