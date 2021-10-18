dad2f8a06998445c0a23ecfacc723f854e46d891
Kevin Charles <kevincharles@users.noreply.github.com>
2021-10-18 15:48:42 -0500

    SEB filter learners and timezone fix (#845)
    Co-authored-by: Kevin Charles <char0042@umn.edu>

--- src/_utils/dateUtilityFunction.js
+++ src/_utils/dateUtilityFunction.js
@@ -19,6 +19,17 @@ export function dateUtilityFunction(date, isTimeIncluded){
     );
   };
 
+  export function formatAMPM(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
+    return strTime;
+  }
+
 
   export function DateToUTCDateString(date){
     var pad = function(num) { return ('00'+num).slice(-2) };
