cac27574561f7c3200d7142327015af6b7d00dca
Duane Nykamp <nykamp@umn.edu>
2021-10-22 12:38:52 -0500

    fixAxes for graph (#853)

--- src/Viewer/renderers/graph.jsx
+++ src/Viewer/renderers/graph.jsx
@@ -29,8 +29,10 @@ export default class Graph extends DoenetRenderer {
         boundingbox,
         axis: false,
         showCopyright: false,
-        showNavigation: this.doenetSvData.showNavigation,
+        showNavigation: this.doenetSvData.showNavigation && !this.doenetSvData.fixAxes,
         keepAspectRatio: this.doenetSvData.identicalAxisScales,
+        zoom: { wheel: !this.doenetSvData.fixAxes },
+        pan: { enabled: !this.doenetSvData.fixAxes }
       });
 
     if (this.doenetSvData.displayXAxis) {
