51df3766072f6dee488574ae02591eed11fa4dc9
Kevin Charles <char0042@umn.edu>
2021-10-25 18:28:49 -0500

    Start of conversion

--- src/Tools/_framework/HeaderControls/ChooserBreadCrumb.jsx
+++ src/Tools/_framework/HeaderControls/ChooserBreadCrumb.jsx
@@ -1,22 +1,23 @@
 import React, { Suspense } from 'react';
 import { BreadCrumb } from '../../../_reactComponents/PanelHeaderComponents/BreadCrumb';
 // import BreadCrumb from '../../../_reactComponents/Breadcrumb/BreadCrumb';
-import { faTh } from '@fortawesome/free-solid-svg-icons';
+// import { faTh } from '@fortawesome/free-solid-svg-icons';
+import { courseChooserCrumb } from '../../../_utils/breadcrumbUtil';
 
 export default function ChooserBreadCrumb() {
 
   let crumbs = [];
-  crumbs.push({icon:faTh, onClick:()=>{console.log('>>>>clicked first')}})
-  crumbs.push({label:'second really really really long!!!',onClick:()=>{console.log('>>>>clicked second')}})
-  crumbs.push({label:'third',onClick:()=>{console.log('>>>>clicked third')}})
-  crumbs.push({label:'fourth really really really long!!!',onClick:()=>{console.log('>>>>clicked fourth')}})
-  // crumbs.push({label:'fourth  really really really really really really long!!!',onClick:()=>{console.log('>>>>clicked fourth')}})
-  crumbs.push({label:'fifth',onClick:()=>{console.log('>>>>clicked fifth')}})
-  crumbs.push({label:'sixth really really really long!!!',onClick:()=>{console.log('>>>>clicked sixth')}})
-  crumbs.push({label:'seventh',onClick:()=>{console.log('>>>>clicked seventh')}})
-  crumbs.push({label:'eighth really really really long!!!',onClick:()=>{console.log('>>>>clicked eighth')}})
-  crumbs.push({label:'ninth',onClick:()=>{console.log('>>>>clicked ninth')}})
-  // crumbs.push({label:'ninth really really really really really really long!!!',onClick:()=>{console.log('>>>>clicked ninth')}})
+  crumbs.push(courseChooserCrumb)
+  // crumbs.push({label:'second',onClick:()=>{console.log('>>>>clicked second')}})
+  // crumbs.push({label:'third',onClick:()=>{console.log('>>>>clicked third')}})
+  // crumbs.push({label:'fourth',onClick:()=>{console.log('>>>>clicked fourth')}})
+  // // crumbs.push({label:'fourth  really really really',onClick:()=>{console.log('>>>>clicked fourth')}})
+  // crumbs.push({label:'fifth',onClick:()=>{console.log('>>>>clicked fifth')}})
+  // crumbs.push({label:'sixth',onClick:()=>{console.log('>>>>clicked sixth')}})
+  // crumbs.push({label:'seventh',onClick:()=>{console.log('>>>>clicked seventh')}})
+  // crumbs.push({label:'eighth',onClick:()=>{console.log('>>>>clicked eighth')}})
+  // crumbs.push({label:'ninth',onClick:()=>{console.log('>>>>clicked ninth')}})
+  // crumbs.push({label:'ninth really really really',onClick:()=>{console.log('>>>>clicked ninth')}})
 
   return (
     <Suspense fallback={<div>loading Breadcrumbs...</div>}>
---
+++ src/_utils/breadcrumbUtil.js
@@ -0,0 +1,7 @@
+import { faTh } from '@fortawesome/free-solid-svg-icons';
+
+// export function courseChooserCrumb(){
+//   return {icon:faTh, onClick:()=>{console.log('>>>>clicked first')}}
+// }
+
+export var courseChooserCrumb = {icon:faTh, onClick:()=>{console.log('>>>>clicked first')}}
