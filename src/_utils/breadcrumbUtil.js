2199b68dfd68b548f5d8e38ce28de9f3a89a5646
Kevin Charles <char0042@umn.edu>
2021-10-25 22:45:03 -0500

    chooser and dashboard crumbs

--- src/_utils/breadcrumbUtil.js
+++ src/_utils/breadcrumbUtil.js
@@ -1,7 +1,51 @@
 import { faTh } from '@fortawesome/free-solid-svg-icons';
+import { useEffect, useState } from 'react';
+import { useRecoilCallback, useSetRecoilState } from 'recoil';
+import { pageToolViewAtom } from '../Tools/_framework/NewToolRoot';
+import { fetchDrivesQuery } from '../_reactComponents/Drive/NewDrive';
 
-// export function courseChooserCrumb(){
-//   return {icon:faTh, onClick:()=>{console.log('>>>>clicked first')}}
-// }
+export function useCourseChooserCrumb(){
 
-export var courseChooserCrumb = {icon:faTh, onClick:()=>{console.log('>>>>clicked first')}}
+  const setPageToolView = useSetRecoilState(pageToolViewAtom);
+
+  return {icon:faTh, onClick:()=>{
+        setPageToolView({
+          page: 'course',
+          tool: 'courseChooser',
+          view: '',
+        });
+  }}
+}
+
+export function useDashboardCrumb(driveId){
+  
+  const setPageToolView = useSetRecoilState(pageToolViewAtom);
+  const [label,setLabel] = useState('')
+
+  const getDriveLabel = useRecoilCallback(({snapshot})=> async (driveId)=>{
+    let drives = await snapshot.getPromise(fetchDrivesQuery);
+    for (let driveIdLabel of drives.driveIdsAndLabels){
+      if (driveIdLabel.driveId == driveId){
+        setLabel(driveIdLabel.label);
+        return;
+      }
+    }
+    
+  })
+  useEffect(()=>{
+    getDriveLabel(driveId);
+  },[driveId])
+
+  let params = {
+    path: `${driveId}:${driveId}:${driveId}:Drive`,
+  }
+
+  return {label, onClick:()=>{
+        setPageToolView({
+          page: 'course',
+          tool: 'dashboard',
+          view: '',
+          params
+        });
+  }}
+}
