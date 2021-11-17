293a8e35bd8b296f92f495ecb579b3883c5518c0
Duane Nykamp <nykamp@umn.edu>
2021-11-16 20:37:56 -0600

    equilibriumLine component (#912)

--- src/Core/components/dynamicalSystems/index.js
+++ src/Core/components/dynamicalSystems/index.js
@@ -1,2 +1,4 @@
 export { default as ODESystem } from './ODESystem';
 export { default as CobwebPolyline } from './CobwebPolyline';
+export { default as EquilibriumPoint } from './EquilibriumPoint';
+export { default as EquilibriumLine } from './EquilibriumLine';
