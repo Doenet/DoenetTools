0335db28bfbd291bbeff8da5af8959d466a0cd94
Kevin Charles <char0042@umn.edu>
2021-11-22 14:52:35 -0600

    Start of due date override

--- src/Tools/_framework/Menus/GradeSettings.jsx
+++ src/Tools/_framework/Menus/GradeSettings.jsx
@@ -1,12 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import Button from '../../../_reactComponents/PanelHeaderComponents/Button';
-import { useRecoilState } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import CalendarButton from '../../../_reactComponents/PanelHeaderComponents/CalendarToggle';
+import DateTime from '../../../_reactComponents/PanelHeaderComponents/DateTime';
+import { searchParamAtomFamily } from '../NewToolRoot';
 
 export default function GradeSettings(){
-  // const [overdue,setOverdue] = useRecoilState(showOverdueAtom);
-  // const [completed,setCompleted] = useRecoilState(showCompletedAtom);
+  let doenetId = useRecoilValue(searchParamAtomFamily('doenetId'))
+    let userId = useRecoilValue(searchParamAtomFamily('userId'))
+    let [effectiveDueDate,seteffectiveDueDate] = useState(null)
+    //get dueDate and dueDateOverride
+
+
+    let dueDateJSX =  <>Due Date Override:
+    <div
+            style={{ display: 'flex' }}
+            onClick={(e) => {
+              e.preventDefault();
+            }}
+          >
+    <CalendarButton
+              checked={effectiveDueDate !== null}
+              onClick={(e) => {
+                // let valueDescription = 'None';
+                // let value = null;
+
+                // if (aInfo.dueDate === null) {
+                //   valueDescription = 'Next Week';
+                //   let nextWeek = new Date();
+                //   nextWeek.setDate(nextWeek.getDate() + 7);
+                //   value = DateToDateString(nextWeek);
+                // }
+
+                // updateAssignment({
+                //   doenetId,
+                //   keyToUpdate: 'dueDate',
+                //   value,
+                //   description: 'Due Date',
+                //   valueDescription,
+                // });
+              }}
+            />
+    <DateTime
+    value={effectiveDueDate ? new Date(effectiveDueDate) : null}
+    onBlur={({ valid, value }) => {
+      // if (valid) {
+      //   try {
+      //     value = value.toDate();
+      //   } catch (e) {
+      //     // console.log('value not moment');
+      //   }
+      //   if (
+      //     new Date(DateToDateString(value)).getTime() !==
+      //     new Date(aInfo.dueDate).getTime()
+      //   ) {
+      //     updateAssignment({
+      //       doenetId,
+      //       keyToUpdate: 'dueDate',
+      //       value: DateToDateString(value),
+      //       description: 'Due Date',
+      //     });
+      //   }
+      // } else {
+      //   addToast('Invalid Due Date');
+      // }
+    }}
+  />
+  </div>
+  </>
 
   return <div>
-    Grade Settings
+    <div>Due Date: </div>
+    {dueDateJSX}
   </div>
 }
