ee4362be430ea8a2be40b853f5bb178117424eb1
Kevin Charles <char0042@umn.edu>
2022-01-03 12:53:03 -0600

    Codemirror for code editor

--- src/Viewer/renderers/codeEditor.jsx
+++ src/Viewer/renderers/codeEditor.jsx
@@ -1,60 +1,75 @@
 import React, { useState, useRef } from 'react';
 import useDoenetRenderer from './useDoenetRenderer';
 import { sizeToCSS } from './utils/css';
+import CodeMirror from '../../Tools/_framework/CodeMirror';
 
 export default function CodeEditor(props){
   let {name, SVs, actions} = useDoenetRenderer(props,false);
   const [currentValue,setCurrentValue] = useState(SVs.value)
   let timer = useRef(null)
+  let editorRef = useRef(null)
+  console.log(SVs)
 
 if (SVs.hidden) {
   return null;
 }
 
-function onChangeHandler(e){
-  setCurrentValue(e.target.value);
-  actions.updateImmediateValue({
-    text: e.target.value
-  });
-
-  if (timer.current === null){
-    timer.current = setTimeout(function(){
-        actions.updateValue();
-      timer.current = null;
-    },3000)//3 seconds
-  }
-}
-
-function handleBlur(e) {
-  if (SVs.immediateValue !== SVs.value) {
-    actions.updateValue();
-  }
-}
 const inputKey = name + '_input';
+const codemirrorKey = name + '_codemirror';
 
 //Recieved update from core to immediateValue
 if (SVs.immediateValue !== currentValue) {
   setCurrentValue(SVs.immediateValue)
 }
 
-//TODO Focus changes style
-let input = <textarea
-        key={inputKey}
-        id={inputKey}
-        value={currentValue}
-        disabled={SVs.disabled}
-        onChange={onChangeHandler}
-        onBlur={handleBlur}
-        // onFocus={this.handleFocus}
-        style={{
-          width: sizeToCSS(SVs.width),
-          height: sizeToCSS(SVs.height),
-          fontSize: "14px",
-          borderWidth: "1px",
-          // borderColor: surroundingBorderColor,
-          padding: "4px",
-        }}
-      />
+let updateInternalValue = SVs.value;
+
+//TODO: on update scrolling is wrong
+//TODO: option for inline for height (and no scroll)
+
+let input = <div 
+            key={inputKey}
+            id={inputKey}
+
+            style={{
+              width: sizeToCSS(SVs.width),
+              height: sizeToCSS(SVs.height),
+              padding: "0px",
+              border: "1px solid black",
+              overflowY: "scroll"
+              // display: "inline"
+            }}>
+  <CodeMirror
+  key = {codemirrorKey}
+  editorRef = {editorRef}
+  setInternalValue = {updateInternalValue}
+  readOnly = {SVs.disabled}
+   //TODO: wire up onBlur in codemirror
+
+  onBlur={(e)=>{
+    console.log("BLUR!!!!!")
+    if (SVs.immediateValue !== SVs.value) {
+      actions.updateValue();
+    }
+  }}
+  onBeforeChange={(value) => {
+    setCurrentValue(value);
+    actions.updateImmediateValue({
+      text: value
+    });
+   //TODO: READ ONLY SHOULD STOP TIMERS
+  
+    if (timer.current === null){
+      timer.current = setTimeout(function(){
+          actions.updateValue();
+        timer.current = null;
+      // },3000)//3 seconds
+    },10000)//10 seconds
+    }
+
+  }}
+/>
+</div>
 
   return <>
   <a name={name} />
