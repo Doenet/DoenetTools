fa2811af5050a9dcd665a84aa62e00792c8e20e7
Keagan Eng <eng00029@umn.edu>
2022-01-24 13:46:13 -0600

    added props to card, made card functional in dashboard

--- src/Tools/_framework/ToolPanels/Dashboard.jsx
+++ src/Tools/_framework/ToolPanels/Dashboard.jsx
@@ -2,10 +2,12 @@ import React, { useEffect } from 'react';
 import { useRecoilValue, useSetRecoilState, useRecoilValueLoadable } from 'recoil';
 import Button from '../../../_reactComponents/PanelHeaderComponents/Button';
 import ButtonGroup from '../../../_reactComponents/PanelHeaderComponents/ButtonGroup';
+import Card from '../../../_reactComponents/PanelHeaderComponents/Card';
 import { pageToolViewAtom, searchParamAtomFamily, profileAtom } from '../NewToolRoot';
 import Next7Days from '../Widgets/Next7Days';
 import { effectiveRoleAtom } from '../../../_reactComponents/PanelHeaderComponents/RoleDropdown';
 import { suppressMenusAtom } from '../NewToolRoot';
+import { faCode, faUser, faChartPie, faTasks } from '@fortawesome/free-solid-svg-icons';
 
 
 export default function Dashboard(props) {
@@ -30,7 +32,80 @@ export default function Dashboard(props) {
     <div style={props?.style ?? {}}>
       <div style={{marginLeft: '10px', marginRight: '10px'}}>
         <h1>Welcome!</h1>
-        <ButtonGroup vertical>
+        <div style={{display: 'flex', justifyContent: 'space-between', col: '1', width: '850px'}}>
+          <Card 
+            name='Content' 
+            icon={faCode}
+            value="Content"
+            onClick={() => {
+              setPageToolView((was) => {
+                // console.log(">>>>was",was);
+              return { ...was, tool: 'navigation' }
+            })}}
+          />
+          {effectiveRole === 'instructor' ?
+          <>
+          <Card 
+            name='Enrollment' 
+            icon={faUser}
+            value="Enrollment"
+            onClick={() =>
+              setPageToolView({
+                page: 'course',
+                tool: 'enrollment',
+                view: '',
+                params: { driveId },
+              })
+            }
+          />
+          <Card 
+            name='Surveys' 
+            icon={faChartPie}
+            value="Surveys"
+            onClick={() =>
+              setPageToolView({
+                page: 'course',
+                tool: 'surveyList',
+                view: '',
+                params: { driveId },
+              })
+            }
+          />
+          </>
+          : null}
+          {effectiveRole === 'instructor' ?
+          <Card 
+            name='Gradebook' 
+            icon={faTasks}
+            value="Gradebook" 
+            onClick={() => 
+            setPageToolView((was)=>{return {
+              page: 'course',
+              tool: 'gradebook',
+              view: was.view,
+              params: { driveId },
+              }})
+            } 
+          />
+          :
+          <Card 
+            name='Gradebook' 
+            icon={faTasks}
+            value="Gradebook" 
+            onClick={() => 
+            setPageToolView((was)=>{return {
+              page: 'course',
+              tool: 'gradebookStudent',
+              view: was.view,
+              params: { driveId, userId:profile.userId },
+              }})
+
+            } 
+          />
+          }
+        </div>
+  
+        {/* <ButtonGroup vertical>
         <Button
           value="Content"
           onClick={() => {
@@ -91,7 +166,7 @@ export default function Dashboard(props) {
         />
          }
         
-        </ButtonGroup>
+        </ButtonGroup> */}
       </div>
       <div style={{ marginTop: '10px', margin: '10px'}}>
       <Next7Days driveId={driveId}/>
--- src/_reactComponents/PanelHeaderComponents/Card.jsx
+++ src/_reactComponents/PanelHeaderComponents/Card.jsx
@@ -1,30 +1,93 @@
 import React, { useState ,useRef, useEffect} from 'react';
-import { doenetComponentForegroundInactive, doenetComponentForegroundActive } from "./theme.js";
+import { doenetComponentForegroundInactive, doenetComponentForegroundActive, doenetComponentBackgroundActive } from "./theme.js";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import Button from './Button.jsx';
 
 
 export default function Card(props) {
+
+    const [labelVisible, setLabelVisible] = useState(props.label ? 'static' : 'none');
+    const [align, setAlign] = useState(props.vertical ? 'static' : 'flex');
+
     var card = {
-        boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)',
+        // boxShadow: `0 4px 8px 0 ${doenetComponentForegroundInactive}`,
+        backgroundImage: `linear-gradient(to bottom left, white, white, white, rgb(143, 184, 222))`,
+        // backgroundColor: `${doenetComponentForegroundActive}`,
         transition: '0.3s',
-        width: '400px',
-        height: '200px'
-    }
-    var container = {
-        padding: '2px 16px',
+        borderRadius: '5px',
+        width: '190px',
+        height: '100px',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        color: 'black',
+        border: '2px solid black',
+        cursor: 'pointer',
     }
     var title = {
-        fontSize: '20px',
-        fontFamily:"Open Sans",
-        textAlign: 'center'
+        value: 'Card',
+        fontSize: '24px',
+        fontFamily: 'Open Sans',
+        margin: '0',
+    }
+    var label = {
+        value: 'Label:',
+        fontSize: '14px',
+        marginRight: '5px',
+        display: `${labelVisible}`,
+        margin: '0px 5px 2px 0px'
+    }
+    var container = {
+        display: `${align}`, 
+        // width: '235px',
+        alignItems:'center',
+    }
+    // var circle = {
+    //     height: '50px',
+    //     width: '50px',
+    //     backgroundColor: `${doenetComponentBackgroundActive}`,
+    //     borderRadius: '50%',
+    //     position: 'absolute',
+    //     top: '40px',
+    //     left: '350px'
+    // }
+    if (props.value){
+        title.value = props.value;
+    }
+    var icon = '';
+    if (props.icon){
+        icon = props.icon
+    }
+    if (props.label) {
+        label.value = props.label;
+    }
+    if (props.alert) {
+        card.borderColor = '#C1292E';
+      }
+      
+    if (props.disabled) {
+        card.borderColor = '#e2e2e2';
+        card.color = 'black';
+        card.cursor = 'not-allowed';
+    }
+    function handleClick(e) {
+        if (props.onClick) props.onClick(e);
     }
 
     return (
-        <div style={card}>
-            <div style={container}>
-                <h4 style={title}><b>Enrollment</b></h4>
-            </div>
+        <div style={container}>
+            <p style={label}>{label.value}</p>
+            <button 
+                style={card}
+                onClick={(e) => { handleClick(e) }}
+                >
+                {/* <FontAwesomeIcon icon={faUser} style={{padding: '8px', fontSize: '20px'}}/> */}
+                {/* <div style={circle}></div> */}
+                <h4 style={title}><b>{title.value}</b></h4>
+                <FontAwesomeIcon icon={icon} style={{name: 'faUser', padding: '8px', fontSize: '20px'}}
+                />
+            </button>
         </div>
     );
 }
