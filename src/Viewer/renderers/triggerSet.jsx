7649bb0e10076a085cf4a5032e0797d50348efe9
Kevin Charles <kevincharles@users.noreply.github.com>
2022-01-25 17:22:01 -0600

    Five More Functional Component Renderers (#982)
    * video renderers actions
    
    * video width, height and aspectRatio
    
    * updateValue funcrtional Renderer
    
    * triggerset updated
    
    * testList and textOrInline renderers
    
    * mathList Renderer

--- src/Viewer/renderers/triggerSet.jsx
+++ src/Viewer/renderers/triggerSet.jsx
@@ -1,19 +1,49 @@
 import React from 'react';
-import DoenetRenderer from './DoenetRenderer';
+import useDoenetRender from './useDoenetRenderer';
+import Button from '../../_reactComponents/PanelHeaderComponents/Button';
 
-export default class TriggerSet extends DoenetRenderer {
 
-  static initializeChildrenOnConstruction = false;
+export default function TriggerSet(props) {
+  let { name, SVs, actions, callAction } = useDoenetRender(props,false);
 
-  render() {
-
-    if (this.doenetSvData.hidden) {
-      return null;
-    }
-
-    return <span id={this.componentName}><a name={this.componentName} />
-    <button id={this.componentName + "_button"} onClick={this.actions.triggerActions} disabled={this.doenetSvData.disabled}>{this.doenetSvData.label}</button>
-    </span>;
-
+  if (SVs.hidden) {
+    return null;
   }
+
+  return <span id={name}><a name={name} />
+  <Button
+  id={name + "_button"} 
+  onClick={()=>callAction({ action:actions.triggerActions })} 
+  disabled={SVs.disabled}
+  value={SVs.label}
+  />
+    {/* <button 
+    id={name + "_button"} 
+    onClick={()=>callAction({ action:actions.triggerActions })} 
+    disabled={SVs.disabled}
+    >{SVs.label}
+    </button> */}
+    </span>;
 }
+
+
+
+// import React from 'react';
+// import DoenetRenderer from './DoenetRenderer';
+
+// export default class TriggerSet extends DoenetRenderer {
+
+//   static initializeChildrenOnConstruction = false;
+
+//   render() {
+
+//     if (this.doenetSvData.hidden) {
+//       return null;
+//     }
+
+//     return <span id={this.componentName}><a name={this.componentName} />
+//     <button id={this.componentName + "_button"} onClick={this.actions.triggerActions} disabled={this.doenetSvData.disabled}>{this.doenetSvData.label}</button>
+//     </span>;
+
+//   }
+// }
