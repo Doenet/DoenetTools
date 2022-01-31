d0eb40408faa95ccc20d903f5bc36a9fdbc53fd1
Kelton O'Brien <kelton@keltono.net>
2022-01-31 08:42:28 -0600

    Switch to new parser (#995)
    * move to new parser
    
    * grammar fixes
    
    * update parser/text editor dependencies
    
    * fix precedence in whitespace
    
    * minor parser fixes

--- src/Tools/temp/index.jsx
+++ src/Tools/temp/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {doenetMLToSerializedComponents} from  '../../Core/utils/serializedStateProcessing';
+import {parseAndCompile} from '../../Parser/parser'
 import { returnAllPossibleVariants } from '../../Core/utils/returnAllPossibleVariants';
 //import DateTime from '../../_reactComponents/PanelHeaderComponents/DateTime'
 import Card from '../../_reactComponents/PanelHeaderComponents/Card.jsx';
@@ -17,6 +19,22 @@ import Card from '../../_reactComponents/PanelHeaderComponents/Card.jsx';
 // function gotAll({ allPossibleVariants }) {
 //   console.log(`all possible variants`, allPossibleVariants)
 // }
+
+// let doenetML =  `<variantControl nVariants='5' variantNames='hello cat dog mouse' /> <copy assignNames="problem1" uri="doenet:conTentId=a666134b719e70e8acb48d91d582d1efd90d7f11fb499ab77f9f1fa5dafdb96d&DoenEtiD=abcdefg" />`
+ // let doenetML =  `<answer name=\"ans\"/>`
+ let doenetML =  `<answer name=\"$ans\"></answer>`
+   
+// serializeFunctions.expandDoenetMLsToFullSerializedComponents({
+//     contentIds: [],
+//     doenetMLs: [doenetML],
+//     callBack: args => finishReturnAllPossibleVariants(
+//       args,
+//       { callback, componentInfoObjects }),
+//     componentInfoObjects,
+//     componentTypeLowerCaseMapping,
+//     flags,
+//     contentIdsToDoenetMLs
+// })
 
 
 ReactDOM.render(
