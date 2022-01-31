68915d7cdf34c46716c15501d278edc1e4db8f4e
Kevin Charles <kevinmcharles@gmail.com>
2022-01-31 12:56:39 -0600

    CodeEditor and CodeViewer size updates

--- src/Viewer/renderers/codeViewer.jsx
+++ src/Viewer/renderers/codeViewer.jsx
@@ -1,26 +1,32 @@
 import React, { useState, useRef } from 'react';
 import useDoenetRenderer from './useDoenetRenderer';
 import { sizeToCSS } from './utils/css';
-import CodeMirror from '../../Tools/_framework/CodeMirror';
 
 export default function CodeViewer(props){
   let {name, SVs, children, actions, callAction} = useDoenetRenderer(props,false);
 
-
   if (SVs.hidden) {
     return null;
   }
-  let viewerMaxHeight = {...SVs.maxHeight};
-  viewerMaxHeight.size = viewerMaxHeight.size - 30;
+  let viewerHeight = {...SVs.height};
+  viewerHeight.size = viewerHeight.size - 30;
 
   let viewerWidth = {...SVs.width};
   viewerWidth.size = viewerWidth.size - 4;
   
+
+  let surroundingBoxStyle = {
+    width: sizeToCSS(SVs.width),
+  }
+
+  // console.log("SVs",SVs)
+  // if (something){
+  //   surroundingBoxStyle.border = "1px solid black";
+  // }
  
   let contentPanel = <div style={{
     width: sizeToCSS(SVs.width),
-    minHeight: sizeToCSS(SVs.minHeight),
-    maxHeight: sizeToCSS(SVs.maxHeight),
+    height: sizeToCSS(SVs.height),
     padding: "2px",
     // border: "1px solid black",
     // overflowY: "scroll"
@@ -31,14 +37,16 @@ height:"28px",
     <div style={{
       overflowY: "scroll",
       width: sizeToCSS(viewerWidth),
-    minHeight: sizeToCSS(SVs.minHeight),
-    maxHeight: sizeToCSS(viewerMaxHeight),
+      height: sizeToCSS(viewerHeight),
       }}>{children}</div>
   </div>
 
   return <>
   <a name={name} />
-  <div className="codeViewerSurroundingBox" id={name} >
+  <div 
+  style = {surroundingBoxStyle}
+  className="codeViewerSurroundingBox" 
+  id={name} >
   
     {contentPanel}
     
