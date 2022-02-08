2da9448aa233bf165887baeb32614c6871080491
Kevin Charles <kevinmcharles@gmail.com>
2022-02-08 16:32:09 -0600

    CID

--- src/Api/upload.php
+++ src/Api/upload.php
@@ -5,10 +5,13 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Credentials: true");
 header('Content-Type: application/json');
 // header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+// use Base32\Base32;
 
 include "db_connection.php";
 $jwtArray = include "jwtArray.php";
 $userId = $jwtArray['userId'];
+
+include "randomId.php";
 
 // echo "\n\nuserId $userId \n\n";
 
@@ -46,9 +49,8 @@ if ($type == 'image/jpeg'){
   $extension = '.csv';
 }
 // $contentId = hash('sha256', $dangerousContent);
-$contentId = 'testme';
-$newFileName = $contentId . $extension;
-$destination = $uploads_dir . $newFileName;
+
+$tmp_dest = $uploads_dir . 'tmp_' . $random_id . $extension;
 // $newfile = fopen($destination, "w") or die("Unable to open file!");
 //     $status = fwrite($newfile, $dangerousContent);
 //     if ($status === false){
@@ -56,7 +58,33 @@ $destination = $uploads_dir . $newFileName;
 //     }
 //     fclose($newfile);
 
-move_uploaded_file($tmp_name, $destination);
+move_uploaded_file($tmp_name, $tmp_dest);
+
+$SHA = hash_file("sha256",$tmp_dest);
+echo "\n SHA $SHA \n";
+
+$hashLen = '20'; //32 bit
+$algorithm = '12'; //18 in decimal
+// (eventually we want dag-pb)
+$multiHash =  $algorithm . $hashLen . $SHA; 
+$multiCodec = '00'; //raw binary
+$CIDversion = '01'; //2nd version of Multiformat (starts at 0) 
+$base = 'f'; //code https://github.com/multiformats/multibase/blob/master/multibase.csv
+
+$CID = $base . $CIDversion . $multiCodec . $multiHash; //hexadecimal string
+
+// $to_base32 = Base32::encode(hex2bin($CIDversion . $multiCodec . $multiHash)); 
+// $base = 'b';
+// $CID = $base . $to_base32;
+
+
+$contentId = $CID;
+$newFileName = $contentId . $extension;
+$destination = $uploads_dir . $newFileName;
+
+rename($tmp_dest,$destination);
+
+
 
 $sql = "
 INSERT INTO support_files 
@@ -65,9 +93,6 @@ VALUES
 ('$userId','$newFileName','$contentId','$doenetId','$type','$size',NOW())
 ";
 $result = $conn->query($sql);
-// echo $sql;
-
-
 
 // set response code - 200 OK
 http_response_code(200);
@@ -80,4 +105,3 @@ $response_arr = array("success" => $success,
 echo json_encode($response_arr);
 
 $conn->close();
-
