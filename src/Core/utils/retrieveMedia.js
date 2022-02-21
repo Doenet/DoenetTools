edddc5ffcbf35a76b9c8f0ca05eaad4f1d341973
Duane Nykamp <nykamp@umn.edu>
2022-02-21 16:38:32 -0600

    Update to fetch API for retrieving content

--- src/Core/utils/retrieveMedia.js
+++ src/Core/utils/retrieveMedia.js
@@ -22,102 +22,79 @@ export async function retrieveMediaForCID(CID) {
 }
 
 
-function retrieveMediaFromIPFS(CID) {
-
-  return new Promise((resolve, reject) => {
-
-    let timeoutId;
-
-    let request = new XMLHttpRequest();
-    request.open("GET", `https://${CID}.ipfs.dweb.link/`, true);
-
-    request.onload = async function () {
-
-      clearTimeout(timeoutId);
-
-      if (request.status === 200) {
-
-        let mediaBlob = request.response;
-
-        let CIDRetrieved = await CIDFromArrayBuffer(await mediaBlob.arrayBuffer());
+async function retrieveMediaFromIPFS(CID) {
 
 
+  let controller = new AbortController();
+  let signal = controller.signal;
 
-        if (CIDRetrieved === CID) {
-          let mediaURL = URL.createObjectURL(request.response);
-          resolve({ mediaBlob, mediaURL })
-        } else {
-          reject(new Error("CID mismatch"));
-        }
+  // If the IPFS gateway cannot find the CID,
+  // it hangs for a long time before timing out.
+  // To avoid the long wait, abort the request after 1 second.
+  let timeoutId = setTimeout(() => {
+    controller.abort();
+  }, 1000)
 
-        return;
+
+  try {
+    let response = await fetch(`https://${CID}.ipfs.dweb.link/`, { signal });
+
+    // if got a response, then we won't abort
+    clearTimeout(timeoutId);
+
+    if (response.ok) {
+      let mediaBlob = await response.blob();
+
+      let CIDRetrieved = await CIDFromArrayBuffer(await mediaBlob.arrayBuffer());
+
+      if (CIDRetrieved === CID) {
+        let mediaURL = URL.createObjectURL(mediaBlob);
+        return { mediaBlob, mediaURL };
+      } else {
+        return Promise.reject(new Error("CID mismatch"));
       }
-
-      // got a response other than success
-
-      reject(new Error(`CID not found: ${CID}`));
-
+    } else {
+      return Promise.reject(new Error(`CID not found: ${CID}`))
     }
 
-    request.responseType = "blob";
-    request.send();
-
-    // If the IPFS gateway cannot find the CID,
-    // it hangs for a long time before timing out.
-    // To avoid the long wait, stop the request and send failure after 1 second.
-    timeoutId = setTimeout(() => {
-      if (request.status === 0) {
-        request.abort();
-        reject(new Error(`CID not found: ${CID}`));
-      }
-    }, 1000)
-
-  })
+  }
+  catch (e) {
+    return Promise.reject(new Error(`CID not found: ${CID}`))
+  }
 
 
 }
 
 
-function retrieveMediaFromServer(CID, mimeType) {
+async function retrieveMediaFromServer(CID, mimeType) {
 
-  return new Promise((resolve, reject) => {
-
+  try {
     let extension = extensionFromMimeType(mimeType)
 
-    let request = new XMLHttpRequest();
-    request.open("GET", `/media/${CID}.${extension}`, true);
+    let response = await fetch(`/media/${CID}.${extension}`);
 
-    request.onload = async function () {
-      if (request.status === 200) {
+    if (response.ok) {
+      let mediaBlob = await response.blob();
 
-        let mediaBlob = request.response;
+      let CIDRetrieved = await CIDFromArrayBuffer(await mediaBlob.arrayBuffer());
 
-        let CIDRetrieved = await CIDFromArrayBuffer(await mediaBlob.arrayBuffer());
-
-        if (CIDRetrieved === CID) {
-          let mediaURL = URL.createObjectURL(request.response);
-          resolve({ mediaBlob, mediaURL })
-        } else {
-          reject(new Error("CID mismatch"));
-        }
-
-        return;
-
+      if (CIDRetrieved === CID) {
+        let mediaURL = URL.createObjectURL(mediaBlob);
+        return { mediaBlob, mediaURL };
+      } else {
+        return Promise.reject(new Error("CID mismatch"));
       }
-
-      // got a response other than success
-
-      reject(new Error(`CID not found: ${CID}`));
-
+    } else {
+      return Promise.reject(new Error(`CID not found: ${CID}`));
     }
-
-    request.responseType = "blob";
-    request.send();
-
-  })
-
+  }
+  catch (e) {
+    return Promise.reject(new Error(`CID not found: ${CID}`));
+  }
 
 }
+
+
 
 
 function extensionFromMimeType(mimeType) {
