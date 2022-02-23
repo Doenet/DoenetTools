bc9f754ea12caae22cc19b3483fe0cb491a92350
Keagan Eng <eng00029@umn.edu>
2022-02-23 09:59:45 -0600

    replaced icon

--- src/_reactComponents/PanelHeaderComponents/CollapseSection.jsx
+++ src/_reactComponents/PanelHeaderComponents/CollapseSection.jsx
@@ -41,46 +41,46 @@ There are no preset action icons, you must implement these in the parent compone
 
 */
 
-import React, {
-  useState
-} from "react";
+import React, { useState } from "react";
 import Styled from "styled-components";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 
 const Section = Styled.div `
-transition: height .25s;
-border-radius: .5em;
+  transition: height .25s;
+  border-radius: .5em;
 `;
 
 const SectionHeader = Styled.div `
-font-weight: bold;
-height: 1.5em;
-width: 100%;
-line-height: 1.5em;
-user-select: none;
-cursor: pointer;
-overflow: auto;
+  font-weight: bold;
+  height: 1.5em;
+  width: 100%;
+  line-height: 1.5em;
+  user-select: none;
+  cursor: pointer;
+  overflow: auto;
 `;
 
-const ArrowIcon = Styled.span `
-display: inline-block;
-margin: 0em .5em;
-transition: transform .25s;
+const CaretIcon = Styled.span `
+  display: inline-block;
+  margin: 0em .5em;
+  transition: transform .25s;
 `;
 
-const HeaderIconContainer = Styled.div `
-float: right;
-margin-right: .5em;
-`;
+// const HeaderIconContainer = Styled.div `
+//   float: right;
+//   margin-right: .5em;
+// `;
 
 const SectionContent = Styled.div `
-padding: 1em;
-border-radius: 0 0 .5em .5em;
+  padding: 1em;
+  border-radius: 0 0 .5em .5em;
 `;
 
 export default function CollapseSection(props) {
   const [collapsed, setCollapsed] = useState(Boolean(props.collapsed));
 
-  const arrowStyle = collapsed ? {} : {transform: "rotate(90deg)"};
+  // const arrowStyle = collapsed ? {} : {transform: "rotate(90deg)"};
   let contentStyle = collapsed ? {display: "none"} : {display: "block"};
   contentStyle.backgroundColor = props.bodyBackground || "#f8f8f8";
   let headerStyle = collapsed ? {borderRadius: ".5em"} : {borderRadius: ".5em .5em 0 0"};
@@ -89,13 +89,13 @@ export default function CollapseSection(props) {
 
   const sectionStyle = {width: props.widthCSS || "240px"};
 
-  if (props.iconElements !== undefined) {
-      var iconElements = props.iconElements.map((iconEl, index) => (
-          <HeaderIconContainer key={"aaaaa" + index}>
-              {iconEl}
-          </HeaderIconContainer>
-      ));
-  }
+  // if (props.iconElements !== undefined) {
+  //     var iconElements = props.iconElements.map((iconEl, index) => (
+  //         <HeaderIconContainer key={"aaaaa" + index}>
+  //             {iconEl}
+  //         </HeaderIconContainer>
+  //     ));
+  // }
 
   return (
       <Section style={sectionStyle}>
@@ -103,11 +103,12 @@ export default function CollapseSection(props) {
               style={headerStyle}
               onClick = {() => {setCollapsed(!collapsed);}}
           >
-              <ArrowIcon style={arrowStyle}>
+              {/* <ArrowIcon style={arrowStyle}>
                   &gt;
-              </ArrowIcon>
+              </ArrowIcon> */}
+              <FontAwesomeIcon icon={faCaretDown} style={CaretIcon}/>
               {props.title ? String(props.title) : "Untitled Section"}
-              {iconElements}
+              {/* {iconElements} */}
           </SectionHeader>
           <SectionContent style={contentStyle}>
               {props.children}
