1ef997eb994d87d6c86335cae706e5098fb5f8f5
Parth Sarathi Verma <parthsarathiverma@gmail.com>
2022-03-06 13:30:59 -0600

    Added checkboxes to enrollment, current content, and Sign in

--- src/Tools/_framework/Menus/CurrentContent.jsx
+++ src/Tools/_framework/Menus/CurrentContent.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
 // import Button from '../../../_reactComponents/PanelHeaderComponents/Button';
 import { showCompletedAtom, showOverdueAtom } from '../Widgets/Next7Days';
-import Switch from '../Switch';
+import Checkbox from '../../../_reactComponents/PanelHeaderComponents/Checkbox';
 import { useRecoilState } from 'recoil';
 
-export default function CurrentContent(){
-  const [overdue,setOverdue] = useRecoilState(showOverdueAtom);
-  const [completed,setCompleted] = useRecoilState(showCompletedAtom);
+export default function CurrentContent() {
+  const [overdue, setOverdue] = useRecoilState(showOverdueAtom);
+  const [completed, setCompleted] = useRecoilState(showCompletedAtom);
 
-  return <div>
-    Show Completed <Switch checked={completed} onChange={(e)=>{
-      setCompleted(e.target.checked);
-      }} />
-    Show Overdue <Switch checked={overdue} onChange={(e)=>{
-      setOverdue(e.target.checked);
-    }} />
-  </div>
+  return (
+    <div>
+      <div>
+        <Checkbox
+          style={{ marginRight: '2px' }}
+          checked={completed}
+          onClick={(e) => {
+            setCompleted(!completed);
+          }}
+        />
+        Show Completed{' '}
+      </div>
+      <div>
+        <Checkbox
+          style={{ marginRight: '2px' }}
+          checked={overdue}
+          onClick={(e) => {
+            setOverdue(!overdue);
+          }}
+        />
+        Show Overdue{' '}
+      </div>
+    </div>
+  );
 }
