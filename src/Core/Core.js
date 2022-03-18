a1d7b370e7487c081d06016158f95b3ccc74ebd4
Duane Nykamp <nykamp@umn.edu>
2022-03-17 20:13:05 -0500

    animateFromSequence works with core webworker

--- src/Core/Core.js
+++ src/Core/Core.js
@@ -55,7 +55,6 @@ export default class Core {
     this.triggerChainedActions = this.triggerChainedActions.bind(this);
     this.requestRecordEvent = this.requestRecordEvent.bind(this);
     this.requestAnimationFrame = this.requestAnimationFrame.bind(this);
-    this._requestAnimationFrame = this._requestAnimationFrame.bind(this);
     this.cancelAnimationFrame = this.cancelAnimationFrame.bind(this);
     this.calculateScoredItemNumberOfContainer = this.calculateScoredItemNumberOfContainer.bind(this);
 
@@ -101,8 +100,6 @@ export default class Core {
     this.cumulativeStateVariableChanges = JSON.parse(JSON.stringify(stateVariableChanges, serializeFunctions.serializedComponentsReplacer), serializeFunctions.serializedComponentsReviver);
 
 
-    this.animationIDs = {};
-    this.lastAnimationID = 0;
     this.requestedVariantIndex = requestedVariantIndex;
     this.requestedVariant = requestedVariant;
     if (!this.requestedVariant) {
@@ -9540,58 +9537,19 @@ export default class Core {
     ))();
   }
 
-  requestAnimationFrame(animationFunction, delay) {
-    if (!this.preventMoreAnimations) {
-
-      // // create new animationID
-      // let animationID = ++this.lastAnimationID;
-
-      // if (delay) {
-      //   // set a time out to call actual request animation frame after a delay
-      //   let timeoutID = window.setTimeout(
-      //     x => this._requestAnimationFrame(animationFunction, animationID),
-      //     delay);
-      //   this.animationIDs[animationID] = { timeoutID: timeoutID };
-      //   return animationID;
-      // } else {
-      //   // call actual request animation frame right away
-      //   this.animationIDs[animationID] = {};
-      //   return this._requestAnimationFrame(animationFunction, animationID);
-      // }
-    }
+  requestAnimationFrame(args) {
+    postMessage({
+      messageType: "requestAnimationFrame",
+      args
+    });
   }
 
-  _requestAnimationFrame(animationFunction, animationID) {
-    // let animationFrameID = window.requestAnimationFrame(animationFunction);
-    // let animationIDObj = this.animationIDs[animationID];
-    // delete animationIDObj.timeoutID;
-    // animationIDObj.animationFrameID = animationFrameID;
-    // return animationID;
+  cancelAnimationFrame(args) {
+    postMessage({
+      messageType: "cancelAnimationFrame",
+      args
+    });
   }
-
-
-  async cancelAnimationFrame(animationID) {
-    // let animationIDObj = this.animationIDs[animationID];
-    // let timeoutID = animationIDObj.timeoutID;
-    // if (timeoutID !== undefined) {
-    //   window.clearTimeout(timeoutID);
-    // }
-    // let animationFrameID = animationIDObj.animationFrameID;
-    // if (animationFrameID !== undefined) {
-    //   window.cancelAnimationFrame(animationFrameID);
-    // }
-    // delete this.animationIDs[animationID];
-
-  }
-
-  componentWillUnmount() {
-    this.preventMoreAnimations = true;
-    for (let id in this.animationIDs) {
-      this.coreFunctions.cancelAnimationFrame(id);
-    }
-    this.animationIDs = {};
-  }
-
 }
 
 
