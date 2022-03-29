ed405ef403ea7bf1ca5d1ac23c252b615e87501e
Kevin Charles <kevinmcharles@gmail.com>
2022-03-29 11:20:40 -0500

    Get pages with activity

--- src/Api/getCourseItems.php
+++ src/Api/getCourseItems.php
@@ -35,6 +35,7 @@ if ($result->num_rows > 0) {
 	$row = $result->fetch_assoc();
 	$canEditContent = $row['canEditContent'];
 }
+$activityDoenetIds = [];
 	//Can the user edit content?
 	//Yes then all items and json
 	//No then no Recipies, no banks and no unused files
@@ -72,6 +73,10 @@ if ($result->num_rows > 0) {
 					"isGloballyAssigned"=>$row['isGloballyAssigned'],
 					"isPublic"=>$row['isPublic'],
 				);
+
+				if ($row['contentType'] == 'activity'){
+					array_push($activityDoenetIds,$row['doenetId']);
+				}
 				
 				$json = json_decode($row['json'],true);
 				// var_dump($json);
@@ -83,10 +88,37 @@ if ($result->num_rows > 0) {
 
 				array_push($items,$item);
 			}
+			foreach($activityDoenetIds as $aDoenetId){
+				$sql = "
+				SELECT 
+				doenetId,
+				cid,
+				draftCid
+				FROM activity_pages
+				WHERE activityDoenetId = '$aDoenetId'
+				AND isDeleted = '0'
+				";
+				$result = $conn->query($sql);
+				if ($result->num_rows > 0) {
+					while($row = $result->fetch_assoc()){
+						$item = array(
+							"contentType"=>"page",
+							"doenetId"=>$row['doenetId'],
+							"cid"=>$row['cid'],
+							"draftCid"=>$row['draftCid'],
+							"label"=>$row['label']
+						);
+						$item['isSelected'] = false;
+						array_push($items,$item);
+
+					}
+				}
+
+			}
 		}
 
 	}else{
-
+		//TODO: student can't edit version
 	}
 }
 
