384957d151d47a92a47945550aae828ea9457fcd
ernzher <leeernzher@gmail.com>
2022-04-03 00:40:10 -0500

    Revert "Revert "Fix: text and layout of menu panel caps""
    This reverts commit 6f8c441ff966affe6132a60b97e243e2dc55fab4.

--- src/Tools/_framework/MenuPanelCaps/DriveInfoCap.jsx
+++ src/Tools/_framework/MenuPanelCaps/DriveInfoCap.jsx
@@ -38,10 +38,10 @@ export default function DriveInfoCap(){
     <div style={{ position: "relative", width: "100%", height: "135px", overflow: "hidden"}}>
       <img src={image} style={{ position: "absolute", width: "100%", top: "50%", transform: "translateY(-50%)" }}  />
     </div>
-    <div style={{padding:'8px'}}>
-    <div>{label}</div>
-    <div>{roles}</div>
-    <div><RoleDropdown /></div>
+    <div style={{ padding:'16px 12px' }}>
+      <span style={{ marginBottom: "15px" }}>{label}</span> <br />
+      <span style={{ marginBottom: "15px" }}>{roles}</span> <br />
+      <RoleDropdown />
     </div>
   </>
 }
