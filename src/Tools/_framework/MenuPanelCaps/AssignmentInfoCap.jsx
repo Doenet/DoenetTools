6523700adaf70d604aeaf4465525bff36932111b
Emilio Alvarez <e.l.alvarez@icloud.com>
2022-04-11 12:44:06 -0500

    assignment infocap

--- src/Tools/_framework/MenuPanelCaps/AssignmentInfoCap.jsx
+++ src/Tools/_framework/MenuPanelCaps/AssignmentInfoCap.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { useRecoilValue } from 'recoil';
-import { folderDictionary, fetchCoursesQuery, loadAssignmentSelector } from '../../../_reactComponents/Drive/NewDrive';
+import { loadAssignmentSelector } from '../../../_reactComponents/Drive/NewDrive';
 import { searchParamAtomFamily, pageToolViewAtom } from '../NewToolRoot';
 import axios from 'axios';
 import { currentAttemptNumber } from '../ToolPanels/AssignmentViewer';
+import { authorItemByDoenetId, useCourse } from '../../../_reactComponents/Course/CourseActions';
 
 
 export default function AssignmentInfoCap(){
@@ -11,62 +12,31 @@ export default function AssignmentInfoCap(){
   let { page } = useRecoilValue(pageToolViewAtom);
   const { numberOfAttemptsAllowed } = useRecoilValue(loadAssignmentSelector(doenetId));
   const recoilAttemptNumber = useRecoilValue(currentAttemptNumber);
-
-  let [driveId,setDriveId] = useState("");
-  let [folderId,setFolderId] = useState("");
-  let [doenetIdLabel,setDoenetIdLabel] = useState("");
+  
+  let [courseId,setCourseId] = useState("");
+  let [contentLabel,setContentLabel] = useState("");
+  const { image, label: courseLabel}  = useCourse(courseId);
+  const {label: itemLabel} = useRecoilValue(authorItemByDoenetId(doenetId));
 
   useEffect(()=>{
-    axios.get('/api/findDriveIdFolderId.php', {
+    axios.get('/api/findCourseIdAndParentDoenetId.php', {
       params: { doenetId },
     }).then((resp)=>{
-      setDriveId(resp.data.driveId);
-      setFolderId(resp.data.parentFolderId)
+      setCourseId(resp.data.courseId);
     })
     if (page === 'exam'){
   axios.get('/api/getExamLabel.php', {
       params: { doenetId },
     }).then((resp)=>{
-      setDoenetIdLabel(resp.data.label);
+      setContentLabel(resp.data.label);
     })
+    } else if (page === 'course') {
+      setContentLabel(itemLabel)
     }
    
 
-  },[doenetId])
-  const driveInfo = useRecoilValue(fetchCoursesQuery)
+  },[doenetId, itemLabel, page])
 
-  let contentLabel = '';
-
-  if (page === 'course'){
-  let folderInfo = useRecoilValue(folderDictionary({driveId,folderId}))
-  const docInfo = folderInfo?.contentsDictionaryByDoenetId?.[doenetId]
-  contentLabel = docInfo?.label;
-  }else if (page === 'exam'){
-
-    contentLabel = doenetIdLabel
-  }
-  // if (contentLabel)
-  // if (!docInfo){ return null;}
-
-  let image;
-  let color;
-  let driveLabel = "";
- for (let info of driveInfo.driveIdsAndLabels){
-   if (info.driveId === driveId){
-     image = info.image;
-     color = info.color;
-     driveLabel = info.label;
-     break;
-   }
- }
-
- if (image != 'none'){
-  image = '/media/drive_pictures/' + image;
- }
- if (color != 'none'){
-  color = '#' + color;
- }
-//  let imageURL = `/media/drive_pictures/${image}`
  let attemptsAllowedDescription = numberOfAttemptsAllowed;
  if (!numberOfAttemptsAllowed){
   attemptsAllowedDescription = "Unlimited";
@@ -76,15 +46,16 @@ export default function AssignmentInfoCap(){
  if (recoilAttemptNumber){
   attemptInfo = <div>{recoilAttemptNumber} out of {attemptsAllowedDescription}</div>
  }
-
-  return <div>
+  //TODO: image and color defaults
+  return (
+  <div>
     <div style={{ position: "relative", width: "100%", height: "135px", overflow: "hidden"}}>
-      <img src={image} style={{ position: "absolute", width: "100%", top: "50%", transform: "translateY(-50%)" }}  />
+      <img src={`/media/drive_pictures/${image ?? ''}`} style={{ position: "absolute", width: "100%", top: "50%", transform: "translateY(-50%)" }} alt="" />
     </div>
     <div style={{ padding:'16px 12px' }}>
-      <span style={{ marginBottom: "15px" }}>{driveLabel}</span> <br />
+      <span style={{ marginBottom: "15px" }}>{courseLabel ?? ''}</span> <br />
       <span style={{ marginBottom: "15px" }}>{contentLabel}</span> <br />
       <span>{ attemptInfo }</span>
     </div>
-  </div>
+  </div>)
 }
