bab722715375c60610c2ae01f5cc0b77d9d46294
Parth Sarathi Verma <parthsarathiverma@gmail.com>
2022-04-18 13:19:16 -0500

    style

--- src/Viewer/renderers/feedback.jsx
+++ src/Viewer/renderers/feedback.jsx
@@ -3,24 +3,49 @@ import useDoenetRender from './useDoenetRenderer';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faComment as thoughtBubble } from '@fortawesome/free-regular-svg-icons';
 
+import { doenetLightGray } from '../../_reactComponents/PanelHeaderComponents/theme';
+
 export default function Feedback(props) {
-  let {name, SVs, children} = useDoenetRender(props);
+  let { name, SVs, children } = useDoenetRender(props);
 
   if (SVs.hidden) {
     return null;
   }
-  
-  let icon = <FontAwesomeIcon icon={thoughtBubble} />
 
-    return <>
-     <span style={{ display: "block", margin: "0px 4px 0px 4px", padding: "6px", border: "1px solid #C9C9C9", backgroundColor: "#ebebeb" }} >
-       {icon} Feedback
-        </span>
-    <aside id={name} style={{ backgroundColor: "white", margin: "0px 4px 0px 4px", padding: "1em", border: "1px solid #C9C9C9"}} >
-      <a name={name} />
-     
-      {SVs.feedbackText}
-      {children}
-    </aside>
+  let icon = <FontAwesomeIcon icon={thoughtBubble} />;
+
+  return (
+    <>
+      <span
+        style={{
+          display: 'block',
+          margin: '12px 4px 0px 4px',
+          padding: '6px',
+          border: '2px solid black',
+          borderTopLeftRadius: '5px',
+          borderTopRightRadius: '5px',
+          backgroundColor: `${doenetLightGray}`,
+        }}
+      >
+        {icon} Feedback
+      </span>
+      <aside
+        id={name}
+        style={{
+          backgroundColor: 'white',
+          margin: '0px 4px 12px 4px',
+          padding: '1em',
+          border: '2px solid black',
+          borderTop: '0px',
+          borderBottomLeftRadius: '5px',
+          borderBottomRightRadius: '5px',
+        }}
+      >
+        <a name={name} />
+
+        {SVs.feedbackText}
+        {children}
+      </aside>
     </>
+  );
 }
--- src/Viewer/renderers/hint.jsx
+++ src/Viewer/renderers/hint.jsx
@@ -5,12 +5,11 @@ import { faLightbulb as lightOff } from '@fortawesome/free-solid-svg-icons';
 import { faLightbulb as lightOn } from '@fortawesome/free-regular-svg-icons';
 import { faCaretRight as twirlIsClosed } from '@fortawesome/free-solid-svg-icons';
 import { faCaretDown as twirlIsOpen } from '@fortawesome/free-solid-svg-icons';
-
-
+import { doenetLightGray } from '../../_reactComponents/PanelHeaderComponents/theme';
 
 export default function Hint(props) {
   let { name, SVs, children, actions, callAction } = useDoenetRender(props);
-  
+
   if (!SVs.showHints) {
     return null;
   }
@@ -19,48 +18,66 @@ export default function Hint(props) {
 
   let title = SVs.title;
   if (SVs.titleDefinedByChildren) {
-    title = children[0]
+    title = children[0];
     //BADBADBAD
     childrenToRender = children.slice(1); // remove title
-  } 
-
-  let twirlIcon = <FontAwesomeIcon icon={twirlIsClosed} />
-  let icon = <FontAwesomeIcon icon={lightOff} />
-  let info = null;
-  let infoBlockStyle = { display: "none" };
-  let onClickFunction = ()=>{
-      callAction({
-        action: actions.revealHint
-      });
   }
 
+  let twirlIcon = <FontAwesomeIcon icon={twirlIsClosed} />;
+  let icon = <FontAwesomeIcon icon={lightOff} />;
+  let info = null;
+  let infoBlockStyle = { display: 'none' };
+  let onClickFunction = () => {
+    callAction({
+      action: actions.revealHint,
+    });
+  };
 
-  let openCloseText = "open";
+  let openCloseText = 'open';
 
   if (SVs.open) {
-    twirlIcon = <FontAwesomeIcon icon={twirlIsOpen} />
-    openCloseText = "close";
-    icon = <FontAwesomeIcon icon={lightOn} />
+    twirlIcon = <FontAwesomeIcon icon={twirlIsOpen} />;
+    openCloseText = 'close';
+    icon = <FontAwesomeIcon icon={lightOn} />;
     info = childrenToRender;
     infoBlockStyle = {
-      display: "block", margin: "0px 4px 4px 4px", padding: "6px", border: "1px solid #C9C9C9", backgroundColor: "#fcfcfc",
+      display: 'block',
+      margin: '0px 4px 12px 4px',
+      padding: '6px',
+      border: '2px solid black',
+      borderTop: '0px',
+      borderBottomLeftRadius: '5px',
+      borderBottomRightRadius: '5px',
+      backgroundColor: '#fcfcfc',
     };
-    onClickFunction = ()=>{
+    onClickFunction = () => {
       callAction({
-        action: actions.closeHint
+        action: actions.closeHint,
       });
-    }
-    
-  } 
+    };
+  }
 
-  return <aside id={name} key={name}>
-    <a name={name} />
-    <span style={{ display: "block", margin: "4px 4px 0px 4px", padding: "6px", border: "1px solid #C9C9C9", backgroundColor: "#ebebeb", cursor: "pointer" }} onClick={onClickFunction}>
-     {twirlIcon} {icon} {title} (click to {openCloseText})
+  return (
+    <aside id={name} key={name}>
+      <a name={name} />
+      <span
+        style={{
+          display: 'block',
+          margin: SVs.open ? '12px 4px 0px 4px' : '12px 4px 12px 4px',
+          padding: '6px',
+          border: '2px solid black',
+          borderTopLeftRadius: '5px',
+          borderTopRightRadius: '5px',
+          borderBottomLeftRadius: SVs.open ? '0px' : '5px',
+          borderBottomRightRadius: SVs.open ? '0px' : '5px',
+          backgroundColor: `${doenetLightGray}`,
+          cursor: 'pointer',
+        }}
+        onClick={onClickFunction}
+      >
+        {twirlIcon} {icon} {title} (click to {openCloseText})
       </span>
-    <span style={infoBlockStyle}>
-      {info}
-    </span>
-  </aside>
-
+      <span style={infoBlockStyle}>{info}</span>
+    </aside>
+  );
 }
