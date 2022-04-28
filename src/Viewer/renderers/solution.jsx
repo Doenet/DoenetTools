f13e8833916cdcbca2a1088a5eedb928adfc932f
Parth Sarathi Verma <parthsarathiverma@gmail.com>
2022-04-28 18:20:37 -0500

    solution

---
+++ src/Viewer/renderers/solution.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import useDoenetRender from './useDoenetRenderer';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPuzzlePiece as puzzle } from '@fortawesome/free-solid-svg-icons';
+
+export default function Solution(props) {
+  let { name, SVs, children, actions, callAction } = useDoenetRender(props);
+
+  if (SVs.hidden) {
+    return null;
+  }
+
+  let icon;
+  let childrenToRender = null;
+  let infoBlockStyle = { display: 'none' };
+
+  let onClickFunction;
+  let cursorStyle;
+
+  if (SVs.open) {
+    icon = <FontAwesomeIcon icon={puzzle} />;
+
+    childrenToRender = children;
+    infoBlockStyle = {
+      display: 'block',
+      margin: '0px 4px 4px 4px',
+      padding: '6px',
+      border: '1px solid #ebebeb',
+      backgroundColor: '#fcfcfc',
+    };
+
+    if (SVs.canBeClosed) {
+      cursorStyle = 'pointer';
+      onClickFunction = () => {
+        callAction({
+          action: actions.closeSolution,
+        });
+      };
+    } else {
+      onClickFunction = () => {};
+    }
+  } else {
+    icon = <FontAwesomeIcon icon={puzzle} rotation={90} />;
+    cursorStyle = 'pointer';
+    onClickFunction = () => {
+      callAction({
+        action: actions.revealSolution,
+      });
+    };
+  }
+
+  return (
+    <aside id={name}>
+      <a name={name} />
+      <span
+        id={name + '_button'}
+        style={{
+          display: 'block',
+          margin: '4px 4px 0px 4px',
+          padding: '6px',
+          border: '1px solid #ebebeb',
+          backgroundColor: '#ebebeb',
+          cursor: cursorStyle,
+        }}
+        onClick={onClickFunction}
+      >
+        {icon} Solution {SVs.message}
+      </span>
+      <span style={infoBlockStyle}>{childrenToRender}</span>
+    </aside>
+  );
+}
