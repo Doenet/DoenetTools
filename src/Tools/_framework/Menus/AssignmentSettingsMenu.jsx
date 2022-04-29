3bcf9581fe7c8ac0b0c8482ede9104072d5c8096
Emilio Alvarez <e.l.alvarez@icloud.com>
2022-04-29 13:23:31 -0500

    assignment breadcrumb and settings
    using effectiveRole instead of role

--- src/Tools/_framework/Menus/AssignmentSettingsMenu.jsx
+++ src/Tools/_framework/Menus/AssignmentSettingsMenu.jsx
@@ -3,9 +3,11 @@ import { useRecoilValue } from 'recoil';
 import { AssignmentSettings } from './SelectedActivity';
 import { searchParamAtomFamily } from '../NewToolRoot';
 
-export default function AssignmentSettingsMenu(){
+export default function AssignmentSettingsMenu() {
   const doenetId = useRecoilValue(searchParamAtomFamily('doenetId'));
-  return <div style={{paddingTop:"6px",paddingBottom:"6px"}}>
-    <AssignmentSettings role='instructor' doenetId={doenetId} />
-  </div>
+  return (
+    <div style={{ paddingTop: '6px', paddingBottom: '6px' }}>
+      <AssignmentSettings effectiveRole="instructor" doenetId={doenetId} />
+    </div>
+  );
 }
