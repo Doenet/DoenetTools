826bd9b19954141b5a00b148f63465a67c34f2e4
ernzher <leeernzher@gmail.com>
2022-05-06 21:03:35 -0500

    Refactor: button component

--- src/_reactComponents/PanelHeaderComponents/Button.jsx
+++ src/_reactComponents/PanelHeaderComponents/Button.jsx
@@ -1,54 +1,50 @@
 import React, { useState } from 'react';
 import styled from "styled-components";
 
-const ButtonStyling = styled.button`
-  margin: ${props => props.theme.margin};
+const StyledButton = styled.button`
+  margin: 0;
   height: 24px;
   border-style: hidden;
-  // border-color: black;
-  // border-width: 2px;
-  color: white;
-  background-color: ${props => props.alert ? 'var(--mainRed)' : 'var(--mainBlue)'};
-  border-radius: ${props => props.theme.borderRadius};
-  padding: ${props => props.theme.padding};
-  cursor: pointer;
+  color: ${props => props.disabled ? 'black' : 'white'};
+  background-color: ${props => props.alert ? 'var(--mainRed)' : props.disabled ? 'var(--mainGray)' : 'var(--mainBlue)'};
+  border-radius: 'var(--mainBorderRadius)';
+  padding: '0 10px';
+  cursor: ${props => props.disabled ? 'not-allowed' : 'pointer'};
   font-size: 12px;
   border-radius: 20px;
-
+  width: ${props => props.width === "menu" ? '100%' : props.width ? props.width : 'auto'};
   &:hover {
-    background-color: ${props => props.alert ? 'var(--lightRed)' : 'var(--lightBlue)'};
+    background-color: ${props => props.alert ? 'var(--lightRed)' : props.disabled ? 'var(--mainGray)' : 'var(--lightBlue)'};
     color: black;
   };
 `;
 
-ButtonStyling.defaultProps = {
-  theme: {
-    margin: 0,
-    borderRadius: 'var(--mainBorderRadius)',
-    padding: '0 10px'
-  }
-};
-
-const Label = styled.p`
+const Label = styled.span`
   font-size: 14px;
-  display: ${props => props.labelVisible};
   margin-right: 5px;
-  margin-bottom: ${props => props.align == 'flex' ? 'none' : '2px'};
 `;
 
 const Container = styled.div`
-  display: ${ props => props.align ? props.align : 'inline-block'};
-  width: auto;
-  align-items: center;
+  display: ${ props => props.label ? 'flex' : 'block'};
+  flex-direction: ${ props => props.label && props.vertical && 'column' };
+  align-items: ${props => props.label && !props.vertical && 'center'};
 `;
+
+// ButtonStyling.defaultProps = {
+//   theme: {
+//     margin: 0,
+//     borderRadius: 'var(--mainBorderRadius)',
+//     padding: '0 10px'
+//   }
+// };
 
 export default function Button(props) {
   //Assume small
-  var container = {};
-  var align = 'flex';
-  var button = {
-    value: 'Button',
-  };
+  // var container = {};
+  // var align = 'flex';
+  // var button = {
+  //   value: 'Button',
+  // };
   // var button = {
   //       margin: '0px',
   //       height: '24px',
@@ -62,64 +58,73 @@ export default function Button(props) {
   //       cursor: 'pointer',
   //       fontSize: '12px'
   //     };
-  if (props.width) {
-    if (props.width === "menu") {
-      button.width = '100%';
-      if (props.label) {
-        container.width = 'menu';
-        button.width = '100%';
-      }
-    } 
-  };
+  // if (props.width) {
+  //   if (props.width === "menu") {
+  //     button.width = '100%';
+  //     if (props.label) {
+  //       container.width = 'menu';
+  //       button.width = '100%';
+  //     }
+  //   } 
+  // };
 
-  const [labelVisible, setLabelVisible] = useState(props.label ? 'static' : 'none');
-  var label = '';
-  if (props.label) {
-    label = props.label;
-    if (props.vertical) {
-      align = 'static';
-    }
-  };
+  // const [labelVisible, setLabelVisible] = useState(props.label ? 'static' : 'none');
+  // var label = '';
+  // if (props.label) {
+  //   label = props.label;
+  //   if (props.vertical) {
+  //     align = 'static';
+  //   }
+  // };
 
-  var icon = '';
-  if (props.value || props.icon){
-    if (props.value && props.icon){
-        icon = props.icon;
-        button.value = props.value
-    }
-    else if (props.value){
-        button.value = props.value
-    }
-    else if (props.icon){
-        icon = props.icon;
-        button.value = ''
-    }
-  };
+  // var icon = '';
+  // if (props.value || props.icon){
+  //   if (props.value && props.icon){
+  //       icon = props.icon;
+  //       button.value = props.value
+  //   }
+  //   else if (props.value){
+  //       button.value = props.value
+  //   }
+  //   else if (props.icon){
+  //       icon = props.icon;
+  //       button.value = ''
+  //   }
+  // };
 
   // if (props.alert) {
   //   button.backgroundColor = 'var(--mainRed)'
   // }
 
-  if (props.disabled) {
-    button.backgroundColor = 'var(--mainGray)';
-    button.color = 'black';
-    button.cursor = 'not-allowed';
-  };
+  // if (props.disabled) {
+  //   button.backgroundColor = 'var(--mainGray)';
+  //   button.color = 'black';
+  //   button.cursor = 'not-allowed';
+  // };
 
   // if (props.value) {
   //     button.value = props.value;
   // };
+ 
+  // function handleClick(e) {
+  //   if (props.onClick) props.onClick(e)
+  // };
 
-  function handleClick(e) {
-    if (props.onClick) props.onClick(e)
-  };
-
-    return (
-        <>
-            <Container style={container} align={align}>
-              <Label labelVisible={labelVisible} align={align}>{label}</Label>
-              <ButtonStyling style={button} {...props} onClick={(e) => { handleClick(e) }}>{icon}{' '}{button.value}</ButtonStyling>
-            </Container>
-        </>
-    )
+  return (
+    <Container 
+      label={props.label} 
+      vertical={props.vertical} 
+    >
+      { props.label && <Label>{props.label}</Label> }
+      <StyledButton 
+        disabled={props.disabled} 
+        alert={props.alert} 
+        width={props.width} 
+        onClick={(e) => props.onClick && props.onClick(e)}
+      >
+        {props.icon && props.icon}  
+        {(props.icon ? " " : "" ) + (props.value ? props.value : "Button")} 
+      </StyledButton>
+    </Container>
+  )
 };
