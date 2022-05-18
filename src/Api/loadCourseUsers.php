9c5373de2abb2fa6e9d40d9cd58a8e9d4e17d046
Emilio Alvarez <e.l.alvarez@icloud.com>
2022-05-18 18:29:22 +0200

    load Course Users

---
+++ src/Api/loadCourseUsers.php
@@ -0,0 +1,76 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: access');
+header('Access-Control-Allow-Methods: GET');
+header('Access-Control-Allow-Credentials: true');
+header('Content-Type: application/json');
+
+include 'db_connection.php';
+include 'permissionsAndSettingsForOneCourseFunction.php';
+
+$jwtArray = include 'jwtArray.php';
+$userId = $jwtArray['userId'];
+
+$success = true;
+$message = '';
+
+$courseId = mysqli_real_escape_string($conn, $_REQUEST['courseId']);
+if ($courseId == '') {
+    $success = false;
+    $message = 'Internal Error: missing courseId';
+}
+
+$permissions = permissionsAndSettingsForOneCourseFunction(
+    $conn,
+    $userId,
+    $courseId
+);
+
+if ($permissions['canViewUsers'] != '1') {
+    $success = false;
+    $message = "You need permission to view a course's users";
+}
+
+if ($success) {
+    $sql = "SELECT 
+      CAST(cu.roleLabels as CHAR) as roles,
+      u.screenName AS screenName,
+      u.email AS email,
+      u.userId AS userId
+      FROM course_user AS cu
+      LEFT JOIN user AS u
+      ON cu.userId = u.userId
+      WHERE cu.courseId = '$courseId' 
+    ";
+    $result = $conn->query($sql);
+
+    $users = [];
+    if ($result == false) {
+        $success = false;
+        $message = 'server error';
+    } elseif ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            array_push($users, [
+                'email' => $row['email'],
+                'screenName' => $row['screenName'],
+                'isUser' => $row['userId'] == $userId,
+                'roles' => json_decode($row['roles']),
+            ]);
+        }
+    }
+}
+
+$response_arr = [
+    'success' => $success,
+    'users' => $users,
+    'message' => $message,
+];
+
+// set response code - 200 OK
+http_response_code(200);
+
+// make it json format
+echo json_encode($response_arr);
+$conn->close();
+
+?>
