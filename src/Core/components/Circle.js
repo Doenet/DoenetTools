f9246f3788741529dfbea6820593b9282a80d3e5
Duane Nykamp <nykamp@umn.edu>
2022-06-02 23:15:13 -0400

    constraints can handle 1D point (#1341)

--- src/Core/components/Circle.js
+++ src/Core/components/Circle.js
@@ -2108,8 +2108,8 @@ export default class Circle extends Curve {
           setValue: {
             nearestPoint: function ({ variables, scales }) {
 
-              let x1 = variables.x1.evaluate_to_constant();
-              let x2 = variables.x2.evaluate_to_constant();
+              let x1 = variables.x1?.evaluate_to_constant();
+              let x2 = variables.x2?.evaluate_to_constant();
 
               if (!(Number.isFinite(x1) && Number.isFinite(x2))) {
                 return {};
