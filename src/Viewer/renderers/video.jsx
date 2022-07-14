c016ac38ebd5dba29a77b185e4e20af05fd23a8b
Duane Nykamp <nykamp@umn.edu>
2022-07-14 16:50:37 -0500

    new size conventions, justification for video

--- src/Viewer/renderers/video.jsx
+++ src/Viewer/renderers/video.jsx
@@ -20,7 +20,7 @@ export default React.memo(function Video(props) {
   let timer = useRef(null);
   let lastPausedTime = useRef(0);
   let lastPlayedTime = useRef(0);
-  
+
 
   let onChangeVisibility = isVisible => {
     callAction({
@@ -28,6 +28,7 @@ export default React.memo(function Video(props) {
       args: { isVisible }
     })
   }
+
 
   useEffect(() => {
     return () => {
@@ -38,15 +39,13 @@ export default React.memo(function Video(props) {
     }
   }, [])
 
-  useEffect(()=>{
+  useEffect(() => {
     if (SVs.youtube) {
 
       let cName = cssesc(name);
 
       player.current = new window.YT.Player(cName, {
-        videoId: SVs.youtube,
-        width: sizeToCSS(SVs.width),
-        height: sizeToCSS(SVs.height),
+        // videoId: SVs.youtube,
         playerVars: {
           autoplay: 0,
           controls: 1,
@@ -59,14 +58,13 @@ export default React.memo(function Video(props) {
           'onStateChange': onPlayerStateChange,
           'onPlaybackRateChange': onPlaybackRateChange,
         }
-      })
-
+      });
     }
 
+  }, [])
 
-  },[])
 
-  function onPlayerReady(event){
+  function onPlayerReady(event) {
     //setPlaybackQuality doesn't seem to work
     // event.target.setPlaybackQuality("hd720");
     // player.current.setPlaybackQuality("hd720");
@@ -81,7 +79,7 @@ export default React.memo(function Video(props) {
 
     window.setInterval(function () {
       let newTime = player.current.getCurrentTime();
-        let timeInterval;
+      let timeInterval;
 
       // if previously skipped a time (because the jump was too large)
       // we need to calculate the interval from that skipped time
@@ -108,12 +106,12 @@ export default React.memo(function Video(props) {
     }, 200);
   }
 
-  async function onPlayerStateChange(event){
+  async function onPlayerStateChange(event) {
     //setPlaybackQuality doesn't seem to work
     // if (event.data == YT.PlayerState.BUFFERING) {
-      // event.target.setPlaybackQuality('hd720');
-  //     event.target.setPlaybackQuality('hd1080');
-  // }
+    // event.target.setPlaybackQuality('hd720');
+    //     event.target.setPlaybackQuality('hd1080');
+    // }
 
 
     let duration = player.current.getDuration();
@@ -139,14 +137,14 @@ export default React.memo(function Video(props) {
               //     duration,
               //   })
 
-                callAction({
-                  action:actions.recordVideoSkipped,
-                  args:{
-                    beginTime: lastPausedTime.current,
-                    endTime: currentTime,
-                    duration,
-                  }
-                })
+              callAction({
+                action: actions.recordVideoSkipped,
+                args: {
+                  beginTime: lastPausedTime.current,
+                  endTime: currentTime,
+                  duration,
+                }
+              })
 
             }
           }
@@ -166,16 +164,16 @@ export default React.memo(function Video(props) {
           //     duration,
           //     rate,
           //   })
-            callAction({
-              action:actions.recordVideoStarted,
-              args:{
-                beginTime: player.current.getCurrentTime(),
-                duration,
-                rate,
-              }
-            })
-    
-          
+          callAction({
+            action: actions.recordVideoStarted,
+            args: {
+              beginTime: player.current.getCurrentTime(),
+              duration,
+              rate,
+            }
+          })
+
+
           lastPlayerState.current = event.data;
 
         }
@@ -196,24 +194,24 @@ export default React.memo(function Video(props) {
               //     duration,
               //     rates: rates.current,
               //   })
-                callAction({
-                  action:actions.recordVideoWatched,
-                  args:{
-                    beginTime: lastPlayedTime.current,
-                    endTime: currentTime,
-                    duration,
-                    rates: rates.current,
-                  }
-                })
-             
+              callAction({
+                action: actions.recordVideoWatched,
+                args: {
+                  beginTime: lastPlayedTime.current,
+                  endTime: currentTime,
+                  duration,
+                  rates: rates.current,
+                }
+              })
+
             }
             // console.log("recordVideoPaused",{
             //   endTime: currentTime,
             //   duration,
             // })
             callAction({
-              action:actions.recordVideoPaused,
-              args:{
+              action: actions.recordVideoPaused,
+              args: {
                 endTime: currentTime,
                 duration,
               }
@@ -244,8 +242,8 @@ export default React.memo(function Video(props) {
           // })
 
           callAction({
-            action:actions.recordVideoWatched,
-            args:{
+            action: actions.recordVideoWatched,
+            args: {
               beginTime: lastPlayedTime.current,
               endTime: currentTime,
               duration,
@@ -261,8 +259,8 @@ export default React.memo(function Video(props) {
           // })
 
           callAction({
-            action:actions.recordVideoSkipped,
-            args:{
+            action: actions.recordVideoSkipped,
+            args: {
               beginTime: lastPlayedTime.current,
               endTime: currentTime,
               duration,
@@ -270,7 +268,7 @@ export default React.memo(function Video(props) {
           })
 
 
-           lastPlayerState.current = event.data;
+          lastPlayerState.current = event.data;
 
 
         }
@@ -292,8 +290,8 @@ export default React.memo(function Video(props) {
         //   rates: rates.current,
         // })
         callAction({
-          action:actions.recordVideoWatched,
-          args:{
+          action: actions.recordVideoWatched,
+          args: {
             beginTime: lastPlayedTime.current,
             endTime: player.current.getCurrentTime(),
             duration,
@@ -306,8 +304,8 @@ export default React.memo(function Video(props) {
         //   duration,
         // })
         callAction({
-          action:actions.recordVideoCompleted,
-          args:{
+          action: actions.recordVideoCompleted,
+          args: {
             duration,
           }
         })
@@ -327,7 +325,7 @@ export default React.memo(function Video(props) {
 
   }
 
-  function onPlaybackRateChange(event){
+  function onPlaybackRateChange(event) {
 
     let currentTime = player.current.getCurrentTime();
 
@@ -340,61 +338,44 @@ export default React.memo(function Video(props) {
   }
 
   if (SVs.hidden) return null;
+
+  let outerStyle = {};
+
+  if (SVs.displayMode === "inline") {
+    outerStyle = { display: "inline-block", verticalAlign: "middle", margin: "12px 0" }
+  } else {
+    outerStyle = { display: "flex", justifyContent: SVs.horizontalAlign, margin: "12px 0" };
+  }
+
+  let videoStyle = {
+    maxWidth: '100%',
+    width: sizeToCSS(SVs.width),
+    aspectRatio: String(SVs.aspectRatio),
+  }
+
+
+  let videoTag;
+
+  if (SVs.youtube) {
+    videoTag = <iframe id={name} style={videoStyle} src={"https://www.youtube.com/embed/" + SVs.youtube + "?enablejsapi=1"} />
+  } else if (SVs.source) {
+    videoTag = <video className="video" id={name} controls style={videoStyle} >
+      <source src={SVs.source} type={`video/${SVs.source.split('/').pop().split('.').pop()}`} />
+      Your browser does not support the &lt;video&gt; tag.
+    </video>
+  } else {
+    videoTag = <span id={name}></span>
+  }
+
   return (
     <VisibilitySensor partialVisibility={true} onChange={onChangeVisibility}>
-    <div style={{ margin:"12px 0", display: "flex", justifyContent: "left", alignItems:"center"}}>
-      <a name={name} />
-      {
-        SVs.youtube ? 
-          <div className="video" id={name} />
-        : SVs.source ? 
-          <video className="video" id={name} controls width="100%" >
-            <source src={SVs.source} type={`video/${SVs.source.split('/').pop().split('.').pop()}`} />
-            Your browser does not support the &lt;video&gt; tag.
-          </video>
-        :
-          <span id={name}>{SVs.text}</span>
-      }
-    </div>
+      <div style={outerStyle}>
+        <a name={name} />
+        {videoTag}
+      </div>
     </VisibilitySensor>
-  ) 
+  )
 
-  // if (SVs.youtube) {
-  //   return <>
-  //     <a name={name} />
-  //     <div className="video" id={name} />
-  //   </>
-  // } else if (SVs.source) {
-  //   let extension = SVs.source.split('/').pop().split('.').pop();
-    // let type;
-    // if (extension === "ogg") {
-    //   type = "video/ogg";
-    // } else if (extension === "webm") {
-    //   type = "video/webm";
-    // } else if (extension === "mp4") {
-    //   type = "video/mp4";
-    // } else {
-    //   console.warn("Haven't implemented video for any extension other than .ogg, .webm, .mp4");
-    // }
-    // if (!type) return null
-
-    // return (
-    // <React.Fragment>
-    //   <a name={name} />
-    //   <video className="video" id={name} style={{ objectFit: "fill" }} controls={true} width={sizeToCSS(SVs.width)} height={sizeToCSS(SVs.height)}>
-    //     <source src={SVs.source} type={type} />
-    //   Your browser does not support the &lt;video&gt; tag.
-    // </video>
-    // </React.Fragment>
-    //   ) 
-    
-  
-
-  // console.warn("No video returned youtube or no valid sources specified");
-  // return null;
-
-
-  // return <><a name={name} /><span id={name}>{SVs.text}</span></>
 })
 
 
