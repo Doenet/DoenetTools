f03654349936e1903a779f0267d695b588ace68e
Emilio Alvarez <e.l.alvarez@icloud.com>
2022-08-08 16:14:42 -0500

    Fix: HTML console error (#1450)

---
+++ src/Tools/_framework/DarkmodeController.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { atom, useRecoilValue } from 'recoil';
+// import useMediaQuery from '../../_utils/hooks/useMediaQuery';
+
+export const darkModeAtom = atom({
+  key: 'darkModeAtom',
+  default: JSON.parse(localStorage.getItem('theme')),
+  effects: [
+    ({ onSet }) => {
+      onSet((newValue) => {
+        localStorage.setItem('theme', JSON.stringify(newValue));
+      });
+    },
+  ],
+});
+
+export default function DarkmodeController({ children }) {
+  // const defaultDark = useMediaQuery('(prefers-color-scheme: dark)');
+  const atomPrefernce = useRecoilValue(darkModeAtom);
+
+  return <div data-theme={atomPrefernce ? 'dark' : 'light'}>{children}</div>;
+}
