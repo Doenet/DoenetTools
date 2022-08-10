28c7804e704c869867cd495bccd5f26f3e5eecba
Emilio Alvarez <e.l.alvarez@icloud.com>
2022-08-10 13:21:26 -0500

    fix: permission spelling error

--- src/Api/updateRolePermissons.php
+++ src/Api/updateRolePermissons.php
@@ -26,7 +26,7 @@ $permissonKeys = [
     'canViewActivitySettings',
     'canModifyActivitySettings',
     'canModifyCourseSettings',
-    'dataAccessPermisson',
+    'dataAccessPermission',
     'canViewUsers',
     'canManageUsers',
     'isAdmin',
@@ -36,7 +36,9 @@ $permissonKeys = [
 $_POST = json_decode(file_get_contents('php://input'), true);
 $courseId = mysqli_real_escape_string($conn, $_POST['courseId']);
 $roleId = mysqli_real_escape_string($conn, $_POST['roleId']);
+$actionType = 'update';
 $segments = [];
+$updatedPermissions = ['isOwner' => '0'];
 
 if ($courseId == '') {
     $success = false;
@@ -45,6 +47,7 @@ if ($courseId == '') {
 if ($roleId == '') {
     $roleId = include 'randomId.php';
     $message = 'Creating new role';
+    $actionType = 'add';
 }
 if (!array_key_exists('permissions', $_POST)) {
     $success = false;
@@ -65,7 +68,7 @@ if ($success) {
     }
 }
 
-//Bail on user trying to grant or remove isOwner through a role change.
+//Bail on user trying to grant or remove isOwner through a role permisson update.
 if ($success && array_key_exists('isOwner', $_POST['permissions'])) {
     $success = false;
     $message = 'isOwner is only available on the owner role';
@@ -90,93 +93,136 @@ if ($success && array_key_exists('label', $_POST['permissions'])) {
         $message = "Role labels must be unique, please relable $conflictLabel first";
     } else {
         array_push($segments, "label = '$newRoleLabel'");
+        $updatedPermissions['label'] = $newRoleLabel;
+
         unset($_POST['permissions']['label']);
     }
 }
 
 //enforce permisson pairs
 if ($success) {
-    // editContent > viewContentSource && viewUnassignedContent
-    if (array_key_exists('canEditContent', $_POST['permissions'])) {
-        $newRoleCanEditContent = mysqli_real_escape_string(
-            $conn,
-            $_POST['permissions']['canEditContent']
-        );
-        if ($newRoleCanEditContent == '1') {
-            array_push($segments, "canViewContentSource = '1'");
-            unset($_POST['permissions']['canViewContentSource']);
-            array_push($segments, "canViewUnassignedContent = '1'");
-            unset($_POST['permissions']['canViewUnassignedContent']);
+    //isAdmin grants all
+    $newRoleIsAdmin = mysqli_real_escape_string(
+        $conn,
+        $_POST['permissions']['isAdmin']
+    );
+    if ($newRoleIsAdmin == '1') {
+        foreach ($permissonKeys as $permisson) {
+            if (
+                $permisson != 'label' &&
+                $permisson != 'isIncludedInGradebook' &&
+                $permisson != 'dataAccessPermission'
+            ) {
+                array_push($segments, "$permisson = '1'");
+                $updatedPermissions[$permisson] = '1';
+                unset($_POST['permissions'][$permisson]);
+            }
         }
-    }
-    // modifyActivitySettings > viewActivitySettings
-    if (array_key_exists('canModifyActivitySettings', $_POST['permissions'])) {
-        $newRoleCanModifyActivitySettings = mysqli_real_escape_string(
-            $conn,
-            $_POST['permissions']['canModifyActivitySettings']
-        );
-        if ($newRoleCanModifyActivitySettings == '1') {
-            array_push($segments, "canViewActivitySettings = '1'");
-            unset($_POST['permissions']['canViewActivitySettings']);
+        array_push($segments, "dataAccessPermission = 'Identified'");
+        $updatedPermissions['dataAccessPermission'] = 'Identified';
+        unset($_POST['permissions']['dataAccessPermission']);
+    } else {
+        // editContent > viewContentSource && viewUnassignedContent
+        if (array_key_exists('canEditContent', $_POST['permissions'])) {
+            $newRoleCanEditContent = mysqli_real_escape_string(
+                $conn,
+                $_POST['permissions']['canEditContent']
+            );
+            if ($newRoleCanEditContent == '1') {
+                array_push($segments, "canViewContentSource = '1'");
+                $updatedPermissions['canViewContentSource'] = '1';
+                unset($_POST['permissions']['canViewContentSource']);
+                array_push($segments, "canViewUnassignedContent = '1'");
+                $updatedPermissions['canViewUnassignedContent'] = '1';
+                unset($_POST['permissions']['canViewUnassignedContent']);
+            }
         }
-    }
-    //isAdmin > manageUsers > viewUsers
-    if (array_key_exists('isAdmin', $_POST['permissions'])) {
-        $newRoleIsAdmin = mysqli_real_escape_string(
-            $conn,
-            $_POST['permissions']['isAdmin']
-        );
-        if ($newRoleIsAdmin == '1') {
-            array_push($segments, "canManageUsers = '1'");
-            unset($_POST['permissions']['canManageUsers']);
-            array_push($segments, "canViewUsers = '1'");
-            unset($_POST['permissions']['canViewUsers']);
+        // modifyActivitySettings > viewActivitySettings
+        if (
+            array_key_exists('canModifyActivitySettings', $_POST['permissions'])
+        ) {
+            $newRoleCanModifyActivitySettings = mysqli_real_escape_string(
+                $conn,
+                $_POST['permissions']['canModifyActivitySettings']
+            );
+            if ($newRoleCanModifyActivitySettings == '1') {
+                array_push($segments, "canViewActivitySettings = '1'");
+                $updatedPermissions['canViewActivitySettings'] = '1';
+
+                unset($_POST['permissions']['canViewActivitySettings']);
+            }
         }
-    } elseif (array_key_exists('canManageUsers', $_POST['permissions'])) {
-        $newRoleCanManageUsers = mysqli_real_escape_string(
-            $conn,
-            $_POST['permissions']['canManageUsers']
-        );
-        if ($newRoleCanManageUsers == '1') {
-            array_push($segments, "canViewUsers = '1'");
-            unset($_POST['permissions']['canViewUsers']);
+        //canManageUsers > canViewUsers
+        if (array_key_exists('canManageUsers', $_POST['permissions'])) {
+            $newRoleCanManageUsers = mysqli_real_escape_string(
+                $conn,
+                $_POST['permissions']['canManageUsers']
+            );
+            if ($newRoleCanManageUsers == '1') {
+                array_push($segments, "canViewUsers = '1'");
+                $updatedPermissions['canViewUsers'] = '1';
+                unset($_POST['permissions']['canViewUsers']);
+            }
         }
     }
 }
 
 //Make permissons assignment
 if ($success) {
-    foreach ($permissonKeys as $permisson) {
-        if (array_key_exists($permisson, $_POST['permissions'])) {
-            $sanitizedNewValue = mysqli_real_escape_string(
-                $conn,
-                $_POST['permissions'][$permisson]
-            );
-            array_push($segments, "$permisson = '$sanitizedNewValue'");
-        }
-    }
-    $updates = implode(',', $segments);
-
-    $result = $conn->query(
-        "INSERT INTO course_role
-        SET
-        courseId ='$courseId',
-        roleId = '$roleId',
-        $updates
-        ON DUPLICATE KEY UPDATE
-            $updates
-        "
+    $isDeleted = mysqli_real_escape_string(
+        $conn,
+        $_POST['permissions']['isDeleted']
     );
+    if ($isDeleted == '1') {
+        $actionType = 'delete';
+        $result = $conn->query(
+            "DELETE FROM course_role
+            WHERE
+            courseId ='$courseId'
+            AND
+            roleId = '$roleId'"
+        );
+        if ($result == false) {
+            $success = false;
+            $message =
+                'Please remove all people from this role before deleting';
+        }
+    } else {
+        foreach ($permissonKeys as $permisson) {
+            if (array_key_exists($permisson, $_POST['permissions'])) {
+                $sanitizedNewValue = mysqli_real_escape_string(
+                    $conn,
+                    $_POST['permissions'][$permisson]
+                );
+                array_push($segments, "$permisson = '$sanitizedNewValue'");
+                $updatedPermissions[$permisson] = "'$sanitizedNewValue'";
+            }
+        }
+        $updates = implode(',', $segments);
 
-    if ($result == false) {
-        $success = false;
-        $message = 'server error: invalid course or role id';
+        $result = $conn->query(
+            "INSERT INTO course_role
+            SET
+            courseId ='$courseId',
+            roleId = '$roleId',
+            $updates
+            ON DUPLICATE KEY UPDATE
+                $updates
+            "
+        );
+        if ($result == false) {
+            $success = false;
+            $message = 'Internal Server Error';
+        }
     }
 }
 
 echo json_encode([
     'success' => $success,
     'message' => $message,
+    'roleId' => $roleId,
+    'actionType' => $actionType,
+    'updatedPermissions' => $updatedPermissions,
 ]);
 
 $conn->close();
