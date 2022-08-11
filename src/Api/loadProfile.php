9aff5ad29c31b101efb52fcee574bb75c051b987
Emilio Alvarez <e.l.alvarez@icloud.com>
2022-08-11 15:22:20 -0500

    fix profile

--- src/Api/loadProfile.php
+++ src/Api/loadProfile.php
@@ -1,93 +1,54 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: access");
-header("Access-Control-Allow-Methods: GET");
-header("Access-Control-Allow-Credentials: true");
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: access');
+header('Access-Control-Allow-Methods: GET');
+header('Access-Control-Allow-Credentials: true');
 header('Content-Type: application/json');
 
-include "db_connection.php";
+include 'db_connection.php';
 
-$jwtArray = include "jwtArray.php";
+$jwtArray = include 'jwtArray.php';
 $userId = $jwtArray['userId'];
 
-$sql = "SELECT screenName, email, lastName, firstName, profilePicture, trackingConsent, roleStudent, roleInstructor, roleCourseDesigner, roleWatchdog, roleCommunityTA, roleLiveDataCommunity
-        FROM user
-        WHERE userId = '$userId'";
-
-$result = $conn->query($sql);
-$response_arr = array("success" => "0");
-
-
-if ($result->num_rows > 0){
-
-    $row = $result->fetch_assoc();
-    $profile = array(
-        "screenName" => $row['screenName'],
-        "email" => $row['email'],
-        "firstName" => $row['firstName'],
-        "lastName" => $row['lastName'],
-        "profilePicture" => $row['profilePicture'],
-        "trackingConsent" => $row['trackingConsent'],
-        "roleStudent" => $row['roleStudent'],
-        "roleInstructor" => $row['roleInstructor'],
-        "roleCourseDesigner" => $row['roleCourseDesigner'],
-        "roleWatchdog" => $row['roleWatchdog'],
-        "roleCommunityTA" => $row['roleCommunityTA'],
-        "roleLiveDataCommunity" => $row['roleLiveDataCommunity'],
-        "signedIn" => "1",
-        
+$result = $conn->query(
+    "SELECT screenName, email, lastName, firstName, profilePicture, trackingConsent
+  FROM user
+  WHERE userId = '$userId'"
 );
-$roleAccessList = array(
-    "roleStudent" => array("Account", "Library", "Course"),
-    "roleInstructor" => array("Account", "Library", "Course", "Documentation", "Gradebook"),
-    "roleCourseDesigner" => array("Account", "Library", "Course", "Documentation"),
-    "roleWatchdog" => array(/*???*/),
-    "roleCommunityTA" => array(/*???*/),
-    "roleLiveDataCommunity" => array(/*???*/)
-  );
-  
-  $toolAccessList = [];
+$response_arr = ['success' => '0'];
 
-    foreach ($profile as $key => $value) {
-       if ($roleAccessList[$key] != NULL && $value == 1){
-      $toolAccessList = array_values(array_unique(array_merge($toolAccessList, $roleAccessList[$key])));
-       }
-    }
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $profile = [
+        'screenName' => $row['screenName'],
+        'email' => $row['email'],
+        'firstName' => $row['firstName'],
+        'lastName' => $row['lastName'],
+        'profilePicture' => $row['profilePicture'],
+        'trackingConsent' => $row['trackingConsent'],
+        'signedIn' => '1',
+        'device' => $jwtArray['deviceName'],
+    ];
 
-  $profile["toolAccess"] = $toolAccessList;
-  $profile["device"] = $jwtArray["deviceName"];
-$profile["userId"] = $userId;
+    $response_arr = [
+        'success' => '1',
+        'profile' => $profile,
+    ];
+} else {
+    //Send back not signed in profile
+    $profile = [
+        'screenName' => 'anonymous',
+        'email' => '',
+        'firstName' => '',
+        'lastName' => '',
+        'profilePicture' => 'anonymous',
+        'trackingConsent' => true,
+        'signedIn' => '0',
+        'userId' => $userId,
+    ];
+    $profile['device'] = 'N/A';
 
-    $response_arr = array("success" => "1",
-                          "profile" => $profile);
-    
-}else{
-  //Send back not signed in profile
-  // $toolAccessList = array("Library", "Course", "Documentation");
-  $toolAccessList = array("Documentation");
-  $profile = array(
-    "screenName" => "anonymous",
-    "email" => "",
-    "firstName" => "",
-    "lastName" => "",
-    "profilePicture" => "anonymous",
-    "trackingConsent" => true,
-    "roleStudent" => "0",
-    "roleInstructor" => "0",
-    "roleCourseDesigner" => "0",
-    "roleWatchdog" => "0", 
-    "roleCommunityTA" => "0",
-    "roleLiveDataCommunity" => "0",
-    "signedIn" => "0",
-    "userId"=>$userId,
-  );
-// $profile["toolAccess"] = array("Library", "Course", "Documentation");
-$profile["toolAccess"] = $toolAccessList;
-$profile["device"] = "N/A";
-
-$response_arr = array("success" => "1",
-                      "profile" => $profile,
-                    );
+    $response_arr = ['success' => '1', 'profile' => $profile];
 }
 
 // set response code - 200 OK
