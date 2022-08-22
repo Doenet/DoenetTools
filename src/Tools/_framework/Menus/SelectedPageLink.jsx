831f8604c78374f11db559f1a1c31d2a079ff705
Kevin Charles <kevinmcharles@gmail.com>
2022-08-21 22:31:18 -0500

    Rename page links

--- src/Tools/_framework/Menus/SelectedPageLink.jsx
+++ src/Tools/_framework/Menus/SelectedPageLink.jsx
@@ -1,17 +1,32 @@
 import { faLink } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useEffect, useState } from 'react';
-import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { useRecoilCallback, useRecoilValue, useSetRecoilState } from 'recoil';
 import { itemByDoenetId, selectedCourseItems } from '../../../_reactComponents/Course/CourseActions';
-import { pageToolViewAtom } from '../NewToolRoot';
+import { pageToolViewAtom, searchParamAtomFamily } from '../NewToolRoot';
 import ActionButton from '../../../_reactComponents/PanelHeaderComponents/ActionButton';
 import ActionButtonGroup from '../../../_reactComponents/PanelHeaderComponents/ActionButtonGroup';
+import Textfield from '../../../_reactComponents/PanelHeaderComponents/Textfield';
+import axios from 'axios';
+
+
 
 export default function SelectedPageLink() {
   const setPageToolView = useSetRecoilState(pageToolViewAtom);
+  const courseId = useRecoilValue(searchParamAtomFamily('courseId'));
   const doenetId = useRecoilValue(selectedCourseItems)[0];
   const pageObj = useRecoilValue(itemByDoenetId(doenetId));
   const [itemTextFieldLabel,setItemTextFieldLabel] = useState(pageObj.label)
+
+  const renamePageLink = useRecoilCallback(({set})=>async (courseId,doenetId,label)=>{
+    const { data } = await axios.get('/api/renamePageLink.php',{params:{courseId,doenetId,label}})
+    // console.log("data",data)
+    set(itemByDoenetId(doenetId),(prev)=>{
+      let next = {...prev}
+      next.label = label
+      return next
+    })
+  })
 
   useEffect(()=>{
     if (itemTextFieldLabel !== pageObj.label){
@@ -19,11 +34,13 @@ export default function SelectedPageLink() {
     }
   },[doenetId]) //Only check when the pageId changes
 
-  console.log(">>>>SelectedPageLink pageObj",pageObj)
-
   let heading = (<h2 data-test="infoPanelItemLabel" style={{ margin: "16px 5px" }} >
     <FontAwesomeIcon icon={faLink} /> {pageObj.label} 
   </h2>)
+
+  function handleLabelModfication(){
+    renamePageLink(courseId,doenetId,itemTextFieldLabel);
+  }
 
   
   return <>
@@ -44,7 +61,18 @@ export default function SelectedPageLink() {
           }}
         />
   </ActionButtonGroup>
+  <Textfield
+      label="Label"
+      vertical
+      width="menu"
+      value={itemTextFieldLabel}
+      onChange={(e) => setItemTextFieldLabel(e.target.value)}
+      onKeyDown={(e) => {
+        if (e.keyCode === 13) handleLabelModfication();
+      }}
+      onBlur={handleLabelModfication}
+    />
   <br />
-  <div>Rename Goes Here</div>
+
   </>
 }
