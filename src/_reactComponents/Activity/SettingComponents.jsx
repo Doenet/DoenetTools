c271d35a487a08a81cf31b7c3d1a19ee3837edfd
Emilio Alvarez <e.l.alvarez@icloud.com>
2022-09-21 02:08:09 -0500

    fix file imports

--- src/_reactComponents/Activity/SettingComponents.jsx
+++ src/_reactComponents/Activity/SettingComponents.jsx
@@ -25,6 +25,7 @@ import { toastType, useToast } from '@Toast';
 import { searchParamAtomFamily } from '../../Tools/_framework/NewToolRoot';
 import { prerenderActivity } from '../../_utils/activtyWebWorker';
 import Textfield from '../PanelHeaderComponents/Textfield';
+import { useSaveDraft } from '../../_utils/hooks/useSaveDraft';
 
 const InputWrapper = styled.div`
   margin: 0 5px 10px 5px;
