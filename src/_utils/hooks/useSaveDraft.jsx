082a0f6e3ac1668e3f3833b932fea4942805e752
Emilio Alvarez <e.l.alvarez@icloud.com>
2022-09-21 01:29:54 -0500

    extract useSaveDraft to hooks file

---
+++ src/_utils/hooks/useSaveDraft.jsx
@@ -0,0 +1,40 @@
+import axios from 'axios';
+import { useRecoilCallback } from 'recoil';
+import { fileByPageId } from '../../Tools/_framework/ToolHandlers/CourseToolHandler';
+import { updateTextEditorDoenetMLAtom } from '../../_sharedRecoil/EditorViewerRecoil';
+
+export function useSaveDraft() {
+  const saveDraft = useRecoilCallback(
+    ({ snapshot, set }) =>
+      async ({ pageId, courseId, backup = false }) => {
+        const doenetML = await snapshot.getPromise(
+          updateTextEditorDoenetMLAtom,
+        );
+
+        //Save in localStorage
+        // localStorage.setItem(cid,doenetML)
+
+        try {
+          const params = {
+            doenetML,
+            pageId,
+            courseId,
+            backup,
+          };
+          const { data } = await axios.post('/api/saveDoenetML.php', params);
+          set(fileByPageId(pageId), doenetML);
+          if (!data.success) {
+            //   //TODO: Toast here
+            console.log('ERROR', data.message);
+          }
+          return { success: data.success };
+        } catch (error) {
+          console.log('ERROR', error);
+          return { success: false };
+        }
+      },
+    [],
+  );
+
+  return { saveDraft };
+}
