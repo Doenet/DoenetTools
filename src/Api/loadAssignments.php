0f25972413d29cfe96d1f601d7d0716e31887726
Emilio Alvarez <e.l.alvarez@icloud.com>
2022-09-30 09:25:41 -0500

    reset loadAssignments

--- src/Api/loadAssignments.php
+++ src/Api/loadAssignments.php
@@ -12,7 +12,6 @@ $jwtArray = include 'jwtArray.php';
 $requestorUserId = $jwtArray['userId'];
 $success = true;
 $message = '';
-$allUsers = true;
 
 if (!isset($_GET['courseId'])) {
     $success = false;
@@ -31,50 +30,24 @@ if ($success) {
 
     if ($requestorPermissions == false) {
         $success = false;
-        $message = 'You are not authorized to view or modify grade data';
-    } elseif ($requestorPermissions['canViewAndModifyGrades'] != '1') {
-        $allUsers = false;
-        $message = 'You are only allowed to view your own data';
+        $message = 'You are not authorized to view assignments for this course';
     }
 }
 
 if ($success) {
-    if ($allUsers) {
-        $result = $conn->query(
-            "SELECT a.doenetId, 
-                cc.label, 
-                a.gradeCategory,
-                a.totalPointsOrPercent
-            FROM assignment AS a
-            LEFT JOIN course_content as cc
-                ON a.doenetId = cc.doenetId
-            LEFT JOIN user_assignment as ua
-                ON a.doenetId = ua.doenetId
-            WHERE a.courseId = '$courseId'
-                AND cc.isAssigned = '1'
-                AND cc.isDeleted = '0'
-            ORDER BY a.dueDate"
-        );
-    } else {
-        $result = $conn->query(
-            "SELECT a.doenetId, 
-                cc.label, 
-                a.gradeCategory,
-                a.totalPointsOrPercent
-            FROM assignment AS a
-            LEFT JOIN course_content as cc
-                ON a.doenetId = cc.doenetId
-            LEFT JOIN user_assignment as ua
-                ON a.doenetId = ua.doenetId
-                AND ua.userId = '$requestorUserId'
-            WHERE a.courseId = '$courseId'
-                AND cc.isAssigned = '1'
-                AND cc.isDeleted = '0'
-                AND (ua.isUnassigned IS NULL OR ua.isUnassigned = '0')
-            ORDER BY a.dueDate"
-        );
-    }
-
+    $result = $conn->query(
+        "SELECT a.doenetId, 
+            cc.label, 
+            a.gradeCategory,
+            a.totalPointsOrPercent
+        FROM assignment AS a
+        LEFT JOIN course_content as cc
+        ON a.doenetId = cc.doenetId
+        WHERE a.courseId = '$courseId'
+            AND cc.isAssigned = '1'
+            AND cc.isDeleted = '0'
+        ORDER BY a.dueDate"
+    );
     $response_arr = [];
 
     if ($result->num_rows > 0) {
