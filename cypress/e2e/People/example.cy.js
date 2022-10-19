701760bb10efa4f8e8b1a6e523d7c810cd21d811
Kevin Charles <kevincharles@users.noreply.github.com>
2022-10-18 22:31:40 -0500

    Example in Cypress of how to update a users perms and enable file upload (#1736)

---
+++ cypress/e2e/People/example.cy.js
@@ -0,0 +1,32 @@
+
+describe('Examples', function () {
+    const userId = "cyuserId";
+    const courseId = "courseid1";
+
+  
+  before(() => {
+    cy.clearCoursePeople({ courseId });
+    cy.createCourse({ userId, courseId });
+  })
+
+
+  beforeEach(() => {
+    cy.signin({ userId });
+    cy.visit(`localhost`)
+  })
+   
+  it('set can upload',()=>{
+    // cy.setUserUpload({userId,newValue:'0'})
+    cy.setUserUpload({userId,newValue:'1'})
+  })
+
+  it.only("set user's role can canEditContent",()=>{
+
+  const roleId = "canEditRoleId";
+  cy.createUserRole({courseId,roleId,label:"can edit role"});
+  cy.updateRolePerm({roleId,permName:"canEditContent",newValue:"1"})
+  cy.setUserRole({userId,courseId,roleId});
+  })
+
+
+})
