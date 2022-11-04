e239fc2cd05b800e416ce9c002a7995c56167501
Emilio Alvarez <e.l.alvarez@icloud.com>
2022-11-04 16:14:12 +0000

    cleanup

--- src/Api/db_connection.php
+++ src/Api/db_connection.php
@@ -1,21 +1,6 @@
 <?php
 
 $env_path = '../etc/env.ini';
-$remoteuser = 'devuser';
-// $remoteuser = $_SERVER[ 'REMOTE_USER' ];
-// $db_temp = "cse_doenet";
-
-// if (
-//     $_SERVER['HTTP_HOST'] == 'localhost' ||
-//     $_SERVER['HTTP_HOST'] == 'localhost:3000' ||
-//     $_SERVER['HTTP_HOST'] == 'localhost:8080' ||
-//     $_SERVER['HTTP_HOST'] == 'apache' ||
-//     $_SERVER['HTTP_HOST'] == 'localhost:81'
-// ) {
-//     $env_path = 'env.ini';
-//     $remoteuser = 'devuser';
-// }
-
 $ini_array = parse_ini_file($env_path);
 
 $dbhost = $ini_array['dbhost'];
@@ -24,19 +9,14 @@ $password = $ini_array['password'];
 $database = $ini_array['database'];
 
 if (
-    // $_SERVER['HTTP_HOST'] == 'localhost' ||
-    // $_SERVER['HTTP_HOST'] == 'localhost:8080' ||
-    // $_SERVER['HTTP_HOST'] == 'apache' ||
-    // $_SERVER['HTTP_HOST'] == 'localhost:81'
     $database = 'doenet_local'
 ) {
-    // $database = 'doenet_local';
-    // $dbhost = 'mysql';
     $_SERVER['givenName'] = "chocolate";  //For shib test on localhost
     $_SERVER['surname'] = "eclair"; //For shib test on localhost
     $_SERVER['mail'] = "eclair@doenet.org"; //For shib test on localhost
 }
 
+// unsure why this is here - Emilio
 if ($_SERVER['HTTP_HOST'] == 'localhost:3000') {
     $dbhost = '127.0.0.1';
     $password = 'root';
