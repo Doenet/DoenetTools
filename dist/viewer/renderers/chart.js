8f1830b07483623025fd649fb02ef26caaf80297
Kevin Charles <kevinmcharles@gmail.com>
2022-11-08 18:30:35 -0600

    Now really chart!!!

--- dist/viewer/renderers/chart.js
+++ dist/viewer/renderers/chart.js
@@ -20,6 +20,35 @@ export default React.memo(function Chart(props) {
       });
     };
   }, []);
+  useEffect(() => {
+    if (SVs.dataFrame !== null) {
+      let colInds;
+      if (SVs.colInd !== null) {
+        colInds = [SVs.colInd];
+      } else {
+        colInds = SVs.dataFrame.columnTypes.map((v, i) => v === "number" ? i : null).filter((x) => x !== null);
+      }
+      let data = [];
+      for (let colInd of colInds) {
+        if (SVs.type === "box") {
+          data.push({
+            y: extractColumn(SVs.dataFrame.data, colInd),
+            type: "box",
+            name: SVs.dataFrame.columnNames[colInd]
+          });
+        } else {
+          data.push({
+            x: extractColumn(SVs.dataFrame.data, colInd),
+            type: "histogram",
+            name: SVs.dataFrame.columnNames[colInd]
+          });
+        }
+      }
+      Plotly.newPlot(name, {
+        data
+      });
+    }
+  }, []);
   return /* @__PURE__ */ React.createElement(React.Fragment, null, /* @__PURE__ */ React.createElement("a", {
     name: id
   }), /* @__PURE__ */ React.createElement(VisibilitySensor, {
