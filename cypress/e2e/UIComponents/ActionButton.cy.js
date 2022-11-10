878d774b945b3efaccc356a5db241b7e5bc228c2
Kevin Charles <kevinmcharles@gmail.com>
2022-11-10 16:17:46 -0600

    ActionButton set up for click

--- cypress/e2e/UIComponents/ActionButton.cy.js
+++ cypress/e2e/UIComponents/ActionButton.cy.js
@@ -8,12 +8,18 @@ before(() => {
 
 
  
-it('Button Width CSS Test',()=>{
-  cy.get('[data-test="ActionButton width example"]').contains('Action Button');
-})
+// it('Button Width CSS Test',()=>{
+//   cy.get('[data-test="ActionButton width example"]').contains('Action Button');
+// })
 
 it('Button value has Edit',()=>{
   cy.get('[data-test="ActionButton Edit Value example"]').contains('Edit');
+})
+
+it.only('Test onClick',()=>{
+  cy.get('[data-test="ActionButton click example"]').click();
+  cy.get('[data-test="action result"]').contains('ActionButton clicked');
+  cy.get('[data-test="clear action result"]').click();
 })
 
 
