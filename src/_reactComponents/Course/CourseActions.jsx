b474404599f98a99713c8ac783e7decffe18fd7c
Emilio Alvarez <e.l.alvarez@icloud.com>
2022-12-04 09:47:31 -0600

    remove name conflict

--- src/_reactComponents/Course/CourseActions.jsx
+++ src/_reactComponents/Course/CourseActions.jsx
@@ -810,9 +810,6 @@ export const useCourse = (courseId) => {
   const { label, color, image, defaultRoleId, canAutoEnroll } = useRecoilValue(
     coursePermissionsAndSettingsByCourseId(courseId)
   );
-  let coursePermissionsAndSettings = useRecoilValue(
-    coursePermissionsAndSettingsByCourseId(courseId)
-  );
   const addToast = useToast();
 
   function insertPageOrOrderToActivityInSpecificOrder({
