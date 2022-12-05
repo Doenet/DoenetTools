837a3b8bcd7a814065a97c93387d8d8c8e546729
lee <leeernzher@gmail.com>
2022-12-05 16:39:08 -0600

    Full refactor: Progress Bar

--- src/_reactComponents/PanelHeaderComponents/ProgressBar.jsx
+++ src/_reactComponents/PanelHeaderComponents/ProgressBar.jsx
@@ -1,129 +1,139 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
-  // Depend on if we have a label or the showProgress props
-  display: ${props => props.align};
-  align-items: ${props => props.alignItems};
+  display: flex;
+  flex-direction: ${props => props.isLabelVertical && 'column'} ;
+  align-items:  ${props => !props.isLabelVertical && 'center'};
+  width: ${props => props.width ? props.width : "200px"};
+`;
+
+const Label = styled.span`
+  font-size: 14px;
+  margin-right: 5px;
+  margin-bottom: ${props => props.isVertical && '2px'};
 `;
 
 const BarWrapper = styled.div`
-  transform:  ${props => props.rotated ? "scaleX(-1)" : ""};
+  display: flex;
+  align-items: center;
+  width: ${props => props.width ? props.width : "200px"};
+  height: ${props => props.isDonut ? "25px" : props.height ? props.height : "10px"};
 `;
 
-const Svg = styled.svg``;
-
-const Rect = styled.rect`
-  x: 0px;
-  y: 0px;
-  rx: ${props => props.radius}; // Depends on if we have the donutIcon prop
-  fill: ${props => props.color};
-  stroke: none;
-  stroke-width: 0;
-  height: ${props => props.height};
-  // width prop is passed in as a number without "px"
-  // Usage: <ProgressBar width=400/> to set the width of the progress bar to 400px
-  max-width: ${props => (props.width).toString() + "px"};
+const FullBar = styled.div`
+  width: 100%;
+  height: 100%;
+  position: relative;
+  overflow: hidden;
+  border-radius: ${props => props.isDonut ? "12.5px" : props.radius ? props.radius : "5px"};
+  background-color: var(--mainGray); 
 `;
 
-const DonutG = styled.g``;
+const InnerBar = styled.div`
+  border-radius: ${props => props.isDonut ? "12.5px" : props.radius ? props.radius : "5px"};
+  position: absolute;
+  height: 100%;
+  background-color: var(--mainBlue); 
+  left: ${props => !props.rotated && 0};
+  right: ${props => props.rotated && 0};
+  width: ${props => props.progress + "%"};
+`;
+
+const Donut = styled.div`
+  position: absolute;
+  right: ${props => !props.rotated && 0};
+  left: ${props => props.rotated && 0};
+`;
+
+const DonutWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+`;
 
 const Circle = styled.circle`
   cy: 12.5px;
 `;
 
-const Label = styled.p`
-  font-size: 14px;
-  display: ${props => props.labelVisible}; // Only visible with label prop
-  margin-right: 5px;
-  margin-bottom: ${props => props.align == 'flex' ? 'none' : '2px'};
-`;
-
-const Progress = styled.p`
-    font-size: 12px;
-    display: ${props => props.showProgress}; // Only visible with showProgress prop
-    margin-left: 5px; // Percentage appears to the right of the progress bar
+const Progress = styled.span`
+  font-size: 12px;
+  margin-left: 5px; 
 `;
 
 export default function ProgressBar(props) {
-  const [fillWidth, setFillWidth] = useState("0px");
-  const [donutPosition, setDonutPosition] = useState("12.5px");
-  const [barWidth, setBarWidth] = useState(props.width ? props.width : 200); // Default to the width of the navigation panel
-  const height = props.donutIcon ? "25px" : props.height || "10px";
-  const radius = props.donutIcon ? "12.5px" : props.radius || "5px";
-  const ariaLabel = props.ariaLabel ? props.ariaLabel : null;
-  const labelVisible = props.label ? 'static' : 'none';
 
-  // donutIcon styled-component
-  // Only visible with donutIcon prop
-  var donut = 
-  <DonutG>
-    <Circle
-      id="donut" 
-      cx={donutPosition}
-      fill="var(--donutBody)" 
-      r="12.5" 
-    />
-    <Circle
-      id="donut-topping"
-      cx={donutPosition}
-      fill="var(--donutTopping)"
-      r="10"
-    />
-    <Circle 
-      id="donut-hole"  
-      cx={donutPosition}
-      r="4" 
-      fill="var(--mainGray)"
-    />
-  </DonutG>
+  var donutIcon = 
+    <svg width='25px' height='25px'>
+      <g>
+        <Circle
+          id="donut" 
+          cx={12.5}
+          fill="var(--donutBody)" 
+          r="12.5" 
+        />
+        <Circle
+          id="donut-topping"
+          cx={12.5}
+          fill="var(--donutTopping)"
+          r="10"
+        />
+        <Circle 
+          id="donut-hole"  
+          cx={12.5}
+          r="4" 
+          fill="var(--mainGray)"
+        />
+      </g>
+    </svg>
     
-  var align = 'flex';
-  var alignItems = 'none';
-
-  var label = '';
-  if (props.label) {
-    label = props.label;
-    alignItems = 'center';
-    if (props.vertical) {
-      align = 'static';
-    }
-  };
-
-  var percent = '';
-  if (props.showProgress) {
-    percent = (props.progress * 100).toString() + "%";
-    alignItems = 'center';
-  };
-
-  // progress prop should ALWAYS be defined
-  // Usage: <ProgressBar progress={0.4}/> to fill the progress bar 40%
-  useEffect(() => {
-    let progress = props.progress ? props.progress : 0;
-    progress *= barWidth;
-
-    // Fill the bar and/or move the donut based on the progress prop
-    setFillWidth(progress);    
-    setDonutPosition(progress-12.5); 
-
-    // If the showProgress prop is used, shorten the progress bar
-    // We want to keep the total length of the component to be 235px
-    if (props.showProgress) {
-      setBarWidth(200);
-    };
-  }, [props.progress, props.showProgress, barWidth]);
 
   return (
-    <Container align={align} alignItems={alignItems} aria-labelledby="progress-bar-label" aria-label={"progress bar" + percent}>
-      <Label id="progress-bar-label" labelVisible={labelVisible} align={align}>{label}</Label>
-      <BarWrapper rotated={props.rotated}>
-        <Svg width={barWidth} height={height} >
-          <Rect id="main" color="var(--mainGray)" width={barWidth} height={height} radius={radius} aria-label={ariaLabel}/>
-          <Rect id="moving" color={props.color || "var(--mainBlue)"} width={fillWidth} height={height} radius={radius}/>
-          {props.donutIcon ? donut : ''}
-        </Svg>
+    <Container 
+      isLabelVertical={props.label && props.vertical} 
+      aria-labelledby="progress-bar-label" 
+      aria-label={"progress bar " + props.progress * 100}
+      width={props.width}
+    >
+      {
+        props.label ?
+          <Label id="progress-bar-label" isVertical={props.vertical}>
+            {props.label}
+          </Label>
+        :
+          null
+      }
+      <BarWrapper
+       width={props.width}
+       height={props.height}
+       isDonut={props.donutIcon}
+      >
+        <FullBar radius={props.radius}>
+          <InnerBar 
+            progress={props.progress * 100}
+            isDonut={props.donutIcon}
+            rotated={props.rotated}
+            radius={props.radius}
+          >
+            { 
+            props.donutIcon ? 
+              <DonutWrapper>
+                <Donut rotated={props.rotated}>
+                  {donutIcon}
+                </Donut>
+              </DonutWrapper>
+            : 
+              null 
+            }
+          </InnerBar>
+        </FullBar>
+        { 
+        props.showProgress ? 
+          <Progress>{props.progress * 100}%</Progress> 
+        : 
+          null
+        }
       </BarWrapper>
-      {props.showProgress ? <Progress>{percent}</Progress> : ''}
     </Container>
   );
 };
