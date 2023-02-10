f031744f84b63163eae28365273949ae5070b70d
Duane Nykamp <nykamp@umn.edu>
2023-02-09 19:23:30 -0600

    Renderers protect again null children

--- src/Viewer/renderers/figure.jsx
+++ src/Viewer/renderers/figure.jsx
@@ -4,7 +4,7 @@ import VisibilitySensor from 'react-visibility-sensor-v2';
 import Measure from 'react-measure';
 
 export default React.memo(function Figure(props) {
-  let {name, id, SVs, children, actions, callAction} = useDoenetRender(props);
+  let { name, id, SVs, children, actions, callAction } = useDoenetRender(props);
 
   let onChangeVisibility = isVisible => {
     callAction({
@@ -27,81 +27,82 @@ export default React.memo(function Figure(props) {
   }
 
   // BADBADBAD: need to redo how getting the caption child
-    // getting it using the internal guts of componentInstructions
-    // is just asking for trouble
-    let childrenToRender = children;
-    let caption = null; // The <div/> element holding the figureName and the caption description
-    let captionChild = null; // The caption description
+  // getting it using the internal guts of componentInstructions
+  // is just asking for trouble
+  let childrenToRender = children;
+  let caption = null; // The <div/> element holding the figureName and the caption description
+  let captionChild = null; // The caption description
 
-    if (SVs.captionChildName) {
-      let captionChildInd;
-      for (let [ind, child] of children.entries()) {
-        if (typeof child !== "string" && child.props.componentInstructions.componentName === SVs.captionChildName) {
-          captionChildInd = ind;
-          break;
-        }
+  if (SVs.captionChildName) {
+    let captionChildInd;
+    for (let [ind, child] of children.entries()) {
+      //child might be null or a string
+      if (child?.props?.componentInstructions.componentName === SVs.captionChildName) {
+        captionChildInd = ind;
+        break;
       }
-
-      captionChild = children[captionChildInd];
-      childrenToRender.splice(captionChildInd, 1); // remove caption
     }
 
-    if (!SVs.suppressFigureNameInCaption) {
-      let figureName = <strong>{SVs.figureName}</strong>
-      if (captionChild) {
-        caption = <div>{figureName}: {captionChild}</div>
-      } else { 
-        caption = <div>{figureName}</div>
-      }
+    captionChild = children[captionChildInd];
+    childrenToRender.splice(captionChildInd, 1); // remove caption
+  }
+
+  if (!SVs.suppressFigureNameInCaption) {
+    let figureName = <strong>{SVs.figureName}</strong>
+    if (captionChild) {
+      caption = <div>{figureName}: {captionChild}</div>
     } else {
-      if (captionChild) {
-        caption = <div>{captionChild}</div>
-      }
+      caption = <div>{figureName}</div>
     }
-
-    const [captionTextAlign, setCaptionTextAlign] = useState("center");
-
-    // Helper function for countCaptionLines
-    function getLineHeight(el) {
-      var temp = document.createElement(el.nodeName), ret;
-      temp.setAttribute("style", "margin:0; padding:0; "
-          + "font-family:" + (el.style.fontFamily || "inherit") + "; "
-          + "font-size:" + (el.style.fontSize || "inherit"));
-      temp.innerHTML = "A";
-  
-      el.parentNode.appendChild(temp);
-      ret = temp.clientHeight;
-      temp.parentNode.removeChild(temp);
-  
-      return ret;
+  } else {
+    if (captionChild) {
+      caption = <div>{captionChild}</div>
     }
+  }
 
-    // Helper function for handleResize
-    // Count the number of lines in the caption
-    function countCaptionLines() {
-      var el = document.getElementById(id + "_caption");
-      var divHeight = el.offsetHeight;
-      var lineHeight = getLineHeight(document.getElementById(id + "_caption"));
-      var lines = Math.round(divHeight / lineHeight);
-      return lines;
-    }
+  const [captionTextAlign, setCaptionTextAlign] = useState("center");
 
-    // Change the display of the caption based on the number of lines in the caption
-    // Same behavior as LaTeX
-    function handleResize() {
-      if (countCaptionLines() >= 2) {
-        setCaptionTextAlign("left"); // If the caption is 2 or more lines long, it is left-aligned
-      } else { 
-        setCaptionTextAlign("center"); // Otherwise, it is centered
-      }
+  // Helper function for countCaptionLines
+  function getLineHeight(el) {
+    var temp = document.createElement(el.nodeName), ret;
+    temp.setAttribute("style", "margin:0; padding:0; "
+      + "font-family:" + (el.style.fontFamily || "inherit") + "; "
+      + "font-size:" + (el.style.fontSize || "inherit"));
+    temp.innerHTML = "A";
+
+    el.parentNode.appendChild(temp);
+    ret = temp.clientHeight;
+    temp.parentNode.removeChild(temp);
+
+    return ret;
+  }
+
+  // Helper function for handleResize
+  // Count the number of lines in the caption
+  function countCaptionLines() {
+    var el = document.getElementById(id + "_caption");
+    var divHeight = el.offsetHeight;
+    var lineHeight = getLineHeight(document.getElementById(id + "_caption"));
+    var lines = Math.round(divHeight / lineHeight);
+    return lines;
+  }
+
+  // Change the display of the caption based on the number of lines in the caption
+  // Same behavior as LaTeX
+  function handleResize() {
+    if (countCaptionLines() >= 2) {
+      setCaptionTextAlign("left"); // If the caption is 2 or more lines long, it is left-aligned
+    } else {
+      setCaptionTextAlign("center"); // Otherwise, it is centered
     }
-   
-    return (
-      <VisibilitySensor partialVisibility={true} onChange={onChangeVisibility}>
+  }
+
+  return (
+    <VisibilitySensor partialVisibility={true} onChange={onChangeVisibility}>
       <figure id={id} style={{ margin: "12px 0" }}>
         <a name={id} />
         {childrenToRender}
-        <figcaption id={ id + "_caption" }>
+        <figcaption id={id + "_caption"}>
           <Measure onResize={handleResize}>
             {({ measureRef }) => (
               <div ref={measureRef} style={{ textAlign: captionTextAlign }}>
@@ -111,7 +112,7 @@ export default React.memo(function Figure(props) {
           </Measure>
         </figcaption>
       </figure>
-      </VisibilitySensor>
-    )
+    </VisibilitySensor>
+  )
 })
 
--- src/Viewer/renderers/table.jsx
+++ src/Viewer/renderers/table.jsx
@@ -25,47 +25,48 @@ export default React.memo(function Table(props) {
     return null;
   }
 
-    let heading = null;
+  let heading = null;
 
-    let childrenToRender = [...children];
+  let childrenToRender = [...children];
 
-    // BADBADBAD: need to redo how getting the title child
-    // getting it using the internal guts of componentInstructions
-    // is just asking for trouble
+  // BADBADBAD: need to redo how getting the title child
+  // getting it using the internal guts of componentInstructions
+  // is just asking for trouble
 
-    let title;
-    if (SVs.titleChildName) {
-      let titleChildInd;
-      for (let [ind, child] of children.entries()) {
-        if (typeof child !== "string" && child.props.componentInstructions.componentName === SVs.titleChildName) {
-          titleChildInd = ind;
-          break;
-        }
+  let title;
+  if (SVs.titleChildName) {
+    let titleChildInd;
+    for (let [ind, child] of children.entries()) {
+      //child might be null or a string
+      if (child?.props?.componentInstructions.componentName === SVs.titleChildName) {
+        titleChildInd = ind;
+        break;
       }
-      title = children[titleChildInd];
-      childrenToRender.splice(titleChildInd, 1); // remove title
+    }
+    title = children[titleChildInd];
+    childrenToRender.splice(titleChildInd, 1); // remove title
+  } else {
+    title = SVs.title;
+  }
+
+  if (!SVs.suppressTableNameInTitle) {
+    let tableName = <strong>{SVs.tableName}</strong>
+    if (title) {
+      title = <>{tableName}: {title}</>
     } else {
-      title = SVs.title;
+      title = tableName;
     }
+  }
 
-    if (!SVs.suppressTableNameInTitle) {
-      let tableName = <strong>{SVs.tableName}</strong>
-      if (title) {
-        title = <>{tableName}: {title}</>
-      } else {
-        title = tableName;
-      }
-    }
+  heading = <div id={id + "_title"}>{title}</div>
 
-    heading = <div id={id + "_title"}>{title}</div>
-
-    return (
-      <VisibilitySensor partialVisibility={true} onChange={onChangeVisibility}>
+  return (
+    <VisibilitySensor partialVisibility={true} onChange={onChangeVisibility}>
       <div id={id} style={{ margin: "12px 0" }}>
         <a name={id} />
         {heading}
         {childrenToRender}
       </div>
-      </VisibilitySensor>
-    )
+    </VisibilitySensor>
+  )
 })
