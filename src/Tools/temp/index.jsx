e6cc4456732d43330cee40ce1d0c4fe37446aa99
Kevin Charles <kevinmcharles@gmail.com>
2023-02-16 17:39:44 +0000

    Carousel

--- src/Tools/temp/index.jsx
+++ src/Tools/temp/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
+import { Carousel } from '../../_reactComponents/PanelHeaderComponents/Carousel';
 
 ReactDOM.render(
   <>
-  test here
+    <Carousel />
   </>,
   document.getElementById('root'),
 );
---
+++ src/_reactComponents/PanelHeaderComponents/Carousel.jsx
@@ -0,0 +1,22 @@
+import React, { useRef } from "react";
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+
+export function Carousel() {
+  return (
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={3}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      {/* <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      ... */}
+    </Swiper>
+  );
+};
