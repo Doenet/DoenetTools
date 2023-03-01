ab4c0b8d7b843d31e3ae18d085612510f9f10370
Emilio Alvarez <e.l.alvarez@icloud.com>
2023-03-01 10:57:59 -0600

    move files (app disbled)

---
+++ src/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
+
+import ToolRoot from './Tools/_framework/NewToolRoot';
+import { MathJaxContext } from 'better-react-mathjax';
+import { mathjaxConfig } from './Core/utils/math';
+import DarkmodeController from './Tools/_framework/DarkmodeController';
+
+ReactDOM.render(
+  <div>hello2</div>,
+  // <RecoilRoot>
+  //   <Router>
+  //     <Routes>
+  //       <Route
+  //         path="*"
+  //         element={
+  //           <DarkmodeController>
+  //             <MathJaxContext
+  //               version={2}
+  //               config={mathjaxConfig}
+  //               onStartup={(mathJax) => (mathJax.Hub.processSectionDelay = 0)}
+  //             >
+  //               <ToolRoot />
+  //             </MathJaxContext>
+  //           </DarkmodeController>
+  //         }
+  //       />
+  //     </Routes>
+  //   </Router>
+  // </RecoilRoot>,
+  document.getElementById('root'),
+);
