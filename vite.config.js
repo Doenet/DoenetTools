45856ded7d17e301b716f318dbe6e7f48f5fba35
Emilio Alvarez <e.l.alvarez@icloud.com>
2023-03-01 12:37:26 -0600

    swtich to mpa

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,18 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  appType: 'mpa',
   plugins: [react()],
+  resolve: {
+    alias: [{ find: '@Toast', replacement: './src/Tools/_framework/Toast' }],
+  },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8080',
+      '/cyapi': 'http://localhost:8080',
+    },
+  },
+  build: {
+    rollupOptions: {},
+  },
 });
