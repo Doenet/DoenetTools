fdbdbe212206f7ea79e446324b1e5925512c2c90
Duane Nykamp <nykamp@umn.edu>
2023-03-03 17:02:47 -0600

    doenetML state variable and displayDoenetML component (#1964)

---
+++ cypress/e2e/DoenetML/baseComponent/doenetMLtext.cy.js
@@ -0,0 +1,126 @@
+import cssesc from 'cssesc';
+
+function cesc(s) {
+  s = cssesc(s, { isIdentifier: true });
+  if (s.slice(0, 2) === '\\#') {
+    s = s.slice(1);
+  }
+  return s;
+}
+
+describe('DoenetML tests', function () {
+
+  beforeEach(() => {
+    cy.clearIndexedDB();
+    cy.visit('/cypressTest')
+  })
+
+  it('doenetML state variable', () => {
+    cy.window().then(async (win) => {
+      win.postMessage({
+        doenetML: `
+        <p name="theP">
+          Did you know that
+          <math name="m">1 + 1</math>
+          =
+          $m{simplify}?
+        </p>
+      
+        <pre name="theDoenetML">$theP.doenetML</pre>
+      
+  `}, "*");
+    });
+
+    let thePDoenetML = `<p name="theP">
+  Did you know that
+  <math name="m">1 + 1</math>
+  =
+  $m{simplify}?
+</p>
+`
+
+    cy.get('#\\/theP').should('contains.text', '\n          Did you know that\n          1+1');
+    cy.get('#\\/theP').should('contains.text', '1\n          =\n          2');
+    cy.get('#\\/theDoenetML').should('have.text', thePDoenetML)
+
+
+    cy.window().then(async (win) => {
+      let stateVariables = await win.returnAllStateVariables1();
+      expect(stateVariables["/theP"].stateValues.doenetML).eqls(thePDoenetML);
+
+      let preChild = stateVariables["/theDoenetML"].activeChildren[0].componentName;
+      expect(stateVariables[preChild].stateValues.value).eqls(thePDoenetML);
+    })
+
+
+  })
+
+  it('doenetML from displayDoenetML', () => {
+    cy.window().then(async (win) => {
+      win.postMessage({
+        doenetML: `
+        <pre name="theDoenetML"><displayDoenetML name="ddml">
+          <p>A graph of a point</p>
+
+          <graph>
+            <point name="P">(3,4)</point>
+          </graph>
+
+          A string by itself!
+        </displayDoenetML></pre>
+      
+        <p name="pxcoord">The x-coordinate is $P.x.</p>
+        <p>Change the x-coordinate: <mathinput name="mixcoord" bindValueTo="$P.x" /></p>
+
+
+        <pre name="theDoenetML2"><displayDoenetML>String with no space.</displayDoenetML></pre>
+
+  `}, "*");
+    });
+
+    let thePDoenetML = `<p>A graph of a point</p>
+
+<graph>
+  <point name="P">(3,4)</point>
+</graph>
+
+A string by itself!
+`
+
+    cy.get('#\\/theDoenetML').should('have.text', thePDoenetML)
+    cy.get('#\\/theDoenetML2').should('have.text', "String with no space.\n")
+
+    cy.get('#\\/pxcoord .mjx-mrow').eq(0).should('have.text', '3')
+
+
+    cy.window().then(async (win) => {
+      let stateVariables = await win.returnAllStateVariables1();
+      expect(stateVariables["/ddml"].stateValues.value).eq(thePDoenetML);
+      expect(stateVariables["/ddml"].stateValues.text).eq(thePDoenetML);
+      expect(stateVariables["/_displaydoenetml2"].stateValues.value).eq("String with no space.\n")
+
+      expect(stateVariables["/P"].stateValues.xs).eqls([3, 4]);
+    })
+
+
+
+    cy.log("Change point coords, don't change DoenetML")
+    cy.get("#\\/mixcoord textarea").type("{end}{backspace}9{enter}", { force: true })
+
+    cy.get('#\\/pxcoord .mjx-mrow').should('contain.text', '9')
+    cy.get('#\\/pxcoord .mjx-mrow').eq(0).should('have.text', '9')
+
+    cy.get('#\\/theDoenetML').should('have.text', thePDoenetML)
+
+    cy.window().then(async (win) => {
+      let stateVariables = await win.returnAllStateVariables1();
+      expect(stateVariables["/ddml"].stateValues.value).eq(thePDoenetML);
+      expect(stateVariables["/ddml"].stateValues.text).eq(thePDoenetML);
+      expect(stateVariables["/_displaydoenetml2"].stateValues.value).eq("String with no space.\n")
+
+      expect(stateVariables["/P"].stateValues.xs).eqls([9, 4]);
+    })
+  })
+
+
+})
--- src/Core/components/Verbatim.js
+++ src/Core/components/Verbatim.js
@@ -17,9 +17,87 @@ export class Pre extends BlockComponent {
   static returnChildGroups() {
 
     return [{
-      group: "inlines",
-      componentTypes: ["_inline"]
+      group: "allChildren",
+      componentTypes: ["_base"]
     }]
+
+  }
+
+  recordVisibilityChange({ isVisible, actionId }) {
+    this.coreFunctions.requestRecordEvent({
+      verb: "visibilityChanged",
+      object: {
+        componentName: this.componentName,
+        componentType: this.componentType,
+      },
+      result: { isVisible }
+    })
+    this.coreFunctions.resolveAction({ actionId });
+  }
+
+}
+
+
+
+export class DisplayDoenetML extends BlockComponent {
+  constructor(args) {
+    super(args);
+
+    Object.assign(this.actions, {
+      recordVisibilityChange: this.recordVisibilityChange.bind(this),
+    });
+
+  }
+  static componentType = "displayDoenetML";
+  static rendererType = "text";
+
+  static includeBlankStringChildren = true;
+
+  static returnChildGroups() {
+
+    return [{
+      group: "allChildren",
+      componentTypes: ["_base"]
+    }]
+  }
+
+  static returnStateVariableDefinitions() {
+
+    let stateVariableDefinitions = super.returnStateVariableDefinitions();
+
+    stateVariableDefinitions.value = {
+      returnDependencies: () => ({
+        childrenDoenetML: {
+          dependencyType: "doenetML",
+          displayOnlyChildren: true,
+        }
+      }),
+      definition({ dependencyValues }) {
+        return { setValue: { value: dependencyValues.childrenDoenetML } }
+      }
+
+    }
+
+
+    stateVariableDefinitions.text = {
+      public: true,
+      shadowingInstructions: {
+        createComponentOfType: "text",
+      },
+      forRenderer: true,
+      returnDependencies: () => ({
+        value: {
+          dependencyType: "stateVariable",
+          variableName: "value"
+        }
+      }),
+      definition: ({ dependencyValues }) => ({
+        setValue: { text: dependencyValues.value }
+      })
+    }
+
+
+    return stateVariableDefinitions;
 
   }
 
