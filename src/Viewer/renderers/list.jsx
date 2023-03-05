e3a1fa7d8f0a8f087cb2c2eeed2cc772debe2dc7
Duane Nykamp <nykamp@umn.edu>
2023-03-05 03:16:08 +0000

    nested list items switch numbering scheme

--- src/Viewer/renderers/list.jsx
+++ src/Viewer/renderers/list.jsx
@@ -29,10 +29,44 @@ export default React.memo(function List(props) {
   if (SVs.item) {
     return <VisibilitySensor partialVisibility={true} onChange={onChangeVisibility} requireContentsSize={false}><><a name={id} /><li id={id}>{children}</li></></VisibilitySensor>;
   } else if (SVs.numbered) {
-    return <VisibilitySensor partialVisibility={true} onChange={onChangeVisibility}><><ol id={id}><a name={id} />{children}</ol></></VisibilitySensor>;
+    let list_style = styleTypeByLevel[SVs.level]?.numbered;
+    if (!list_style) {
+      list_style = "decimal";
+    }
+    return <VisibilitySensor partialVisibility={true} onChange={onChangeVisibility}><><ol id={id} style={{ listStyleType: list_style }}><a name={id} />{children}</ol></></VisibilitySensor>;
   } else {
-    return <VisibilitySensor partialVisibility={true} onChange={onChangeVisibility}><><ul id={id}><a name={id} />{children}</ul></></VisibilitySensor>;
+    let list_style = styleTypeByLevel[SVs.level]?.unnumbered;
+    if (!list_style) {
+      list_style = "disc";
+    }
+    return <VisibilitySensor partialVisibility={true} onChange={onChangeVisibility}><><ul id={id} style={{ listStyleType: list_style }}><a name={id} />{children}</ul></></VisibilitySensor>;
   }
 
 })
 
+const styleTypeByLevel = {
+  1: {
+    numbered: 'decimal',
+    unnumbered: 'disc',
+  },
+  2: {
+    numbered: "lower-alpha",
+    unnumbered: 'circle',
+  },
+  3: {
+    numbered: "lower-roman",
+    unnumbered: 'square',
+  },
+  4: {
+    numbered: "upper-alpha",
+    unnumbered: 'disc',
+  },
+  5: {
+    numbered: "upper-roman",
+    unnumbered: 'circle',
+  },
+  6: {
+    numbered: "decimal",
+    unnumbered: 'square',
+  },
+}
