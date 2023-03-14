e63643bff597ded42abf2f8e1117f4edd16fbe95
Kevin Charles <kevinmcharles@gmail.com>
2023-03-14 03:22:42 +0000

    also

---
+++ src/Api/getPortfolio.php
@@ -0,0 +1,67 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: access');
+header('Access-Control-Allow-Methods: GET');
+header('Access-Control-Allow-Credentials: true');
+header('Content-Type: application/json');
+
+include 'db_connection.php';
+
+$jwtArray = include 'jwtArray.php';
+$userId = $jwtArray['userId'];
+
+$success = true;
+$message = '';
+
+if ($userId == '') {
+    $success = false;
+    $message = 'Error: You need to sign in';
+}
+
+$publicActivities = [];
+$privateActivities = [];
+
+//Assume we are updating the activity and need the current settings
+if ($success) {
+    $sql = "
+    SELECT doenetId,
+    imagePath,
+    label,
+    isPublic
+    FROM course_content
+    WHERE courseId = (SELECT courseId FROM course WHERE portfolioCourseForUserId = '$userId')
+    ";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $activity = [
+                'doenetId' => $row['doenetId'],
+                'imagePath' => $row['imagePath'],
+                'label' => $row['label'],
+                'public' => $row['isPublic'],
+            ];
+            if ($row['isPublic'] == '1') {
+                array_push($publicActivities, $activity);
+            } else {
+                array_push($privateActivities, $activity);
+            }
+        }
+    }
+}
+
+$response_arr = [
+    'success' => $success,
+    'message' => $message,
+    'publicActivities' => $publicActivities,
+    'privateActivities' => $privateActivities,
+];
+
+http_response_code(200);
+
+// make it json format
+echo json_encode($response_arr);
+
+$conn->close();
+
+?>
