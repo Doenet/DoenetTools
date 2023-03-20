9eaafa5cbd68a0a39bf37a3e768a08d3464329d7
Kevin Charles <kevinmcharles@gmail.com>
2023-03-20 18:15:47 +0000

    React Dom Root

--- src/Tools/singlepage/index.jsx
+++ src/Tools/singlepage/index.jsx
@@ -1,13 +1,12 @@
 
 import React from "react";
-// import { createRoot } from "react-dom/client";
-import ReactDOM from 'react-dom';
 
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import { RecoilRoot } from 'recoil';
+import { createRoot } from "react-dom/client";
 
 
 import ToolRoot from '../_framework/NewToolRoot';
