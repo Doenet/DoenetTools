e668f5a05b91a8acd22a32a1c89430e6ffb1fd65
Emilio Alvarez <e.l.alvarez@icloud.com>
2023-04-07 17:00:22 +0000

    explicit worker module type

--- vite.config.js
+++ vite.config.js
@@ -41,6 +41,9 @@ export default defineConfig((env) => ({
       NodeModulesPolyfillPlugin(),
     ],
   },
+  worker: {
+    format: 'iife',
+  },
   build: {
     outDir: './dist_local',
     rollupOptions: {
