c2b9a5e8c3c9ad4683dbc3d58825c24c39c6956c
keaganeng <eng00029@umn.edu>
2023-04-11 16:34:25 +0000

    Updates to keyboard buttons and tabs

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+// import type { StyleFunctionProps } from '@chakra-ui/styled-system';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
@@ -7,6 +9,53 @@ import ToolRoot from './Tools/_framework/NewToolRoot';
 import { MathJaxContext } from 'better-react-mathjax';
 import { mathjaxConfig } from './Core/utils/math';
 import DarkmodeController from './Tools/_framework/DarkmodeController';
+
+const theme = extendTheme({
+  fonts: {
+    body: 'Jost',
+  },
+  textStyles: {
+    primary: {
+      fontFamily: 'Jost',
+    },
+  },
+  components: {
+    Button: {
+      baseStyle: {
+        margin: '1px',
+        height: '30px',
+      },
+      variants: {
+        transition: {
+          bg: 'var(--mainBlue)',
+          color: 'white',
+        },
+        letter: {
+          flexBasis: '9.5%',
+          // margin: '1px',
+          // height: '30px',
+          // outline: '2px solid var(--mainBlue)',
+        },
+        letterTransition: {
+          flexBasis: '14%',
+          // margin: '1px',
+          // height: '30px',
+          outline: '2px solid var(--mainBlue)',
+        },
+        space: {
+          flexBasis: '49%',
+          // margin: '1px',
+          // height: '30px',
+          outline: '2px solid var(--mainBlue)',
+        },
+      },
+      defaultProps: {
+        size: 'md',
+        // variant: 'outline',
+      },
+    },
+  },
+});
 
 const root = createRoot(document.getElementById('root'));
 root.render(
@@ -22,12 +71,14 @@ root.render(
                 config={mathjaxConfig}
                 onStartup={(mathJax) => (mathJax.Hub.processSectionDelay = 0)}
               >
-                <ToolRoot />
+                <ChakraProvider theme={theme}>
+                  <ToolRoot />
+                </ChakraProvider>
               </MathJaxContext>
             </DarkmodeController>
           }
         />
       </Routes>
     </Router>
-  </RecoilRoot>
+  </RecoilRoot>,
 );
