d7eaaf42965d6d8f3afde664a3569b3ec7aaa93b
Duane Nykamp <nykamp@umn.edu>
2023-04-12 10:30:19 -0500

    delay animation start until core is initialized (#2024)

--- cypress/e2e/Editor/Editor.cy.js
+++ cypress/e2e/Editor/Editor.cy.js
@@ -1,5 +1,7 @@
 // import {signIn} from '../DoenetSignin/DoenetSignin.cy';
 
+const { cesc2 } = require("../../../src/_utils/url");
+
 
 describe('doenetEditor test', function () {
   const userId = "cyuserId";
@@ -37,25 +39,25 @@ describe('doenetEditor test', function () {
   it('basic test of update button', () => {
     const doenetMLString = 'abcdefg'
     cy.get('.cm-content').type(doenetMLString)
-    cy.get('#\\/_document1').should('not.contain', 'a');
+    cy.get(cesc2('#/_document1')).should('not.contain', 'a');
     cy.get('[data-test="Viewer Update Button"]').click();
-    cy.get('#\\/_document1').contains(doenetMLString)
+    cy.get(cesc2('#/_document1')).contains(doenetMLString)
   })
 
   it('ctrl+s updates viewer', () => {
     const doenetMLString = 'abcdefg'
     cy.get('.cm-content').type(doenetMLString)
-    cy.get('#\\/_document1').should('not.contain', 'a');
+    cy.get(cesc2('#/_document1')).should('not.contain', 'a');
     cy.get('.cm-content').type('{control}{s}')
-    cy.get('#\\/_document1').contains(doenetMLString)
+    cy.get(cesc2('#/_document1')).contains(doenetMLString)
   })
 
   it('command+s updates viewer', () => {
     const doenetMLString = 'abcdefg'
     cy.get('.cm-content').type(doenetMLString)
-    cy.get('#\\/_document1').should('not.contain', 'a');
+    cy.get(cesc2('#/_document1')).should('not.contain', 'a');
     cy.get('.cm-content').type('{command}{s}')
-    cy.get('#\\/_document1').contains(doenetMLString)
+    cy.get(cesc2('#/_document1')).contains(doenetMLString)
   })
 
   it('Page Variant Menu Test', () => {
@@ -64,13 +66,15 @@ describe('doenetEditor test', function () {
     // const doenetMLString = `<selectFromSequence  assignNames='${componentName}' />`
     cy.get('.cm-content').type(doenetMLString)
     cy.get('[data-test="Viewer Update Button"]').click();
+    cy.get(cesc2(`#/${componentName}`)).contains('1')
+
     cy.get('[data-test="PageVariant Menu"]').click();
 
     cy.get('[data-test="Variant Index Input"]').invoke('val', '3').type(' {enter}')
-    cy.get(cesc(`#\\/${componentName}`)).contains('3')
+    cy.get(cesc2(`#/${componentName}`)).contains('3')
 
     cy.get('[data-test="Variant Name Input"]').invoke('val', '4').trigger('change');
-    cy.get(cesc(`#\\/${componentName}`)).contains('4')
+    cy.get(cesc2(`#/${componentName}`)).contains('4')
 
 
   })
@@ -86,10 +90,12 @@ describe('doenetEditor test', function () {
     cy.get('[data-test="Unassign Activity"]').should('be.visible');
 
     cy.visit(`/course?tool=assignment&doenetId=${doenetId}`)
-    cy.get('#\\/problem1_title').contains('Problem 1')
+    cy.get(cesc2('#/problem1_title')).contains('Problem 1')
 
   })
 
+
+  // TODO: This test fails because the bread crumb menu closes immediately after clicking it open
   it('Assign activity and navigate using Breadcrumbs', () => {
     const doenetMLString = '<problem name="problem1"><answer>42</answer></problem>'
 
@@ -103,9 +109,10 @@ describe('doenetEditor test', function () {
     cy.get('.navigationRow').should('have.length', 1); //Need this to wait for the row to appear
     cy.get('.navigationRow').eq(0).get('.navigationColumn1').click();
     cy.get('[data-test="View Assigned Activity"]').click();
-    cy.get('#\\/problem1_title').contains('Problem 1')
+    cy.get(cesc2('#/problem1_title')).contains('Problem 1')
   })
 
+  // TODO: This test fails because the bread crumb menu closes immediately after clicking it open
   it('Assign two-page activity', () => {
     const doenetMLString1 = `
 <section>
@@ -162,15 +169,15 @@ describe('doenetEditor test', function () {
 
     cy.get('[data-test="View Activity"]').click();
 
-    cy.get('#page1\\/_section1_title').should('have.text', 'Section 1')
+    cy.get(cesc2('#page1/_section1_title')).should('have.text', 'Section 1')
 
     cy.get('[data-test="Possible Points"]').should('have.text', '10')
     cy.get('[data-test="Final Score"]').should('have.text', '0')
     cy.get('[data-test="Attempt Percent"]').should('have.text', '0%')
     cy.get('[data-test="Assignment Percent"]').should('have.text', '0%')
 
-    cy.get(cesc('#page1\\/a') + ' textarea').type("{enter}", { force: true })
-    cy.get(cesc('#page1\\/_p2') + ' .mjx-mrow').should('contain.text', '\uff3f')
+    cy.get(cesc2('#page1/a') + ' textarea').type("{enter}", { force: true })
+    cy.get(cesc2('#page1/_p2') + ' .mjx-mrow').should('contain.text', '\uff3f')
 
     cy.get('[data-test="Item 1 Credit"]').should('have.text', '0%')
     cy.get('[data-test="Item 2 Credit"]').should('have.text', '0%')
@@ -181,8 +188,8 @@ describe('doenetEditor test', function () {
 
     // TODO: why does this frequently fail if don't wait here?
     cy.wait(500)
-    cy.get(cesc('#page1\\/a') + ' textarea').type("2{enter}", { force: true })
-    cy.get(cesc('#page1\\/_p2') + ' .mjx-mrow').should('contain.text', '2')
+    cy.get(cesc2('#page1/a') + ' textarea').type("2{enter}", { force: true })
+    cy.get(cesc2('#page1/_p2') + ' .mjx-mrow').should('contain.text', '2')
 
     cy.get('[data-test="Final Score"]').should('have.text', '5')
     cy.get('[data-test="Attempt Percent"]').should('have.text', '50%')
@@ -195,7 +202,7 @@ describe('doenetEditor test', function () {
 
     cy.wait(2000);  // make sure debounce occurred while waiting
 
-    cy.get('#page2\\/_section1_title').should('have.text', 'Section 2')
+    cy.get(cesc2('#page2/_section1_title')).should('have.text', 'Section 2')
 
     cy.get('[data-test="Final Score"]').should('have.text', '5')
     cy.get('[data-test="Attempt Percent"]').should('have.text', '50%')
@@ -205,8 +212,8 @@ describe('doenetEditor test', function () {
     cy.get('[data-test="Possible Points"]').should('have.text', '10')
 
 
-    cy.get(cesc('#page2\\/a') + ' textarea').type("1{enter}", { force: true })
-    cy.get(cesc('#page2\\/_p2') + ' .mjx-mrow').should('contain.text', '1')
+    cy.get(cesc2('#page2/a') + ' textarea').type("1{enter}", { force: true })
+    cy.get(cesc2('#page2/_p2') + ' .mjx-mrow').should('contain.text', '1')
 
     // cy.get('[data-test="Final Score"]').should('have.text', '10')
     // cy.get('[data-test="Attempt Percent"]').should('have.text', '100%')
@@ -219,8 +226,8 @@ describe('doenetEditor test', function () {
     cy.log('Reloading immediately does not save state but saves credit achieved')
     cy.reload();
 
-    cy.get('#page2\\/_section1_title').should('have.text', 'Section 2')
-    cy.get('#page2\\/_p2').should('not.contain.text', '1')
+    cy.get(cesc2('#page2/_section1_title')).should('have.text', 'Section 2')
+    cy.get(cesc2('#/problem1_title')).should('not.contain.text', '1')
 
     cy.get('[data-test="Final Score"]').should('have.text', '10')
     cy.get('[data-test="Attempt Percent"]').should('have.text', '100%')
@@ -231,17 +238,17 @@ describe('doenetEditor test', function () {
 
     cy.log('State on previous page was saved')
     cy.get('[data-test=previous]').click();
-    cy.get('#page1\\/_section1_title').should('have.text', 'Section 1')
-    cy.get(cesc('#page1\\/_p2') + ' .mjx-mrow').should('contain.text', '2')
+    cy.get(cesc2('#page1/_section1_title')).should('have.text', 'Section 1')
+    cy.get(cesc2('#page1/_p2') + ' .mjx-mrow').should('contain.text', '2')
 
     cy.get('[data-test=next]').click();
-    cy.get('#page2\\/_section1_title').should('have.text', 'Section 2')
+    cy.get(cesc2('#page2/_section1_title')).should('have.text', 'Section 2')
 
 
     cy.log('New attempt, goes back to page 1')
     cy.get('[data-test="New Attempt"]').click();
 
-    cy.get('#page1\\/_section1_title').should('have.text', 'Section 1')
+    cy.get(cesc2('#page1/_section1_title')).should('have.text', 'Section 1')
 
     cy.get('[data-test="Attempt Percent"]').should('have.text', '0%')
     cy.get('[data-test="Final Score"]').should('have.text', '10')
@@ -249,9 +256,9 @@ describe('doenetEditor test', function () {
     cy.get('[data-test="Possible Points"]').should('have.text', '10')
 
     cy.get('[data-test=next]').click();
-    cy.get(cesc('#page2\\/a') + ' textarea').type("1{enter}", { force: true })
+    cy.get(cesc2('#page2/a') + ' textarea').type("1{enter}", { force: true })
 
-    cy.get(cesc('#page2\\/_p2') + ' .mjx-mrow').should('contain.text', '1')
+    cy.get(cesc2('#page2/_p2') + ' .mjx-mrow').should('contain.text', '1')
 
     cy.get('[data-test="Attempt Percent"]').should('have.text', '50%')
     cy.get('[data-test="Item 1 Credit"]').should('have.text', '0%')
@@ -266,8 +273,8 @@ describe('doenetEditor test', function () {
     cy.reload();
 
 
-    cy.get('#page2\\/_section1_title').should('have.text', 'Section 2')
-    cy.get(cesc('#page2\\/_p2') + ' .mjx-mrow').should('contain.text', '1')
+    cy.get(cesc2('#page2/_section1_title')).should('have.text', 'Section 2')
+    cy.get(cesc2('#page2/_p2') + ' .mjx-mrow').should('contain.text', '1')
 
     cy.get('[data-test="Final Score"]').should('have.text', '10')
     cy.get('[data-test="Attempt Percent"]').should('have.text', '50%')
@@ -291,49 +298,97 @@ describe('doenetEditor test', function () {
 `
     cy.get('.cm-content').type(doenetMLString)
     cy.get('[data-test="Viewer Update Button"]').click();
-    cy.get('#\\/n').should('have.text', '1')
+    cy.get(cesc2('#/n')).should('have.text', '1')
 
-    cy.get('#\\/ca_button').click();
-    cy.get('#\\/n').should('have.text', '2')
-    cy.get('#\\/n').should('have.text', '3')
+    cy.get(cesc2('#/ca_button')).click();
+    cy.get(cesc2('#/n')).should('have.text', '2')
+    cy.get(cesc2('#/n')).should('have.text', '3')
 
     cy.get('.cm-content').type("{ctrl+end}<p name='np'>More text</p>{enter}")
 
     cy.get('[data-test="Viewer Update Button"]').click();
 
-    cy.get(cesc("#\\/np")).should('have.text', 'More text')
+    cy.get(cesc2("#/np")).should('have.text', 'More text')
 
     cy.log('ensure animation has stopped')
-    cy.get('#\\/n').should('have.text', '1')
+    cy.get(cesc2('#/n')).should('have.text', '1')
     cy.wait(200)
-    cy.get('#\\/n').should('have.text', '1')
+    cy.get(cesc2('#/n')).should('have.text', '1')
 
-    cy.get('#\\/ca_button').click();
-    cy.get('#\\/n').should('have.text', '2')
-    cy.get('#\\/n').should('have.text', '3')
+    cy.get(cesc2('#/ca_button')).click();
+    cy.get(cesc2('#/n')).should('have.text', '2')
+    cy.get(cesc2('#/n')).should('have.text', '3')
 
-    cy.get('#\\/ca_button').click();
+    cy.get(cesc2('#/ca_button')).click();
     cy.wait(200)
-    cy.get('#\\/n').contains(/3|4/)
+    cy.get(cesc2('#/n')).contains(/3|4/)
 
     cy.get('.cm-content').type("{ctrl+end}<p name='np2'>And more</p>{enter}")
 
     cy.get('[data-test="Viewer Update Button"]').click();
 
-    cy.get(cesc("#\\/np2")).should('have.text', 'And more')
+    cy.get(cesc2("#/np2")).should('have.text', 'And more')
 
     cy.log('ensure animation has stopped')
-    cy.get('#\\/n').should('have.text', '1')
+    cy.get(cesc2('#/n')).should('have.text', '1')
     cy.wait(200)
-    cy.get('#\\/n').should('have.text', '1')
+    cy.get(cesc2('#/n')).should('have.text', '1')
 
-    cy.get('#\\/ca_button').click();
-    cy.get('#\\/n').should('have.text', '2')
-    cy.get('#\\/n').should('have.text', '3')
+    cy.get(cesc2('#/ca_button')).click();
+    cy.get(cesc2('#/n')).should('have.text', '2')
+    cy.get(cesc2('#/n')).should('have.text', '3')
 
-    cy.get('#\\/ca_button').click();
+    cy.get(cesc2('#/ca_button')).click();
     cy.wait(200)
-    cy.get('#\\/n').contains(/3|4/)
+    cy.get(cesc2('#/n')).contains(/3|4/)
+
+  })
+
+  it('animation on when click update button', () => {
+    const doenetMLString = `
+<number name="n">1</number>
+
+<animateFromSequence target="n" animationMode="increase once" from="1" to="100" animationInterval="100" name="a" animationOn />
+
+<p><callAction target="a" actionName="toggleAnimation" name="ca" >
+<label>Toggle animation</label>
+</callAction></p>
+`
+    cy.get('.cm-content').type(doenetMLString)
+    cy.get('[data-test="Viewer Update Button"]').click();
+    cy.get(cesc2('#/n')).should('have.text', '1')
+    cy.get(cesc2('#/n')).should('have.text', '2')
+    cy.get(cesc2('#/n')).should('have.text', '3')
+
+    cy.get('.cm-content').type("{ctrl+end}<p name='np'>More text</p>{enter}")
+
+    cy.get('[data-test="Viewer Update Button"]').click();
+
+    cy.get(cesc2("#/np")).should('have.text', 'More text')
+
+    cy.log('ensure animation has started again')
+    cy.get(cesc2('#/n')).should('have.text', '1')
+    cy.get(cesc2('#/n')).should('have.text', '2')
+    cy.get(cesc2('#/n')).should('have.text', '3')
+
+    cy.get(cesc2('#/ca_button')).click();
+    cy.wait(200)
+    cy.get(cesc2('#/n')).contains(/3|4/)
+
+    cy.get('.cm-content').type("{ctrl+end}<p name='np2'>And more</p>{enter}")
+
+    cy.get('[data-test="Viewer Update Button"]').click();
+
+    cy.get(cesc2("#/np2")).should('have.text', 'And more')
+
+    cy.log('ensure animation has started again')
+    cy.get(cesc2('#/n')).should('have.text', '1')
+    cy.get(cesc2('#/n')).should('have.text', '2')
+    cy.get(cesc2('#/n')).should('have.text', '3')
+
+    cy.get(cesc2('#/ca_button')).click();
+    cy.wait(200)
+    cy.get(cesc2('#/n')).contains(/3|4/)
 
   })
 
@@ -356,7 +411,7 @@ describe('doenetEditor test', function () {
     cy.get('.cm-content').type(doenetMLString)
     cy.get('[data-test="Viewer Update Button"]').click();
 
-    cy.get('#\\/_section1_title').should('have.text', 'Section 1')
+    cy.get(cesc2('#/_section1_title')).should('have.text', 'Section 1')
     cy.url().should('match', /[^#]/)
 
     cy.get('[data-test="Main Panel"]').then(el => {
@@ -364,40 +419,40 @@ describe('doenetEditor test', function () {
     })
 
 
-    cy.get('#\\/asideTitle').should('have.text', 'The aside');
-    cy.get('#\\/insideAside').should('not.exist');
+    cy.get(cesc2('#/asideTitle')).should('have.text', 'The aside');
+    cy.get(cesc2('#/insideAside')).should('not.exist');
 
 
-    cy.get('#\\/toAside').click();
-    cy.url().should('match', /#\/aside$/)
+    cy.get(cesc2('#/toAside')).click();
+    cy.url().should('match', /#\\\/aside$/)
 
-    cy.get('#\\/aside').then(el => {
+    cy.get(cesc2('#/aside')).then(el => {
       let rect = el[0].getBoundingClientRect();
       expect(rect.top).gt(headerPixels - 1).lt(headerPixels + 1)
     })
 
 
-    cy.get('#\\/insideAside').should('have.text', 'Content in aside');
+    cy.get(cesc2('#/insideAside')).should('have.text', 'Content in aside');
 
-    cy.get('#\\/asideTitle').click();
-    cy.get('#\\/insideAside').should('not.exist');
+    cy.get(cesc2('#/asideTitle')).click();
+    cy.get(cesc2('#/insideAside')).should('not.exist');
 
-    cy.get('#\\/toAside').scrollIntoView();
+    cy.get(cesc2('#/toAside')).scrollIntoView();
 
-    cy.get('#\\/toAside').then(el => {
+    cy.get(cesc2('#/toAside')).then(el => {
       let rect = el[0].getBoundingClientRect();
       expect(rect.top).gt(headerPixels - 1).lt(headerPixels + 1)
     })
 
-    cy.url().should('match', /#\/aside$/)
+    cy.url().should('match', /#\\\/aside$/)
 
 
-    cy.get('#\\/toAside').click();
-    cy.get('#\\/insideAside').should('have.text', 'Content in aside');
+    cy.get(cesc2('#/toAside')).click();
+    cy.get(cesc2('#/insideAside')).should('have.text', 'Content in aside');
 
-    cy.url().should('match', /#\/aside$/)
+    cy.url().should('match', /#\\\/aside$/)
 
-    cy.get('#\\/aside').then(el => {
+    cy.get(cesc2('#/aside')).then(el => {
       let rect = el[0].getBoundingClientRect();
       expect(rect.top).gt(headerPixels - 1).lt(headerPixels + 1)
     })
@@ -440,26 +495,26 @@ describe('doenetEditor test', function () {
     })
 
 
-    cy.get('#\\/asideTitle').should('have.text', 'The aside');
-    cy.get('#\\/insideAside').should('not.exist');
+    cy.get(cesc2('#/asideTitle')).should('have.text', 'The aside');
+    cy.get(cesc2('#/insideAside')).should('not.exist');
 
-    cy.get('#\\/toAside').scrollIntoView();
+    cy.get(cesc2('#/toAside')).scrollIntoView();
 
-    cy.get('#\\/toAside').then(el => {
+    cy.get(cesc2('#/toAside')).then(el => {
       let rect = el[0].getBoundingClientRect();
       expect(rect.top).gt(headerPixels - 1).lt(headerPixels + 1)
     })
 
-    cy.get('#\\/toAside').click();
+    cy.get(cesc2('#/toAside')).click();
     cy.url().should('match', /#\/aside$/)
 
-    cy.get('#\\/aside').then(el => {
+    cy.get(cesc2('#/aside')).then(el => {
       let rect = el[0].getBoundingClientRect();
       expect(rect.top).gt(headerPixels - 1).lt(headerPixels + 1)
     })
 
 
-    cy.get('#\\/insideAside').should('have.text', 'Content in aside');
+    cy.get(cesc2('#/insideAside')).should('have.text', 'Content in aside');
 
 
     cy.get('#\\/bottom').scrollIntoView();
@@ -472,7 +527,7 @@ describe('doenetEditor test', function () {
     cy.go("back");
     cy.url().should('match', /[^#]/)
 
-    cy.get('#\\/toAside').then(el => {
+    cy.get(cesc2('#/toAside')).then(el => {
       let rect = el[0].getBoundingClientRect();
       expect(rect.top).gt(headerPixels - 1).lt(headerPixels + 1)
     })
