6ab297b3e23b786d394b5805f390de7adc31ed25
Kevin Charles <kevinmcharles@gmail.com>
2023-04-25 20:09:28 +0000

    Fixed jumping of text editor

--- src/Tools/_framework/Paths/PortfolioActivityEditor.jsx
+++ src/Tools/_framework/Paths/PortfolioActivityEditor.jsx
@@ -68,21 +68,6 @@ export async function loader({ params }) {
   };
 }
 
-const ViewerInsideContainer = styled.div`
-  grid-column: 2 / 3;
-  width: 850px;
-  max-width: 850px;
-  min-width: 600px;
-  min-height: calc(100vh - 100px);
-  background: var(--canvas);
-  border: 1px solid #949494; //Viewer Outline
-  margin: 20px 0px 20px 0px; //Only need when there is an outline
-  padding: 20px 5px 20px 5px;
-  @media (max-width: 850px) {
-    width: 100vw;
-  }
-`;
-
 export function PortfolioActivityEditor() {
   const { doenetML, pageId, activityData } = useLoaderData();
   console.log("activityData", activityData);
@@ -113,17 +98,16 @@ export function PortfolioActivityEditor() {
   return (
     <Grid
       minHeight="calc(100vh - 40px)" //40px header height
-      // background="grey"
       templateAreas={`"header header header header header"
       "leftGutter viewer middleGutter textEditor rightGutter "
       `}
       templateRows="40px auto"
-      templateColumns="auto minmax(400px,600px) auto minmax(300px,600px) auto"
-      // templateColumns="auto minmax(400px,600px) auto minmax(300px,auto)"
-      // templateColumns="auto minMax('400px','850px') auto min-content"
-      // templateColumns="auto max-content auto min-content"
+      templateColumns="auto minmax(500px,800px) minmax(6px,auto) minmax(350px,600px) auto"
       position="relative"
     >
+      <GridItem area="leftGutter" background="doenet.lightBlue"></GridItem>
+      <GridItem area="middleGutter" background="doenet.lightBlue"></GridItem>
+      <GridItem area="rightGutter" background="doenet.lightBlue"></GridItem>
       <GridItem
         area="header"
         position="fixed"
@@ -137,30 +121,22 @@ export function PortfolioActivityEditor() {
           <Text>Label Here</Text>
         </Flex>
       </GridItem>
-      <GridItem area="leftGutter" background="doenet.lightBlue"></GridItem>
-      <GridItem area="middleGutter" background="doenet.lightBlue"></GridItem>
-      <GridItem area="rightGutter" background="doenet.lightBlue"></GridItem>
+
       <GridItem
         area="viewer"
         placeSelf="center"
-        // background="blue.400"
-        // minWidth="400px"
         minHeight="100%"
         width="100%"
         background="doenet.lightBlue"
-        overflow="scroll"
-        // maxWidth="850px"
       >
         <Box
-          // width="850px"
-          maxWidth="850px"
-          minWidth="600px"
           minHeight="calc(100vh - 100px)"
           background="var(--canvas)"
           border="1px solid #949494" //Viewer Outline
           margin="20px 0px 20px 0px" //Only need when there is an outline
           padding="20px 5px 20px 5px"
           flexGrow={1}
+          overflow="scroll"
         >
           <PageViewer
             key={`HPpageViewer`}
@@ -190,27 +166,17 @@ export function PortfolioActivityEditor() {
       <GridItem
         area="textEditor"
         maxWidth="600px"
-        paddingTop="30px"
         background="doenet.lightBlue"
         height="100%"
-        // justifySelf="stretch"
-        position="relative"
-        placeSelf="stretch"
+        alignSelf="start"
+        paddingTop="30px"
       >
         <Box
-          // position="fixed" //sticky?
           position="sticky"
-          top={0}
-          alignSelf="start"
-          // bottom={0}
-          // left={0}
-          // right={0}
-          // background="black"
+          top="70px"
+          boxSizing="border-box"
           background="doenet.canvas"
           height="calc(100vh - 120px)"
-          // height="200px"
-          // width="inherit"
-          // width="600px" //This should change with
           overflowY="scroll"
         >
           <CodeMirror
