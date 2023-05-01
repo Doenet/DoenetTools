fadaa81394ef56da9b0766e2dc8bb407d02d2978
ernzher <leeernzher@gmail.com>
2023-05-01 16:40:56 -0500

    Fix: SiteHeader using Chakra only

--- src/Tools/_framework/Paths/SiteHeader.jsx
+++ src/Tools/_framework/Paths/SiteHeader.jsx
@@ -1,14 +1,26 @@
-import { Box, Center, Icon, Text } from "@chakra-ui/react";
-import React, { useRef } from "react";
-import { Outlet, useLoaderData, useNavigate } from "react-router";
-import { NavLink } from "react-router-dom";
+import {
+  Box,
+  Center,
+  Icon,
+  Text,
+  useColorMode,
+  Button,
+  Flex,
+  Tabs,
+  Tab,
+  TabList,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import React, { useRef, useState, useEffect } from "react";
+import { Outlet, useLoaderData, useLocation, useNavigate } from "react-router";
+import { Link, NavLink } from "react-router-dom";
 import styled from "styled-components";
-import Button from "../../../_reactComponents/PanelHeaderComponents/Button";
+// import Button from "../../../_reactComponents/PanelHeaderComponents/Button";
 import { checkIfUserClearedOut } from "../../../_utils/applicationUtils";
 import RouterLogo from "../RouterLogo";
 import { pageToolViewAtom } from "../NewToolRoot";
 import { useRecoilState } from "recoil";
-import { FaCog } from "react-icons/fa";
+import { FaCog, FaMoon, FaSun } from "react-icons/fa";
 
 export async function loader() {
   //Check if signedIn
@@ -35,61 +47,66 @@ export async function loader() {
   return { signedIn, portfolioCourseId, isAdmin };
 }
 
-const SignInButtonContainer = styled.div`
-  margin: auto 10px auto 0px;
-`;
+// const SignInButtonContainer = styled.div`
+//   margin: auto 10px auto 0px;
+// `;
 
-const Branding = styled.span`
-  margin: 4px 0px 4px 10px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 110px;
-  cursor: default;
-  font-size: 16px;
-`;
+// const Branding = styled.span`
+//   margin: 4px 0px 4px 10px;
+//   display: flex;
+//   justify-content: space-between;
+//   align-items: center;
+//   width: 110px;
+//   cursor: default;
+//   font-size: 16px;
+// `;
 
-const StyledMenuItem = styled(NavLink)`
-  padding: 8px;
-  color: black;
-  cursor: pointer;
-  text-decoration: none;
-  &.active {
-    color: var(--mainBlue);
-    border-bottom: 2px solid var(--mainBlue);
-  }
-`;
+// const StyledMenuItem = styled(NavLink)`
+//   padding: 8px;
+//   color: black;
+//   cursor: pointer;
+//   text-decoration: none;
+//   &.active {
+//     color: var(--mainBlue);
+//     border-bottom: 2px solid var(--mainBlue);
+//   }
+// `;
 
-const BarMenu = styled.div`
-  margin: 0px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  font-size: 16px;
-  column-gap: 20px;
-`;
+// const BarMenu = styled.div`
+//   margin: 0px;
+//   display: flex;
+//   justify-content: space-between;
+//   align-items: center;
+//   font-size: 16px;
+//   column-gap: 20px;
+// `;
 
-function MenuItem({ to, children, dataTest }) {
-  return (
-    <StyledMenuItem
-      to={to}
-      data-test={dataTest}
-      className={({ isActive, isPending }) =>
-        location.pathname === "/" || isActive
-          ? "active"
-          : isPending
-          ? "pending"
-          : ""
-      }
-    >
-      {children}
-    </StyledMenuItem>
-  );
-}
+// function MenuItem({ to, children, dataTest }) {
+//   return (
+//     <StyledMenuItem
+//       to={to}
+//       data-test={dataTest}
+//       className={({ isActive, isPending }) =>
+//         location.pathname === "/" || isActive
+//           ? "active"
+//           : isPending
+//           ? "pending"
+//           : ""
+//       }
+//     >
+//       {children}
+//     </StyledMenuItem>
+//   );
+// }
 
 export function SiteHeader(props) {
   let data = useLoaderData();
   const isAdmin = data?.isAdmin;
+  const { colorMode, toggleColorMode } = useColorMode();
+  const [activeTab, setActiveTab] = useState(0);
+  let location = useLocation();
+
+   const navColor = useColorModeValue("#ffffff", "gray.800")
   // const navigate = useNavigate();
 
   const [recoilPageToolView, setRecoilPageToolView] =
@@ -103,40 +120,43 @@ export function SiteHeader(props) {
     location.href = newHref;
   }
 
-  let signInButton = (
-    <Button
-      dataTest="Nav to course"
-      size="medium"
-      value="My Courses"
-      onClick={() => {
-        navigateTo.current = "/course";
-        setRecoilPageToolView({
-          page: "course",
-          tool: "",
-          view: "",
-          params: {},
-        });
-      }}
-    />
-  );
-  if (!data.signedIn) {
-    signInButton = (
-      <Button
-        dataTest="Nav to signin"
-        size="medium"
-        value="Sign In"
-        onClick={() => {
-          navigateTo.current = "/signin";
-          setRecoilPageToolView({
-            page: "signin",
-            tool: "",
-            view: "",
-            params: {},
-          });
-        }}
-      />
-    );
-  }
+  // let signInButton = (
+  //   <Button
+  //     // dataTest="Nav to course"
+  //     size="xs"
+  //     borderRadius={20}
+  //     onClick={() => {
+  //       navigateTo.current = "/course";
+  //       setRecoilPageToolView({
+  //         page: "course",
+  //         tool: "",
+  //         view: "",
+  //         params: {},
+  //       });
+  //     }}
+  //   >
+  //     <Text fontSize="xs">My Courses</Text>
+  //   </Button>
+  // );
+  // if (!data.signedIn) {
+  //   signInButton = (
+  //     <Button
+  //       // dataTest="Nav to signin"
+  //       size="xs"
+  //       onClick={() => {
+  //         navigateTo.current = "/signin";
+  //         setRecoilPageToolView({
+  //           page: "signin",
+  //           tool: "",
+  //           view: "",
+  //           params: {},
+  //         });
+  //       }}
+  //     >
+  //       <Text fontSize="xs">My Courses</Text>
+  //     </Button>
+  //   );
+  // }
 
   return (
     <>
@@ -146,30 +166,39 @@ export function SiteHeader(props) {
         width="100vw"
         height="100vh"
       >
-        <Box
+        <Flex
           as="header"
-          gridRow="1 / 2"
-          backgroundColor="#fff"
-          color="#000"
+          backgroundColor={navColor}
+          opacity="90%"
+          px={5}
           height="40px"
           position="fixed"
-          top="0"
-          width="100%"
-          margin="0"
-          display="flex"
+          top={0}
+          left={0}
+          right={0}
           justifyContent="space-between"
-          borderBottom="1px solid var(--mainGray)"
-          zIndex="1200"
+          alignItems="center"
+          boxShadow="0 2px 2px -2px rgba(0,0,0,.2)"
+          zIndex="10"
         >
-          {data.signedIn ? (
-            <Center columnGap="6px">
+          <Flex gap={3} justifyContent="center" alignItems="center">
+            <Flex>
               <RouterLogo />
-              <Text>Doenet</Text>
-              <Icon
-                ml="10px"
-                cursor="pointer"
-                fontSize="16pt"
-                as={FaCog}
+              <Text ml={1}>Doenet</Text>
+            </Flex>
+            <Button
+              size="xs"
+              variant="ghost"
+              borderRadius={5}
+              onClick={toggleColorMode}
+            >
+              <Icon as={colorMode === "light" ? FaMoon : FaSun} h={4} w={4}/>
+            </Button>
+            {data.signedIn && (
+              <Button
+                size="xs"
+                variant="ghost"
+                borderRadius={5}
                 onClick={() => {
                   navigateTo.current = "/settings";
                   setRecoilPageToolView({
@@ -179,37 +208,59 @@ export function SiteHeader(props) {
                     params: {},
                   });
                 }}
-              />
-            </Center>
-          ) : (
-            <Branding>
-              <RouterLogo />
-              <Text>Doenet</Text>
-            </Branding>
-          )}
-          <BarMenu>
-            <MenuItem dataTest="Home" to="/">
-              Home
-            </MenuItem>
-            <MenuItem dataTest="Community" to="community">
-              Community
-            </MenuItem>
-            {data.signedIn ? (
-              <MenuItem
-                dataTest="Portfolio"
-                to={`portfolio/${data.portfolioCourseId}`}
               >
-                Portfolio
-              </MenuItem>
-            ) : null}
-            {isAdmin ? <MenuItem to={`/admin`}>Admin</MenuItem> : null}
-          </BarMenu>
-          <SignInButtonContainer>{signInButton}</SignInButtonContainer>
-        </Box>
+                <Icon as={FaCog} h={4} w={4}/>
+              </Button>
+            )}
+          </Flex>
 
-        <Box as="main" gridRow="2 / 3" margin="0" overflowY="scroll">
-          <Outlet context={{ signedIn: data.signedIn }} />
-        </Box>
+          <Tabs index={activeTab}> 
+            <TabList
+              position="absolute"
+              top="50%"
+              left="50%"
+              transform="translate(-50%, -50%)"
+              display={{ base: "none", md: "flex" }}
+            >
+              <Link to="/">
+                <Tab onClick={() => setActiveTab(0)}>Home</Tab>
+              </Link>
+              <Link to="/community">
+                <Tab onClick={() => setActiveTab(1)}>Community</Tab>
+              </Link>
+              {data.signedIn && (
+                <Link to={`portfolio/${data.portfolioCourseId}`}>
+                  <Tab onClick={() => setActiveTab(2)}>Portfolio</Tab>
+                </Link>
+              )}
+              {isAdmin && (
+                <Link to="/admin">
+                  <Tab onClick={() => setActiveTab(3)}>Admin</Tab>
+                </Link>
+              )}
+            </TabList>
+          </Tabs>
+          <Box>
+            <Button
+              // dataTest="Nav to course"
+              size="xs"
+              borderRadius={20}
+              onClick={() => {
+                navigateTo.current = data.signedIn ? "/course" : "/signin";
+                setRecoilPageToolView({
+                  page: data.signedIn ? "course" : "signin",
+                  tool: "",
+                  view: "",
+                  params: {},
+                });
+              }}
+            >
+              <Text fontSize="xs">My Courses</Text>
+            </Button>
+          </Box>
+        </Flex>
+        <Box h={40} w="100%" />
+        <Outlet context={{ signedIn: data.signedIn }} />
       </Box>
     </>
   );
