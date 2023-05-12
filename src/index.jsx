f941f537713b83be9c76d01499645112edf27eec
Jason Altekruse <jaltekruse@users.noreply.github.com>
2023-05-12 15:07:56 -0500

    Hook up error page from Keagan to the router (#2115)

--- src/index.jsx
+++ src/index.jsx
@@ -40,11 +40,12 @@ import {
   action as portfolioActivityViewerAction,
   PortfolioActivityViewer,
 } from "./Tools/_framework/Paths/PortfolioActivityViewer";
-import { ChakraProvider, ColorModeScript, extendTheme } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import {
   action as editorSupportPanelAction,
   loader as editorSupportPanelLoader,
 } from "./Tools/_framework/Panels/NewSupportPanel";
+import ErrorPage from "./Tools/_framework/Paths/ErrorPage";
 
 import "@fontsource/jost";
 
@@ -278,6 +279,15 @@ const router = createBrowserRouter([
           <ToolRoot />
         </MathJaxContext>
       </DarkmodeController>
+    ),
+    // TODO - probably not a good idea long term, this is to populate the site header
+    // on the 404 page, but this results in extra network requests when loading
+    // ToolRoot content
+    loader: siteLoader,
+    errorElement: (
+      <ChakraProvider theme={theme}>
+        <SiteHeader childComponent={<ErrorPage />} />
+      </ChakraProvider>
     ),
   },
 ]);
