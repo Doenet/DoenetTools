94ae0449eac053ebb1aaac250c3877daae211fe2
keaganeng <eng00029@umn.edu>
2023-05-17 18:24:20 +0000

    Put ID in MathInputKeyboard

--- src/Tools/_framework/Footers/MathInputKeyboard.jsx
+++ src/Tools/_framework/Footers/MathInputKeyboard.jsx
@@ -1091,7 +1091,7 @@ export default function VirtualKeyboard() {
   );
 
   return (
-    <Panel tabIndex="0" ref={containerRef}>
+    <Panel tabIndex="0" ref={containerRef} id="keyboard">
       {/* <ContainerSection>
           <ToggleButtonSection>
             <ToggleButtonGroup onClick={handleGreekToggle}>
