3085ed369d26b3c128807d004a03e6f2e394d60f
Jason Altekruse <altekrusejason@gmail.com>
2023-05-30 16:15:54 -0500

    Fix various renderers, add math keyboard
    - need to decide how to share code between core and the app, for example our CodeMirror wrapper and the keyboard
    - a few smaller things in the _reactComponents directory may also be shared

---
+++ DoenetML/src/_reactComponents/PanelHeaderComponents/ToggleButtonGroup.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import styled, { ThemeProvider } from "styled-components";
+
+const Container = styled.div`
+  display: ${(props) => (props.vertical ? "static" : "flex")};
+  width: ${(props) => (props.width == "menu" ? "var(--menuWidth)" : "")};
+  // height: 'fit-content';
+  // margin: 2px 0px 2px 0px ;
+  /* flex-wrap: wrap; */
+  overflow: clip;
+`;
+
+const toggleGroup = {
+  margin: "0px -2px 0px -2px",
+  borderRadius: "0",
+  padding: "0px 12px 0px 10px",
+};
+
+const verticalToggleGroup = {
+  margin: "-2px 4px -2px 4px",
+  borderRadius: "0",
+  padding: "0px 10px 0px 10px",
+};
+
+const ToggleButtonGroup = (props) => {
+  // if (props.width) {
+  //   if (props.width === "menu") {
+  //     actionGroup.width = '235px'
+  //   } else {
+  //     actionGroup.width = props.width
+  //   }
+  // }
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const handleClick = (index) => {
+    setSelectedIndex(index);
+    if (props.onClick) {
+      props.onClick(index);
+    }
+  };
+
+  let first_prop = props.vertical ? "first_vert" : "first";
+  let last_prop = props.vertical ? "last_vert" : "last";
+
+  let elem = React.Children.toArray(props.children);
+
+  let modElem = elem.map((element, index) => {
+    let props = {
+      index,
+      isSelected: index === selectedIndex,
+      onClick: handleClick,
+    };
+
+    if (index === 0) {
+      props["num"] = first_prop;
+    } else if (index === elem.length - 1) {
+      props["num"] = last_prop;
+    }
+
+    return React.cloneElement(element, props);
+  });
+
+  return (
+    <Container
+      style={{ height: "fit-content" }}
+      vertical={props.vertical}
+      width={props.width}
+      role="group"
+    >
+      <ThemeProvider theme={props.vertical ? verticalToggleGroup : toggleGroup}>
+        {modElem}
+      </ThemeProvider>
+    </Container>
+  );
+};
+
+export default ToggleButtonGroup;
--- DoenetML/src/main.jsx
+++ DoenetML/src/main.jsx
@@ -1,13 +1,94 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createRoot } from "react-dom/client";
 import PageViewer from "./Viewer/PageViewer.jsx";
-import axios from "axios";
 import { RecoilRoot } from "recoil";
 import { MathJaxContext } from "better-react-mathjax";
 import { mathjaxConfig } from "./Core/utils/math.js";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import DarkmodeController from "./Tools/_framework/DarkmodeController.jsx";
+import VirtualKeyboard from "./Tools/_framework/Footers/VirtualKeyboard";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  fonts: {
+    body: "Jost",
+  },
+  textStyles: {
+    primary: {
+      fontFamily: "Jost",
+    },
+  },
+  config: {
+    initialColorMode: "light",
+    useSystemColorMode: false,
+    // initialColorMode: "system",
+    // useSystemColorMode: true,
+  },
+  colors: {
+    doenet: {
+      mainBlue: "#1a5a99",
+      lightBlue: "#b8d2ea",
+      solidLightBlue: "#8fb8de",
+      mainGray: "#e3e3e3",
+      mediumGray: "#949494",
+      lightGray: "#e7e7e7",
+      donutBody: "#eea177",
+      donutTopping: "#6d4445",
+      mainRed: "#c1292e",
+      lightRed: "#eab8b8",
+      mainGreen: "#459152",
+      canvas: "#ffffff",
+      canvastext: "#000000",
+      lightGreen: "#a6f19f",
+      lightYellow: "#f5ed85",
+      whiteBlankLink: "#6d4445",
+      mainYellow: "#94610a",
+      mainPurple: "#4a03d9",
+    },
+  },
+  components: {
+    Button: {
+      baseStyle: {
+        fontWeight: "normal",
+        letterSpacing: ".5px",
+        _focus: {
+          outline: "2px solid #2D5994",
+          outlineOffset: "2px",
+        },
+        _disabled: {
+          bg: "#E2E2E2",
+          color: "black",
+          cursor: "none",
+        },
+      },
+      variants: {
+        // We can override existing variants
+        solid: {
+          bg: "doenet.mainBlue",
+          color: "white",
+          _hover: {
+            bg: "doenet.solidLightBlue",
+            color: "black",
+          },
+        },
+        outline: {
+          borderColor: "#2D5994",
+          _hover: {
+            bg: "solidLightBlue",
+          },
+        },
+        ghost: {
+          _hover: {
+            bg: "solidLightBlue",
+          },
+        },
+        link: {
+          color: "solidLightBlue",
+        },
+      },
+    },
+  },
+});
 
 // function CypressTest(props){
 
@@ -16,53 +97,63 @@ import DarkmodeController from "./Tools/_framework/DarkmodeController.jsx";
 //   return <p>test</p>
 // }
 
-//let doenetML = "<graph><regularPolygon /></graph><mathinput name='f'/>$f";
-let doenetML = "<graph><regularPolygon /></graph>";
+let doenetML = `<graph><regularPolygon center="($s, 0)"/></graph><mathinput name='f'/>$f<spreadsheet />
+  <video youtube='ZZ5LpwO-An4' name='v'/><callaction target='v' actionName='playVideo' />
+  <subsetOfRealsInput /><orbitalDiagramInput /><slider name="s"/><matrixInput />
+  <codeEditor showResults/><booleanInput asToggleButton="$bool" name="bool"/><booleanInput />
+  <textInput /> <textInput expanded /><choiceInput><choice>dog</choice><choice>not dog</choice></choiceInput>
+  <choiceInput inline><choice>dog</choice><choice>not dog</choice></choiceInput>
+
+  `;
+//let doenetML = "<graph><regularPolygon /></graph>";
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-  <RecoilRoot>
-    <Router>
-      <Routes>
-        <Route
-          path="*"
-          element={
-            <DarkmodeController>
-              <MathJaxContext
-                version={2}
-                config={mathjaxConfig}
-                onStartup={(mathJax) => (mathJax.Hub.processSectionDelay = 0)}
-              >
-                <PageViewer
-                  doenetML={doenetML}
-                  // cid={"185fd09b6939d867d4faee82393d4a879a2051196b476acdca26140864bc967a"}
-                  updateDataOnContentChange={true}
-                  flags={{
-                    showCorrectness: true,
-                    readOnly: false,
-                    showFeedback: true,
-                    showHints: true,
-                    allowLoadState: false,
-                    allowSaveState: false,
-                    allowLocalState: false,
-                    allowSaveSubmissions: false,
-                    allowSaveEvents: false,
-                    autoSubmit: false,
-                  }}
-                  attemptNumber={1}
-                  requestedVariantIndex={1}
-                  doenetId=""
-                  pageIsActive={true}
-                  // collaborate={true}
-                  // viewerExternalFunctions = {{ allAnswersSubmitted: this.setAnswersSubmittedTrueCallback}}
-                  // functionsSuppliedByChild = {this.functionsSuppliedByChild}
-                />
-              </MathJaxContext>
-            </DarkmodeController>
-          }
-        />
-      </Routes>
-    </Router>
-  </RecoilRoot>,
+  <ChakraProvider theme={theme}>
+    <RecoilRoot>
+      <Router>
+        <Routes>
+          <Route
+            path="*"
+            element={
+              <DarkmodeController>
+                <MathJaxContext
+                  version={2}
+                  config={mathjaxConfig}
+                  onStartup={(mathJax) => (mathJax.Hub.processSectionDelay = 0)}
+                >
+                  <PageViewer
+                    doenetML={doenetML}
+                    // cid={"185fd09b6939d867d4faee82393d4a879a2051196b476acdca26140864bc967a"}
+                    updateDataOnContentChange={true}
+                    flags={{
+                      showCorrectness: true,
+                      readOnly: false,
+                      showFeedback: true,
+                      showHints: true,
+                      allowLoadState: false,
+                      allowSaveState: false,
+                      allowLocalState: false,
+                      allowSaveSubmissions: false,
+                      allowSaveEvents: false,
+                      autoSubmit: false,
+                    }}
+                    attemptNumber={1}
+                    requestedVariantIndex={1}
+                    doenetId=""
+                    pageIsActive={true}
+                    // collaborate={true}
+                    // viewerExternalFunctions = {{ allAnswersSubmitted: this.setAnswersSubmittedTrueCallback}}
+                    // functionsSuppliedByChild = {this.functionsSuppliedByChild}
+                  />
+                  <VirtualKeyboard />
+                </MathJaxContext>
+              </DarkmodeController>
+            }
+          />
+        </Routes>
+      </Router>
+    </RecoilRoot>
+  </ChakraProvider>,
   // </React.StrictMode>,
 );
 
