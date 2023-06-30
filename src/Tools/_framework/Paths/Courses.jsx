9a0b90c967d87cd9c91421f8c59d7b4ec7599420
Kevin Charles <kevinmcharles@gmail.com>
2023-06-30 17:25:46 -0500

    Grid and button improvements

--- src/Tools/_framework/Paths/Courses.jsx
+++ src/Tools/_framework/Paths/Courses.jsx
@@ -2,11 +2,11 @@
 import {
   Box,
   Button,
-  Icon,
   Text,
-  Center,
   Grid,
   GridItem,
+  Wrap,
+  Flex,
 } from "@chakra-ui/react";
 
 import React from "react";
@@ -62,53 +62,67 @@ export function Courses() {
   const fetcher = useFetcher();
 
   return (
-    <>
-      <Box>
-        <Center m={8} mb={0}>
-          <Text fontSize="xl" fontWeight="bold">
+    <Grid
+      templateAreas={`"siteHeader" 
+        "courseCards"`}
+      gridTemplateRows="80px auto"
+      height="100vh"
+    >
+      <GridItem area="siteHeader">
+        <Box
+          gridRow="1/2"
+          backgroundColor="#fff"
+          color="#000"
+          height="80px"
+          position="fixed"
+          width="100%"
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+          textAlign="center"
+          zIndex="500"
+        >
+          <Text fontSize="24px" fontWeight="700">
             My Courses
           </Text>
-        </Center>
-        <Center>
-          <Grid
-            templateColumns={[
-              "repeat(1, 1fr)",
-              "repeat(2, 1fr)",
-              "repeat(3, 1fr)",
-              "repeat(4, 1fr)",
-              "repeat(5, 1fr)",
-            ]}
-            gap={6}
-            p="30px"
-          >
-            {courses.map((course, index) => (
-              <GridItem key={`course-${course.courseId}`} tabIndex={index}>
-                <CourseCard course={course} />
-              </GridItem>
-            ))}
-          </Grid>
-        </Center>
-        <Button
-          position="absolute"
-          right={8}
-          s
-          bottom={8}
-          borderRadius="50%"
-          w={14}
-          h={14}
-          zIndex={10}
-          onClick={() => {
-            // Have to amend on this post request if we want a functionality to fill data for the new course before adding it into the db
-            // I suggest creating another drawer/modal with a form to do this
-            fetcher.submit(
-              { _action: "Create New Course" },
-              { method: "post" },
-            );
-          }}
+
+          <div style={{ position: "absolute", top: "48px", right: "10px" }}>
+            <Button
+              dataTest="Add Course"
+              size="xs"
+              onClick={() => {
+                fetcher.submit(
+                  { _action: "Create New Course" },
+                  { method: "post" },
+                );
+              }}
+            >
+              Add Course
+            </Button>
+          </div>
+        </Box>
+      </GridItem>
+      <GridItem area="courseCards" background="doenet.mainGray">
+        <Flex
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+          alignContent="center"
+          minHeight={200}
+          width="100%"
         >
-          <Icon as={GoPlus} boxSize={8} />
-        </Button>
-      </Box>
-    </>
+          <Wrap overflow="visible" p="10px">
+            {courses.map((course, index) => (
+              <CourseCard
+                course={course}
+                key={`course-${course.courseId}`}
+                tabIndex={index}
+              />
+            ))}
+          </Wrap>
+        </Flex>
+      </GridItem>
+    </Grid>
   );
 }
