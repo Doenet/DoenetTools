8eaac62cdb2e40c2eda8bfe68dd67e6574307b1f
Duane Nykamp <nykamp@umn.edu>
2023-07-07 16:45:36 +0000

    animateFromSequence handles target subnames and subnames component index

--- src/Core/components/AnimateFromSequence.js
+++ src/Core/components/AnimateFromSequence.js
@@ -49,6 +49,19 @@ export default class AnimateFromSequence extends BaseComponent {
       public: true,
     };
 
+    attributes.targetSubnames = {
+      createPrimitiveOfType: "stringArray",
+      createStateVariable: "targetSubnames",
+      defaultValue: null,
+      public: true,
+    };
+    attributes.targetSubnamesComponentIndex = {
+      createComponentOfType: "numberList",
+      createStateVariable: "targetSubnamesComponentIndex",
+      defaultValue: null,
+      public: true,
+    };
+
     attributes.animationOn = {
       createComponentOfType: "boolean",
       createStateVariable: "animationOn",
@@ -363,6 +376,8 @@ export default class AnimateFromSequence extends BaseComponent {
       stateVariablesDeterminingDependencies: [
         "targetComponent",
         "componentIndex",
+        "targetSubnames",
+        "targetSubnamesComponentIndex",
       ],
       returnDependencies: function ({ stateValues, componentInfoObjects }) {
         let dependencies = {};
@@ -372,13 +387,26 @@ export default class AnimateFromSequence extends BaseComponent {
             componentInfoObjects.isCompositeComponent({
               componentType: stateValues.targetComponent.componentType,
               includeNonStandard: false,
-            })
+            }) ||
+            (componentInfoObjects.isCompositeComponent({
+              componentType: stateValues.targetComponent.componentType,
+              includeNonStandard: true,
+            }) &&
+              stateValues.componentIndex !== null)
           ) {
+            let targetSubnamesComponentIndex =
+              stateValues.targetSubnamesComponentIndex;
+            if (targetSubnamesComponentIndex) {
+              targetSubnamesComponentIndex = [...targetSubnamesComponentIndex];
+            }
+
             dependencies.targets = {
               dependencyType: "replacement",
               compositeName: stateValues.targetComponent.componentName,
               recursive: true,
               componentIndex: stateValues.componentIndex,
+              targetSubnames: stateValues.targetSubnames,
+              targetSubnamesComponentIndex,
             };
           } else if (
             stateValues.componentIndex === null ||
