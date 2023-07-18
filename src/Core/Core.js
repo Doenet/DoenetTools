c9dd3f7b786b8a2e6bd139001a9ce2e41f43f859
Duane Nykamp <nykamp@umn.edu>
2023-07-18 22:38:16 +0000

    fix mark stale bug

--- src/Core/Core.js
+++ src/Core/Core.js
@@ -7585,12 +7585,12 @@ export default class Core {
 
             if (result.updateRenderedChildren) {
               this.componentsWithChangedChildrenToRender.add(
-                component.componentName,
+                upDepComponent.componentName,
               );
             }
 
             if (result.updateDescendantRenderers) {
-              await this.markDescendantsToUpdateRenderers(component);
+              await this.markDescendantsToUpdateRenderers(upDepComponent);
             }
 
             if (result.updateActionChaining) {
@@ -7612,7 +7612,7 @@ export default class Core {
 
             if (result.updateDependencies) {
               for (let vName of result.updateDependencies) {
-                component.state[vName].needDependenciesUpdated = true;
+                upDepComponent.state[vName].needDependenciesUpdated = true;
               }
             }
 
