3c87e56f7f40eb76a0085e9500ae0aff3ff1b03b
Kevin Charles <kevinmcharles@gmail.com>
2023-07-27 16:47:48 -0500

    fixed indenting

--- public/api/duplicatePortfolioActivity.php
+++ public/api/duplicatePortfolioActivity.php
@@ -206,60 +206,60 @@ try {
     //content_contributor_history
     //Don't update history if duplicating to the same course
     if ($nextCourseId != $prevCourseId){
-    $insert_content_contributor_history = [];
-    //Get history
-    $sql = "
-    SELECT
-    doenetId,
-    courseId,
-    assignedCID,
-    isUserPortfolio,
-    timestamp
-    FROM content_contributor_history
-    WHERE doenetId = '$prevActivityDoenetId'
-    ";
-    $result = $conn->query($sql);
-    if ($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()){
-            $doenetId = $row['doenetId'];
-            $courseId = $row['courseId'];
-            $assignedCID = $row['assignedCID'];
-            $isUserPortfolio = $row['isUserPortfolio'];
-            $timestamp = $row['timestamp'];
-            array_push(
-                $insert_content_contributor_history,
-                "('$nextActivityDoenetId','$doenetId','$courseId','$assignedCID','$isUserPortfolio','$timestamp')"
-            );
+        $insert_content_contributor_history = [];
+        //Get history
+        $sql = "
+        SELECT
+        doenetId,
+        courseId,
+        assignedCID,
+        isUserPortfolio,
+        timestamp
+        FROM content_contributor_history
+        WHERE doenetId = '$prevActivityDoenetId'
+        ";
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()){
+                $doenetId = $row['doenetId'];
+                $courseId = $row['courseId'];
+                $assignedCID = $row['assignedCID'];
+                $isUserPortfolio = $row['isUserPortfolio'];
+                $timestamp = $row['timestamp'];
+                array_push(
+                    $insert_content_contributor_history,
+                    "('$nextActivityDoenetId','$doenetId','$courseId','$assignedCID','$isUserPortfolio','$timestamp')"
+                );
+            }
         }
-    }
 
-    //add prevous one to history with timestamp = now
-    $sql = "
-    SELECT portfolioCourseForUserId
-    FROM course
-    WHERE courseId = '$prevCourseId'
-    ";
-    $result = $conn->query($sql);
-    if ($result->num_rows > 0) {
-        $row = $result->fetch_assoc();
-        $isUserPortfolio = is_null($row["portfolioCourseForUserId"]) ? "0" : "1";
-        $prevUserId = $row["portfolioCourseForUserId"];
-    }
-    array_push(
-        $insert_content_contributor_history,
-        "('$nextActivityDoenetId','$prevActivityDoenetId','$prevCourseId','$assignedCid','$isUserPortfolio',NOW())"
-    );
+        //add prevous one to history with timestamp = now
+        $sql = "
+        SELECT portfolioCourseForUserId
+        FROM course
+        WHERE courseId = '$prevCourseId'
+        ";
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $isUserPortfolio = is_null($row["portfolioCourseForUserId"]) ? "0" : "1";
+            $prevUserId = $row["portfolioCourseForUserId"];
+        }
+        array_push(
+            $insert_content_contributor_history,
+            "('$nextActivityDoenetId','$prevActivityDoenetId','$prevCourseId','$assignedCid','$isUserPortfolio',NOW())"
+        );
 
-    $str_insert_content_contributor_history = implode(',', $insert_content_contributor_history);
+        $str_insert_content_contributor_history = implode(',', $insert_content_contributor_history);
 
 
-    // INSERT into content_contributor_history table
-    $sql = "
-    INSERT INTO content_contributor_history (doenetId,prevDoenetId,courseId,assignedCID,isUSerPortfolio,timestamp)
-    VALUES
-    $str_insert_content_contributor_history
-    ";
-    $result = $conn->query($sql);
+        // INSERT into content_contributor_history table
+        $sql = "
+        INSERT INTO content_contributor_history (doenetId,prevDoenetId,courseId,assignedCID,isUSerPortfolio,timestamp)
+        VALUES
+        $str_insert_content_contributor_history
+        ";
+        $result = $conn->query($sql);
     }
 
     $response_arr = [
