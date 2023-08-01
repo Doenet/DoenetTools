2a3b59760b82b8a3db90cfa0ded98e75b03b463a
Kevin Charles <kevinmcharles@gmail.com>
2023-08-01 15:47:07 -0500

    Added Cypress test

---
+++ cypress/e2e/Editor/PortfolioEditor.cy.js
@@ -0,0 +1,82 @@
+// import {signIn} from '../DoenetSignin/DoenetSignin.cy';
+
+const { cesc2 } = require("../../../src/_utils/url");
+
+describe("Share Activities Using Portfolio", function () {
+  const userId = "cyuserId";
+  const userId2 = "cyuserId2";
+
+  before(() => {
+    // cy.clearAllOfAUsersActivities({userId})
+  });
+  beforeEach(() => {
+    cy.signin({ userId });
+    cy.visit(`/`);
+  });
+
+  Cypress.on("uncaught:exception", (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false;
+  });
+
+  it("Update Button", () => {
+    const label = "Portfolio Variant Control";
+    const text1 = "Hello World";
+    const text2 = "Hi";
+    const text3 = "There";
+
+    cy.log("Make an activity in the portfolio");
+    cy.get('[data-test="Portfolio"]').click();
+    cy.get('[data-test="Add Activity"]').click();
+
+    cy.get('[data-test="Controls Button"]').click();
+    cy.get('[data-test="Activity Label"]').clear().type(label).blur();
+    cy.get('[data-test="Close Settings Button"]').click();
+
+    cy.log("No content")
+
+    cy.get('[data-test="Viewer Update Button"]').should('be.disabled');
+
+    cy.log("Enter content")
+
+    cy.get(".cm-content").type(
+      `<p>${text1}</p> {enter}`,
+    );
+    cy.get('[data-test="Viewer Update Button"]').should('be.enabled');
+
+
+    cy.get('[data-test="Viewer Update Button"]').click();
+    cy.get(cesc2("#/_document1")).should("contain", text1);
+    cy.get('[data-test="Viewer Update Button"]').should('be.disabled');
+
+    cy.get(".cm-content").type(
+      `{ctrl+end}<p>${text2}</p> {enter}`,
+    );
+    cy.get('[data-test="Viewer Update Button"]').should('be.enabled');
+
+    cy.get('[data-test="View Mode Button"]').click();
+
+    cy.get('[data-test="Edit Mode Button"]').click();
+
+    cy.get('[data-test="Viewer Update Button"]').should('be.enabled');
+
+    cy.get('[data-test="Viewer Update Button"]').click();
+
+    cy.get('[data-test="Viewer Update Button"]').should('be.disabled');
+
+    cy.get(".cm-content").type(
+      `{ctrl+end}<p>${text3}</p> {enter}`,
+    );
+
+    cy.get('[data-test="Viewer Update Button"]').should('be.enabled');
+
+    cy.get('[data-test="Viewer Update Button"]').click();
+
+    cy.get(cesc2("#/_document1")).should("contain", text1);
+    cy.get(cesc2("#/_document1")).should("contain", text2);
+    cy.get(cesc2("#/_document1")).should("contain", text3);
+
+  });
+
+});
