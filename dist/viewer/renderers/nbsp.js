8f6938100c7edc5e670bd7f69feb6cc22b1be94f
Emilio Alvarez <e.l.alvarez@icloud.com>
2024-03-11 14:35:05 -0500

    Remove dist folder (#2372)
    * add prod build command

--- dist/viewer/renderers/nbsp.js
+++
@@ -1,9 +0,0 @@
-import React from "../../_snowpack/pkg/react.js";
-import useDoenetRender from "./useDoenetRenderer.js";
-export default React.memo(function Nbsp(props) {
-  let {SVs} = useDoenetRender(props, false);
-  if (SVs.hidden) {
-    return null;
-  }
-  return /* @__PURE__ */ React.createElement(React.Fragment, null, " ");
-});
