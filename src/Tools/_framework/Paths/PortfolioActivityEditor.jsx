68d0f038d477d1872008d1405891d20e50158820
Duane Nykamp <nykamp@umn.edu>
2024-05-13 08:58:39 -0500

    reuse code for resizable side-by-side

--- src/Tools/_framework/Paths/PortfolioActivityEditor.jsx
+++ src/Tools/_framework/Paths/PortfolioActivityEditor.jsx
@@ -72,6 +72,7 @@ import VariantSelect from "../ChakraBasedComponents/VariantSelect";
 import ErrorWarningPopovers from "../ChakraBasedComponents/ErrorWarningPopovers";
 import { useLocation, useNavigate } from "react-router";
 import findFirstPageIdInContent from "../../../_utils/findFirstPage";
+import { ResizableSideBySide } from "./ResizableSideBySide";
 
 export async function action({ params, request }) {
   const formData = await request.formData();
@@ -1542,7 +1543,7 @@ export function PortfolioActivityEditor() {
         {mode == "Edit" && (
           <>
             <GridItem area="centerContent">
-              <ResizeableSideBySide
+              <ResizableSideBySide
                 left={
                   <>
                     <VStack spacing={0}>
@@ -1847,118 +1848,3 @@ export function PortfolioActivityEditor() {
     </>
   );
 }
-
-const clamp = (
-  value,
-  min = Number.POSITIVE_INFINITY,
-  max = Number.NEGATIVE_INFINITY,
-) => {
-  return Math.min(Math.max(value, min), max);
-};
-
-const ResizeableSideBySide = ({
-  left,
-  right,
-  centerWidth = "10px",
-  headerHeight = 80,
-}) => {
-  const wrapperRef = useRef();
-
-  useEffect(() => {
-    wrapperRef.current.handleClicked = false;
-    wrapperRef.current.handleDragged = false;
-  }, []);
-
-  const onMouseDown = (event) => {
-    event.preventDefault();
-    wrapperRef.current.handleClicked = true;
-  };
-
-  const onMouseMove = (event) => {
-    //TODO: minimum movment calc
-    if (wrapperRef.current.handleClicked) {
-      event.preventDefault();
-      wrapperRef.current.handleDragged = true;
-
-      let proportion = clamp(
-        (event.clientX - wrapperRef.current.offsetLeft) /
-          wrapperRef.current.clientWidth,
-        0.18,
-        1,
-      );
-
-      //using a ref to save without react refresh
-      wrapperRef.current.style.gridTemplateColumns = `${proportion}fr ${centerWidth} ${
-        1 - proportion
-      }fr`;
-      wrapperRef.current.proportion = proportion;
-    }
-  };
-
-  const onMouseUp = () => {
-    if (wrapperRef.current.handleClicked) {
-      wrapperRef.current.handleClicked = false;
-      if (wrapperRef.current.handleDragged) {
-        wrapperRef.current.handleDragged = false;
-      }
-    }
-  };
-
-  return (
-    <Grid
-      width="100%"
-      height={`calc(100vh - ${headerHeight}px)`}
-      templateAreas={`"viewer middleGutter textEditor"`}
-      templateColumns={`.5fr ${centerWidth} .5fr`}
-      overflow="hidden"
-      onMouseUp={onMouseUp}
-      onMouseMove={onMouseMove}
-      onMouseLeave={onMouseUp}
-      ref={wrapperRef}
-    >
-      <GridItem
-        area="viewer"
-        width="100%"
-        placeSelf="center"
-        height="100%"
-        maxWidth="850px"
-        overflow="hidden"
-      >
-        {left}
-      </GridItem>
-      <GridItem
-        area="middleGutter"
-        background="doenet.lightBlue"
-        width="100%"
-        height="100%"
-        paddingTop="42px"
-        alignSelf="start"
-      >
-        <Center
-          cursor="col-resize"
-          background="doenet.mainGray"
-          borderLeft="solid 1px"
-          borderTop="solid 1px"
-          borderBottom="solid 1px"
-          borderColor="doenet.mediumGray"
-          height={`calc(100vh - ${headerHeight + 52}px)`}
-          width="10px"
-          onMouseDown={onMouseDown}
-          data-test="contentPanelDragHandle"
-          paddingLeft="1px"
-        >
-          <Icon ml="0" as={BsGripVertical} />
-        </Center>
-      </GridItem>
-      <GridItem
-        area="textEditor"
-        width="100%"
-        background="doenet.lightBlue"
-        alignSelf="start"
-        paddingTop="10px"
-      >
-        {right}
-      </GridItem>
-    </Grid>
-  );
-};
