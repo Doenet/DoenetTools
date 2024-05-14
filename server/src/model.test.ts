bc279fbfa2eba41c60f1f809cee4aba4a2018c43
Jason Altekruse <altekrusejason@gmail.com>
2024-05-14 11:11:57 -0500

    Tests for creating and deleting docs

--- server/src/model.test.ts
+++ server/src/model.test.ts
@@ -1,10 +1,14 @@
 import { expect, test, vi } from 'vitest'
-import { findOrCreateUser, listUserDocs } from './model'
+import { createDocument, deleteDocument, findOrCreateUser, getDocEditorData, listUserDocs } from './model'
 
-test('Do something with database', async () => {
+async function createTestUser() {
   const username = "vitest-" + new Date().toJSON() + "@vitest.test"
-  const ret = await findOrCreateUser(username);
-  const docs = await listUserDocs(ret);
+  return await findOrCreateUser(username);
+}
+
+test('New user has an empty portfolio', async () => {
+  const userId = await createTestUser();
+  const docs = await listUserDocs(userId);
   expect(docs).toStrictEqual(
   {
     success: true,
@@ -14,3 +18,46 @@ test('Do something with database', async () => {
     notMe: false,
   });
 });
+
+test('New document starts out private', async () => {
+  const userId = await createTestUser();
+  const docId = await createDocument(userId);
+  const docContent = await getDocEditorData(docId);
+  expect(docContent).toStrictEqual(
+  {
+         "activity": {
+           "content": "",
+           "doenetmlVersion": {
+             "default": true,
+             "deprecated": false,
+             "deprecationMessage": "",
+             "displayedVersion": "0.7",
+             "fullVersion": "0.7.0-alpha1",
+             "removed": false,
+             "versionId": 2,
+           },
+           "imagePath": "/activity_default.jpg",
+           "isPublic": false,
+           "isSinglePage": true,
+           "label": "untitled doc",
+           "learningOutcomes": [],
+           "type": "activity",
+           "version": "",
+         },
+         "courseId": null,
+          "success": true,
+  });
+
+  const docs = await listUserDocs(userId);
+
+  expect(docs.privateActivities.length).toBe(1);
+  expect(docs.publicActivities.length).toBe(0);
+
+  await deleteDocument(docId);
+
+  const docsAfterDelete = await listUserDocs(userId);
+
+  expect(docsAfterDelete.privateActivities.length).toBe(0);
+  expect(docsAfterDelete.publicActivities.length).toBe(0);
+});
+
