26eac7094caa0c7789243e669c683bce68bd0072
Charles Nykamp <cqnykamp@gmail.com>
2024-08-14 21:07:03 -0500

    Continuous integration via Github Actions (#2475)

---
+++ server/eslint.config.mjs
@@ -0,0 +1,27 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  { files: ["**.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { ignores: ["dist/"] },
+  { languageOptions: { globals: { ...globals.node } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          args: "all",
+          argsIgnorePattern: "^_",
+          caughtErrors: "all",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+  },
+];
