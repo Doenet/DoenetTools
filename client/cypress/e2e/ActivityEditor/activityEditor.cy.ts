9afa1e66033c2cf376ad697a0bb5f61e3903d950
Duane Nykamp <nykamp@umn.edu>
2024-12-14 22:43:38 -0600

    correctly restore activity editor state after switching to view mode

---
+++ client/cypress/e2e/ActivityEditor/activityEditor.cy.ts
@@ -0,0 +1,36 @@
+describe("Create Folders Tests", function () {
+  it("correctly restore editor state after clicking view", () => {
+    // test bug where activity editor was not restoring itself with the correct state
+    // after one switched to view mode and back
+
+    cy.loginAsTestUser();
+
+    cy.createActivity("Hello!", "Initial content").then((activityId) => {
+      cy.visit(`/activityEditor/${activityId}`);
+
+      cy.iframe()
+        .find(".doenet-viewer")
+        .should("contain.text", `Initial content`);
+
+      cy.iframe().find(".cm-editor").type(`{end}{enter}More!`);
+
+      cy.get(`[data-test="View Mode Button"]`).click();
+
+      cy.iframe().find(".cm-editor").should("not.exist");
+
+      cy.iframe()
+        .find(".doenet-viewer")
+        .should("contain.text", `Initial content\nMore!`);
+
+      cy.get(`[data-test="Edit Mode Button"]`).click();
+
+      cy.iframe()
+        .find(".cm-editor")
+        .should("contain.text", "Initial contentMore!");
+
+      cy.iframe()
+        .find(".doenet-viewer")
+        .should("contain.text", `Initial content\nMore!`);
+    });
+  });
+});
