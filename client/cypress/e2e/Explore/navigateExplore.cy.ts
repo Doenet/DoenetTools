66562882c8bf8e56ff84dea49bee2c2143c1c1ca
Duane Nykamp <nykamp@umn.edu>
2025-02-17 12:24:14 -0600

    cypress test with query

--- client/cypress/e2e/Explore/navigateExplore.cy.ts
+++ client/cypress/e2e/Explore/navigateExplore.cy.ts
@@ -59,6 +59,62 @@ describe("Navigate Explore Tests", function () {
         "aria-selected",
         "false",
       );
+
+      // search for name and select author tab
+      cy.get('[data-test="Search"]').type("Test{enter}");
+
+      cy.get('[data-test="Authors Tab"]').click();
+      cy.get('[data-test="Authors Tab"]').should(
+        "have.attr",
+        "aria-selected",
+        "true",
+      );
+      cy.get('[data-test="Community Tab"]').should(
+        "have.attr",
+        "aria-selected",
+        "false",
+      );
+
+      cy.get('[data-test="Home"]').click();
+      cy.get('[data-test="Home"]').should("have.attr", "aria-current", "page");
+      cy.get('[data-test="Explore"]').should("not.have.attr", "aria-current");
+
+      // if go back, query is still active and authors tab is still selected
+      cy.go("back");
+      cy.get('[data-test="Explore"]').should(
+        "have.attr",
+        "aria-current",
+        "page",
+      );
+      cy.get('[data-test="Home"]').should("not.have.attr", "aria-current");
+      cy.get('[data-test="Authors Tab"]').should(
+        "have.attr",
+        "aria-selected",
+        "true",
+      );
+
+      cy.get('[data-test="Home"]').click();
+      cy.get('[data-test="Home"]').should("have.attr", "aria-current", "page");
+      cy.get('[data-test="Explore"]').should("not.have.attr", "aria-current");
+
+      // if click on "Explore" tab, the query, and hence the author tab, is gone, which means curated tab should be open
+      cy.get('[data-test="Explore"]').click();
+      cy.get('[data-test="Explore"]').should(
+        "have.attr",
+        "aria-current",
+        "page",
+      );
+      cy.get('[data-test="Home"]').should("not.have.attr", "aria-current");
+      cy.get('[data-test="Curated Tab"]').should(
+        "have.attr",
+        "aria-selected",
+        "true",
+      );
+      cy.get('[data-test="Authors Tab"]').should(
+        "have.attr",
+        "aria-selected",
+        "false",
+      );
     });
   });
 });
