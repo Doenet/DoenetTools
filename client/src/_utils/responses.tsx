7166a1022f6521f2f11b185b8ec7b7e69f9ca8fb
Duane Nykamp <nykamp@umn.edu>
2025-03-19 08:41:01 -0500

    bugfix: properly revive math responses

--- client/src/_utils/responses.tsx
+++ client/src/_utils/responses.tsx
@@ -2,8 +2,22 @@ import React from "react";
 import me from "math-expressions";
 import { MathJax } from "better-react-mathjax";
 
+const nanInfinityReviver = function (_key, value) {
+  if (value && value.objectType === "special-numeric") {
+    if (value.stringValue === "NaN") {
+      return NaN;
+    } else if (value.stringValue === "Infinity") {
+      return Infinity;
+    } else if (value.stringValue === "-Infinity") {
+      return -Infinity;
+    }
+  }
+
+  return value;
+};
+
 export function parseAndFormatResponse(response: string): React.JSX.Element {
-  const parsedResp = JSON.parse(response);
+  const parsedResp = JSON.parse(response, nanInfinityReviver);
 
   return parsedResp.response.map((v, i) => {
     const componentType = parsedResp.componentTypes[i];
