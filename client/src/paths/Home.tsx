1d4e67f6181b877db5aa2bace9a608475ee3f6a1
Duane Nykamp <nykamp@umn.edu>
2025-08-01 11:42:25 -0500

    Answer response menu is a button

--- client/src/paths/Home.tsx
+++ client/src/paths/Home.tsx
@@ -32,7 +32,7 @@ export async function loader() {
 
 const HomeIntroVideo = lazy(() => import("../widgets/HomeIntroVideo"));
 
-const doenetmlVersion = "0.7.0-alpha53";
+const doenetmlVersion = "0.7.0-alpha54";
 const doenetML = `
 <example>
 <setup>
