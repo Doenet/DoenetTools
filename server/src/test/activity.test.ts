f194b6c1550a31da50c29253c6ac25b7e2a86465
Duane Nykamp <nykamp@umn.edu>
2025-11-13 09:09:36 -0600

    upgrade DoenetML to 0.7.0-beta10

--- server/src/test/activity.test.ts
+++ server/src/test/activity.test.ts
@@ -58,7 +58,7 @@ import {
 const currentDoenetmlVersion = {
   id: 2,
   displayedVersion: "0.7",
-  fullVersion: "0.7.0-beta9",
+  fullVersion: "0.7.0-beta10",
   default: true,
   deprecated: false,
   removed: false,
