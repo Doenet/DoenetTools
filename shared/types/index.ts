afbb042934771840d0c2f009c6e5ab8b2cfd699f
Charles Nykamp <cqnykamp@gmail.com>
2025-11-28 15:57:47 -0600

    Cleaner TS config

---
+++ shared/types/index.ts
@@ -0,0 +1,4 @@
+// Put types thare are shared between client and server here
+// We have many types that are ~almost~ the same
+// between the client and the server.
+// In the future, we may want to consolidate them here.
