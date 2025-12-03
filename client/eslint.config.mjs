bbfae4542ca692ec6e5fc4de82bf98b4057dfef0
Charles Nykamp <cqnykamp@gmail.com>
2025-12-03 08:44:19 -0600

    Edit github actions and relevant npm commands

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -1,15 +1,12 @@
 // @ts-check
 
-import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
-import unusedImports from "eslint-plugin-unused-imports";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
+import rootConfig from "../eslint.config.mjs";
 
 export default tseslint.config(
-  { ignores: ["dist/"] },
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
+  ...rootConfig,
   react.configs.flat.recommended,
   reactHooks.configs["recommended-latest"],
   {
@@ -21,38 +18,6 @@ export default tseslint.config(
     rules: {
       "react/react-in-jsx-scope": "off", // Not needed with new JSX transform
       "react/jsx-uses-react": "off", // Not needed with new JSX transform
-    },
-  },
-  //For now, allow explicit anys
-  {
-    rules: {
-      "@typescript-eslint/no-explicit-any": "off",
-    },
-  },
-  // eslint-plugin-unused-imports allows linter to fix unused imports
-  // It works by splitting the normal "unused-imports" rule into tw
-  {
-    plugins: {
-      "unused-imports": unusedImports,
-    },
-    rules: {
-      "@typescript-eslint/no-unused-vars": "off",
-      "unused-imports/no-unused-imports": "error",
-
-      // Allow unused vars that start with underscore
-      // See https://typescript-eslint.io/rules/no-unused-vars/
-      "unused-imports/no-unused-vars": [
-        "warn",
-        {
-          args: "all",
-          argsIgnorePattern: "^_",
-          caughtErrors: "all",
-          caughtErrorsIgnorePattern: "^_",
-          destructuredArrayIgnorePattern: "^_",
-          varsIgnorePattern: "^_",
-          ignoreRestSiblings: true,
-        },
-      ],
     },
   },
 );
