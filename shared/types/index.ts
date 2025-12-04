538027b54d169fdceb876542e40e3815268b184c
Charles Nykamp <cqnykamp@gmail.com>
2025-12-04 11:15:44 -0600

    Remove old files

--- shared/types/index.ts
+++
@@ -1,4 +0,0 @@
-// Put types thare are shared between client and server here
-// We have many types that are ~almost~ the same
-// between the client and the server.
-// In the future, we may want to consolidate them here.
