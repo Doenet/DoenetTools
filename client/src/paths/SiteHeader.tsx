f89c035a895a2e95b06008065584e59b32a6c5ee
Charles Nykamp <cqnykamp@gmail.com>
2025-12-08 12:28:07 -0600

    Remove comment

--- client/src/paths/SiteHeader.tsx
+++ client/src/paths/SiteHeader.tsx
@@ -81,8 +81,6 @@ function NavLinkTab({
   children: ReactNode;
   dataTest: string;
 }) {
-  // console.log("process.env.DISCOURSE_URL=", import.meta.env.VITE_DISCOURSE_URL);
-
   // TODO: use end only when path is "/"
   return (
     <NavLink to={to} end data-test={dataTest}>
