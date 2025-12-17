289d5ea92581bd0fa6745002ad74b6eaf38aad3a
Duane Nykamp <nykamp@umn.edu>
2025-12-17 17:22:39 -0600

    upgrade to DoenetML 0.7.0-rc-4

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -29,7 +29,7 @@ async function seedDoenetMLVersions() {
   });
   await updateOrCreateDoenetMLVersion({
     displayedVersion: "0.7",
-    fullVersion: "0.7.0-beta-17",
+    fullVersion: "0.7.0-rc-4",
     default: true,
   });
 }
