21bf3bc459190870a16415f2f7e84630e9f4ade8
Duane Nykamp <nykamp@umn.edu>
2025-12-20 13:47:55 -0600

    clear scratch pad on logout

--- client/src/paths/SiteHeader.tsx
+++ client/src/paths/SiteHeader.tsx
@@ -399,7 +399,13 @@ export function SiteHeader() {
                         >
                           Update {user.isAnonymous ? "pseudonym" : "name"}
                         </MenuItem>
-                        <MenuItem as="a" href="/api/login/logout">
+                        <MenuItem
+                          as="a"
+                          href="/api/login/logout"
+                          onClick={() => {
+                            localStorage.removeItem("scratchPad");
+                          }}
+                        >
                           {user.isAnonymous
                             ? "Clear anonymous data"
                             : "Log Out"}
