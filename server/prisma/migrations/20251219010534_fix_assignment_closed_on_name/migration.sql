fefebd4385f4925cf8f106ee95d4f79e7c3d3269
Charles Nykamp <cqnykamp@gmail.com>
2026-01-03 16:46:08 -0500

    Remove temp migrations

--- server/prisma/migrations/20251219010534_fix_assignment_closed_on_name/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `assignedClosedOn` on the `content` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `content` DROP COLUMN `assignedClosedOn`,
-    ADD COLUMN `assignmentClosedOn` DATETIME(3) NULL;
