4f31c07255ce3281250ca4c36312dd37c31d869b
Duane Nykamp <nykamp@umn.edu>
2026-01-03 20:06:31 -0600

    A scratch pad for temporary DoenetML editing (#2715)

--- server/src/test/activity.test.ts
+++ server/src/test/activity.test.ts
@@ -22,6 +22,7 @@ import {
   restoreDeletedContent,
   getContentSource,
   getDescendantIds,
+  getDefaultDoenetmlVersion,
 } from "../query/activity";
 import { getActivityViewerData, getContent } from "../query/activity_edit_view";
 import { getMyContent, getMyTrash } from "../query/content_list";
@@ -60,6 +61,12 @@ const currentDoenetmlVersion = {
   removed: false,
   deprecationMessage: "",
 };
+
+test("Get default DoenetML version", async () => {
+  const { defaultDoenetmlVersion } = await getDefaultDoenetmlVersion();
+
+  expect(defaultDoenetmlVersion).eqls(currentDoenetmlVersion);
+});
 
 test("New activity starts out private, then delete it", async () => {
   const user = await createTestUser();
@@ -2090,4 +2097,38 @@ test("getContent does not provide email", async () => {
     includeOwnerDetails: true,
   });
   expect(result.owner).not.toHaveProperty("email");
+});
+
+test("Create new activity with DoenetML", async () => {
+  const user = await createTestUser();
+  const userId = user.userId;
+  const { contentId: contentId } = await createContent({
+    loggedInUserId: userId,
+    contentType: "singleDoc",
+    parentId: null,
+    doenetml: "My DoenetML source",
+    name: "The new document",
+  });
+
+  const data = await getMyContent({
+    ownerId: userId,
+    loggedInUserId: userId,
+    parentId: null,
+  });
+
+  if (data.notMe) {
+    throw Error("shouldn't happen");
+  }
+  expect(data.content).toBeDefined();
+  expect(data.content.length).toBe(1);
+
+  const newDoc = data.content[0];
+
+  if (newDoc.type !== "singleDoc") {
+    throw Error("shouldn't happen");
+  }
+
+  expect(newDoc.contentId).toStrictEqual(contentId);
+  expect(newDoc.name).toBe("The new document");
+  expect(newDoc.doenetML).toBe("My DoenetML source");
 });
