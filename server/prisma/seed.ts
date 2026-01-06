9e6c3eda7f3ac3e682d1b0983671a4357c56b331
Charles Nykamp <cqnykamp@gmail.com>
2026-01-06 09:09:01 -0500

    Upgrade doenetml to 0.7 official

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -29,7 +29,7 @@ async function seedDoenetMLVersions() {
   });
   await updateOrCreateDoenetMLVersion({
     displayedVersion: "0.7",
-    fullVersion: "0.7.0-rc-6",
+    fullVersion: "0.7.0",
     default: true,
   });
 }
--- server/src/test/activity.test.ts
+++ server/src/test/activity.test.ts
@@ -48,7 +48,7 @@ import {
 const currentDoenetmlVersion = {
   id: 2,
   displayedVersion: "0.7",
-  fullVersion: "0.7.0-rc-6",
+  fullVersion: "0.7.0",
   default: true,
   deprecated: false,
   removed: false,
