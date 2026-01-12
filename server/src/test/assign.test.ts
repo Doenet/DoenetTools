a029c08047be629093e84b6036936bec4e8b613e
Duane Nykamp <nykamp@umn.edu>
2026-01-12 11:25:24 -0600

    update DoenetML to version 0.7.3

--- server/src/test/assign.test.ts
+++ server/src/test/assign.test.ts
@@ -916,7 +916,7 @@ test("get assignment data from anonymous users", async () => {
     source: {
       name: "Activity 1",
       doenetML: "Some content",
-      doenetmlVersion: { fullVersion: "0.7.2" },
+      doenetmlVersion: { fullVersion: "0.7.3" },
     },
   });
 
@@ -953,7 +953,7 @@ test("get assignment data from anonymous users", async () => {
     source: {
       name: "Activity 1",
       doenetML: "Some content",
-      doenetmlVersion: { fullVersion: "0.7.2" },
+      doenetmlVersion: { fullVersion: "0.7.3" },
     },
   });
 
@@ -990,7 +990,7 @@ test("get assignment data from anonymous users", async () => {
     source: {
       name: "Activity 1",
       doenetML: "Some content",
-      doenetmlVersion: { fullVersion: "0.7.2" },
+      doenetmlVersion: { fullVersion: "0.7.3" },
     },
   });
 
@@ -1063,7 +1063,7 @@ test("get assignment data from anonymous users", async () => {
     source: {
       name: "Activity 1",
       doenetML: "Some content",
-      doenetmlVersion: { fullVersion: "0.7.2" },
+      doenetmlVersion: { fullVersion: "0.7.3" },
     },
   });
 });
