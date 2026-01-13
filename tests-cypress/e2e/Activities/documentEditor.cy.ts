a1a66e0c89e273316dfa0c5021b2e8dbd081e547
Duane Nykamp <nykamp@umn.edu>
2026-01-12 19:27:20 -0600

    break up assertion changes to handle DOM elements changing

--- tests-cypress/e2e/Activities/documentEditor.cy.ts
+++ tests-cypress/e2e/Activities/documentEditor.cy.ts
@@ -11,31 +11,40 @@ describe("Activity Editor Tests", function () {
     }).then((activityId) => {
       cy.visit(`/documentEditor/${activityId}/edit`);
 
+      // Wait for viewer to display initial content
       cy.iframe()
         .find(".doenet-viewer")
         .should("contain.text", `Initial content`);
 
-      cy.iframe().find(".cm-activeLine").type("{enter}");
+      // Edit the content - break up command chain to avoid detached DOM issues
+      cy.iframe().find(".cm-activeLine").as("activeLine");
+      cy.get("@activeLine").type("{enter}");
+
+      // Wait a moment for editor to settle, then re-query the active line
+      cy.wait(300);
       cy.iframe().find(".cm-activeLine").invoke("text", "More!");
 
+      // Switch to view mode
       cy.get(`[data-test="View Mode Button"]`).click();
       cy.wait(1000);
 
+      // Verify editor is hidden and viewer shows updated content
       cy.iframe().find(".cm-editor").should("not.exist");
+      cy.iframe().find(".doenet-viewer").as("viewer");
+      cy.get("@viewer").should("contain.text", `Initial content`);
+      cy.get("@viewer").should("contain.text", `More!`);
 
-      cy.iframe()
-        .find(".doenet-viewer")
-        .should("contain.text", `Initial content\nMore!`);
-
+      // Switch back to edit mode
       cy.get(`[data-test="Edit Mode Button"]`).click();
 
-      cy.iframe()
-        .find(".cm-editor")
-        .should("contain.text", "Initial contentMore!");
+      // Verify editor is back and shows content, then separately verify viewer
+      cy.iframe().find(".cm-editor").as("editor");
+      cy.get("@editor").should("contain.text", "Initial content");
+      cy.get("@editor").should("contain.text", "More!");
 
-      cy.iframe()
-        .find(".doenet-viewer")
-        .should("contain.text", `Initial content\nMore!`);
+      cy.iframe().find(".doenet-viewer").as("viewerAgain");
+      cy.get("@viewerAgain").should("contain.text", `Initial content`);
+      cy.get("@viewerAgain").should("contain.text", `More!`);
     });
   });
 });
