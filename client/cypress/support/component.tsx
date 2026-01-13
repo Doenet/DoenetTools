98080d55261bd5115497e55081925bacf6fde6f8
Duane Nykamp <nykamp@umn.edu>
2026-01-13 12:29:55 -0600

    Revert "try removing catch for mathjax errors"
    This reverts commit dbf5094ba74503eade204810efb09d5082aae036.

--- client/cypress/support/component.tsx
+++ client/cypress/support/component.tsx
@@ -1,5 +1,15 @@
 /// <reference path="./component.d.ts" />
 
+// Handle MathJax async typesetting errors that aren't critical for tests
+Cypress.on("uncaught:exception", (err) => {
+  // Suppress MathJax typesetting errors
+  if (err.message?.includes("Typesetting failed")) {
+    return false; // Suppress the error
+  }
+  // Let other errors fail the test
+  return true;
+});
+
 // ***********************************************************
 // This example support/component.ts is processed and
 // loaded automatically before your test files.
