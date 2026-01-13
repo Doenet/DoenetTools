df32b3e87c059bf0d52c8fc7b1113ca69e8dc6e8
Duane Nykamp <nykamp@umn.edu>
2026-01-13 11:48:39 -0600

    lint failure on exclusive cypress test

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -4,6 +4,7 @@ import tseslint from "typescript-eslint";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import rootConfig from "../eslint.config.mjs";
+import mocha from "eslint-plugin-mocha";
 
 // Override the tsconfigRootDir and project to point to this directory
 const configWithClientPaths = rootConfig.map((config) => {
@@ -28,6 +29,9 @@ export default tseslint.config(
   react.configs.flat.recommended,
   reactHooks.configs["recommended-latest"],
   {
+    plugins: {
+      mocha,
+    },
     settings: {
       react: {
         version: "detect",
@@ -37,6 +41,7 @@ export default tseslint.config(
       "react/react-in-jsx-scope": "off", // Not needed with new JSX transform
       "react/jsx-uses-react": "off", // Not needed with new JSX transform
       "@typescript-eslint/triple-slash-reference": "off",
+      "mocha/no-exclusive-tests": "error", // Prevent .only in specs
     },
   },
 
