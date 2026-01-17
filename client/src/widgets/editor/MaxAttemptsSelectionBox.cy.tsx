49246bbfa34332ecbf88df7e09904dd1aeb7056a
Duane Nykamp <nykamp@umn.edu>
2026-01-17 08:36:13 -0600

    stepper button tests

--- client/src/widgets/editor/MaxAttemptsSelectionBox.cy.tsx
+++ client/src/widgets/editor/MaxAttemptsSelectionBox.cy.tsx
@@ -149,4 +149,48 @@ describe("MaxAttemptsSelectionBox Component", () => {
       .find("input")
       .should("have.value", "5");
   });
+
+  it("value should change when clicking the increment stepper button", () => {
+    cy.mount(<MaxAttemptsWrapper initialAttempts={5} />, {
+      action,
+    });
+
+    // Verify initial value is 5
+    cy.get('[data-test="max-attempts-input"]')
+      .find("input")
+      .should("have.value", "5");
+
+    // Click the increment button
+    cy.get('[data-test="max-attempts-input"]')
+      .find("[role='button']")
+      .first()
+      .click();
+
+    // Value should increment to 6 immediately
+    cy.get('[data-test="max-attempts-input"]')
+      .find("input")
+      .should("have.value", "6");
+  });
+
+  it("value should change when clicking the decrement stepper button", () => {
+    cy.mount(<MaxAttemptsWrapper initialAttempts={5} />, {
+      action,
+    });
+
+    // Verify initial value is 5
+    cy.get('[data-test="max-attempts-input"]')
+      .find("input")
+      .should("have.value", "5");
+
+    // Click the decrement button
+    cy.get('[data-test="max-attempts-input"]')
+      .find("[role='button']")
+      .last()
+      .click();
+
+    // Value should decrement to 4 immediately
+    cy.get('[data-test="max-attempts-input"]')
+      .find("input")
+      .should("have.value", "4");
+  });
 });
