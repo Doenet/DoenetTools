c7b34a7b47c65dd0d63c8ca9c255dd0d429b92cc
Charles Nykamp <cqnykamp@gmail.com>
2026-02-10 16:41:47 -0600

    Replace generic metadata

--- blog/src/consts.ts
+++ blog/src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Doenet Blog";
+export const SITE_DESCRIPTION =
+  "A place where the Doenet community shares thoughts and ideas about math education";
