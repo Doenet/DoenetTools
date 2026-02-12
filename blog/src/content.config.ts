c978bdc19fd65d7fa0b62803d521a5b053b27896
Duane Nykamp <nykamp@umn.edu>
2026-02-12 15:17:07 -0600

    embed react components in the astro blog (#2798)

--- blog/src/content.config.ts
+++ blog/src/content.config.ts
@@ -2,7 +2,7 @@ import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
 
 const blog = defineCollection({
-  // Load Markdown and MDX files in the `src/content/blog/` directory.
+  // Load Markdown and MDX files in the `src/content/` directory.
   loader: glob({ base: "./src/content", pattern: "**/*.{md,mdx}" }),
   // Type-check frontmatter using a schema
   schema: ({ image }) =>
