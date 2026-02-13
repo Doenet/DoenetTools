63a311eaa1c9d46b5461b849162fd777b18b530c
Duane Nykamp <nykamp@umn.edu>
2026-02-12 22:48:26 -0600

    Add an Events pages, reformat Get Involved page

---
+++ client/src/paths/Events.tsx
@@ -0,0 +1,273 @@
+import { useEffect } from "react";
+import {
+  Box,
+  Heading,
+  Text,
+  VStack,
+  Link as ChakraLink,
+} from "@chakra-ui/react";
+import { WithSideBanners } from "../layout/WithSideBanners";
+
+export function Events() {
+  useEffect(() => {
+    document.title = `Events - Doenet`;
+  }, []);
+
+  return (
+    <>
+      <WithSideBanners bgColor="background">
+        <Box
+          p="40px"
+          w="100%"
+          // Hack: ensure background color extends full height
+          // Remove once we implement background color globally
+          minH="calc(100vh - 40px)"
+        >
+          <Heading size="lg" mb={6}>
+            Events
+          </Heading>
+
+          <VStack align="start" spacing={8}>
+            {/* Virtual Office Hours Section */}
+            <Box w="100%">
+              <Heading size="md" mb={3}>
+                Virtual office hours
+              </Heading>
+              <VStack align="start" spacing={3}>
+                <Text fontSize="1.3rem" lineHeight="1.3">
+                  Join us for office hours to ask questions, get support, and
+                  connect with the Doenet community. All are welcome!
+                </Text>
+                <Text fontSize="1.3rem" lineHeight="1.3">
+                  Drop in anytime during the two hours.
+                </Text>
+                <Text fontSize="1rem" lineHeight="1.4">
+                  <strong>Dates:</strong> Each Tuesday except for the first
+                  Tuesday of each month, which is reserved for a training
+                  workshop.
+                </Text>
+                <Text fontSize="1rem" lineHeight="1.4">
+                  <strong>Time:</strong> 2-4pm EST, 11am-1pm PST
+                </Text>
+                <Text fontSize="1rem" lineHeight="1.4">
+                  <strong>Location:</strong> Online, via Zoom. Link:{" "}
+                  <ChakraLink
+                    href="https://mathtech.org/dropin"
+                    isExternal
+                    textDecoration="underline"
+                  >
+                    https://mathtech.org/dropin
+                  </ChakraLink>
+                </Text>
+              </VStack>
+            </Box>
+
+            {/* Training Workshops Section */}
+            <Box w="100%" id="workshops">
+              <Heading size="md" mb={3}>
+                Virtual training workshops
+              </Heading>
+
+              <Text fontSize="1.3rem" lineHeight="1.3" mb={6}>
+                Virtual training workshops are one hour long and held on zoom.
+                Immediately following each workshop, the instructors will hold
+                an office hour to answer any questions and help you with any
+                activities you want to create.
+              </Text>
+
+              {/* Workshop 1 */}
+              <VStack
+                align="start"
+                spacing={3}
+                mb={6}
+                pl={4}
+                borderLeft="3px solid"
+                borderColor="border"
+              >
+                <Heading size="sm">Getting started with Doenet</Heading>
+                <Text fontSize="1.3rem" lineHeight="1.3">
+                  Learn the basics of how to write Doenet activities in this
+                  introductory workshop.
+                </Text>
+                <Text fontSize="1rem" lineHeight="1.4">
+                  <strong>Date:</strong> Tuesday, March 3, 2026
+                </Text>
+                <Text fontSize="1rem" lineHeight="1.4">
+                  <strong>Time:</strong> 2-3pm EST, 11am-noon PST (followed by
+                  office hours from 3-4pm EST, noon-1pm PST)
+                </Text>
+                <Text fontSize="1rem" lineHeight="1.4">
+                  <strong>Registration</strong>:{" "}
+                  <ChakraLink
+                    href="https://scholarlattice.org/collections/b428f8ce-9981-4f9b-bb32-526994c57caf"
+                    isExternal
+                    textDecoration="underline"
+                  >
+                    Register here
+                  </ChakraLink>
+                </Text>
+              </VStack>
+
+              {/* Workshop 2 */}
+              <VStack
+                align="start"
+                spacing={3}
+                mb={6}
+                pl={4}
+                borderLeft="3px solid"
+                borderColor="border"
+              >
+                <Heading size="sm">
+                  Creating accessible activities in Doenet
+                </Heading>
+                <Text fontSize="1.3rem" lineHeight="1.3">
+                  In this workshop, we will discuss best practices for creating
+                  accessible activities in Doenet, and how to use the
+                  accessibility features of Doenet to make your activities more
+                  inclusive.
+                </Text>
+                <Text fontSize="1rem" lineHeight="1.4">
+                  <strong>Date:</strong> Tuesday, April 7, 2026
+                </Text>
+                <Text fontSize="1rem" lineHeight="1.4">
+                  <strong>Time:</strong> 2-3pm EST, 11am-noon PST (followed by
+                  office hours from 3-4pm EST, noon-1pm PST)
+                </Text>
+                <Text fontSize="1rem" lineHeight="1.4">
+                  <strong>Registration</strong>:{" "}
+                  <ChakraLink
+                    href="https://scholarlattice.org/collections/eab4d5ce-064d-409a-a576-bcab507f7836"
+                    isExternal
+                    textDecoration="underline"
+                  >
+                    Register here
+                  </ChakraLink>
+                </Text>
+              </VStack>
+
+              {/* Workshop 3 */}
+              <VStack
+                align="start"
+                spacing={3}
+                pl={4}
+                borderLeft="3px solid"
+                borderColor="border"
+              >
+                <Heading size="sm">Getting started with Doenet</Heading>
+                <Text fontSize="1.3rem" lineHeight="1.3">
+                  Learn the basics of how to write Doenet activities in this
+                  introductory workshop.
+                </Text>
+                <Text fontSize="1rem" lineHeight="1.4">
+                  <strong>Date:</strong> Tuesday, May 5, 2026
+                </Text>
+                <Text fontSize="1rem" lineHeight="1.4">
+                  <strong>Time:</strong> 2-3pm EST, 11am-noon PST (followed by
+                  office hours from 3-4pm EST, noon-1pm PST)
+                </Text>
+                <Text fontSize="1rem" lineHeight="1.4">
+                  <strong>Registration</strong>:{" "}
+                  <ChakraLink
+                    href="https://scholarlattice.org/collections/b8ec4af3-8f71-4e4a-b86e-838242e47058"
+                    isExternal
+                    textDecoration="underline"
+                  >
+                    Register here
+                  </ChakraLink>
+                </Text>
+              </VStack>
+            </Box>
+
+            {/* In-person Workshops and Conferences Section */}
+            <Box w="100%">
+              <Heading size="md" mb={3}>
+                In-person workshops and conferences
+              </Heading>
+
+              <Text fontSize="1.3rem" lineHeight="1.3" mb={6}>
+                Doenet will be at the following workshops and conferences. We
+                hope to see you there!
+              </Text>
+
+              {/* Event 1 */}
+              <VStack
+                align="start"
+                spacing={3}
+                mb={6}
+                pl={4}
+                borderLeft="3px solid"
+                borderColor="border"
+              >
+                <Heading size="sm">
+                  2026 MAA Florida /FTYCMA Joint Annual Meeting
+                </Heading>
+                <Text fontSize="1rem" lineHeight="1.4">
+                  <strong>Dates:</strong> February 20 - 21, 2026
+                </Text>
+                <Text fontSize="1rem" lineHeight="1.4">
+                  <strong>Location:</strong> State College of Florida /
+                  Bradenton campus
+                </Text>
+                <Text fontSize="1rem" lineHeight="1.4">
+                  <ChakraLink
+                    href="https://www.florida.maa.org/events/2026-maa-florida-ftycma-joint-annual-meeting"
+                    isExternal
+                    textDecoration="underline"
+                  >
+                    More information
+                  </ChakraLink>
+                </Text>
+              </VStack>
+
+              {/* Event 2 */}
+              <VStack
+                align="start"
+                spacing={3}
+                mb={6}
+                pl={4}
+                borderLeft="3px solid"
+                borderColor="border"
+              >
+                <Heading size="sm">Doenet Community Workshop</Heading>
+                <Text fontSize="1rem" lineHeight="1.4">
+                  <strong>Dates:</strong> May/June 2026 (exact date TBD)
+                </Text>
+                <Text fontSize="1rem" lineHeight="1.4">
+                  <strong>Location:</strong> University of Minnesota
+                </Text>
+                <Text fontSize="1.3rem" lineHeight="1.3">
+                  More information coming soon!
+                </Text>
+              </VStack>
+
+              {/* Event 3 */}
+              <VStack
+                align="start"
+                spacing={3}
+                pl={4}
+                borderLeft="3px solid"
+                borderColor="border"
+              >
+                <Heading size="sm">MathFest 2026</Heading>
+                <Text fontSize="1rem" lineHeight="1.4">
+                  <strong>Dates:</strong> August 5-8, 2026
+                </Text>
+                <Text fontSize="1rem" lineHeight="1.4">
+                  <strong>Location:</strong> Boston, MA
+                </Text>
+                <Text fontSize="1.3rem" lineHeight="1.3">
+                  During MathFest, Doenet will be leading a minicourse:
+                  Upgrading Online Assignments: Building Scaffolded Activities
+                  for Conceptual Understanding
+                </Text>
+                <Text fontSize="1.3rem" lineHeight="1.3">
+                  More information, including registration details, coming soon!
+                </Text>
+              </VStack>
+            </Box>
+          </VStack>
+        </Box>
+      </WithSideBanners>
+    </>
+  );
+}
---
+++ client/src/paths/GetInvolved.tsx
@@ -0,0 +1,301 @@
+import { useEffect } from "react";
+import {
+  Box,
+  Heading,
+  Text,
+  VStack,
+  Link as ChakraLink,
+} from "@chakra-ui/react";
+import { WithSideBanners } from "../layout/WithSideBanners";
+
+export function GetInvolved() {
+  useEffect(() => {
+    document.title = `Get Involved - Doenet`;
+  }, []);
+
+  return (
+    <>
+      <WithSideBanners bgColor="background">
+        <Box
+          p="40px"
+          w="100%"
+          // Hack: ensure background color extends full height
+          // Remove once we implement background color globally
+          minH="calc(100vh - 40px)"
+        >
+          <Heading size="lg">Get involved with Doenet</Heading>
+
+          <VStack align="start" spacing={8} mt={6}>
+            <Text fontSize="1.3rem" lineHeight="1.3">
+              There are many ways to get involved with Doenet. Just by writing
+              activities on{" "}
+              <ChakraLink
+                href="https://beta.doenet.org"
+                isExternal
+                textDecoration="underline"
+              >
+                Doenet
+              </ChakraLink>{" "}
+              and sharing them publicly, you are contributing to the content
+              that we can all use.
+            </Text>
+
+            <Text fontSize="1.3rem" lineHeight="1.3">
+              If you'd like more specific ways to contribute, you are welcome to
+              get involved with these groups.
+            </Text>
+
+            {/* Documentation Group */}
+            <Box w="100%">
+              <Heading size="md" mb={3}>
+                Documentation Group
+              </Heading>
+              <VStack align="start" spacing={2}>
+                <Text fontSize="1.3rem" lineHeight="1.3">
+                  The documentation group writes and updates our{" "}
+                  <ChakraLink
+                    href="https://docs.doenet.org"
+                    isExternal
+                    textDecoration="underline"
+                  >
+                    documentation
+                  </ChakraLink>{" "}
+                  for people getting started with Doenet. The documentation is a
+                  work in progress, and we welcome any contributions, small or
+                  large, toward improving it!
+                </Text>
+                <Text fontSize="1.3rem" lineHeight="1.3">
+                  To get involved, check out the{" "}
+                  <ChakraLink
+                    href="https://community.doenet.org/tag/documentation"
+                    isExternal
+                    textDecoration="underline"
+                  >
+                    documentation discussions
+                  </ChakraLink>{" "}
+                  or email{" "}
+                  <ChakraLink
+                    href="mailto:docs@doenet.org"
+                    textDecoration="underline"
+                  >
+                    docs@doenet.org
+                  </ChakraLink>
+                  .
+                </Text>
+                <Text fontStyle="italic" fontSize="1rem">
+                  Documentation group members: Virginia Mae, Jon Rogness
+                </Text>
+              </VStack>
+            </Box>
+
+            {/* Curriculum Group */}
+            <Box w="100%">
+              <Heading size="md" mb={3}>
+                Curriculum Group
+              </Heading>
+              <VStack align="start" spacing={2}>
+                <Text fontSize="1.3rem" lineHeight="1.3">
+                  The curriculum group helps shepherd the development of the
+                  content on{" "}
+                  <ChakraLink
+                    href="https://beta.doenet.org"
+                    isExternal
+                    textDecoration="underline"
+                  >
+                    Doenet
+                  </ChakraLink>
+                  . They help organize groups of authors working on different
+                  topics, curate content for{" "}
+                  <ChakraLink
+                    href="https://beta.doenet.org/explore"
+                    isExternal
+                    textDecoration="underline"
+                  >
+                    library section
+                  </ChakraLink>
+                  , and develop our content classification system.
+                </Text>
+                <Text fontSize="1.3rem" lineHeight="1.3">
+                  To get involved, check out the{" "}
+                  <ChakraLink
+                    href="https://community.doenet.org/tag/curriculum"
+                    isExternal
+                    textDecoration="underline"
+                  >
+                    curriculum discussions
+                  </ChakraLink>{" "}
+                  or email{" "}
+                  <ChakraLink
+                    href="mailto:curriculum@doenet.org"
+                    textDecoration="underline"
+                  >
+                    curriculum@doenet.org
+                  </ChakraLink>
+                  .
+                </Text>
+                <Text fontStyle="italic" fontSize="1rem">
+                  Curriculum group members: Kris Hollingsworth, Anurag Katyal,
+                  Oscar Levin, Virginia Mae, Jon Rogness, Mike Weimerskirch
+                </Text>
+              </VStack>
+            </Box>
+
+            {/* Training Group */}
+            <Box w="100%">
+              <Heading size="md" mb={3}>
+                Training Group
+              </Heading>
+              <VStack align="start" spacing={2}>
+                <Text fontSize="1.3rem" lineHeight="1.3">
+                  The training group helps people get started with using Doenet,
+                  with a particular focus in helping people write content. The
+                  group responds to author questions, hosts online office hours,
+                  and organizes training workshops.
+                </Text>
+                <Text fontSize="1.3rem" lineHeight="1.3">
+                  To get involved, check out the{" "}
+                  <ChakraLink
+                    href="https://community.doenet.org/tag/training"
+                    isExternal
+                    textDecoration="underline"
+                  >
+                    training discussions
+                  </ChakraLink>{" "}
+                  or email{" "}
+                  <ChakraLink
+                    href="mailto:training@doenet.org"
+                    textDecoration="underline"
+                  >
+                    training@doenet.org
+                  </ChakraLink>
+                  .
+                </Text>
+                <Text fontStyle="italic" fontSize="1rem">
+                  Training group members: Anurag Katyal, Melissa Lynn, Virginia
+                  Mae, Duane Nykamp, Ozlem Ugurlu, Mike Weimerskirch
+                </Text>
+              </VStack>
+            </Box>
+
+            {/* Communications Group */}
+            <Box w="100%">
+              <Heading size="md" mb={3}>
+                Communications Group
+              </Heading>
+              <VStack align="start" spacing={2}>
+                <Text fontSize="1.3rem" lineHeight="1.3">
+                  Interested in telling others about Doenet? Join our
+                  communications group, and help us write blog entries, social
+                  media posts, and/or a newsletter.
+                </Text>
+                <Text fontSize="1.3rem" lineHeight="1.3">
+                  To get involved, check out the{" "}
+                  <ChakraLink
+                    href="https://community.doenet.org/tag/communications"
+                    isExternal
+                    textDecoration="underline"
+                  >
+                    communications discussions
+                  </ChakraLink>{" "}
+                  or email{" "}
+                  <ChakraLink
+                    href="mailto:communications@doenet.org"
+                    textDecoration="underline"
+                  >
+                    communications@doenet.org
+                  </ChakraLink>
+                  .
+                </Text>
+                <Text fontStyle="italic" fontSize="1rem">
+                  Communication group members: Melissa Lynn, Charles Nykamp, Jon
+                  Rogness
+                </Text>
+              </VStack>
+            </Box>
+
+            {/* Software Group */}
+            <Box w="100%">
+              <Heading size="md" mb={3}>
+                Software Group
+              </Heading>
+              <VStack align="start" spacing={2}>
+                <Text fontSize="1.3rem" lineHeight="1.3">
+                  The software group developes the software for the Doenet
+                  website and the language used to write Doenet activities,
+                  called DoenetML (Doenet Markup Language). You can help the
+                  software group by creating bug reports (called Issues) on our{" "}
+                  <ChakraLink
+                    href="https://github.com/Doenet/DoenetTools/issues"
+                    isExternal
+                    textDecoration="underline"
+                  >
+                    GitHub site
+                  </ChakraLink>{" "}
+                  or finding an issue that you can help fix.
+                </Text>
+                <Text fontSize="1.3rem" lineHeight="1.3">
+                  To get involved, check out our{" "}
+                  <ChakraLink
+                    href="https://github.com/orgs/Doenet/discussions"
+                    isExternal
+                    textDecoration="underline"
+                  >
+                    GitHub
+                  </ChakraLink>{" "}
+                  or email{" "}
+                  <ChakraLink
+                    href="mailto:dev@doenet.org"
+                    textDecoration="underline"
+                  >
+                    dev@doenet.org
+                  </ChakraLink>
+                  .
+                </Text>
+                <Text fontStyle="italic" fontSize="1rem">
+                  Software group members: Melissa Lynn, Charles Nykamp, Duane
+                  Nykamp, Jason Siefken
+                </Text>
+              </VStack>
+            </Box>
+
+            {/* Accessibility Group */}
+            <Box w="100%">
+              <Heading size="md" mb={3}>
+                Accessibility Group
+              </Heading>
+              <VStack align="start" spacing={2}>
+                <Text fontSize="1.3rem" lineHeight="1.3">
+                  Test accessibility group works to make sure that Doenet is
+                  accessible to all. They test the accessibility of the Doenet
+                  website and activities, and they work to improve the way users
+                  using assistive technology can interact with the content.
+                </Text>
+                <Text fontSize="1.3rem" lineHeight="1.3">
+                  To get involved, check out the{" "}
+                  <ChakraLink
+                    href="https://community.doenet.org/tag/accessibility"
+                    isExternal
+                    textDecoration="underline"
+                  >
+                    accessibility discussions
+                  </ChakraLink>{" "}
+                  or email{" "}
+                  <ChakraLink
+                    href="mailto:accessibility@doenet.org"
+                    textDecoration="underline"
+                  >
+                    accessibility@doenet.org
+                  </ChakraLink>
+                  .
+                </Text>
+                <Text fontStyle="italic" fontSize="1rem">
+                  Accessibility group members: Virginia Mae, Duane Nykamp
+                </Text>
+              </VStack>
+            </Box>
+          </VStack>
+        </Box>
+      </WithSideBanners>
+    </>
+  );
+}
