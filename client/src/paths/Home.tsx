1870a9691ccca10b741b297b24315ddb85710386
Charles Nykamp <cqnykamp@gmail.com>
2026-03-04 22:26:28 -0600

    Give newsletter its own section

--- client/src/paths/Home.tsx
+++ client/src/paths/Home.tsx
@@ -19,7 +19,6 @@ import {
   Button,
   Stack,
   VStack,
-  Spacer,
 } from "@chakra-ui/react";
 import { Link as ReactRouterLink, useOutletContext } from "react-router";
 import { SiteContext } from "./SiteHeader";
@@ -37,6 +36,8 @@ export function Home() {
   }, []);
 
   const { user } = useOutletContext<SiteContext>();
+
+  const discussHref = getDiscourseUrl(user);
 
   // The first string is the video source path,
   // the second string is the link to try the activity.
@@ -94,13 +95,12 @@ export function Home() {
               color="text"
               fontSize={{ base: "28px", sm: "34px", md: "44px", lg: "56px" }}
               fontWeight="500"
-              // mb={{ base: "24px", md: "40px", lg: "60px" }}
               mb="24px"
             >
               Richly interactive classroom activities
             </Heading>
 
-            <Heading
+            <Text
               color="text"
               fontSize={{
                 base: "16px",
@@ -110,7 +110,6 @@ export function Home() {
               }}
               fontWeight="400"
               mb={{ base: "12px", md: "16px", lg: "20px" }}
-              // lineHeight={{ base: "1.3", md: "1.4" }}
               maxW={{ base: "100%", md: "680px", lg: "800px" }}
             >
               We're a community of STEM instructors and authors. Find, create,
@@ -118,9 +117,7 @@ export function Home() {
               community-driven tool.
               {/* TODO: Find spot to mention AI, such as:
                 "Human-crafted activities in the age of AI" */}
-            </Heading>
-
-            <Spacer />
+            </Text>
 
             <HStack
               spacing={{ base: "12px", md: "16px" }}
@@ -217,17 +214,17 @@ export function Home() {
         <HStack spacing="20px">
           {/* TODO: Instant feedback image */}
 
-          <p>
+          <Text fontSize="22px" fontWeight="400">
             <strong>Instant feedback for students.</strong> Guide students with
             immediate validation or hints based on their mistake
-          </p>
+          </Text>
         </HStack>
 
         <HStack spacing="20px">
-          <p>
+          <Text fontSize="22px" fontWeight="400">
             <strong>Interactive graphics.</strong> Construct gradable graphical
             applets from points, lines, derivatives, etc.
-          </p>
+          </Text>
 
           {/* TODO: interactive graphics image */}
         </HStack>
@@ -236,46 +233,44 @@ export function Home() {
           {/* TODO: variant control image */}
 
           <Flex flex="1 0 75%">
-            <p>
+            <Text fontSize="22px" fontWeight="400">
               <strong>Variant control.</strong> Generate multiple variants of
               your activity
-            </p>
+            </Text>
           </Flex>
         </Flex>
       </VStack>
+    </WithSideBanners>
+  );
+
+  const newsletterSection = (
+    <WithSideBanners bgColor="doenet.mainBlue" padding="70px">
+      <Heading size="lg" color="white">
+        Interested? Stay in the loop
+      </Heading>
+      <Text fontSize="22px" fontWeight="400" mb="15px" color="white">
+        Receive occasional emails about Doenet, upcoming training workshops, and
+        what's going on in the community.
+      </Text>
       <Button
         as="a"
         href="https://docs.google.com/forms/d/e/1FAIpQLSc9VSrYnybkB5WC5YpPKicclF_z14UIq5O7l19f1Vxz8EFXCw/viewform?usp=header"
-        colorScheme="blue"
         target="_blank"
         rel="noopener noreferrer"
-        mt="1em"
-        mb="2em"
-        size={{ base: "md", md: "lg" }}
         fontSize={{ base: "16px", md: "18px" }}
-        px={{ base: "24px", md: "32px" }}
-        py={{ base: "12px", md: "16px" }}
-        height="auto"
       >
-        Interested in Doenet? Stay in the loop
+        Keep me updated
       </Button>
     </WithSideBanners>
   );
 
-  const discussHref = getDiscourseUrl(user);
-
   const communitySection = (
     <WithSideBanners bgColor="white" padding="70px">
-      <Heading size="lg">Interested in Doenet?</Heading>
+      <Heading size="lg">A community project</Heading>
 
-      <Heading size="lg">Join the community</Heading>
-
-      <Text marginTop="10px" fontSize="1.4rem">
+      <Text marginTop="10px" fontSize="22px" fontWeight="400">
         Doenet—as a free and open-source platform—derives its value from its
-        community of authors and contributors.
-      </Text>
-      <Text marginTop="10px" fontSize="1.4rem">
-        To get support, check out our{" "}
+        community of authors and contributors. To get support, check out our{" "}
         <ChakraLink href={discussHref} textDecoration="underline">
           community discussions
         </ChakraLink>{" "}
@@ -286,16 +281,13 @@ export function Home() {
         >
           Discord server
         </ChakraLink>
-        .
-      </Text>
-      <Text marginTop="10px" fontSize="1.4rem">
-        To learn how to get involved, check out{" "}
+        . To learn how to get involved, check out{" "}
         <ChakraLink href="/get-involved" textDecoration="underline">
           How to get involved with Doenet
         </ChakraLink>
         .
       </Text>
-      <Heading size="lg" marginTop="40px">
+      <Heading size="lg" marginTop="70px">
         Events
       </Heading>
       <Stack direction={{ base: "column", md: "row" }}>
@@ -441,6 +433,7 @@ export function Home() {
       {heroSection}
       {/* {exploreSection} */}
       {featuresSection}
+      {newsletterSection}
       {communitySection}
       {footerSection}
     </>
