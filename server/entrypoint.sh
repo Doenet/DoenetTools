bf310ad76e328ffddc80e2710e83515bc5dbf14e
Charles Nykamp <cqnykamp@gmail.com>
2026-03-12 14:45:19 -0500

    Use entrypoint file

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,27 +1,3 @@
 #!/bin/bash
-
-# # Command to be executed and checked
-# COMMAND="mysqladmin ping -h $DATABASE_HOST -u $DATABASE_USER -p$DATABASE_PASSWORD | grep 'mysqld is alive'"
-
-# # Desired exit status to wait for (e.g., 1 for a specific error)
-# DESIRED_EXIT_STATUS=1
-
-
-# # Loop while the command's exit status is NOT the desired value
-# while true; do
-#     $COMMAND
-#     EXIT_STATUS=$?
-#     if [ "$EXIT_STATUS" -eq "$DESIRED_EXIT_STATUS" ]; then
-#         echo "Command '$COMMAND' returned the desired exit status ($DESIRED_EXIT_STATUS)."
-#         break # Exit the loop
-#     else
-#         echo "Command '$COMMAND' returned exit status $EXIT_STATUS. Retrying in 5 seconds..."
-#         sleep 5
-#     fi
-# done
-
-
-#npx prisma migrate dev
-#npx prisma db seed
-npx prisma migrate deploy
+npm run prisma:deploy
 npm run start
