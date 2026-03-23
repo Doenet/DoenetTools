3e75683448eed1c22301f13e9f4df61f2ac027c4
Duane Nykamp <nykamp@umn.edu>
2026-03-23 16:54:39 -0500

    Fix MathJax rendering in answer response drawer (#2849)

--- client/src/drawers/AnswerResponseDrawer.tsx
+++ client/src/drawers/AnswerResponseDrawer.tsx
@@ -56,6 +56,7 @@ export function AnswerResponseDrawer({
   >([]);
 
   useEffect(() => {
+    setResponses([]);
     async function getAnswerResponses() {
       const itemQuery =
         (itemNumber === null ? "" : `&requestedItemNumber=${itemNumber}`) +
