cf570ab579192ed6f78f2973750e47e8169199e6
Charles Nykamp <cqnykamp@gmail.com>
2026-03-31 10:24:12 -0500

    test `visibility` result for parity with `isPublic`

--- server/src/test/share.test.ts
+++ server/src/test/share.test.ts
@@ -40,11 +40,13 @@ describe("Share tests", () => {
       loggedInUserId: ownerId,
     });
     expect(result.isPublic).eq(true);
+    expect(result.visibility).eq("public");
     let content = await getContent({
       contentId,
       loggedInUserId: ownerId,
     });
     expect(content.isPublic).eq(true);
+    expect(content.visibility).eq("public");
 
     result = await setContentIsPublic({
       contentId,
@@ -52,11 +54,14 @@ describe("Share tests", () => {
       loggedInUserId: ownerId,
     });
     expect(result.isPublic).eq(false);
+    expect(result.visibility).eq("private");
+
     content = await getContent({
       contentId,
       loggedInUserId: ownerId,
     });
     expect(content.isPublic).eq(false);
+    expect(content.visibility).eq("private");
   });
 
   test("shareContentWithEmail/unshareContent shares/unshares content and returns result", async () => {
@@ -150,10 +155,12 @@ describe("Share tests", () => {
 
     expect(content[0].contentId).eqls(contentId);
     expect(content[0].isPublic).eq(true);
+    expect(content[0].visibility).eq("public");
     expect(content[0].licenseCode).eq("CCBYSA");
 
     expect(content[1].contentId).eqls(folderId);
     expect(content[1].isPublic).eq(true);
+    expect(content[1].visibility).eq("public");
     expect(content[1].licenseCode).eq("CCBYSA");
   });
 
@@ -251,6 +258,7 @@ describe("Share tests", () => {
       loggedInUserId: ownerId,
     });
     expect(activity.isPublic).eq(true);
+    expect(activity.visibility).eq("public");
     expect(activity.licenseCode).eq("CCBYNCSA");
 
     // change license of content is OK
@@ -264,6 +272,7 @@ describe("Share tests", () => {
       loggedInUserId: ownerId,
     });
     expect(activity.isPublic).eq(true);
+    expect(activity.visibility).eq("public");
     expect(activity.licenseCode).eq("CCDUAL");
 
     // since have public parent, cannot make child private
@@ -288,6 +297,7 @@ describe("Share tests", () => {
       loggedInUserId: ownerId,
     });
     expect(activity.isPublic).eq(false);
+    expect(activity.visibility).eq("private");
 
     // make the content public
     await setContentIsPublic({
@@ -300,6 +310,7 @@ describe("Share tests", () => {
       loggedInUserId: ownerId,
     });
     expect(activity.isPublic).eq(true);
+    expect(activity.visibility).eq("public");
 
     // can make it private now
     await setContentIsPublic({
@@ -312,6 +323,7 @@ describe("Share tests", () => {
       loggedInUserId: ownerId,
     });
     expect(activity.isPublic).eq(false);
+    expect(activity.visibility).eq("private");
   });
 
   // TODO: make test, if content has a parent that is shared with a user, cannot unshare with that user
@@ -843,9 +855,11 @@ describe("Share tests", () => {
 
     expect(content[1].contentId).eqls(activity1Id);
     expect(content[1].isPublic).eq(false);
+    expect(content[1].visibility).eq("private");
     expect(content[1].licenseCode).eq("CCDUAL");
     expect(content[2].contentId).eqls(folder1Id);
     expect(content[2].isPublic).eq(false);
+    expect(content[2].visibility).eq("private");
     expect(content[2].licenseCode).eq("CCDUAL");
 
     results = await getMyContent({
@@ -859,6 +873,7 @@ describe("Share tests", () => {
     content = results.content;
     expect(content[0].contentId).eqls(folder2Id);
     expect(content[0].isPublic).eq(false);
+    expect(content[0].visibility).eq("private");
     expect(content[0].licenseCode).eq("CCDUAL");
 
     results = await getMyContent({
@@ -872,6 +887,7 @@ describe("Share tests", () => {
     content = results.content;
     expect(content[0].contentId).eqls(activity2Id);
     expect(content[0].isPublic).eq(false);
+    expect(content[0].visibility).eq("private");
     expect(content[0].licenseCode).eq("CCDUAL");
 
     // move content into public folder
@@ -900,9 +916,11 @@ describe("Share tests", () => {
 
     expect(content[0].contentId).eqls(activity1Id);
     expect(content[0].isPublic).eq(true);
+    expect(content[0].visibility).eq("public");
     expect(content[0].licenseCode).eq("CCDUAL");
     expect(content[1].contentId).eqls(folder1Id);
     expect(content[1].isPublic).eq(true);
+    expect(content[1].visibility).eq("public");
     expect(content[1].licenseCode).eq("CCDUAL");
 
     results = await getMyContent({
@@ -916,6 +934,7 @@ describe("Share tests", () => {
     content = results.content;
     expect(content[0].contentId).eqls(folder2Id);
     expect(content[0].isPublic).eq(true);
+    expect(content[0].visibility).eq("public");
     expect(content[0].licenseCode).eq("CCDUAL");
 
     results = await getMyContent({
@@ -929,6 +948,7 @@ describe("Share tests", () => {
     content = results.content;
     expect(content[0].contentId).eqls(activity2Id);
     expect(content[0].isPublic).eq(true);
+    expect(content[0].visibility).eq("public");
     expect(content[0].licenseCode).eq("CCDUAL");
 
     // Create a private folder and move content into that folder.
@@ -965,9 +985,11 @@ describe("Share tests", () => {
 
     expect(content[0].contentId).eqls(activity1Id);
     expect(content[0].isPublic).eq(true);
+    expect(content[0].visibility).eq("public");
     expect(content[0].licenseCode).eq("CCDUAL");
     expect(content[1].contentId).eqls(folder1Id);
     expect(content[1].isPublic).eq(true);
+    expect(content[0].visibility).eq("public");
     expect(content[1].licenseCode).eq("CCDUAL");
 
     results = await getMyContent({
@@ -981,6 +1003,7 @@ describe("Share tests", () => {
     content = results.content;
     expect(content[0].contentId).eqls(folder2Id);
     expect(content[0].isPublic).eq(true);
+    expect(content[0].visibility).eq("public");
     expect(content[0].licenseCode).eq("CCDUAL");
 
     results = await getMyContent({
@@ -994,6 +1017,7 @@ describe("Share tests", () => {
     content = results.content;
     expect(content[0].contentId).eqls(activity2Id);
     expect(content[0].isPublic).eq(true);
+    expect(content[0].visibility).eq("public");
     expect(content[0].licenseCode).eq("CCDUAL");
   });
 
@@ -1429,11 +1453,12 @@ describe("Share tests", () => {
       contentId,
       loggedInUserId: ownerId,
     });
-    let { isPublic } = await getEditorShareStatus({
+    let { isPublic, visibility } = await getEditorShareStatus({
       contentId,
       loggedInUserId: ownerId,
     });
     expect(isPublic).eq(true);
+    expect(visibility).eq("public");
 
     expect(licenseCode).eq("CCBYSA");
 
@@ -1451,11 +1476,12 @@ describe("Share tests", () => {
       loggedInUserId: ownerId,
       isPublic: false,
     });
-    ({ isPublic } = await getEditorShareStatus({
+    ({ isPublic, visibility } = await getEditorShareStatus({
       contentId,
       loggedInUserId: ownerId,
     }));
     expect(isPublic).eq(false);
+    expect(visibility).eq("private");
 
     // make public with CCBYNCSA license
     await setContentLicense({
@@ -1468,7 +1494,7 @@ describe("Share tests", () => {
       loggedInUserId: ownerId,
       isPublic: true,
     });
-    ({ isPublic } = await getEditorShareStatus({
+    ({ isPublic, visibility } = await getEditorShareStatus({
       contentId,
       loggedInUserId: ownerId,
     }));
@@ -1477,6 +1503,7 @@ describe("Share tests", () => {
       loggedInUserId: ownerId,
     }));
     expect(isPublic).eq(true);
+    expect(visibility).eq("public");
 
     expect(licenseCode).eq("CCBYNCSA");
     fullLicense = allLicenses.find((v) => v.code === licenseCode)!;
@@ -1496,7 +1523,7 @@ describe("Share tests", () => {
       licenseCode: "CCDUAL",
     });
 
-    ({ isPublic } = await getEditorShareStatus({
+    ({ isPublic, visibility } = await getEditorShareStatus({
       contentId,
       loggedInUserId: ownerId,
     }));
@@ -1507,6 +1534,7 @@ describe("Share tests", () => {
     fullLicense = allLicenses.find((v) => v.code === licenseCode)!;
 
     expect(isPublic).eq(true);
+    expect(visibility).eq("public");
 
     expect(fullLicense.code).eq("CCDUAL");
     expect(fullLicense.name).eq(
@@ -1782,8 +1810,10 @@ describe("shareContent()", () => {
 
     expect(content[0].contentId).eqls(activity1Id);
     expect(content[0].isPublic).eqls(false);
+    expect(content[0].visibility).eq("private");
     expect(content[1].contentId).eqls(folder1Id);
     expect(content[1].isPublic).eq(false);
+    expect(content[1].visibility).eq("private");
 
     results = await getMyContent({
       ownerId,
@@ -1796,6 +1826,7 @@ describe("shareContent()", () => {
     content = results.content;
     expect(content[0].contentId).eqls(folder2Id);
     expect(content[0].isPublic).eq(false);
+    expect(content[0].visibility).eq("private");
 
     results = await getMyContent({
       ownerId,
@@ -1808,6 +1839,7 @@ describe("shareContent()", () => {
     content = results.content;
     expect(content[0].contentId).eqls(activity2Id);
     expect(content[0].isPublic).eq(false);
+    expect(content[0].visibility).eq("private");
 
     await setContentIsPublic({
       contentId: publicFolderId,
@@ -1827,8 +1859,10 @@ describe("shareContent()", () => {
 
     expect(content[0].contentId).eqls(activity1Id);
     expect(content[0].isPublic).eq(true);
+    expect(content[0].visibility).eq("public");
     expect(content[1].contentId).eqls(folder1Id);
     expect(content[1].isPublic).eq(true);
+    expect(content[1].visibility).eq("public");
 
     results = await getMyContent({
       ownerId,
@@ -1841,6 +1875,7 @@ describe("shareContent()", () => {
     content = results.content;
     expect(content[0].contentId).eqls(folder2Id);
     expect(content[0].isPublic).eq(true);
+    expect(content[0].visibility).eq("public");
 
     results = await getMyContent({
       ownerId,
@@ -1853,6 +1888,7 @@ describe("shareContent()", () => {
     content = results.content;
     expect(content[0].contentId).eqls(activity2Id);
     expect(content[0].isPublic).eq(true);
+    expect(content[0].visibility).eq("public");
 
     await setContentIsPublic({
       contentId: publicFolderId,
@@ -1872,8 +1908,10 @@ describe("shareContent()", () => {
 
     expect(content[0].contentId).eqls(activity1Id);
     expect(content[0].isPublic).eq(false);
+    expect(content[0].visibility).eq("private");
     expect(content[1].contentId).eqls(folder1Id);
     expect(content[1].isPublic).eq(false);
+    expect(content[1].visibility).eq("private");
 
     results = await getMyContent({
       ownerId,
@@ -1886,6 +1924,7 @@ describe("shareContent()", () => {
     content = results.content;
     expect(content[0].contentId).eqls(folder2Id);
     expect(content[0].isPublic).eq(false);
+    expect(content[0].visibility).eq("private");
 
     results = await getMyContent({
       ownerId,
@@ -1898,6 +1937,7 @@ describe("shareContent()", () => {
     content = results.content;
     expect(content[0].contentId).eqls(activity2Id);
     expect(content[0].isPublic).eq(false);
+    expect(content[0].visibility).eq("private");
   });
 
   describe("correctly tracks root (isRootShare)", () => {
